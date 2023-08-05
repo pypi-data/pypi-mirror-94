948a5e66da0f5ee638b43e5ce2f0646f64e2fa67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:41 +0000

    Add package python3-discogs-client/2.3.9/python3-discogs-client-2.3.9.tar.gz

---
+++ packages/python3-discogs-client/python3-discogs-client-2.3.9.tar.gz/python3-discogs-client-2.3.9/discogs_client/__init__.py
@@ -0,0 +1,6 @@
+__version_info__ = 2, 2, 2
+__version__ = '2.2.2'
+
+from discogs_client.client import Client
+from discogs_client.models import Artist, Release, Master, Label, User, \
+    Listing, Track, Price, Video, List, ListItem
