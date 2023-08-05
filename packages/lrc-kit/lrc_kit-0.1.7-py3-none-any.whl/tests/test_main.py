6365d35cb9f70869fff86cdc6c51ca17556303a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:35 +0000

    Add package lrc-kit/0.1.7/lrc_kit-0.1.7-py3-none-any.whl

---
+++ packages/lrc-kit/lrc_kit-0.1.7-py3-none-any.whl/tests/test_main.py
@@ -0,0 +1,54 @@
+from lrc_kit import ComboLyricsProvider, SearchRequest, KugouProvider, Flac123Provider, MegalobizProvider, PROVIDERS
+import logging, os
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel('DEBUG')
+
+def test_mega():
+    engine = MegalobizProvider()
+    search = SearchRequest('current joys', 'kids')
+    result = engine.search(search)
+    result.export(os.path.join('files', 'kids'))
+def test_individual_success_multi_word():
+    search = SearchRequest('Playboi Carti', 'Broke Boi')
+    LOGGER.info(list(map(lambda p:p.name, PROVIDERS)))
+    for provider in PROVIDERS:
+        engine = provider()
+        result = engine.search(search)
+        if result != None:
+            result.export(os.path.join('files', f'{engine.name}_stan'))
+            LOGGER.info(engine.name + ' Success!')
+        else:
+            LOGGER.info(engine.name + " Fail :(")
+def test_individual_success():
+    search = SearchRequest('eminem', 'stan')
+    LOGGER.info(list(map(lambda p:p.name, PROVIDERS)))
+    for provider in PROVIDERS:
+        engine = provider()
+        result = engine.search(search)
+        if result != None:
+            result.export(os.path.join('files', f'{engine.name}_stan'))
+            LOGGER.info(engine.name + ' Success!')
+        else:
+            LOGGER.info(engine.name + " Fail :(")
+def test_individual_fail():
+    search = SearchRequest('Felly', 'Fabrics')
+    for provider in PROVIDERS:
+        engine = provider()
+        result = engine.search(search)
+        if result != None:
+            result.export(os.path.join('files', f'{engine.name}_felly'))
+def test_combo_fail_2():
+    engine = ComboLyricsProvider()
+    search = SearchRequest('431242424234', 'DJ adsfasdfsdafadsfsd')
+    result = engine.search(search)
+    assert result == None
+
+def test_combo_success():
+    engine = ComboLyricsProvider()
+    search = SearchRequest('eminem', 'stan')
+    result = engine.search(search)
+    result.export(os.path.join('files', 'stan'), extension='.lrc')
+
+    assert result != None
+
+
