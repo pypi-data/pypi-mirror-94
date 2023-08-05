686578e0872862900b1365878f1d34c5b9e13a3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:25 +0000

    Add package oscr/1.2.1/oscr-1.2.1.tar.gz

---
+++ packages/oscr/oscr-1.2.1.tar.gz/oscr-1.2.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from oscrmodules.gvars import version
+
+def readme():
+    return open('README.md', 'r').read()
+
+setup(
+    name="oscr",
+    version=version,
+    scripts=["oscr"],
+    author="Murdo Maclachlan",
+    author_email="murdo@maclachlans.org.uk",
+    description="A utility allowing users to delete blacklisted comments from their Reddit profile once they have passed a cutoff time.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/MurdoMaclachlan/oscr",
+    packages=find_packages(),
+    install_requires=[
+        "configparser",
+        "praw>=7.1.2",
+        "alive_progress>=1.6.1"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ]
+)
