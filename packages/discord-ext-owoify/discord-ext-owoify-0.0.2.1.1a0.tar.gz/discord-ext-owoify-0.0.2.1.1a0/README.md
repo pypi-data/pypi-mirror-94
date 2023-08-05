41910d200cde51190e8edaf5c7b65cd1df53addc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:20 +0000

    Add package discord-ext-owoify/0.0.2.1.1a0/discord-ext-owoify-0.0.2.1.1a0.tar.gz

---
+++ packages/discord-ext-owoify/discord-ext-owoify-0.0.2.1.1a0.tar.gz/discord-ext-owoify-0.0.2.1.1a0/README.md
@@ -0,0 +1,14 @@
+# discord-ext-owoify
+Simple ext package for owoifing text.
+
+# Install
+Simple paste this into your terminal
+
+- ``python -m pip install -U git+https://github.com/GreenDiscord/discord-ext-owoify``
+
+# Simple Use
+Firstly, import the package using
+
+- ``from discord.ext import owoify``
+
+# update green
---
+++ packages/discord-ext-owoify/discord-ext-owoify-0.0.2.1.1a0.tar.gz/discord-ext-owoify-0.0.2.1.1a0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import re
+
+
+
+version = '0.0.2.1.1-a'
+
+longdescription = open("README.md", "r").read()
+
+setup(name='discord-ext-owoify',
+      author='Green',
+      url='https://github.com/GreenDiscord/discord-ext-owoify',
+      version=version,
+      packages=['discord.ext.owoify'],
+      license='MIT',
+      long_description=longdescription,
+      download_url = 'https://github.com/GreenDiscord/discord-ext-owoify/archive/v0.0.2.1.1-a.tar.gz',
+      description='An extension module to owoify text',
+      install_requires=['discord.py>=1.2.5'],
+      python_requires='>=3.5.3'
+)
