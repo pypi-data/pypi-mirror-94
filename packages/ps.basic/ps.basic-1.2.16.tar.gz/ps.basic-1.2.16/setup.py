a196ff8891cfe29e46ff793da65c22ef0fb13bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:08 +0000

    Add package ps.basic/1.2.16/ps.basic-1.2.16.tar.gz

---
+++ packages/ps.basic/ps.basic-1.2.16.tar.gz/ps.basic-1.2.16/setup.py
@@ -0,0 +1,40 @@
+"""Setup."""
+
+from setuptools import setup
+
+
+version = "1.2.16"
+
+INSTALL_REQUIRES = ["setuptools", "docopt", "pygraphviz"]
+
+setup(
+    name="ps.basic",
+    version=version,
+    description="Basic class used in PS environment",
+    long_description=open("README.rst").read(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: Other/Proprietary License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Utilities",
+    ],
+    keywords="PS",
+    author="Setz",
+    author_email="thomas@setz.de",
+    url="https://bitbucket.org/drsetz/ps.basic",
+    license="GPL",
+    packages=["ps.basic"],
+    package_dir={"": "src"},
+    # include_package_data=True,
+    zip_safe=False,
+    install_requires=INSTALL_REQUIRES,
+    entry_points={
+        "console_scripts": [
+            "ps_shell_log=ps.basic.ps_shell_log:main",
+        ],
+    },
+)
