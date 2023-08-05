b53472f59f02ccf68e3cdd2fe9e01a0320a6517d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:05 +0000

    Add package fast-ft/0.0.4/fast-ft-0.0.4.tar.gz

---
+++ packages/fast-ft/fast-ft-0.0.4.tar.gz/fast-ft-0.0.4/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   setup.py    
+@Contact :   https://2409256477@qq.com
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2021/2/6 21:37   huangjh      1.0         None
+"""
+import setuptools
+
+with open("README.md", "r", encoding="UTF-8") as fh:
+    long_description = fh.read()
+
+with open("fast_ft/requirements", "r", encoding="UTF-8") as re:
+    install_requires = list()
+    for line in re.readlines():
+        package = line.split("==")[0]
+        install_requires.append(package)
+
+setuptools.setup(
+    name="fast-ft",
+    version="0.0.4",
+    author="Uncle supported wall",
+    author_email="2409256477@qq.com",
+    description="A simple file transfer tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/practice9420/fast-ft",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+            'fast-ft = fast_ft.server:main',
+        ]
+    }
+)
