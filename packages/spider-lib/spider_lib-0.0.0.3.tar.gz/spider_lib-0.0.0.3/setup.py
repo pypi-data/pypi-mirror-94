96446290fcb6175e6f5252d043fc26d8ad551446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:08 +0000

    Add package spider-lib/0.0.0.3/spider_lib-0.0.0.3.tar.gz

---
+++ packages/spider-lib/spider_lib-0.0.0.3.tar.gz/spider_lib-0.0.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+setup(
+    name='spider_lib',
+    version='0.0.0.3',
+    description='Spider',
+    long_description="""Spider in python3 coroutine""",
+    long_description_content_type="text/markdown",
+    author='fubo',
+    author_email='fb_linux@163.com',
+    url='https://gitee.com/fubo_linux/spider',
+    packages=find_packages(where='.', exclude=(), include=('*',)),
+    package_data={
+        "spider_lib": [
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'tqdm==4.56.0',
+        'requests_async==0.6.2',
+        'requests==2.25.1',
+        'beautifulsoup4==4.9.3',
+        'pydantic==1.7.3',
+    ],
+    python_requires='>=3.8'
+)
