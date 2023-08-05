8212c330c744a6a5f5da5933566c97e1067f06bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:00 +0000

    Add package cd-api/1.1.11/cd_api-1.1.11.tar.gz

---
+++ packages/cd-api/cd_api-1.1.11.tar.gz/cd_api-1.1.11/setup.py
@@ -0,0 +1,24 @@
+'''
+@Author: Rqk
+@Date: 2020-04-26 15:01:20
+@Description: 更新。
+'''
+
+from setuptools import setup, find_packages  # 这个包没有的可以pip一下
+# import rqk_cd_api
+version = "1.1.11"
+setup(
+    name="cd_api",  # 这里是pip项目发布的名称
+    version=version,  # 版本号，数值大的会优先被pip
+    keywords=["cdApi", "cd", "rqk"],
+    description="任秋锴的草动工具箱",
+    long_description="任秋锴的草动工具箱",
+    license="MIT Licence",
+    url="http://git.renqiukai.com:1983/renqiukai/rqk-cd-tools.git",  # 项目相关文件地址，一般是github
+    author="Renqiukai",
+    author_email="renqiukai@qq.com",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=["requests"]  # 这个项目需要的第三方库
+)
