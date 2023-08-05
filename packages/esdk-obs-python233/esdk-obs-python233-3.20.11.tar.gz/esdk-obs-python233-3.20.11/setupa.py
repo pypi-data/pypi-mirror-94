15cd9a9b70a74a7de7157889ef276e634c602030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:10 +0000

    Add package esdk-obs-python233/3.20.11/esdk-obs-python233-3.20.11.tar.gz

---
+++ packages/esdk-obs-python233/esdk-obs-python233-3.20.11.tar.gz/esdk-obs-python233-3.20.11/setupa.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='esdk-obs-python233',
+    version='3.20.11',
+    packages=find_packages(),
+    zip_safe=False,
+    description='OBS Python SDK',
+    long_description='OBS Python SDK',
+    license='Apache-2.0',
+    keywords='python obs',
+    url='http://obs-browser.obs.myhuaweicloud.com'
+)
