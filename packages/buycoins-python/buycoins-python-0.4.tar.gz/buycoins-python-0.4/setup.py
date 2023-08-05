721c9d88a3b40efc4fcdcc72ddcc1154245039e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:19 +0000

    Add package buycoins-python/0.4/buycoins-python-0.4.tar.gz

---
+++ packages/buycoins-python/buycoins-python-0.4.tar.gz/buycoins-python-0.4/setup.py
@@ -0,0 +1,26 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="buycoins-python",
+    version="0.4",
+    description="Python SDK for the BuyCoin\"s API",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Youngestdev/BuyCoins-Python",
+    author="Abdulazeez Abdulazeez Adeshina",
+    author_email="youngestdev@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    install_requires=["python-graphql-client", "requests", "python-decouple"],
+    packages=find_packages(),
+    python_requires=">=3.6"
+)
