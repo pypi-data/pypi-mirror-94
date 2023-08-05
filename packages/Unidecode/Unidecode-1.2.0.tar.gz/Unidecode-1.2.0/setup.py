d58ddc8ca5a5b5a7d3b4904cf07fbe0625975758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:53 +0000

    Add package Unidecode/1.2.0/Unidecode-1.2.0.tar.gz

---
+++ packages/Unidecode/Unidecode-1.2.0.tar.gz/Unidecode-1.2.0/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# vi:tabstop=4:expandtab:sw=4
+
+import os
+from setuptools import setup
+
+
+def get_long_description():
+    with open(os.path.join(os.path.dirname(__file__), "README.rst")) as fp:
+        return fp.read()
+
+setup(
+    name='Unidecode',
+    version='1.2.0',
+    description='ASCII transliterations of Unicode text',
+    license='GPL',
+    long_description=get_long_description(),
+    author='Tomaz Solc',
+    author_email='tomaz.solc@tablix.org',
+
+    packages=['unidecode'],
+    package_data={'unidecode': ['py.typed', '__init__.pyi']},
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+
+    test_suite='tests',
+
+    entry_points={
+        'console_scripts': [
+            'unidecode = unidecode.util:main'
+        ]
+    },
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Text Processing",
+        "Topic :: Text Processing :: Filters",
+    ],
+)
