8812a739b899b61bb615908c07665df0cc556cbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:52 +0000

    Add package autogalaxy/0.18.0/autogalaxy-0.18.0.tar.gz

---
+++ packages/autogalaxy/autogalaxy-0.18.0.tar.gz/autogalaxy-0.18.0/autogalaxy/__init__.py
@@ -0,0 +1,67 @@
+from . import aggregator as agg
+from . import plot
+from . import util
+from .dataset.imaging import MaskedImaging, SettingsMaskedImaging, SimulatorImaging
+from .dataset.interferometer import (
+    MaskedInterferometer,
+    SettingsMaskedInterferometer,
+    SimulatorInterferometer,
+)
+
+from autoarray import Grid2DIterate
+from autoarray import Grid2DInterpolate
+from autoarray import Mask2D
+from autoarray import Grid2DIrregular
+from autoarray import Grid2DIrregularGrouped
+from autoarray import VectorField2DIrregular
+from autoarray import TransformerDFT
+from autoarray import pix
+from autoarray import reg
+from autoarray import TransformerNUFFT
+from autoarray import SettingsInversion
+from autoarray import SettingsPixelization
+from autoarray.structures.arrays import Array2D
+from autoarray.structures.grids import Grid2D, Grid2DRectangular, Grid2DVoronoi
+from autoarray.structures.kernel import Kernel2D
+from autoarray.structures.visibilities import Visibilities
+from autoarray.dataset.imaging import Imaging
+from autoarray.dataset.interferometer import Interferometer
+from autoarray.operators.convolver import Convolver
+from autoconf import conf
+
+from .fit.fit import FitImaging, FitInterferometer
+from .galaxy.fit_galaxy import FitGalaxy
+from .galaxy.galaxy import Galaxy, HyperGalaxy, Redshift
+from .galaxy.galaxy_data import GalaxyData
+from .galaxy.galaxy_model import GalaxyModel
+from .galaxy.masked_galaxy_data import MaskedGalaxyDataset
+from .hyper import hyper_data
+from .pipeline.phase.abstract import phase
+from .pipeline.phase.abstract.phase import AbstractPhase
+from .pipeline.phase.extensions.hyper_phase import HyperPhase
+from .pipeline.phase.imaging.phase import PhaseImaging
+from .pipeline.phase.interferometer.phase import PhaseInterferometer
+from .pipeline.phase.phase_galaxy import PhaseGalaxy
+from .pipeline.phase.settings import SettingsPhaseImaging
+from .pipeline.phase.settings import SettingsPhaseInterferometer
+from .pipeline.pipeline import PipelineDataset
+from .pipeline.setup import (
+    SetupPipeline,
+    SetupHyper,
+    SetupLightParametric,
+    SetupLightInversion,
+    SetupMassTotal,
+    SetupMassLightDark,
+    SetupSMBH,
+)
+from .plane.plane import Plane
+from .profiles import (
+    light_profiles as lp,
+    mass_profiles as mp,
+    light_and_mass_profiles as lmp,
+)
+from . import convert
+
+conf.instance.register(__file__)
+
+__version__ = '0.18.0'
---
+++ packages/autogalaxy/autogalaxy-0.18.0.tar.gz/autogalaxy-0.18.0/test_autogalaxy/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from matplotlib import pyplot
+
+
+class PlotPatch:
+    def __init__(self):
+        self.paths = []
+
+    def __call__(self, path, *args, **kwargs):
+        self.paths.append(path)
+
+
+@pytest.fixture(name="plot_patch")
+def make_plot_patch(monkeypatch):
+    plot_patch = PlotPatch()
+    monkeypatch.setattr(pyplot, "savefig", plot_patch)
+    return plot_patch
