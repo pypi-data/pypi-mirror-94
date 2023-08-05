f6a67dfb7b8185355d682e31662f5c727c57bf12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:01 +0000

    Add package cloudmesh-common/4.3.34/cloudmesh-common-4.3.34.tar.gz

---
+++ packages/cloudmesh-common/cloudmesh-common-4.3.34.tar.gz/cloudmesh-common-4.3.34/cloudmesh/common/run/file.py
@@ -0,0 +1,17 @@
+import subprocess
+import os
+
+from cloudmesh.common.util import readfile
+
+
+def run (command):
+    """
+    run the command, redirect the outout to a file and display the content once
+    it is completed.
+
+    :param command:
+    :return:
+    """
+    os.system (f"{command} &> ./cmd-output")
+    content = readfile("./cmd-output")
+    return content
