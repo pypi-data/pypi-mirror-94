aacc51fcd4232d3bea73eb049cd18f7950343b5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:17 +0000

    Add package sicp/0.1.15b232.dev5/sicp-0.1.15b232.dev5.tar.gz

---
+++ packages/sicp/sicp-0.1.15b232.dev5.tar.gz/sicp-0.1.15b232.dev5/sicp/common/rpc/ag_worker.py
@@ -0,0 +1,9 @@
+from typing import List
+from common.rpc.utils import create_service
+
+service = create_service(__name__, "ag-worker")
+
+
+@service.route("/api/batch_grade")
+def batch_grade(*, command: str, jobs: List[str], grading_zip: str):
+    ...
