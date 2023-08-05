bf9845732e59005689dace880f05d68790499da9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:22 +0000

    Add package SplitsPy/0.0.10/SplitsPy-0.0.10.tar.gz

---
+++ packages/SplitsPy/SplitsPy-0.0.10.tar.gz/SplitsPy-0.0.10/setup.py
@@ -0,0 +1,21 @@
+# setup.py
+"""Setup splitspy.py
+
+LICENSE: This is open-source software released under the terms of the
+GPL (http://www.gnu.org/licenses/gpl.html).
+"""
+
+import setuptools
+
+setuptools.setup(name='SplitsPy',
+                 version='0.0.10',
+                 packages=setuptools.find_packages(),
+                 url='http://github.com/husonlab/SplitsPhy',
+                 license='GPL',
+                 author='Daniel H. Huson',
+                 author_email='daniel.huson@uni-tuebingen.de',
+                 description='Phylogenetic outlines',
+                 keywords='phylogenetics, network',
+                 entry_points={'console_scripts': ['outline=splitspy.outline:main', ], },
+                 python_requires='>=3.5, <4',
+                 install_requires=['numpy', 'pillow'])
