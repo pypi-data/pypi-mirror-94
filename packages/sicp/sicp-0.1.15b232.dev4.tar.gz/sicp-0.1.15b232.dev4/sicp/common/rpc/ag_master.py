b646fb150219466db5fcef4a9ce7dc5b11af2133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:03 +0000

    Add package sicp/0.1.15b232.dev4/sicp-0.1.15b232.dev4.tar.gz

---
+++ packages/sicp/sicp-0.1.15b232.dev4.tar.gz/sicp-0.1.15b232.dev4/sicp/common/rpc/ag_master.py
@@ -0,0 +1,49 @@
+from typing import List, Dict
+from common.rpc.utils import create_service
+
+service = create_service(__name__, "ag-master")
+
+
+@service.route("/api/trigger_jobs")
+def trigger_jobs(*, secret: str, assignment_id: str, subms: List[str], jobs: List[str]):
+    ...
+
+
+@service.route("/api/get_zip")
+def get_zip(*, course: str, name: str) -> str:
+    ...
+
+
+@service.route("/api/get_submission")
+def get_submission(*, course: str, bid: str, job_id: str) -> Dict:
+    ...
+
+
+@service.route("/api/handle_output")
+def handle_output(*, course: str, output: str, job_id: str) -> Dict:
+    ...
+
+
+@service.route("/api/set_results")
+def set_results(*, course: str, job_id: str, status: str, result: str):
+    ...
+
+
+@service.route("/api/upload_zip")
+def upload_zip(
+    *, access_token: str = None, course: str, semester: str, name: str, file: str
+):
+    ...
+
+
+@service.route("/api/create_assignment")
+def create_assignment(
+    *,
+    access_token: str = None,
+    course: str,
+    semester: str,
+    name: str,
+    file: str,
+    command: str
+) -> str:
+    ...
