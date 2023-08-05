75cfd76318e26b17a1671db33066ed2b1af2efbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:04 +0000

    Add package vulcan-api/1.3.1/vulcan-api-1.3.1.tar.gz

---
+++ packages/vulcan-api/vulcan-api-1.3.1.tar.gz/vulcan-api-1.3.1/vulcan/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from ._vulcan import Vulcan
+from .hebe import VulcanHebe, Keystore, Account
+
+__version__ = "1.3.1"
+__doc__ = "Unofficial API for UONET+ e-register"
+
+__all__ = ["Vulcan", "VulcanHebe", "Keystore", "Account"]
