166ede64577fb54e0ad63a34b80510ff0902be20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:23 +0000

    Add package datazen/1.4.1/datazen-1.4.1-py3-none-any.whl

---
+++ packages/datazen/datazen-1.4.1-py3-none-any.whl/datazen/__init__.py
@@ -0,0 +1,20 @@
+# =====================================
+# generator=datazen
+# version=1.4.0
+# hash=28274b207d9416e414cb97bba41efee4
+# =====================================
+
+"""
+datazen - Useful defaults and other package metadata.
+"""
+
+PKG_NAME = "datazen"
+VERSION = "1.4.1"
+DESCRIPTION = "Compile and render schema-validated configuration data."
+
+DEFAULT_TYPE = "yaml"
+DEFAULT_MANIFEST = "manifest.{}".format(DEFAULT_TYPE)
+DEFAULT_DIR = "{}-out".format(PKG_NAME)
+CACHE_SUFFIX = "_cache"
+GLOBAL_KEY = "global"
+ROOT_NAMESPACE = "__root__"
