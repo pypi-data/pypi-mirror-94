dcf76b2cd44a8384cdc571e075fa34dc33acdb56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:08 +0000

    Add package search-engine-rishatsadykov/1.0/search_engine_rishatsadykov-1.0.tar.gz

---
+++ packages/search-engine-rishatsadykov/search_engine_rishatsadykov-1.0.tar.gz/search_engine_rishatsadykov-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='search_engine_rishatsadykov',
+    version='1.0',
+    packages=['lemmatization'],
+    url='',
+    license='',
+    author='Rishat Sadykov',
+    author_email='r.sadickov2010@yandex.ru',
+    description='Calculates TF-IDF for data set'
+)
