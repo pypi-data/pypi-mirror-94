d59ef6d1aec7ebd425b9790adf999d9946dad9bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:44 +0000

    Add package zoom-toolkit/1.3/zoom_toolkit-1.3.tar.gz

---
+++ packages/zoom-toolkit/zoom_toolkit-1.3.tar.gz/zoom_toolkit-1.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='zoom_toolkit',
+   version='1.3',
+   description='This is a simple yet useful toolkit implemented for working on zoom records.',
+   license="MIT",
+   url='https://github.com/OnurArdaB/Zoom-Toolkit',
+    download_url = 'https://github.com/OnurArdaB/Zoom-Toolkit/archive/v_01.tar.gz',    # I explain this later on
+   author='Onur Arda Bodur',
+   author_email='onurarda@sabanciuniv.edu',
+   packages=['zoom_toolkit'],  
+   install_requires=['opencv-python', 'moviepy'], #external packages as dependencies
+   classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3.6',
+    ],
+)
