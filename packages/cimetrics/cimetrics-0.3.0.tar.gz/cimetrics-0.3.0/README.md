d962a99d5e13b1fdf04e68e33233fd7e096c6bd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:33 +0000

    Add package cimetrics/0.3.0/cimetrics-0.3.0.tar.gz

---
+++ packages/cimetrics/cimetrics-0.3.0.tar.gz/cimetrics-0.3.0/README.md
@@ -0,0 +1,120 @@
+# cimetrics
+
+[![Build Status](https://dev.azure.com/jumaffre/metrics-devops/_apis/build/status/jumaffre.cimetrics?branchName=master)](https://dev.azure.com/jumaffre/metrics-devops/_build/latest?definitionId=2&branchName=master)
+[![PyPI version](https://badge.fury.io/py/cimetrics.svg)](https://badge.fury.io/py/cimetrics)
+
+`cimetrics` lets you track crucial metrics to avoid unwanted regressions. It is easy to integrate with your existing projects and automatically provides quick feedback in your GitHub Pull Requests. See it in action [here](https://github.com/jumaffre/cimetrics/pull/66#issuecomment-573324351).
+
+<p align="center"> <img src="https://raw.githubusercontent.com/jumaffre/cimetrics/cimetrics/cimetrics/image2020-01-11%2015%3A03%3A37.764906.png" width="600"></p>
+
+## Install
+
+```sh
+pip install cimetrics
+```
+
+## Using cimetrics
+
+### Setup storage
+
+Metrics data are stored by in any MongoDB-compatible database.
+
+An easy way to get storage set up is to spin up a [Cosmos DB](https://docs.microsoft.com/en-us/azure/cosmos-db/introduction) instance in Azure. The connection string should be stored as the `METRICS_MONGO_CONNECTION` secret variable in your CI system.
+
+### Pushing metrics from your tests
+
+You can use the simple python API to push your metrics to your storage:
+
+```python
+import cimetrics.upload
+
+with cimetrics.upload.Metrics() as metrics:
+  # Run some tests and collect some data
+  metrics.put("metric1 name (unit)", metric_1)
+  metrics.put("metric2 name (unit)", metric_2)
+```
+
+Note that `metric_1` and `metric_2` must be instances of [numbers.Real](https://docs.python.org/3.7/library/numbers.html#numbers.Real), for example `float` or `int`.
+
+If a build publishes metrics from multiple instances of a `cimetrics.upload.Metrics`, for example because
+it is running multiple concurrent jobs, it it necessary to publish those as "incomplete",
+and to publish a "complete" entry only once they have all run. This is to prevent metrics comparison from
+happening against an incomplete set of metrics for a build.
+
+For example:
+
+```python
+# Job 1
+with cimetrics.upload.Metrics(complete=False) as metrics:
+  metrics.put("metric1 name (unit)", metric_1)
+
+# Job 2
+with cimetrics.upload.Metrics(complete=False) as metrics:
+  metrics.put("metric2 name (unit)", metric_2)
+
+# Job running after Job 1 and 2 are complete
+with cimetrics.upload.Metrics() as metrics:
+  pass
+```
+
+It is often convenient to use the same job to mark a set of metrics as complete and to plot them.
+A convenience entry-point is supplied to mark the metrics complete for a build:
+
+```python
+python -m cimetrics.upload_complete
+```
+
+### Setup the CI
+
+Your CI is responsible for rendering the metrics report and posting them to your Pull Requests in GitHub. For this, you should create a [personal authentication token](https://help.github.com/en/articles/creating-a-personal-access-token-for-the-command-line) with Write access to the repository for the account you want to post on behalf of `cimetrics`. Then, you should set up the token as the `GITHUB_TOKEN` secret variable in your CI system. Don't forget to add that user as a personal contributor (Write access) to your Github repository as well.
+
+Then, you should add the following steps to your CI configuration file, e.g. for Azure Pipelines:
+
+```yaml
+# Your application. This step collects and uploads your metrics
+# to your MongoDB instance.
+- script: python app/main.py
+  env:
+    METRICS_MONGO_CONNECTION: $(METRICS_MONGO_CONNECTION)
+  displayName: 'Run app and collect metrics'
+
+# This step generates a graph reporting the differences between
+# your branch and the target branch.
+# Only run on Pull Requests build.
+- script: python -m cimetrics.plot
+  env:
+    METRICS_MONGO_CONNECTION: $(METRICS_MONGO_CONNECTION)
+  displayName: 'Plot metrics'
+  condition: eq(variables['Build.Reason'], 'PullRequest')
+
+# This step publishes a report comment on the GitHub Pull Request
+# using GITHUB_TOKEN as authentication (use secret variables!)
+# Only run on Pull Requests build.
+- script: python -m cimetrics.github_pr
+  env:
+    GITHUB_TOKEN: $(GITHUB_TOKEN)
+  displayName: 'Post metrics graphs as PR comment'
+  condition: eq(variables['Build.Reason'], 'PullRequest')
+```
+
+See [azure-pipelines.yml](https://github.com/jumaffre/cimetrics/blob/master/azure-pipelines.yml) for a full working example.
+
+### Create the `metrics.yml` file
+
+The last step is to create a new `metrics.yml` configuration file at the root of your repository. The file should specify the name of the database and collection used for MongoDB. For example:
+
+```yaml
+db: 'metrics'
+collection: 'metrics_performance'
+```
+
+That's it! The next time you create a Pull Request, your CI will automatically store your metrics and publish a graph comparing your metrics against the same metrics on the branch you are merging to. Note that the cimetrics PR comment is updated for each subsequent build.
+
+## Caveats
+
+- If the CI has never run on the target branch (e.g. `master` - likely to happen when you first set up `cimetrics`), the report will only show the values that have been uploaded, without any comparison.
+- The rendered images are currently hosted in the target GitHub repository itself, under the `cimetrics` branch, in the `cimetrics` directory.
+
+## Supported CI pipelines
+
+CI Metrics currently supports [Azure Pipelines](https://azure.microsoft.com/en-us/services/devops/pipelines/), but it should be very easy to add support for other build pipelines by [subclassing GitEnv](https://github.com/jumaffre/cimetrics/blob/master/cimetrics/env.py#L72) and providing the right attributes.
---
+++ packages/cimetrics/cimetrics-0.3.0.tar.gz/cimetrics-0.3.0/cimetrics/env.py
@@ -0,0 +1,207 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+from typing import Optional
+import os
+import yaml
+from git import Repo, exc
+
+
+def get_env():
+    try:
+        repo = Repo(os.getcwd(), search_parent_directories=True)
+    except exc.InvalidGitRepositoryError:
+        print(f"Environment {os.getcwd()} is not a valid git repository.")
+        return None
+
+    if "SYSTEM_TEAMFOUNDATIONCOLLECTIONURI" in os.environ:
+        return AzurePipelinesEnv(repo)
+    else:
+        return GitEnv(repo)
+
+
+class Env(object):
+    def __init__(self) -> None:
+        root = self.repo_root
+        self.CONFIG_FILE = "metrics.yml"
+        self.DEFAULT_TARGET_BRANCH = "main"
+
+        config_file_path = os.path.join(root, self.CONFIG_FILE)
+        if os.path.exists(config_file_path):
+            with open(config_file_path) as fp:
+                self.cfg = yaml.safe_load(fp)
+                if self.cfg is None:
+                    self.cfg = {}
+        else:
+            print(
+                f"{self.CONFIG_FILE} does not exist at the root {root} of the repo,"
+                " metrics will not be recorded."
+            )
+            self.cfg = {}
+
+    @property
+    def config_file(self) -> str:
+        return self.CONFIG_FILE
+
+    @property
+    def repo_root(self) -> str:
+        # implemented by subclass
+        raise NotImplementedError
+
+    @property
+    def mongo_db(self) -> str:
+        return self.cfg["db"]
+
+    @property
+    def mongo_collection(self) -> str:
+        return self.cfg["collection"]
+
+    @property
+    def columns(self) -> int:
+        return self.cfg.get("columns", 2)
+
+    @property
+    def span(self) -> int:
+        return self.cfg.get("span", 30)
+
+    @property
+    def ewma_span(self) -> int:
+        return self.cfg.get("ewma_span", 5)
+
+    @property
+    def monitoring_span(self) -> int:
+        return self.cfg.get("monitoring_span", 50)
+
+    @property
+    def mongo_connection(self) -> str:
+        return os.environ["METRICS_MONGO_CONNECTION"]
+
+    @property
+    def build_id(self):
+        return None
+
+    @property
+    def is_pr(self):
+        return False
+
+    @property
+    def github_token(self) -> str:
+        return os.environ["GITHUB_TOKEN"]
+
+    @property
+    def pr_user(self) -> str:
+        return self.cfg.get("pr_user", "cimetrics")
+
+    @property
+    def monitoring_columns(self) -> int:
+        return self.cfg.get("monitoring_columns", 2)
+
+
+class GitEnv(Env):
+    _target_branch = None
+
+    def __init__(self, repo) -> None:
+        self.repo = repo
+        super().__init__()
+
+    @property
+    def repo_root(self) -> str:
+        return self.repo.working_tree_dir
+
+    @property
+    def branch(self) -> Optional[str]:
+        if not self.repo.head.is_detached:
+            return self.repo.active_branch.name
+        tag_or_none = next(
+            (tag.name for tag in self.repo.tags if tag.commit == self.repo.head.commit),
+            None,
+        )
+        return tag_or_none
+
+    @property
+    def commit(self) -> str:
+        return self.repo.commit().hexsha
+
+    @property
+    def target_branch(self) -> str:
+        if self._target_branch is not None:
+            return self._target_branch
+
+        self._target_branch = os.environ.get("CIMETRICS_TARGET_BRANCH")
+        if self._target_branch is not None:
+            return self._target_branch
+
+        print(
+            f"Target branch defaulting to {self.DEFAULT_TARGET_BRANCH}. Set CIMETRICS_TARGET_BRANCH env var to change it."
+        )
+        self._target_branch = self.DEFAULT_TARGET_BRANCH
+        return self._target_branch
+
+    def build_url_by_id(self, build_id) -> str:
+        return f"{build_id}"
+
+    @property
+    def build_url(self) -> str:
+        return self.build_url_by_id(self.build_id)
+
+    @property
+    def build_id(self) -> str:
+        return "2"
+
+    @property
+    def build_number(self) -> str:
+        return "0000.0"
+
+
+class AzurePipelinesEnv(GitEnv):
+    @property
+    def build_id(self) -> str:
+        return os.environ["BUILD_BUILDID"]
+
+    @property
+    def build_number(self) -> str:
+        return os.environ["BUILD_BUILDNUMBER"]
+
+    @property
+    def is_pr(self) -> bool:
+        return "SYSTEM_PULLREQUEST_SOURCEBRANCH" in os.environ
+
+    @property
+    def target_branch(self) -> str:
+        return os.environ.get(
+            "SYSTEM_PULLREQUEST_TARGETBRANCH", self.DEFAULT_TARGET_BRANCH
+        )
+
+    @property
+    def branch(self) -> Optional[str]:
+        short = None
+        if self.is_pr:
+            short = os.environ["SYSTEM_PULLREQUEST_SOURCEBRANCH"]
+        else:
+            ref = os.environ["BUILD_SOURCEBRANCH"]
+            for prefix in ["refs/heads/", "refs/tags/", "refs/pull/"]:
+                if ref.startswith(prefix):
+                    short = ref[len(prefix) :]
+                    break
+            assert short, f"Unsupported ref type: {ref}"
+        return short
+
+    @property
+    def pull_request_id(self) -> str:
+        if self.is_pr:
+            return os.environ["SYSTEM_PULLREQUEST_PULLREQUESTNUMBER"]
+        else:
+            return self.cfg["monitoring_issue"]
+
+    @property
+    def repo_id(self) -> str:
+        return os.environ["BUILD_REPOSITORY_ID"]
+
+    def build_url_by_id(self, build_id) -> str:
+        prefix = os.environ["SYSTEM_TEAMFOUNDATIONSERVERURI"]
+        project = os.environ["SYSTEM_TEAMPROJECT"]
+        return f"{prefix}{project}/_build/results?buildId={build_id}&view=results"
+
+    @property
+    def build_url(self) -> str:
+        return self.build_url_by_id(self.build_id)
