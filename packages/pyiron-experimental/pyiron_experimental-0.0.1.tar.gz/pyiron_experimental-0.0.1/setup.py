fa49aca9fc6976fa86e86108988a98a072412ad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:39 +0000

    Add package pyiron-experimental/0.0.1/pyiron_experimental-0.0.1.tar.gz

---
+++ packages/pyiron-experimental/pyiron_experimental-0.0.1.tar.gz/pyiron_experimental-0.0.1/setup.py
@@ -0,0 +1,43 @@
+"""
+Setuptools based setup module
+"""
+from setuptools import setup, find_packages
+import versioneer
+
+
+setup(
+    name='pyiron_experimental',
+    version=versioneer.get_version(),
+    description='Repository for experimental plugins to the pyiron IDE.',
+    long_description='http://pyiron.org',
+
+    url='https://github.com/pyiron/pyiron_experimental',
+    author='Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department',
+    author_email='hassani@mpie.de',
+    license='BSD',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Scientific/Engineering :: Physics',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
+
+    keywords='pyiron',
+    packages=find_packages(exclude=["*tests*"]),
+    install_requires=[
+        'pyiron_base>=0.1.45',
+        'temmeta>=0.0.6',
+        'pystem>=0.0.25',
+        'hyperspy>=1.5.2',
+        'scanf>=1.5.2',
+    ],
+    cmdclass=versioneer.get_cmdclass(),
+)
