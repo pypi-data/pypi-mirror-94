34059e2ef08ce049e4e6ca751f7a40002b115c58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:48 +0000

    Add package dingDONG/1.0.18/dingDONG-1.0.18.tar.gz

---
+++ packages/dingDONG/dingDONG-1.0.18.tar.gz/dingDONG-1.0.18/lib/dingDONG/conn/baseConnBatch.py
@@ -0,0 +1,103 @@
+# (c) 2017-2021, Tal Shany <tal.shany@biSkilled.com>
+#
+# This file is part of dingDONG
+#
+# dingDONG is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any late r version.
+#
+# dingDONG is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with dingDONG.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import abc
+import six
+
+from dingDONG.conn.baseConn import baseConn
+from dingDONG.misc.enums    import eConn
+from dingDONG.misc.globalMethods import setProperty, findEnum
+from dingDONG.misc.logger   import p
+
+@six.add_metaclass(abc.ABCMeta)
+class baseConnBatch ( baseConn ):
+    def __init__ (self,propertyDict=None, **args):
+        """ ABSTRACT METHOD THAT MUST BE IMPLEMETED IN ANY CHILD CLASSES """
+
+        baseConn.__init__(self, propertyDict=propertyDict, **args)
+        self.dataTypes = self.setDataTypes(connDataTypes=setProperty(k= eConn.props.DATA_TYPES, o=args, defVal={}))
+
+        self.connType = setProperty(k=eConn.props.TYPE, o=self.propertyDict)
+        self.connName = setProperty(k=eConn.props.NAME, o=self.propertyDict, defVal=self.connType)
+        self.connIsSrc = setProperty(k=eConn.props.IS_SOURCE, o=self.propertyDict, defVal=False)
+        self.connIsTar = setProperty(k=eConn.props.IS_TARGET, o=self.propertyDict, defVal=False)
+        self.connIsSql = setProperty(k=eConn.props.IS_SQL, o=self.propertyDict, defVal=False)
+        self.connFilter = setProperty(k=eConn.props.FILTER, o=self.propertyDict, defVal=None)
+        self.connUpdateMethod = setProperty(k=eConn.props.UPDATE, o=self.propertyDict)
+
+        self.isSingleObject = True
+
+
+        if not findEnum(prop=self.connType, obj=eConn.types):
+            err = "{}: Connection NOT VALID !!".format(self.connType)
+            raise ValueError(err)
+
+    """ ABSTRACT METHOD THAT MUST BE IMPLEMETED IN ANY CHILD CLASSES """
+
+    @abc.abstractmethod
+    def connect(self):
+        raise NotImplemented("baseConn-> Connect Method is not implemented !!!")
+
+    @abc.abstractmethod
+    def close(self):
+        pass
+
+    def test(self):
+        if not  self.connect():
+            p("CONNECTING FAILED: %s, type: %s " % (self.connName, self.connType), "e")
+        return True
+
+    @abc.abstractmethod
+    def isExists(self, **args):
+        pass
+
+    @abc.abstractmethod
+    def create(self, sttDict=None, addIndex=None):
+        pass
+
+    @abc.abstractmethod
+    def getStructure(self, objects=None,  **args):
+        pass
+
+    @abc.abstractmethod
+    def preLoading(self, dictObj=None):
+        pass
+
+    @abc.abstractmethod
+    def extract(self, tar, tarToSrcDict, **args):
+        pass
+
+    @abc.abstractmethod
+    def load(self, **args):
+        pass
+
+    @abc.abstractmethod
+    def execMethod(self, method=None):
+        pass
+
+    @abc.abstractmethod
+    def merge(self, mergeTable, mergeKeys=None, sourceTable=None):
+        pass
+
+    @abc.abstractmethod
+    def cntRows(self, objName=None):
+        pass
+
+    @abc.abstractmethod
+    def createFromDbStrucure(self, stt=None, objName=None, addIndex=None):
+        pass
---
+++ packages/dingDONG/dingDONG-1.0.18.tar.gz/dingDONG-1.0.18/setup.py
@@ -0,0 +1,122 @@
+from __future__ import print_function
+__version__ = '1.0.18'
+__author__  = 'Tal@BiSkilled.com'
+__codename__= 'Why not?'
+
+import os.path
+import sys
+import warnings
+
+try:
+    from setuptools import setup, find_packages
+    from setuptools.command.build_py import build_py as BuildPy
+    from setuptools.command.install_lib import install_lib as InstallLib
+    from setuptools.command.install_scripts import install_scripts as InstallScripts
+except ImportError:
+    print("dingDONG now needs setuptools in order to build. Install it using"
+          " your package manager (usually python-setuptools) or via pip (pip"
+          " install setuptools).", file=sys.stderr)
+    sys.exit(1)
+
+
+sys.path.insert(0, os.path.abspath('lib'))
+#print(sys.path)
+#from release import  __version__, __author__
+
+def read_file(file_name):
+    """Read file and return its contents."""
+    with open(file_name, 'r') as f:
+        print (file_name)
+        return f.read()
+
+def read_requirements(file_name):
+    """Read requirements file as a list."""
+    reqs = read_file(file_name).splitlines()
+    if not reqs:
+        raise RuntimeError(
+            "Unable to read requirements from the %s file"
+            "That indicates this copy of the source code is incomplete."
+            % file_name
+        )
+    return reqs
+
+def get_dynamic_setup_params():
+    """Add dynamically calculated setup params to static ones."""
+    return {
+        # Retrieve the long description from the README
+        'long_description': read_file('README.rst'),
+        'install_requires': read_requirements('requirements.txt')
+    }
+
+# packages=['', 'glob', 'mapp', 'loader', 'connections'],
+#download_url = 'https://github.com/biskilled/popEye-Etl/archive/1.1.0.tar.gz',  # I explain this later on
+#install_requires = ['pyodbc', 'pymysql', 'vertica_python', 'cx_Oracle', 'pymongo', 'sqlparse', 'clr', 'pandas'],
+
+static_setup_params = dict(
+    name        = 'dingDONG',  # How you named your package folder (MyLib)
+    version     = __version__,
+    description = 'Data modeling managing and transforming data',  # Give a short description about your library
+    author      = __author__,
+    author_email= 'Tal@BiSkilled.com',  # Type in your E-Mail
+    keywords    = ['ETL', 'Data preparation', 'Data modeling', 'Python', 'Integration', 'Mapping', 'ML devops', 'CI', 'CD'],   # Keywords that define your package best
+    url         = 'https://github.com/biskilled/dingDONG',  # Provide either the link to your github or to your website
+
+    project_urls={
+        'Bug Tracker'    : 'https://github.com/biskilled/dingDONG/issues',
+        'CI: Shippable'  : 'https://app.shippable.com/github/biskilled/dingDONG',
+        #'Code of Conduct': '',
+        'Documentation'  : 'https://readthedocs.org/projects/dingdong/',
+        #'Mailing lists'  : '',
+        'Source Code'    : 'https://github.com/biskilled/dingDONG'
+    },
+    license='GPLv3+',
+    python_requires= '>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
+    package_dir    = {'': 'lib'},
+    packages       = find_packages('lib'),
+    package_data   ={
+        '': [ 'dll/*.*' ],
+    },
+
+    classifiers=[
+        'Development Status :: 4 - Beta',       # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    scripts=[],
+    data_files=[],
+    # Installing as zip files would break due to references to __file__
+    zip_safe=False,
+    install_requires=read_requirements('requirements.txt')
+)
+
+
+def main():
+    """Invoke installation process using setuptools."""
+    setup_params = dict(static_setup_params, **get_dynamic_setup_params())
+    ignore_warning_regex = (
+        r"Unknown distribution option: '(project_urls|python_requires)'"
+    )
+    warnings.filterwarnings(
+        'ignore',
+        message=ignore_warning_regex,
+        category=UserWarning,
+        module='distutils.dist',
+    )
+    setup(**setup_params)
+    warnings.resetwarnings()
+
+if __name__ == '__main__':
+    main()
