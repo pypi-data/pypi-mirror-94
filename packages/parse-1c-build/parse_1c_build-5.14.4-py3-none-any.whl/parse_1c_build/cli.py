3e022614b569314aaf1dbb41265764f6a275fba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:16 +0000

    Add package parse-1c-build/5.14.4/parse_1c_build-5.14.4-py3-none-any.whl

---
+++ packages/parse-1c-build/parse_1c_build-5.14.4-py3-none-any.whl/parse_1c_build/cli.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from argparse import ArgumentParser
+
+from cjk_commons.logging_ import add_logging_arguments
+from parse_1c_build import __version__, build, parse
+
+
+def get_argparser() -> ArgumentParser:
+    parser = ArgumentParser(prog='p1cb', description='Parse and build utilities for 1C:Enterprise', add_help=False)
+    parser.add_argument(
+        '-h', '--help',
+        action='help',
+        help='Show this help message and exit'
+    )
+    parser.add_argument(
+        '-v', '--version',
+        action='version',
+        version=f'%(prog)s, ver. {__version__}',
+        help='Show version'
+    )
+    add_logging_arguments(parser)
+    subparsers = parser.add_subparsers(dest='subparser_name')
+    build.add_subparser(subparsers)
+    parse.add_subparser(subparsers)
+    return parser
