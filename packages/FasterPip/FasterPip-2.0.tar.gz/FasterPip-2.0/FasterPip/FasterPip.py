4654951ede8be516d97d13a45d54f8d17d14e683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:40 +0000

    Add package FasterPip/2.0/FasterPip-2.0.tar.gz

---
+++ packages/FasterPip/FasterPip-2.0.tar.gz/FasterPip-2.0/FasterPip/FasterPip.py
@@ -0,0 +1,51 @@
+from en_us import web_install
+
+def helpmain():
+    a = 'Visit URL:"https://github.com/mstouk57g/FasterPip/blob/main/README.md"'
+    return a
+def run(laug, mode):
+    web_install()
+    return None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
