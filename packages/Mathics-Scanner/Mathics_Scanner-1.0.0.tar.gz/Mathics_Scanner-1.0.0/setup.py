7a4dbc8d8f54b672df476913859b9700a7c60560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:25 +0000

    Add package Mathics-Scanner/1.0.0/Mathics_Scanner-1.0.0.tar.gz

---
+++ packages/Mathics-Scanner/Mathics_Scanner-1.0.0.tar.gz/Mathics_Scanner-1.0.0/setup.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Setuptools based setup script for Mathics.
+
+For the easiest installation just type the following command (you'll probably
+need root privileges):
+
+    python setup.py install
+
+This will install the library in the default location. For instructions on
+how to customize the install procedure read the output of:
+
+    python setup.py --help install
+
+In addition, there are some other commands:
+
+    python setup.py clean -> will clean all trash (*.pyc and stuff)
+
+To get a full list of avaiable commands, read the output of:
+
+    python setup.py --help-commands
+
+Or, if all else fails, feel free to write to the mathics users list at
+mathics-users@googlegroups.com and ask for help.
+"""
+
+import sys
+import os.path as osp
+import platform
+import subprocess
+from setuptools import setup, Command, Extension
+
+# Ensure user has the correct Python version
+if sys.version_info < (3, 6):
+    print("mathics-scanner does not support Python %d.%d" % sys.version_info[:2])
+    sys.exit(-1)
+
+def get_srcdir():
+    filename = osp.normcase(osp.dirname(osp.abspath(__file__)))
+    return osp.realpath(filename)
+
+
+def read(*rnames):
+    return open(osp.join(get_srcdir(), *rnames)).read()
+
+subprocess.run(["make", "mathics_scanner/data/characters.json"])
+
+# stores __version__ in the current namespace
+exec(compile(open("mathics_scanner/version.py").read(), "mathics_scanner/version.py", "exec"))
+
+# Get/set VERSION and long_description from files
+long_description = read("README.rst") + "\n"
+
+
+is_PyPy = platform.python_implementation() == "PyPy"
+
+INSTALL_REQUIRES = []
+DEPENDENCY_LINKS = []
+
+# General Requirements
+INSTALL_REQUIRES += [
+    "chardet", # Used in mathics_scanner.feed
+    "ujson", # Used in mathics_scanner.characters
+]
+
+
+def subdirs(root, file="*.*", depth=10):
+    for k in range(depth):
+        yield root + "*/" * k + file
+
+
+setup(
+    name="Mathics_Scanner",
+    version=__version__,
+    packages=[
+        "mathics_scanner",
+        "mathics_scanner.generate",
+    ],
+    install_requires=INSTALL_REQUIRES,
+    dependency_links=DEPENDENCY_LINKS,
+    package_data={
+        "mathics_scanner": [
+            "data/*.csv",
+            "data/*.json",
+            "data/ExampleData/*",
+        ],
+    },
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    # don't pack Mathics in egg because of media files, etc.
+    zip_safe=False,
+    # metadata for upload to PyPI
+    maintainer="Mathics Group",
+    description="A general-purpose computer algebra system.",
+    license="GPL",
+    url="https://mathics.org/",
+    keywords=["Mathematica", "Wolfram", "Interpreter", "Shell", "Math", "CAS"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Software Development :: Interpreters",
+    ],
+    # TODO: could also include long_description, download_url,
+)
