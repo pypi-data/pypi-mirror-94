611c6407a9f2bfb19308c3a3fa850323fe90385b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:49 +0000

    Add package check50-junit/0.5/check50_junit-0.5-py3-none-any.whl

---
+++ packages/check50-junit/check50_junit-0.5-py3-none-any.whl/check50_junit/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2020  Patrick Totzke <patricktotzke@gmail.com>
+# This file is released under the GNU GPL, version 3 or a later revision.
+
+from . junit5 import compile_test
+from . junit5 import run_and_interpret_test
+
+
+JAVAC = "javac"
+JAVA = "java"
