2ab00b7ab540afdba88d218c36ca9c5d5d222ab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:06 +0000

    Add package OrzMC/1.0.45/OrzMC-1.0.45-py2.py3-none-any.whl

---
+++ packages/OrzMC/OrzMC-1.0.45-py2.py3-none-any.whl/OrzMC/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf8 -*-
+from .app.OrzMC import start
+from .core.Oracle import install_jdk, uninstall_jdk
---
+++ packages/OrzMC/OrzMC-1.0.45-py2.py3-none-any.whl/OrzMC/app/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf8 -*-
+
+__all__ = [
+    'OrzMC',
+    'Game',
+    'Client',
+    'Server',
+    'Config',
+    'Console'
+    'Constants',
+    'Downloader',
+]
