4ded013dad2096d55d092ffed5b6dd97304dfd2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:51 +0000

    Add package dagster/0.10.4/dagster-0.10.4.tar.gz

---
+++ packages/dagster/dagster-0.10.4.tar.gz/dagster-0.10.4/dagster/daemon/cli/__init__.py
@@ -0,0 +1,118 @@
+import os
+import sys
+import time
+import warnings
+
+import click
+import pendulum
+from dagster import __version__
+from dagster.core.instance import DagsterInstance
+from dagster.daemon.controller import (
+    DagsterDaemonController,
+    all_daemons_healthy,
+    debug_daemon_heartbeats,
+    get_daemon_status,
+)
+from dagster.daemon.types import DaemonType
+
+
+@click.command(
+    name="run", help="Run any daemons configured on the DagsterInstance.",
+)
+def run_command():
+    with DagsterInstance.get() as instance:
+        if instance.is_ephemeral:
+            raise Exception(
+                "dagster-daemon can't run using an in-memory instance. Make sure "
+                "the DAGSTER_HOME environment variable has been set correctly and that "
+                "you have created a dagster.yaml file there."
+            )
+
+        controller = DagsterDaemonController(instance)
+
+        while True:
+            curr_time = pendulum.now("UTC")
+            controller.run_iteration(curr_time)
+            time.sleep(0.5)
+
+
+@click.command(
+    name="health-check", help="DEPRECATED, use liveness-check instead",
+)
+def health_check_command():
+    warnings.warn("health-check is deprecated. Use liveness-check instead.")
+    with DagsterInstance.get() as instance:
+        if all_daemons_healthy(instance):
+            click.echo("Daemon healthy")
+        else:
+            click.echo("Daemon not healthy")
+            sys.exit(1)
+
+
+@click.command(
+    name="liveness-check", help="Check for recent heartbeats from the daemon.",
+)
+def liveness_check_command():
+    with DagsterInstance.get() as instance:
+        if all_daemons_healthy(instance):
+            click.echo("Daemon healthy")
+        else:
+            click.echo("Daemon(s) not running")
+            sys.exit(1)
+
+
+@click.command(
+    name="wipe", help="Wipe all heartbeats from storage.",
+)
+def wipe_command():
+    with DagsterInstance.get() as instance:
+        instance.wipe_daemon_heartbeats()
+        click.echo("Daemon heartbeats wiped")
+
+
+@click.command(
+    name="heartbeat", help="Read and write a heartbeat",
+)
+def debug_heartbeat_command():
+    with DagsterInstance.get() as instance:
+        debug_daemon_heartbeats(instance)
+
+
+@click.command(
+    name="heartbeat-dump", help="Log all heartbeat statuses",
+)
+def debug_heartbeat_dump_command():
+    with DagsterInstance.get() as instance:
+        for daemon_type in DaemonType:
+            click.echo(get_daemon_status(instance, daemon_type))
+
+
+@click.group(
+    commands={"heartbeat": debug_heartbeat_command, "heartbeat-dump": debug_heartbeat_dump_command}
+)
+def debug_group():
+    "Daemon debugging utils"
+
+
+def create_dagster_daemon_cli():
+    commands = {
+        "run": run_command,
+        "health-check": health_check_command,
+        "liveness-check": liveness_check_command,
+        "wipe": wipe_command,
+        "debug": debug_group,
+    }
+
+    @click.group(commands=commands)
+    @click.version_option(version=__version__)
+    def group():
+        "CLI tools for working with the dagster daemon process."
+
+    return group
+
+
+cli = create_dagster_daemon_cli()
+
+
+def main():
+    cli(obj={})  # pylint:disable=E1123
---
+++ packages/dagster/dagster-0.10.4.tar.gz/dagster-0.10.4/dagster_tests/daemon_tests/test_dagster_daemon_health.py
@@ -0,0 +1,119 @@
+import pendulum
+from dagster import DagsterInvariantViolationError
+from dagster.core.test_utils import instance_for_test
+from dagster.daemon.controller import (
+    DAEMON_HEARTBEAT_INTERVAL_SECONDS,
+    DagsterDaemonController,
+    all_daemons_healthy,
+    all_daemons_live,
+    get_daemon_status,
+)
+from dagster.utils.error import SerializableErrorInfo
+
+
+def test_healthy():
+
+    with instance_for_test(
+        overrides={
+            "run_coordinator": {
+                "module": "dagster.core.run_coordinator.queued_run_coordinator",
+                "class": "QueuedRunCoordinator",
+            },
+        }
+    ) as instance:
+        init_time = pendulum.now("UTC")
+        beyond_tolerated_time = init_time.float_timestamp + 100
+
+        controller = DagsterDaemonController(instance)
+        assert not all_daemons_healthy(instance, curr_time_seconds=init_time.float_timestamp)
+        assert not all_daemons_live(instance, curr_time_seconds=init_time.float_timestamp)
+
+        controller.run_iteration(init_time)
+        assert all_daemons_healthy(instance, curr_time_seconds=init_time.float_timestamp)
+        assert all_daemons_live(instance, curr_time_seconds=init_time.float_timestamp)
+
+        assert not all_daemons_healthy(instance, curr_time_seconds=beyond_tolerated_time)
+        assert not all_daemons_live(instance, curr_time_seconds=beyond_tolerated_time)
+
+
+def test_healthy_with_different_daemons():
+    with instance_for_test() as instance:
+        init_time = pendulum.now("UTC")
+        controller = DagsterDaemonController(instance)
+        controller.run_iteration(init_time)
+
+    with instance_for_test(
+        overrides={
+            "run_coordinator": {
+                "module": "dagster.core.run_coordinator.queued_run_coordinator",
+                "class": "QueuedRunCoordinator",
+            },
+        }
+    ) as instance:
+        assert not all_daemons_healthy(instance, curr_time_seconds=init_time.float_timestamp)
+        assert not all_daemons_live(instance, curr_time_seconds=init_time.float_timestamp)
+
+
+def test_error_daemon(monkeypatch):
+    with instance_for_test(overrides={}) as instance:
+        from dagster.daemon.daemon import SensorDaemon
+
+        def run_iteration_error(_):
+            raise DagsterInvariantViolationError("foobar")
+            yield  # pylint: disable=unreachable
+
+        monkeypatch.setattr(SensorDaemon, "run_iteration", run_iteration_error)
+        controller = DagsterDaemonController(instance)
+        init_time = pendulum.now("UTC")
+        controller.run_iteration(init_time)
+
+        status = get_daemon_status(instance, SensorDaemon.daemon_type(), init_time.float_timestamp)
+        assert status.healthy == False
+        assert len(status.last_heartbeat.errors) == 1
+        assert (
+            status.last_heartbeat.errors[0].message.strip()
+            == "dagster.core.errors.DagsterInvariantViolationError: foobar"
+        )
+        assert not all_daemons_healthy(instance, curr_time_seconds=init_time.float_timestamp)
+        assert all_daemons_live(instance, curr_time_seconds=init_time.float_timestamp)
+
+
+def test_multiple_error_daemon(monkeypatch):
+    with instance_for_test(overrides={}) as instance:
+        from dagster.daemon.daemon import SensorDaemon
+
+        def run_iteration_error(_):
+            # ?message stack cls_name cause"
+            yield SerializableErrorInfo("foobar", None, None, None)
+            yield SerializableErrorInfo("bizbuz", None, None, None)
+
+        monkeypatch.setattr(SensorDaemon, "run_iteration", run_iteration_error)
+        controller = DagsterDaemonController(instance)
+        init_time = pendulum.now("UTC")
+        controller.run_iteration(init_time)
+
+        status = get_daemon_status(instance, SensorDaemon.daemon_type(), init_time.float_timestamp)
+        assert status.healthy == False
+        assert len(status.last_heartbeat.errors) == 2
+        assert status.last_heartbeat.errors[0].message.strip() == "foobar"
+        assert status.last_heartbeat.errors[1].message.strip() == "bizbuz"
+
+
+def test_warn_multiple_daemons(capsys):
+    with instance_for_test() as instance:
+        init_time = pendulum.now("UTC")
+        next_time = init_time.add(seconds=100)
+
+        controller1 = DagsterDaemonController(instance)
+        controller1.run_iteration(init_time)
+        captured = capsys.readouterr()
+        assert "Taking over from another SENSOR daemon process" not in captured.out
+
+        controller2 = DagsterDaemonController(instance)
+        controller2.run_iteration(init_time)
+        captured = capsys.readouterr()
+        assert "Taking over from another SENSOR daemon process" not in captured.out
+
+        controller1.run_iteration(next_time)
+        captured = capsys.readouterr()
+        assert "Taking over from another SENSOR daemon process" in captured.out
