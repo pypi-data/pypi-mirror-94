f3bc747f158ed3d78358ac1156badcd175f1c36d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:45 +0000

    Add package wg-finutils/0.1.1/wg-finutils-0.1.1.tar.gz

---
+++ packages/wg-finutils/wg-finutils-0.1.1.tar.gz/wg-finutils-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['wg_finutils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.1.2,<8.0.0']
+
+setup_kwargs = {
+    'name': 'wg-finutils',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'will george',
+    'author_email': 'wrgeorge1983@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
