8801d9b6570776f38ba754f4748f372da8587496
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:46 +0000

    Add package websiteblocker/0.2/websiteblocker-0.2.tar.gz

---
+++ packages/websiteblocker/websiteblocker-0.2.tar.gz/websiteblocker-0.2/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'websiteblocker',         
+  packages = ['websiteblocker'],   
+  version = '0.2',    
+  license='GPLv3',        
+  description = 'Blocks distracting websites in Windows PC.', 
+  long_description='A simple Website blocker in python for windows using hosts file.',
+  author = 'Anish M',                  
+  author_email = 'aneesh25861@gmail.com',     
+  url = 'https://github.com/Anish-M-code/Simple_Website_blocker',     
+  keywords = ['Website Blocker', 'hosts file'],  
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows',
+  ],
+)
+
