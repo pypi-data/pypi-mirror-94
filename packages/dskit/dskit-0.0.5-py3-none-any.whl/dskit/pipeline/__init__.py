f000c22f100c4b099becc6172e596336701c1c9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:31 +0000

    Add package dskit/0.0.5/dskit-0.0.5-py3-none-any.whl

---
+++ packages/dskit/dskit-0.0.5-py3-none-any.whl/dskit/pipeline/__init__.py
@@ -0,0 +1,12 @@
+from .column import ColumnFilter
+from .column import Selector
+from .pipeline import PipelineFrame
+
+from .column import column
+from .column import select
+from .pipeline import append
+from .pipeline import inner
+from .pipeline import prefix
+from .pipeline import rename
+from .pipeline import sufix
+from .pipeline import zipframe
