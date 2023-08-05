1f95c8f4f8c7af75ab96a703e6781bdda0b08383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:57 +0000

    Add package anserializer/1.2.0/anserializer-1.2.0-py3-none-any.whl

---
+++ packages/anserializer/anserializer-1.2.0-py3-none-any.whl/anserializer/__init__.py
@@ -0,0 +1,7 @@
+name = "anserializer"
+
+from .serializer import Serializer
+from .serializers.datetimeserializer import DatetimeSerializer
+from .serializers.objectserializer import ObjectSerializer
+from .serializers.enumserializer import EnumSerializer
+from .serializers.baseserializer import BaseSerializer
