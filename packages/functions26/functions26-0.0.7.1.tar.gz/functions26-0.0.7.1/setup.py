f0b9b93e1a02db4e800eebb5eb92e9618c2a69cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:22 +0000

    Add package functions26/0.0.7.1/functions26-0.0.7.1.tar.gz

---
+++ packages/functions26/functions26-0.0.7.1.tar.gz/functions26-0.0.7.1/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="functions26",
+    version="0.0.7.1",
+    author="Vasilis Niaouris",
+    author_email="vasilisniaouris@gmail.com",
+    description="Supporting functions for Fu lab 26 room",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vasilisniaouris/functions26",
+    packages=setuptools.find_packages(),
+    install_requires=['collections-extended',
+                      'matplotlib',
+                      'nidaqmx',
+                      'numpy',
+                      'pandas',
+                      'py3-validate_email',
+                      'py3dns',
+                      'pydaqmx',
+                      'pyserial',
+                      'pyvisa',
+                      'scipy',
+                      'seaborn',
+                      'windfreak'],
+    dependency_links=['https://github.com/fujiisoup/sif_reader/tarball/master#egg=package-1.0'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
