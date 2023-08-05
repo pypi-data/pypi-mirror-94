3fe10936de50dc08558cb6b82c9f133dc6674d44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:00 +0000

    Add package fst-pso/1.7.15/fst-pso-1.7.15.tar.gz

---
+++ packages/fst-pso/fst-pso-1.7.15.tar.gz/fst-pso-1.7.15/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'fst-pso',
+  packages = ['fstpso'], 
+  version = '1.7.15',
+  description = 'Fuzzy Self-Tuning PSO global optimization library',
+  author = 'Marco S. Nobile',
+  author_email = 'nobile@disco.unimib.it',
+  url = 'https://github.com/aresio/fst-pso', # use the URL to the github repo
+  keywords = ['fuzzy logic', 'optimization', 'discrete optimization', 'continuous optimization', 'particle swarm optimization',  'pso', 'fst-pso', 'fuzzy self-tuning pso'], # arbitrary keywords
+  license='LICENSE.txt',
+  long_description=open('README.txt', encoding='utf-8').read(),
+  classifiers = ['Programming Language :: Python :: 3.7'],
+  install_requires=[ 'numpy', 'miniful' ],
+)
