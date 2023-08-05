352eaa957d3d67dc1fbc6f00e4b643a3fb5e07cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:50 +0000

    Add package quinnat/1.0.1/quinnat-1.0.1.tar.gz

---
+++ packages/quinnat/quinnat-1.0.1.tar.gz/quinnat-1.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="quinnat",
+    version="1.0.1",
+    author="~midsum-salrux",
+    author_email="",
+    description="Library for building chatbots on urbit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/midsum-salrux/quinnat",
+    packages=setuptools.find_packages(),
+    install_requires=['urlock'],
+    classifiers=[],
+    python_requires='>=3.9',
+)
