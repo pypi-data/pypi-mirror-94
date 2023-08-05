f15581373820f511cb874c3c6f1acc81709e46b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:06 +0000

    Add package pytissueoptics/1.0.4/pytissueoptics-1.0.4-py3-none-any.whl

---
+++ packages/pytissueoptics/pytissueoptics-1.0.4-py3-none-any.whl/pytissueoptics/__main__.py
@@ -0,0 +1,53 @@
+from pytissueoptics import *
+
+# We choose a material with scattering properties
+mat    = Material(mu_s=30, mu_a = 0.1, g = 0.8, index = 1.4)
+
+# We want stats: we must determine over what volume we want the energy
+stats  = Stats(min = (-2,-2,-2), max = (2,2,2), size = (50,50,50))
+
+# We pick a light source
+source = PencilSource(direction=UnitVector(0,0,1), maxCount=10000)
+
+# We pick a geometry
+tissue = Layer(thickness=1, material=mat, stats=stats)
+
+# We propagate the photons from the source inside the geometry
+World.place(source, position=Vector(0,0,-1))
+World.place(tissue, position=Vector(0,0,0))
+
+World.compute(graphs=True)
+
+# Report the results for all geometries
+World.report()
+
+print("""
+
+# PyTissueOptics example code
+# ===========================
+# The example code that was just ran is the following:
+# It is available at: {0}
+
+from pytissueoptics import *
+
+# We choose a material with scattering properties
+mat    = Material(mu_s=30, mu_a = 0.1, g = 0.8, index = 1.4)
+
+# We want stats: we must determine over what volume we want the energy
+stats  = Stats(min = (-2,-2,-2), max = (2,2,2), size = (50,50,50))
+
+# We pick a light source
+source = PencilSource(direction=UnitVector(0,0,1), maxCount=10000)
+
+# We pick a geometry
+tissue = Layer(thickness=1, material=mat, stats=stats)
+
+# We propagate the photons from the source inside the geometry
+World.place(source, position=Vector(0,0,-1))
+World.place(tissue, position=Vector(0,0,0))
+
+World.compute(graphs=True)
+
+# Report the results for all geometries
+World.report()
+""".format(__file__))
