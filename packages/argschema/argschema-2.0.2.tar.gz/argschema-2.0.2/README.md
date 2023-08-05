3c68fb4e382e7d7425e0073d5d69fa65da95e72a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:58 +0000

    Add package argschema/2.0.2/argschema-2.0.2.tar.gz

---
+++ packages/argschema/argschema-2.0.2.tar.gz/argschema-2.0.2/README.md
@@ -0,0 +1,89 @@
+[![CircleCI](https://circleci.com/gh/AllenInstitute/argschema/tree/master.svg?style=svg)](https://circleci.com/gh/AllenInstitute/argschema/tree/master)
+[![codecov.io](https://codecov.io/github/AllenInstitute/argschema/coverage.svg?branch=master)](https://codecov.io/github/AllenInstitute/argschema?branch=master)
+[![Documentation Status](https://readthedocs.org/projects/argschema/badge/)](http://argschema.readthedocs.io/en/master/)
+[![Gitter chat](https://badges.gitter.im/gitterHQ/gitter.png)](https://gitter.im/argschema/Lobby)
+# argschema
+
+This python module simplifies the development of modules that would like to define and check a particular set of input parameters, but be able to flexibly define those inputs in different ways in different contexts. 
+
+It will allow you to 
+
+Pass a command line argument to a location of a input_json file which contains the input parameters
+
+OR pass a json_dictionary directly into the module with the parameters defined
+
+AND/OR pass parameters via the command line, in a way that will override the input_json or the json_dictionary given.
+
+## Upgrading to version 2.0
+The major change in argschema 2.0 is becoming
+compatible with marshmallow 3, which changes
+many of the ways your schemas and schema modifications work.  Some noteable differences  are that schemas are strict now by default, so tossing keys in your outputs or inputs that were ignored and stripped before now throw errors unless  
+def
+Please read this document for more guidance
+https://marshmallow.readthedocs.io/en/stable/upgrading.html
+
+## Level of Support
+We are planning on occasional updating this tool with no fixed schedule. Community involvement is encouraged through both issues and pull requests.  Please make pull requests against the dev branch, as we will test changes there before merging into master.
+
+## Documentation
+Continually built docs can be found here http://argschema.readthedocs.io/en/master/
+
+## What does it do
+argschema defines two basic classes, ArgSchemaParser and ArgSchema. ArgSchemaParser takes ArgSchema as an input which is simply an extension of the marshmallow Schema class (http://marshmallow.readthedocs.io/en/latest/).
+
+ArgSchemaParser then takes that schema, and builds a argparse parser from the schema using a standard pattern to convert the schema. Nested elements of the schema are specified with a "." 
+
+so the json 
+
+    {
+        "nested":{
+            "a":5
+        },
+        "b":"a"
+    }
+
+would map to the command line arguments
+
+    $ python mymodule.py --nested.a 5 --b a
+
+ArgSchemaParser then parses the command line arguments into a dictionary using argparse, and then reformatting the dictionary to have the proper nested structure to match the schema it was provided.
+
+Next, ArgSchemaParser reads either the input_data if it was passed, or takes the path of the input_json and reads that in as a dictionary.  
+
+Given that input dictionary and the command line dictionary, ArgSchemaParser then merges the two dictionaries, where the command line dictionary takes precendence. 
+
+Next, that dictionary is parsed and validated using marshmallow to convert the raw dictionary into the types defined by the marshmallow fields.
+
+The resulting dictionary is then stored in self.args available for use.
+
+After that the module does some standard things, such as parsing the parameter args['log_level'] to configure a logging module at self.logger.
+
+## How should I use it
+subclass schemas.ArgSchema using the pattern found in [template_module.py](examples/template_module.py) to define your module parameters, defining default values if you want and help statements that will be displayed by argparse as help statements, and maybe provide some example parameters for someone who is trying to figure out how to user your module (which is also a good way to rapidly test your module as you are developing it)
+
+Look at the set of fields to understand how to build custom fields, or use the default Marshmallow fields to construct your json Schema.  Note the use of InputDir and InputFile, two example custom marshmallow validators that are included in argschema.fields. They will insure that these directory exist, or files exist before trying to run your module and provide errors to the user. Also of note, fields.NumpyArray, which will convert Lists of Lists directly into numpy arrays.  More useful Fields can be found in argschema.fields.
+
+You can use the power of marshmallow to produce custom validators for any data type, 
+and serialize/deserialize methods that will make loading complex parameters as python objects easy and repeatable.
+
+For instance, this could allow you to have a parameter that is simply a string in json, but the deserialization routine loads uses that string to look something up in a database and return a numpy array, or have a string which is actually a filepath to an image file, and deserializes that as a numpy array of the image.  This is the basic power of marshmallow.
+
+## Why did we make this
+You should consider using this module if this pattern seems familar to you.
+
+You start building some code in an ipython notebook to play around with a new idea where you define some variables as you go that make your code work, you fiddle with things for awhile and eventually you get it to a point that works.  You then immediately want to use it over and over again, and are scrolling through your ipython notebook, changing variables, making copies of your notebook for different runs.  Several times you make mistakes typing in the exact filepath of some input file, and your notebook breaks on cell 53, but no big deal, you just fix the filename variable and rerun it. 
+
+It's a mess, and you know you should migrate your code over to a module that you can call from other programs or notebooks.  You start collecting your input variables to the top of the notebook and make yourself a wrapper function that you can call.  However, now your mistake in filename typing is a disaster because the file doesn't exist, and your code doesn't check for the existence of the file until quite late. You start implementing some input validation checks to avoid this problem.
+
+Now you start wanting to integrate this code with other things, including elements that aren't in python.  You decide that you need to have a command line module that executes the code, because then you can use other tools to stitch together your processing, like maybe some shell scripts or docker run commands.  You implement an argparse set of inputs and default values that make your python program a self-contained program, with some help documentation.  Along the way, you have to refactor the parsed argparse variables into your function and strip out your old hacky validation code to avoid maintaining two versions of validation in the future.
+
+This module starts becoming useful enough that you want to integrate it into more complex modules.  You end up copying and pasting various argparse lines over to other modules, and then 5 other modules.  Later you decide to change your original module a little bit, and you have a nightmare of code replace to fix up the other modules to mirror this phenomenon.. you kick yourself for not having thought this through more clearly.
+
+Your code is now really useful, but its so useful you start running it on larger and larger jobs, and you want to deploy it on a cluster in your groups pipeline workflow.  Your pipeline framework needs to dynamically define and control the parameters, and so it would like to simply write all the inputs to a file and pass your program that file, rather than having to parse out the inputs into your argparse format.  You have to refactor your inputs again to deal with this new pattern, by setting up a validation framework to work on json.  Now what do you do with your argparse validators? Throw them away so you don't have to maintain them? If you do that you've lost the ability to run this code on the command line and run test cases easily when things inevitably break.  To avoid this, you decide to maintain two wrapper programs that call the same underlying function and they basically do the same thing, just one does it with argparse and the other one for json inputs.  You are now stuck maintaining both versions of validation and it feels pretty silly. 
+
+If you had only designed things from the beginning to allow for each of these use cases over the lifetime of your module.
+
+This is what argschema is designed to do.
+
+Copyright 2017 Allen Institute
+
---
+++ packages/argschema/argschema-2.0.2.tar.gz/argschema-2.0.2/test/test_first_test.py
@@ -0,0 +1,301 @@
+import pytest
+import json
+import logging
+import marshmallow as mm
+from argschema import ArgSchemaParser, ArgSchema
+import argschema
+
+
+def test_bad_path():
+    with pytest.raises(mm.ValidationError):
+        example = {
+            "input_json": "a bad path",
+            "output_json": "another example",
+            "log_level": "DEBUG"}
+        ArgSchemaParser(input_data=example, args=[])
+
+
+def test_simple_example(tmpdir):
+    file_in = tmpdir.join('test_input_json.json')
+    file_in.write('nonesense')
+
+    file_out = tmpdir.join('test_output.json')
+
+    example = {
+        "input_json": str(file_in),
+        "output_json": str(file_out),
+        "log_level": "CRITICAL"}
+    jm = ArgSchemaParser(input_data=example, args=[])
+
+    assert jm.args['log_level'] == 'CRITICAL'
+
+
+def test_log_catch():
+    with pytest.raises(mm.ValidationError):
+        example = {"log_level": "NOTACHOICE"}
+        jm = ArgSchemaParser(input_data=example, args=[])
+        print(jm.args)
+
+
+class MyExtension(argschema.schemas.DefaultSchema):
+    a = mm.fields.Str(description='a string', required=True)
+    b = mm.fields.Int(description='an integer')
+    c = mm.fields.Int(description='an integer', default=10)
+    d = mm.fields.List(mm.fields.Int,
+                       description='a list of integers')
+
+
+class SimpleExtension(ArgSchema):
+    test = mm.fields.Nested(MyExtension, required=True)
+
+
+def test_simple_extension_required():
+    with pytest.raises(mm.ValidationError):
+        example1 = {}
+        ArgSchemaParser(
+            input_data=example1, schema_type=SimpleExtension, args=[])
+
+
+SimpleExtension_example_invalid = {
+    'test':
+    {
+        'a': 5,
+        'b': 1,
+        'd': ['a', 2, 3]
+    }
+}
+
+SimpleExtension_example_valid = {
+    'test':
+        {
+            'a': "hello",
+            'b': 1,
+            'd': [1, 5, 4]
+        }
+}
+
+
+@pytest.fixture(scope='module')
+def simple_extension_file(tmpdir_factory):
+    file_ = tmpdir_factory.mktemp('test').join('testinput.json')
+    file_.write(json.dumps(SimpleExtension_example_valid))
+    return file_
+
+
+def test_simple_extension_fail():
+    with pytest.raises(mm.ValidationError):
+        ArgSchemaParser(
+            input_data=SimpleExtension_example_invalid,
+            schema_type=SimpleExtension, args=[])
+
+
+def test_simple_extension_pass():
+    mod = ArgSchemaParser(
+        input_data=SimpleExtension_example_valid,
+        schema_type=SimpleExtension, args=[])
+    assert mod.args['test']['a'] == 'hello'
+    assert mod.args['test']['b'] == 1
+    assert mod.args['test']['c'] == 10
+    assert len(mod.args['test']['d']) == 3
+
+
+def test_simple_extension_write_pass(simple_extension_file):
+    args = ['--input_json', str(simple_extension_file)]
+    mod = ArgSchemaParser(
+        input_data=SimpleExtension_example_valid, schema_type=SimpleExtension,
+        args=args)
+    assert mod.args['test']['a'] == 'hello'
+    assert mod.args['test']['b'] == 1
+    assert mod.args['test']['c'] == 10
+    assert len(mod.args['test']['d']) == 3
+    assert mod.logger.getEffectiveLevel() == logging.ERROR
+
+
+def test_simple_extension_write_debug_level(simple_extension_file):
+    args = ['--input_json', str(simple_extension_file), '--log_level', 'DEBUG']
+    mod = ArgSchemaParser(schema_type=SimpleExtension, args=args)
+    assert mod.logger.getEffectiveLevel() == logging.DEBUG
+
+
+def test_simple_extension_write_overwrite(simple_extension_file):
+    args = ['--input_json', str(simple_extension_file), '--test.b', '5']
+    mod = ArgSchemaParser(schema_type=SimpleExtension, args=args)
+    assert mod.args['test']['b'] == 5
+
+
+def test_simple_extension_write_overwrite_list(simple_extension_file):
+    args = ['--input_json', str(simple_extension_file),
+            '--test.d', '6', '7', '8', '9']
+    mod = ArgSchemaParser(schema_type=SimpleExtension, args=args)
+    assert len(mod.args['test']['d']) == 4
+
+
+def test_bad_input_json_argparse():
+    args = ['--input_json', 'not_a_file.json']
+    with pytest.raises(mm.ValidationError):
+        ArgSchemaParser(schema_type=SimpleExtension, args=args)
+
+# TESTS DEMONSTRATING BAD BEHAVIOR OF DEFAULT LOADING
+
+
+class MyExtensionOld(mm.Schema):
+    a = mm.fields.Str(description='a string')
+    b = mm.fields.Int(description='an integer')
+    c = mm.fields.Int(description='an integer', default=10)
+    d = mm.fields.List(mm.fields.Int,
+                       description='a list of integers')
+
+
+class SimpleExtensionOld(ArgSchema):
+    test = mm.fields.Nested(MyExtensionOld, default=None, required=True)
+
+
+def test_simple_extension_old_pass():
+    mod = ArgSchemaParser(
+        input_data=SimpleExtension_example_valid,
+        schema_type=SimpleExtensionOld, args=[])
+    assert mod.args['test']['a'] == 'hello'
+    assert mod.args['test']['b'] == 1
+    assert mod.args['test']['c'] == 10
+    assert len(mod.args['test']['d']) == 3
+
+
+class RecursiveSchema(argschema.schemas.DefaultSchema):
+    children = mm.fields.Nested("self", many=True,
+                                description='children of this node')
+    name = mm.fields.Str(default="anonymous",
+                         description='name of this node')
+
+
+class ExampleRecursiveSchema(ArgSchema):
+    tree = mm.fields.Nested(RecursiveSchema, required=True)
+
+
+recursive_data = {
+    'tree': {
+        'name': 'root',
+                'children': [
+                    {
+                        "name": 'child1'
+                    },
+                    {
+                        "name": "branch1",
+                        "children": [
+                            {
+                                "name": "subchild1"
+                            },
+                            {
+                            },
+                            {
+                            }
+                        ]
+                    }
+                ]
+    }
+}
+
+
+def test_recursive_schema():
+    mod = ArgSchemaParser(
+        input_data=recursive_data,
+        schema_type=ExampleRecursiveSchema, args=[])
+    assert mod.args['tree']['name'] == 'root'
+    assert len(mod.args['tree']['children']) == 2
+    assert mod.args['tree']['children'][0]['name'] == 'child1'
+    assert mod.args['tree']['children'][1]['name'] == 'branch1'
+    assert len(mod.args['tree']['children'][1]['children']) == 3
+    assert mod.args['tree']['children'][1]['children'][2]['name'] == 'anonymous'
+
+
+class BadRecursiveSchema(mm.Schema):
+    children = mm.fields.Nested("self", many=True,
+                                description='children of this node')
+    name = mm.fields.Str(default="anonymous",
+                         description='name of this node')
+
+
+class BadExampleRecursiveSchema(ArgSchema):
+    tree = mm.fields.Nested(BadRecursiveSchema, required=True)
+
+
+def bad_test_recursive_schema():
+    with pytest.raises(mm.ValidationError):
+        ArgSchemaParser(input_data=recursive_data,
+                        schema_type=BadExampleRecursiveSchema, args=[])
+
+
+class ModelFit(argschema.schemas.DefaultSchema):
+    fit_type = argschema.fields.Str(description="")
+    hof_fit = argschema.fields.InputFile(description="")
+    hof = argschema.fields.InputFile(description="")
+
+
+class PopulationSelectionPaths(argschema.schemas.DefaultSchema):
+    fits = argschema.fields.Nested(ModelFit, description="", many=True)
+
+
+class PopulationSelectionParameters(argschema.ArgSchema):
+    paths = argschema.fields.Nested(PopulationSelectionPaths)
+
+
+david_data = {
+    'paths': {
+        'fits': [{
+            'fit_type': 'test',
+            'hof_fit': 'requirements.txt',
+            'hof': 'requirements.txt'
+        },
+            {
+            'fit_type': 'test2',
+            'hof_fit': 'requirements.txt',
+            'hof': 'requirements.txt'
+        }
+        ]
+    }
+}
+
+
+def test_david_example(tmpdir_factory):
+    file_ = tmpdir_factory.mktemp('test').join('testinput.json')
+    file_.write(json.dumps(david_data))
+    args = ['--input_json', str(file_)]
+    mod = argschema.ArgSchemaParser(
+        schema_type=PopulationSelectionParameters, args=args)
+    print(mod.args)
+    assert(len(mod.args['paths']['fits']) == 2)
+
+
+class MyShorterExtension(ArgSchema):
+    a = mm.fields.Str(description='a string')
+    b = mm.fields.Int(description='an integer')
+    c = mm.fields.Int(description='an integer', default=10)
+    d = mm.fields.List(mm.fields.Int, description='a list of integers')
+
+
+def test_simple_description():
+    d = {
+        'a': "hello",
+        'b': 1,
+        'd': [1, 5, 4]
+    }
+    argschema.ArgSchemaParser(
+        input_data=d, schema_type=MyShorterExtension, args=[])
+
+class MySchemaPostLoad(ArgSchema):
+    xid = argschema.fields.Int(required=True)
+    
+    @mm.post_load
+    def my_post(self, data):
+        return data
+
+class MyPostLoadClass(ArgSchemaParser):
+    default_schema = MySchemaPostLoad
+    def run(self):
+        print(self.args)
+
+def test_post_load_schema():
+    example1 = {
+        'xid': 1,
+    }
+    mb = MyPostLoadClass(input_data=example1, args=[])
+    mb.run()
