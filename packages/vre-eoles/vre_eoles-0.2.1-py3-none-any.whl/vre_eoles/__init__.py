d36535cbf5d29e482fec7711ddd3f7fedb79e769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:43 +0000

    Add package vre-eoles/0.2.1/vre_eoles-0.2.1-py3-none-any.whl

---
+++ packages/vre-eoles/vre_eoles-0.2.1-py3-none-any.whl/vre_eoles/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.2.1"
+__author__ = 'F. Bertin'
+__license__ = "MIT"
+
+from .vre_eoles_ninja import *
+from .vre_eoles_math import *
+from .vre_eoles_read import *
+from .vre_eoles_rte import *
+from .vre_eoles_wind import *
+
+
