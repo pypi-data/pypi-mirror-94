c6a19aac08b6e5fcdd3c83a06b736a2c89aae63a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:11 +0000

    Add package shadowd/3.0.1/shadowd-3.0.1-py3-none-any.whl

---
+++ packages/shadowd/shadowd-3.0.1-py3-none-any.whl/shadowd-3.0.1.dist-info/DESCRIPTION.rst
@@ -0,0 +1,90 @@
+.. image:: http://shadowd.zecure.org/img/logo_small.png
+.. image:: https://travis-ci.org/zecure/shadowd_python.svg
+    :target: https://travis-ci.org/zecure/shadowd_python
+.. image:: https://sonarcloud.io/api/project_badges/measure?project=zecure_shadowd_python&metric=alert_status
+    :target: https://sonarcloud.io/dashboard?id=zecure_shadowd_python
+
+**Shadow Daemon** is a collection of tools to **detect**, **record** and **prevent** **attacks** on *web applications*.
+Technically speaking, Shadow Daemon is a **web application firewall** that intercepts requests and filters out malicious parameters.
+It is a modular system that separates web application, analysis and interface to increase security, flexibility and expandability.
+
+This component can be used to connect Python applications with the `background server <https://github.com/zecure/shadowd>`_.
+
+Documentation
+=============
+For the full documentation please refer to `shadowd.zecure.org <https://shadowd.zecure.org/>`_.
+
+Installation
+============
+You can install the package with easy_install or pip:
+
+::
+
+   easy_install shadowd
+   pip install shadowd
+
+It is also possible to clone this repository and install the package manually:
+
+::
+
+    python setup.py install
+
+You also have to create a configuration file. You can copy *misc/examples/connectors.ini* to */etc/shadowd/connectors.ini*.
+The example configuration is annotated and should be self-explanatory.
+
+CGI
+---
+To protect CGI applications you simply have to load the module:
+
+::
+
+    import shadowd.cgi_connector
+
+Django
+------
+Django applications require a small modification. It is necessary to create a hook to intercept requests.
+To do this create the file *middleware/shadowdconnector.py* in the application directory:
+
+::
+
+    from shadowd.django_connector import InputDjango, OutputDjango, Connector
+
+    class ShadowdConnectorMiddleware(object):
+        def process_request(self, request):
+            input = InputDjango(request)
+            output = OutputDjango()
+
+            status = Connector().start(input, output)
+            if not status == True:
+                return status
+
+There also has to be an empty *__init__.py* file in the middleware directory.
+Next you have to register the middleware in the *settings.py* file of your application:
+
+::
+
+    MIDDLEWARE_CLASSES = (
+        'middleware.shadowdconnector.ShadowdConnectorMiddleware',
+        # ...
+    )
+
+The connector should be at the beginning of the *MIDDLEWARE_CLASSES* list.
+
+Flask
+------
+Flask applications require a small modification as well. It is necessary to create a hook to intercept requests:
+
+::
+
+    from shadowd.flask_connector import InputFlask, OutputFlask, Connector
+
+    @app.before_request
+    def before_req():
+        input = InputFlask(request)
+        output = OutputFlask()
+
+        Connector().start(input, output)
+
+
+
+
---
+++ packages/shadowd/shadowd-3.0.1-py3-none-any.whl/shadowd/django_connector.py
@@ -0,0 +1,136 @@
+# Shadow Daemon -- Web Application Firewall
+#
+# Copyright (C) 2014-2021 Hendrik Buchwald <hb@zecure.org>
+#
+# This file is part of Shadow Daemon. Shadow Daemon is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from .connector import Input, Output, Connector
+from django.http import HttpResponseServerError
+
+
+class InputDjango(Input):
+    def __init__(self, request):
+        self.request = request
+
+    def get_client_ip(self):
+        return self.request.META.get(self.config.get('client_ip', default='REMOTE_ADDR'))
+
+    def get_caller(self):
+        return self.request.META.get(self.config.get('caller', default='PATH_INFO'))
+
+    def get_resource(self):
+        return self.request.get_full_path()
+
+    def gather_input(self):
+        # Reset input.
+        self.input = {}
+
+        # Save GET parameters in input.
+        get_input = self.request.GET
+        for key in get_input:
+            path = 'GET|' + self.escape_key(key)
+            values = get_input.getlist(key)
+
+            if len(values) > 1:
+                for index, value in enumerate(values):
+                    self.input[path + '|' + str(index)] = value
+            else:
+                self.input[path] = values[0]
+
+        # Save POST parameters in input.
+        post_input = self.request.POST
+        for key in post_input:
+            path = 'POST|' + self.escape_key(key)
+            values = post_input.getlist(key)
+
+            if len(values) > 1:
+                for index, value in enumerate(values):
+                    self.input[path + '|' + str(index)] = value
+            else:
+                self.input[path] = values[0]
+
+        # Save cookies in input.
+        for key in self.request.COOKIES:
+            self.input['COOKIE|' + self.escape_key(key)] = self.request.COOKIES[key]
+
+        # Save headers in input.
+        for key in self.request.META:
+            if key[:5] == 'HTTP_':
+                self.input['SERVER|' + self.escape_key(key)] = self.request.META[key]
+
+        # Save the file names of uploads.
+        files_input = self.request.FILES
+        for key in files_input:
+            path = 'FILES|' + self.escape_key(key)
+            values = files_input.getlist(key)
+
+            if len(values) > 1:
+                for index, value in enumerate(values):
+                    self.input[path + '|' + str(index)] = value.name
+            else:
+                self.input[path] = values[0].name
+
+    def defuse_input(self, threats):
+        # Get the input and create copy to make it mutable.
+        get_input = self.request.GET.copy()
+        post_input = self.request.POST.copy()
+
+        # Remove threats.
+        for path in threats:
+            path_split = self.split_path(path)
+
+            if len(path_split) < 2:
+                continue
+
+            key = self.unescape_key(path_split[1])
+
+            if path_split[0] == 'SERVER':
+                self.request.META[key] = ''
+            elif path_split[0] == 'COOKIE':
+                self.request.COOKIES[key] = ''
+            elif path_split[0] == 'GET':
+                if len(path_split) == 3:
+                    get_list = get_input.getlist(key)
+                    get_list[int(path_split[2])] = ''
+
+                    get_input.setlist(key, get_list)
+                else:
+                    get_input[key] = ''
+            elif path_split[0] == 'POST':
+                if len(path_split) == 3:
+                    post_list = post_input.getlist(key)
+                    post_list[int(path_split[2])] = ''
+
+                    post_input.setlist(key, post_list)
+                else:
+                    post_input[key] = ''
+            elif path_split[0] == 'FILES':
+                # Can't remove file uploads, so request has to be stopped.
+                return False
+
+        # Update the GET data.
+        self.request.GET = get_input
+
+        # Update the POST data.
+        self.request.POST = post_input
+
+        # Don't stop the complete request.
+        return True
+
+    def gather_hashes(self):
+        # Integrity check not supported, because everything is routed through one file.
+        self.hashes = {}
+
+class OutputDjango(Output):
+    def error(self):
+        return HttpResponseServerError('<h1>500 Internal Server Error</h1>')
