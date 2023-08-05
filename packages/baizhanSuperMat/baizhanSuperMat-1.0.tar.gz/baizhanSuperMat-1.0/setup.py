b52b83e365ce7767e2215327801164c95689deaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:10 +0000

    Add package baizhanSuperMat/1.0/baizhanSuperMat-1.0.tar.gz

---
+++ packages/baizhanSuperMat/baizhanSuperMat-1.0.tar.gz/baizhanSuperMat-1.0/setup.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+from distutils.core import setup
+setup(
+    name='baizhanSuperMat',
+    version='1.0',
+    description='这是第一个对外发布的模块，里面只有数学方法，用于测试哦',
+    author='zhangjunkang',
+    author_email='zhangjunkang@163.com',
+    py_modules=['baizhanSuperMath.demo1','baizhanSuperMath.demo2']
+)
