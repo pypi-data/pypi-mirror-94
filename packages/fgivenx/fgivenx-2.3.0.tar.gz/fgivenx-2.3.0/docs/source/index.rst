480e9bbdcb6ff10bca60a210c45f6ae74ee76c25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:09 +0000

    Add package fgivenx/2.3.0/fgivenx-2.3.0.tar.gz

---
+++ packages/fgivenx/fgivenx-2.3.0.tar.gz/fgivenx-2.3.0/docs/source/index.rst
@@ -0,0 +1,10 @@
+.. title:: Introduction
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Contents
+
+   Introduction <intro>
+   API <fgivenx>
+
+.. include:: ../../README.rst
---
+++ packages/fgivenx/fgivenx-2.3.0.tar.gz/fgivenx-2.3.0/setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+try:
+    from setuptools import setup, Command
+except ImportError:
+    from distutils.core import setup, Command
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+def get_version(short=False):
+    with open('README.rst') as f:
+        for line in f:
+            if ':Version:' in line:
+                ver = line.split(':')[2].strip()
+                if short:
+                    subver = ver.split('.')
+                    return '%s.%s' % tuple(subver[:2])
+                else:
+                    return ver
+
+
+setup(name='fgivenx',
+      version=get_version(),
+      description='fgivenx: Functional Posterior Plotter',
+      long_description=readme(),
+      author='Will Handley',
+      author_email='wh260@cam.ac.uk',
+      url='https://github.com/williamjameshandley/fgivenx',
+      packages=['fgivenx', 'fgivenx.test'],
+      install_requires=['matplotlib', 'numpy', 'scipy'],
+      setup_requires=['pytest-runner'],
+      extras_require={
+          'docs': ['sphinx', 'sphinx_rtd_theme', 'numpydoc'],
+          'parallel': ['joblib'],
+          'progress_bar': ['tqdm'],
+          'getdist_chains': ['getdist']
+          },
+      tests_require=['pytest', 'pytest-mpl'],
+      include_package_data=True,
+      license='MIT',
+      classifiers=[
+                   'Development Status :: 5 - Production/Stable',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: Science/Research',
+                   'Natural Language :: English',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6',
+                   'Topic :: Scientific/Engineering',
+                   'Topic :: Scientific/Engineering :: Astronomy',
+                   'Topic :: Scientific/Engineering :: Physics',
+                   'Topic :: Scientific/Engineering :: Visualization',
+                   'Topic :: Scientific/Engineering :: Information Analysis',
+                   'Topic :: Scientific/Engineering :: Mathematics',
+      ],
+      )
