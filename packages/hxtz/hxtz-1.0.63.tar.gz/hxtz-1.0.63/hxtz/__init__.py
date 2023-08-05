e5b924e0aa0a5e4b5cbe9656878c36a6737aa01b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:05 +0000

    Add package hxtz/1.0.63/hxtz-1.0.63.tar.gz

---
+++ packages/hxtz/hxtz-1.0.63.tar.gz/hxtz-1.0.63/hxtz/__init__.py
@@ -0,0 +1,1 @@
+from .hxtz import *
---
+++ packages/hxtz/hxtz-1.0.63.tar.gz/hxtz-1.0.63/setup.py
@@ -0,0 +1,26 @@
+﻿import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hxtz", 
+    version="1.0.63",
+    author="hxtz",
+    author_email="1375151810@qq.com",
+    description="me",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://cn.bing.com/",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "requests","pandas","pywinauto" # 封装的包所依赖的基础三方库
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
+      
