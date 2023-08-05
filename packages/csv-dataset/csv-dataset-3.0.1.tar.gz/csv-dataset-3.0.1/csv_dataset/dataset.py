69dae3ee2024b18e6ce872a90cce57aeb15572e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:23 +0000

    Add package csv-dataset/3.0.1/csv-dataset-3.0.1.tar.gz

---
+++ packages/csv-dataset/csv-dataset-3.0.1.tar.gz/csv-dataset-3.0.1/csv_dataset/dataset.py
@@ -0,0 +1,203 @@
+from typing import (
+    Optional,
+    TypeVar,
+    Generic
+)
+
+import numpy as np
+
+from common_decorators import lazy
+
+from .reader import AbstractReader
+from .common import rolling_window
+
+T = TypeVar('T')
+
+
+class Dataset(Generic[T]):
+    def __init__(
+        self,
+        reader: AbstractReader[T]
+    ):
+        self._reader = reader
+        self._buffer_read = False
+
+        self._batch = 1
+
+        self._window_size = 1
+        self._window_shift = 1
+        self._window_stride = 1
+        self._window_drop_remainder = False
+
+        self.reset()
+
+    def reset(self) -> None:
+        self._reader.reset()
+        self._buffer = None
+
+        return self
+
+    # |-------- size:3 --------|
+    # |- stride:1 -|           |
+    # |            |           |
+    # 1            2           3 --------|---
+    #                                 shift:2
+    # 3            4           5 --------|---
+    #
+    # 5            6           7
+    #
+    # least: (size - 1) * stride + 1
+    # step : shift * stride
+    @lazy
+    def _single_least(self) -> int:
+        """The list size of the underlying datum referred to a single window
+        """
+
+        return (self._window_size - 1) * self._window_stride + 1
+
+    @lazy
+    def _single_step(self) -> int:
+        """stride without considering the batch size
+        """
+        return self._window_shift * self._window_stride
+
+    @lazy
+    def _least(self) -> int:
+        return self._single_least + (self._batch - 1) * self._single_step
+
+    @lazy
+    def _step(self) -> int:
+        return self._batch * self._single_step
+
+    def _check_start(self, method_name: str):
+        if self._buffer_read:
+            raise RuntimeError(f'calling {method_name}() during reading is forbidden')
+
+    def window(
+        self,
+        size: int,
+        shift: Optional[int] = None,
+        stride: int = 1
+    ) -> 'Dataset':
+        """Combines (nests of) input elements into a dataset of (nests of) windows.
+        """
+
+        self._check_start('window')
+
+        self._window_size = size
+        self._window_shift = shift or size
+        self._window_stride = stride
+
+        return self
+
+    def batch(
+        self,
+        batch: int
+    ) -> 'Dataset':
+        """Combines consecutive elements of this dataset into batches.
+        """
+
+        self._check_start('batch')
+
+        self._batch = batch
+
+        return self
+
+    # def restore(
+    #     self,
+    #     data: np.ndarray
+    # ) -> np.ndarray:
+    #     ...
+
+    def _readlines(
+        self,
+        lines: int,
+        dest_buffer: list,
+        slice_size: bool = False
+    ) -> list:
+        read = 0
+
+        while read < lines:
+            line = self._reader.readline()
+
+            if line is None:
+                # Reaches EOF
+                return []
+
+            dest_buffer.append(line)
+            read += 1
+
+        if slice_size:
+            dest_buffer = dest_buffer[lines:]
+
+        return dest_buffer
+
+    def _read_buffer(self):
+        if self._buffer is None:
+            # Initialize buffer
+            self._buffer_read = True
+            self._buffer = self._readlines(self._least, [])
+        else:
+            # Extend buffer
+            self._buffer = self._readlines(self._step, self._buffer, True)
+
+    def get(self) -> Optional[np.ndarray]:
+        """Gets the data of the next batch
+        """
+
+        self._read_buffer()
+
+        if not self._buffer:
+            # There is no data,
+            # which indicates that the data has been exhausted
+            return
+
+        array = np.array(self._buffer)
+
+        windowed = array if self._window_size == 1 else rolling_window(
+            array,
+            self._window_size,
+            shift=self._window_shift,
+            stride=self._window_stride
+        )
+
+        batched = windowed if self._batch == 1 else rolling_window(
+            windowed,
+            self._batch
+        )
+
+        return batched[0]
+
+    def read(
+        self,
+        amount: int,
+        reset_buffer: bool = False
+    ) -> list:
+        """Reads multiple batches of data
+
+        Args:
+            amount (int): number of batches to be read
+            reset_buffer (:obj:`bool`, optional): if `True`, the dataset will reset the data of the previous window in the buffer
+        """
+
+        if reset_buffer:
+            self._buffer = None
+
+        array = []
+
+        while amount > 0:
+            amount -= 1
+            array.append(self.get())
+
+        return array
+
+    def __iter__(self) -> 'Dataset':
+        return self
+
+    def __next__(self) -> np.ndarray:
+        got = self.get()
+
+        if got is None:
+            raise StopIteration
+
+        return got
---
+++ packages/csv-dataset/csv-dataset-3.0.1.tar.gz/csv-dataset-3.0.1/csv_dataset/reader.py
@@ -0,0 +1,133 @@
+from typing import (
+    List,
+    Generic,
+    TypeVar,
+    Type,
+    Optional,
+    TextIO
+)
+from abc import (
+    ABC,
+    abstractmethod
+)
+
+from common_decorators import lazy
+
+from .normalizer import NormalizerProtocol
+
+
+SPLITTER = ','
+T = TypeVar('T', float, int)
+
+
+class AbstractReader(ABC, Generic[T]):
+    dtype: Type[T]
+
+    @abstractmethod
+    def readline(self) -> List[T]:
+        """Reads a single line and returns the content
+        """
+        ...  # pragma: no cover
+
+    @abstractmethod
+    def reset(self) -> None:
+        """Reset the reader pos
+        """
+        ...  # pragma: no cover
+
+    @property
+    @abstractmethod
+    def lines(self) -> int:
+        ...  # pragma: no cover
+
+
+class CsvReader(AbstractReader[T]):
+    def __init__(
+        self,
+        filepath: str,
+        dtype: Type[T],
+        indexes: List[int],
+        header: bool = False,
+        splitter: str = SPLITTER,
+        normalizers: List[NormalizerProtocol] = [],
+        max_lines: int = -1
+    ):
+        self.dtype = dtype
+        self._splitter = splitter
+        self._indexes = indexes
+        self._filepath = filepath
+        self._normalizers = normalizers
+        self._header = header
+
+        self.max_lines(max_lines)
+
+        if normalizers and len(normalizers) != len(indexes):
+            raise ValueError(
+                f'normalizers has different length with indexes, expect {len(indexes)} but got {len(normalizers)}'
+            )
+
+        self.reset()
+
+    @property
+    def lines(self) -> int:
+        return self._lines
+
+    @lazy
+    def _fd(self) -> TextIO:
+        return open(self._filepath, 'r')
+
+    def reset(self) -> None:
+        self._lines = 0
+        self._fd.seek(0)
+
+        if self._header:
+            self._readline()
+
+    def max_lines(self, max_lines: int) -> None:
+        """Set the max_lines of the reader
+        """
+
+        if max_lines < -1 or max_lines == 0:
+            raise ValueError(
+                f'max_lines must be -1 or postivie, but got `{max_lines}`')
+
+        self._max_lines = max_lines
+
+    def _readline(self) -> str:
+        return self._fd.readline().strip()
+
+    def _normalize(
+        self,
+        data: list
+    ) -> list:
+        if not self._normalizers:
+            return data
+
+        return [
+            self._normalizers[i].normalize(datum)
+            for i, datum in enumerate(data)
+        ]
+
+    def readline(self) -> Optional[List[T]]:
+        if self._lines == self._max_lines:
+            return
+
+        line = self._readline()
+
+        if not line:
+            return
+
+        splitted = line.split(self._splitter)
+
+        try:
+            line = [
+                self.dtype(cell)
+                for i, cell in enumerate(splitted)
+                if i in self._indexes
+            ]
+        except ValueError:
+            return self.readline()
+
+        self._lines += 1
+
+        return self._normalize(line)
