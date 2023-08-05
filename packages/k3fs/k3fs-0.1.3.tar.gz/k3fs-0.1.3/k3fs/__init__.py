4ed7eab30edfd40d143c497172783abe8cd7a748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:28 +0000

    Add package k3fs/0.1.3/k3fs-0.1.3.tar.gz

---
+++ packages/k3fs/k3fs-0.1.3.tar.gz/k3fs-0.1.3/k3fs/__init__.py
@@ -0,0 +1,33 @@
+"""
+k3fs is collection of file-system operation utilities.
+
+Usage::
+
+    >>> fwrite('/tmp/foo', "content")
+
+    >>> fread('/tmp/foo')
+    'content'
+
+    >>> 'foo' in ls_files('/tmp/')
+    True
+
+"""
+
+__version__ = "0.1.3"
+__name__ = "k3fs"
+
+from .fs import fread
+from .fs import fwrite
+from .fs import ls_dirs
+from .fs import ls_files
+from .fs import makedirs
+from .fs import remove
+
+__all__ = [
+    "ls_dirs",
+    "ls_files",
+    "makedirs",
+    "fread",
+    "fwrite",
+    "remove",
+]
