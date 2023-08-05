a2aa68fea898814ca430238408ea23238590904f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:24 +0000

    Add package mktxp/0.24/mktxp-0.24.tar.gz

---
+++ packages/mktxp/mktxp-0.24.tar.gz/mktxp-0.24/mktxp/basep.py
@@ -0,0 +1,65 @@
+# coding=utf8
+## Copyright (c) 2020 Arseniy Kuznetsov
+##
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License
+## as published by the Free Software Foundation; either version 2
+## of the License, or (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+
+from http.server import HTTPServer
+from datetime import datetime
+from prometheus_client.core import REGISTRY
+from prometheus_client import MetricsHandler, start_http_server
+from mktxp.cli.config.config import config_handler
+from mktxp.collectors_handler import CollectorsHandler
+from mktxp.router_entries_handler import RouterEntriesHandler
+
+from mktxp.cli.output.capsman_out import CapsmanOutput
+from mktxp.cli.output.wifi_out import WirelessOutput
+from mktxp.cli.output.dhcp_out import DHCPOutput
+
+
+class MKTXPProcessor:
+    ''' Base Export Processing
+    '''    
+    @staticmethod
+    def start():
+        router_entries_handler = RouterEntriesHandler()
+        REGISTRY.register(CollectorsHandler(router_entries_handler))
+        MKTXPProcessor.run(port=config_handler._entry().port)
+
+    @staticmethod
+    def run(server_class=HTTPServer, handler_class=MetricsHandler, port = None):
+        server_address = ('', port)
+        httpd = server_class(server_address, handler_class)
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f'{current_time} Running HTTP metrics server on port {port}')
+        httpd.serve_forever()
+
+
+class MKTXPCLIProcessor:
+    ''' Base CLI Processing
+    '''    
+    @staticmethod
+    def capsman_clients(entry_name):
+        router_entry = RouterEntriesHandler.router_entry(entry_name)
+        if router_entry:
+            CapsmanOutput.clients_summary(router_entry)
+        
+    @staticmethod
+    def wifi_clients(entry_name):
+        router_entry = RouterEntriesHandler.router_entry(entry_name)
+        if router_entry:
+            WirelessOutput.clients_summary(router_entry)
+        
+    @staticmethod
+    def dhcp_clients(entry_name):
+        router_entry = RouterEntriesHandler.router_entry(entry_name)
+        if router_entry:
+            DHCPOutput.clients_summary(router_entry)
---
+++ packages/mktxp/mktxp-0.24.tar.gz/mktxp-0.24/mktxp/cli/output/capsman_out.py
@@ -0,0 +1,56 @@
+# coding=utf8
+## Copyright (c) 2020 Arseniy Kuznetsov
+##
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License
+## as published by the Free Software Foundation; either version 2
+## of the License, or (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+
+from tabulate import tabulate   
+from mktxp.cli.output.base_out import BaseOutputProcessor
+from mktxp.datasource.dhcp_ds import DHCPMetricsDataSource
+from mktxp.datasource.capsman_ds import CapsmanRegistrationsMetricsDataSource
+
+
+class CapsmanOutput:
+    ''' CAPsMAN CLI Output
+    '''    
+    @staticmethod
+    def clients_summary(router_entry):
+        registration_labels = ['interface', 'ssid', 'mac_address', 'rx_signal', 'uptime', 'tx_rate', 'rx_rate']
+        registration_records = CapsmanRegistrationsMetricsDataSource.metric_records(router_entry, metric_labels = registration_labels, add_router_id = False)
+        if not registration_records:
+            print('No CAPsMAN registration records')
+            return 
+
+        # translate / trim / augment registration records
+        dhcp_lease_labels = ['host_name', 'comment', 'address', 'mac_address']
+        dhcp_lease_records = DHCPMetricsDataSource.metric_records(router_entry, metric_labels = dhcp_lease_labels, add_router_id = False)   
+
+        dhcp_rt_by_interface = {}
+        for registration_record in sorted(registration_records, key = lambda rt_record: rt_record['rx_signal'], reverse=True):
+            BaseOutputProcessor.augment_record(router_entry, registration_record, dhcp_lease_records)
+
+            interface = registration_record['interface']
+            if interface in dhcp_rt_by_interface.keys():
+                dhcp_rt_by_interface[interface].append(registration_record)
+            else:
+                dhcp_rt_by_interface[interface] = [registration_record]         
+
+        num_records = 0
+        output_table = []
+        for key in dhcp_rt_by_interface.keys():
+            for record in dhcp_rt_by_interface[key]:
+                output_table.append(BaseOutputProcessor.OutputCapsmanEntry(**record))
+                num_records += 1
+            output_table.append({})
+        print()
+        print(tabulate(output_table, headers = "keys",  tablefmt="github"))
+        print(tabulate([{0:'Connected Wifi Devices:', 1:num_records}], tablefmt="text"))
+
