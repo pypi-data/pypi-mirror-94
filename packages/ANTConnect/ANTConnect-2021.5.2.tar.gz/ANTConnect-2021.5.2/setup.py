ac7912b5d4c0507f80467c54b23a354485d64871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:46 +0000

    Add package ANTConnect/2021.5.2/ANTConnect-2021.5.2.tar.gz

---
+++ packages/ANTConnect/ANTConnect-2021.5.2.tar.gz/ANTConnect-2021.5.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ANTConnect", # Replace with your own username
+    version="2021.05.2",
+    author="ANT CDE",
+    author_email="info@antcde.io",
+    description="Python SDK for ANT Common Data Engineering",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://antcde.io",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
