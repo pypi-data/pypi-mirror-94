60fa62c4f8b19a9cb4391d53277444e3304c05db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:55 +0000

    Add package r2api/0.1.7/r2api-0.1.7-py3-none-any.whl

---
+++ packages/r2api/r2api-0.1.7-py3-none-any.whl/r2api/__init__.py
@@ -0,0 +1,7 @@
+from r2api.converter.giallo_zafferano import GZConverter
+from r2api.converter.fatto_in_casa import FCConverter
+from r2api.converter.molliche_di_zucchero import MZConverter
+
+from r2api.translate.apply_translation import translate_data
+
+from r2api.utilities.unit_conversion import convert_units_ing, convert_units_prep, simplify_units
