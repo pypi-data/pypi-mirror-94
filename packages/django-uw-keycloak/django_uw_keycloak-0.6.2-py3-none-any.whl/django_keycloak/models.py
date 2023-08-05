d6fe35cc2f9d683c09923d9acc50e5fbbeb0bfc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:31 +0000

    Add package django-uw-keycloak/0.6.2/django_uw_keycloak-0.6.2-py3-none-any.whl

---
+++ packages/django-uw-keycloak/django_uw_keycloak-0.6.2-py3-none-any.whl/django_keycloak/models.py
@@ -0,0 +1,38 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django_keycloak.keycloak import Connect
+
+from .managers import KeycloakUserManager
+
+
+class KeycloakUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(_("username"), unique=True, max_length=20)
+    keycloak_id = models.UUIDField(_("keycloak_id"), unique=True, primary_key=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True, blank=True)
+
+    USERNAME_FIELD = "username"
+
+    objects = KeycloakUserManager()
+
+    @property
+    def email(self):
+        self._confirm_cache()
+        return self._cached_user_info.get("email")
+
+    class Meta(AbstractBaseUser.Meta):
+        swappable = 'AUTH_USER_MODEL'
+
+    def _confirm_cache(self):
+        if not hasattr(self, "_cached_user_info"):
+            keycloak = Connect()
+            self._cached_user_info = keycloak.get_user_info_by_id(self.keycloak_id)
+
+
+class AbstractKeycloakUser(KeycloakUser):
+    class Meta:
+        abstract = True
+        swappable = 'AUTH_USER_MODEL'
