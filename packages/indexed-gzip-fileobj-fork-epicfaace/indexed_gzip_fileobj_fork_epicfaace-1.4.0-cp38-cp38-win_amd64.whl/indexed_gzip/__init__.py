eb37cb9116655ba3a40b2872cbf19769cc8aa9e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:11 +0000

    Add package indexed-gzip-fileobj-fork-epicfaace/1.4.0/indexed_gzip_fileobj_fork_epicfaace-1.4.0-cp38-cp38-win_amd64.whl

---
+++ packages/indexed-gzip-fileobj-fork-epicfaace/indexed_gzip_fileobj_fork_epicfaace-1.4.0-cp38-cp38-win_amd64.whl/indexed_gzip/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#
+# __init__.py - The indexed_gzip namespace.
+#
+# Author: Paul McCarthy <pauldmccarthy@gmail.com>
+#
+"""The indexed_gzip namespace. """
+
+
+from .indexed_gzip import (_IndexedGzipFile,     # noqa
+                           IndexedGzipFile,
+                           open,
+                           NotCoveredError,
+                           NoHandleError,
+                           ZranError)
+
+
+SafeIndexedGzipFile = IndexedGzipFile
+"""Alias for ``IndexedGzipFile``, to preserve compatibility with older
+versions of ``nibabel``.
+"""
+
+
+__version__ = '1.4.0'
