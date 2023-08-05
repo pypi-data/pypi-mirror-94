2bfe5a9a7e576eb12fa3f73042348a0552a67e4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:36 +0000

    Add package galaxie-viewer/0.4.4/galaxie-viewer-0.4.4.tar.gz

---
+++ packages/galaxie-viewer/galaxie-viewer-0.4.4.tar.gz/galaxie-viewer-0.4.4/glxviewer/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+APPLICATION_AUTHORS = ["Tuuuux"]
+APPLICATION_VERSION = '0.4.4'
+APPLICATION_NAME = "Galaxie Viewer"
+APPLICATION_COPYRIGHT = "2019-2021 - Galaxie Viewer Team all right reserved"
+__all__ = ['Viewer']
+
+from glxviewer.viewer import Viewer
+
+viewer = Viewer()
