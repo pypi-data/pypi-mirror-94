5e9b15d5e77e67c81ff0d149fbaf5e35713fe01f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:25 +0000

    Add package mplot/0.21002/mplot-0.21002.tar.gz

---
+++ packages/mplot/mplot-0.21002.tar.gz/mplot-0.21002/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='mplot',
+      version='0.21002',
+      description='This is a plotting library designed to be similar in usage to maple',
+      url='',
+      author='Steve Mohr',
+      author_email='steve.h.mohr@gmail.com',
+      packages=['mplot'],
+      zip_safe=False)
