cf1bc4e635f2a26bedfce9167865c579ed9fb3cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:07 +0000

    Add package aa-srp/0.1.0b16/aa_srp-0.1.0b16-py3-none-any.whl

---
+++ packages/aa-srp/aa_srp-0.1.0b16-py3-none-any.whl/aasrp/__init__.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+
+"""
+a couple of variable to use throughout the app
+"""
+
+default_app_config: str = "aasrp.apps.AaSrpConfig"
+
+__version__ = "0.1.0-beta.16"
+__title__ = "Ship Replacement"
