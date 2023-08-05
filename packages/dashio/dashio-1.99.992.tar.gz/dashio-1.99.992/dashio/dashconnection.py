2e61c7903945988d337bbbb4a07348a58e75eee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:46 +0000

    Add package dashio/1.99.992/dashio-1.99.992.tar.gz

---
+++ packages/dashio/dashio-1.99.992.tar.gz/dashio-1.99.992/dashio/dashconnection.py
@@ -0,0 +1,162 @@
+import threading
+import paho.mqtt.client as mqtt
+import ssl
+import logging
+import zmq
+import shortuuid
+
+
+class dashConnection(threading.Thread):
+    """Setups and manages a connection thread to the Dash Server."""
+
+    def __on_connect(self, client, userdata, flags, rc):
+        if rc == 0:
+            self.connected = True
+            self.disconnect = False
+            if self.device_list:
+                for device in self.device_list:
+                    control_topic = "{}/{}/control".format(self.username, device.device_id)
+                    self.dash_c.subscribe(control_topic, 0)
+                    self.__send_dash_announce(device)
+            logging.debug("connected OK")
+        else:
+            logging.debug("Bad connection Returned code=%s", rc)
+
+    def __on_disconnect(self, client, userdata, rc):
+        logging.debug("disconnecting reason  " + str(rc))
+        self.connected = False
+        self.disconnect = True
+
+    def __on_message(self, client, obj, msg):
+        data = str(msg.payload, "utf-8").strip()
+        logging.debug("DASH RX: %s", data)
+        self.tx_zmq_pub.send_multipart([self.b_connection_id, b'1', msg.payload])
+
+    def __on_publish(self, client, obj, mid):
+        pass
+
+    def __on_subscribe(self, client, obj, mid, granted_qos):
+        logging.debug("Subscribed: %s %s", str(mid), str(granted_qos))
+
+    def __on_log(self, client, obj, level, string):
+        logging.debug(string)
+
+    def add_device(self, device):
+        if device not in self.device_list:
+            self.device_list.append(device)
+            device.add_connection(self)
+            if self.connected:
+                control_topic = "{}/{}/control".format(self.username, device.device_id)
+                self.dash_c.subscribe(control_topic, 0)
+                self.__send_dash_announce(device)
+
+    def __send_dash_announce(self, device):
+        data_topic = "{}/{}/announce".format(self.username, device.device_id)
+        data = device.device_id_str + "\tWHO\t{}\t{}\n".format(device.device_type, device.device_name_cntrl.control_id)
+        self.dash_c.publish(data_topic, data)
+
+    def set_connection(self, username, password):
+        self.dash_c.disconnect()
+        self.username = username
+        self.dash_c.username_pw_set(username, password)
+        self.dash_c.connect(self.host, self.port)
+
+    def __init__(self, username="", password="", host='dash.dashio.io', port=8883, set_by_iotdashboard=False, context=None):
+        """
+        Arguments:
+            host {str} -- The server name of the dash host.
+            port {int} -- Port number to connect to.
+            username {str} -- username for the dash connection.
+            password {str} -- password for the dash connection.
+        """
+
+        threading.Thread.__init__(self, daemon=True)
+
+        self.context = context or zmq.Context.instance()
+        self.connected = False
+        self.connection_id = shortuuid.uuid()
+        self.b_connection_id = self.connection_id.encode('utf-8')
+        self.device_list = []
+        self.LWD = "OFFLINE"
+        self.running = True
+        self.username = username
+        self.dash_c = mqtt.Client()
+        self.host = host
+        self.port = port
+        self.set_by_iotdashboard = set_by_iotdashboard
+        # Assign event callbacks
+        self.dash_c.on_message = self.__on_message
+        self.dash_c.on_connect = self.__on_connect
+        self.dash_c.on_disconnect = self.__on_disconnect
+        # self.dash_c.on_publish = self.__on_publish
+        self.dash_c.on_subscribe = self.__on_subscribe
+
+        self.dash_c.tls_set(
+            ca_certs=None,
+            certfile=None,
+            keyfile=None,
+            cert_reqs=ssl.CERT_REQUIRED,
+            tls_version=ssl.PROTOCOL_TLSv1_2,
+            ciphers=None,
+        )
+        self.dash_c.tls_insecure_set(False)
+
+        self.dash_c.on_log = self.__on_log
+        # self.dash_c.will_set(self.data_topic, self.LWD, qos=1, retain=False)
+        # Connect
+        if username and password:
+            self.dash_c.username_pw_set(username, password)
+            self.dash_c.connect(host, port)
+        # Start subscribe, with QoS level 0
+        self.start()
+
+    def close(self):
+        self.running = False
+
+    def run(self):
+        self.dash_c.loop_start()
+
+        tx_url_internal = "inproc://TX_{}".format(self.connection_id)
+        rx_url_internal = "inproc://RX_{}".format(self.connection_id)
+
+        self.tx_zmq_pub = self.context.socket(zmq.PUB)
+        self.tx_zmq_pub.bind(tx_url_internal)
+
+        rx_zmq_sub = self.context.socket(zmq.SUB)
+        rx_zmq_sub.bind(rx_url_internal)
+
+        # Subscribe on ALL, and my connection
+        rx_zmq_sub.setsockopt(zmq.SUBSCRIBE, b"ALL")
+        rx_zmq_sub.setsockopt(zmq.SUBSCRIBE, b"ANNOUNCE")
+        rx_zmq_sub.setsockopt(zmq.SUBSCRIBE, b"ALARM")
+        rx_zmq_sub.setsockopt(zmq.SUBSCRIBE, self.b_connection_id)
+        poller = zmq.Poller()
+        poller.register(rx_zmq_sub, zmq.POLLIN)
+
+        while self.running:
+            try:
+                socks = dict(poller.poll(50))
+            except zmq.error.ContextTerminated:
+                break
+
+            if rx_zmq_sub in socks:
+                [address, id, data] = rx_zmq_sub.recv_multipart()
+                msg_l = data.split(b'\t')
+                try:
+                    device_id = msg_l[1].decode('utf-8').strip()
+                except IndexError:
+                    continue
+                if address == b'ALARM':
+                    data_topic = "{}/{}/alarm".format(self.username, device_id)
+                else:
+                    data_topic = "{}/{}/data".format(self.username, device_id)
+                if self.connected:
+                    logging.debug("DASH TX: %s", data.decode('utf-8').rstrip())
+                    self.dash_c.publish(data_topic, data.decode('utf-8'))
+
+        if self.connected:
+            self.dash_c.publish(self.announce_topic, "disconnect")
+        self.dash_c.loop_stop()
+
+        self.tx_zmq_pub.close()
+        rx_zmq_sub.close()
---
+++ packages/dashio/dashio-1.99.992.tar.gz/dashio-1.99.992/dashio/iotcontrol/page.py
@@ -0,0 +1,134 @@
+from .enums import Color, Icon
+from .control import Control
+
+
+class Page(Control):
+    """A Config only control"""
+
+    def __init__(
+        self,
+        control_id,
+        title="A page",
+        icon=Icon.SQUARE,
+        page_color=Color.BLACK,
+        control_title_box_color=Color.BLACK,
+        control_title_box_transparency=0,
+        control_color=Color.WHITE_SMOKE,
+        control_border_color=Color.WHITE_SMOKE,
+        control_border_on=False,
+        control_background_color=Color.BLACK,
+        control_title_font_size=16,
+        control_max_font_size=20,
+        control_background_transparency=0,
+    ):
+        super().__init__("PAGE", control_id)
+        self.title = title
+        self.icon_name = icon
+        self.page_color = page_color
+        self.control_title_box_color = control_title_box_color
+        self.control_title_box_transparency = control_title_box_transparency
+        self.control_color = control_color
+        self.control_border_color = control_border_color
+        self.control_border_on = control_border_on
+        self.control_background_color = control_background_color
+        self.control_title_font_size = control_title_font_size
+        self.control_max_font_size = control_max_font_size
+        self.control_background_transparency = control_background_transparency
+        self._state_str = ""
+
+    def add_control(self, control):
+        control.parent_id = self.control_id
+
+    @property
+    def icon_name(self) -> Icon:
+        return self._icon_name
+
+    @icon_name.setter
+    def icon_name(self, val: Icon):
+        self._icon_name = val
+        self._cfg["iconName"] = val.value
+
+    @property
+    def page_color(self) -> Color:
+        return self._page_color
+
+    @page_color.setter
+    def page_color(self, val: Color):
+        self._page_color = val
+        self._cfg["pageColor"] = str(val.value)
+
+    @property
+    def control_title_box_color(self) -> Color:
+        return self._control_title_box_color
+
+    @control_title_box_color.setter
+    def control_title_box_color(self, val: Color):
+        self._control_title_box_color = val
+        self._cfg["ctrlTitleBoxColor"] = str(val.value)
+
+    @property
+    def control_title_box_transparency(self) -> int:
+        return self._cfg["ctrlTitleBoxTransparency"]
+
+    @control_title_box_transparency.setter
+    def control_title_box_transparency(self, val: int):
+        self._cfg["ctrlTitleBoxTransparency"] = val
+
+    @property
+    def control_color(self) -> Color:
+        return self._control_color
+
+    @control_color.setter
+    def control_color(self, val: Color):
+        self._control_color = val
+        self._cfg["ctrlColor"] = str(val.value)
+
+    @property
+    def control_border_color(self) -> Color:
+        return self._control_border_color
+
+    @control_border_color.setter
+    def control_border_color(self, val: Color):
+        self._control_border_color = val
+        self._cfg["ctrlBorderColor"] = str(val.value)
+
+    @property
+    def control_border_on(self) -> bool:
+        return self._cfg["ctrlBorderOn"]
+
+    @control_border_on.setter
+    def control_border_on(self, val: bool):
+        self._cfg["ctrlBorderOn"] = val
+
+    @property
+    def control_background_color(self) -> Color:
+        return self._control_background_color
+
+    @control_background_color.setter
+    def control_background_color(self, val: Color):
+        self._control_background_color = val
+        self._cfg["ctrlBkgndColor"] = str(val.value)
+
+    @property
+    def control_title_font_size(self) -> int:
+        return self._cfg["ctrlTitleFontSize"]
+
+    @control_title_font_size.setter
+    def control_title_font_size(self, val: int):
+        self._cfg["ctrlTitleFontSize"] = val
+
+    @property
+    def control_max_font_size(self) -> int:
+        return self._cfg["ctrlMaxFontSize"]
+
+    @control_max_font_size.setter
+    def control_max_font_size(self, val: int):
+        self._cfg["ctrlMaxFontSize"] = val
+
+    @property
+    def control_background_transparency(self) -> int:
+        return self._cfg["ctrlBkgndTransparency"]
+
+    @control_background_transparency.setter
+    def control_background_transparency(self, val: int):
+        self._cfg["ctrlBkgndTransparency"] = val
