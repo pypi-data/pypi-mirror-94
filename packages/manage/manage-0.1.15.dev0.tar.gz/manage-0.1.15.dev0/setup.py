ed6caa87fea2cc0cfb99632467f53557b75bd504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:20 +0000

    Add package manage/0.1.15.dev0/manage-0.1.15.dev0.tar.gz

---
+++ packages/manage/manage-0.1.15.dev0.tar.gz/manage-0.1.15.dev0/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup
+
+
+def long_description():
+    desc = []
+    for filename in ['README.rst', 'HISTORY.rst']:
+        with open(filename, 'r') as fd:
+            desc.append(fd.read())
+
+    return '\n\n'.join(desc)
+
+
+setup(
+    name='manage',
+    version='0.1.15-dev0',
+    description="Command Line Manager + Interactive Shell for Python Projects",
+    long_description=long_description(),
+    long_description_content_type='text/x-rst',
+    author="Bruno Rocha",
+    author_email='rochacbruno@gmail.com',
+    url='https://github.com/pthon-manage/manage',
+    packages=['manage'],
+    package_dir={'manage': 'manage'},
+    entry_points={'console_scripts': ['manage=manage.cli:main']},
+    include_package_data=True,
+    install_requires=[
+        'Click>=7.1',
+        'PyYAML>=5.3'
+    ],
+    license="ISC license",
+    zip_safe=False,
+    keywords='manage',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ],
+    test_suite='tests',
+    tests_require=[
+        'pytest>=6.2',
+        'pytest-xdist>=2.2'
+    ],
+)
---
+++ packages/manage/manage-0.1.15.dev0.tar.gz/manage-0.1.15.dev0/tests/test_manage.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_manage
+----------------------------------
+
+Tests for `manage` module.
+py.test should run with --boxed argument
+"""
+import os
+
+from click.testing import CliRunner
+
+from manage.cli import cli, init_cli
+
+BASEPATH = os.path.abspath('.')
+
+
+class TestManage(object):
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    def go_to_example(self, path):
+        path = os.path.join(BASEPATH, 'examples', path)
+        os.chdir(path)
+        init_cli(cli, reset=True)
+
+    def test_command_line_interface(self):
+        runner = CliRunner()
+        result = runner.invoke(cli, ['--help'])
+        assert result.exit_code == 0
+        assert 'shell' in result.output
+        help_result = runner.invoke(cli, ['--help'])
+        assert help_result.exit_code == 0
+        assert 'Show this message and exit.' in help_result.output
+
+    def test_custom_command_names(self):
+        runner = CliRunner()
+        self.go_to_example('custom_command_names')
+        result = runner.invoke(cli, ['--help'])
+        assert result.exit_code == 0
+        assert 'new_user' in result.output
+        assert 'new_group' in result.output
+        create_user = runner.invoke(cli, ['new_user', '--name=Bruno'])
+        assert 'Creating the user Bruno' in create_user.output
+        create_group = runner.invoke(cli, ['new_group', '--name=Users'])
+        assert 'Creating the group Users' in create_group.output
+
+    def test_custom_namespaces(self):
+        runner = CliRunner()
+        self.go_to_example('custom_namespaces')
+        result = runner.invoke(cli, ['--help'])
+        assert result.exit_code == 0
+        assert 'bar_create-user' in result.output
+        assert 'foo_new_group' in result.output
+        create_user = runner.invoke(cli, ['bar_create-user', '--name=Bruno'])
+        assert 'Creating the user Bruno' in create_user.output
+        create_group = runner.invoke(cli, ['foo_new_group', '--name=Users'])
+        assert 'Creating the group Users' in create_group.output
+
+    def test_function_commands(self):
+        runner = CliRunner()
+        self.go_to_example('function_commands')
+        result = runner.invoke(cli, ['--help'])
+        assert result.exit_code == 0
+        assert 'new_user' in result.output
+        assert (
+            "Error: Missing option '--password'"
+            in runner.invoke(cli, ['new_user', '--name=Bruno']).output
+        )
+        create_user = runner.invoke(cli, ['new_user', '--name=Bruno', '--password=123'])
+        assert 'Creating user Bruno' in create_user.output
+
+    def test_inline_commands(self):
+        runner = CliRunner()
+        self.go_to_example('inline_commands')
+        result = runner.invoke(cli, ['--help'])
+        assert result.exit_code == 0
+        assert 'clear_cache' in result.output
+        clear_cache = runner.invoke(cli, ['clear_cache'])
+        assert "'clean_days': 100" in clear_cache.output
+
+        clear_cache = runner.invoke(cli, ['clear_cache', '--days=15'])
+        assert "'clean_days': 15" in clear_cache.output
+
+    def test_multiple_click_commands(self):
+        runner = CliRunner()
+        self.go_to_example('multiple_commands')
+        result = runner.invoke(cli, ['--help'])
+        assert result.exit_code == 0
+        assert 'create-user' in result.output
+        assert 'create-group' in result.output
+        create_user = runner.invoke(cli, ['create-user', '--name=Bruno'])
+        assert 'Creating the user Bruno' in create_user.output
+        create_group = runner.invoke(cli, ['create-group', '--name=Users'])
+        assert 'Creating the group Users' in create_group.output
+
+    def test_namespaced_commands(self):
+        runner = CliRunner()
+        self.go_to_example('namespaced_commands')
+        result = runner.invoke(cli, ['--help'])
+        assert result.exit_code == 0
+        assert 'user_create-user' in result.output
+        assert 'group_new_group' in result.output
+        create_user = runner.invoke(cli, ['user_create-user', '--name=Bruno'])
+        assert 'Creating the user Bruno' in create_user.output
+        create_group = runner.invoke(cli, ['group_new_group', '--name=Users'])
+        assert 'Creating the group Users' in create_group.output
+
+    def test_simple(self):
+        runner = CliRunner()
+        self.go_to_example('simple')
+        result = runner.invoke(cli, ['--help'])
+        assert result.exit_code == 0
+
+    def test_simple_commands(self):
+        runner = CliRunner()
+        self.go_to_example('simple_commands')
+        result = runner.invoke(cli, ['--help'])
+        assert result.exit_code == 0
+        assert 'create-user' in result.output
+        assert 'create-group' in result.output
+        create_user = runner.invoke(cli, ['create-user', '--name=Bruno'])
+        assert 'Creating the user Bruno' in create_user.output
+        create_group = runner.invoke(cli, ['create-group', '--name=Users'])
+        assert 'Creating the group Users' in create_group.output
+
+    def test_naval(self):
+        runner = CliRunner()
+        self.go_to_example('naval')
+
+        # ship and mine is there?
+        result = runner.invoke(cli, ['--help'])
+        assert result.exit_code == 0
+        assert 'ship' in result.output
+        assert 'mine' in result.output
+
+        # help for ship
+        result = runner.invoke(cli, ['ship', '--help'])
+        assert result.exit_code == 0
+        assert 'new' in result.output
+        assert 'move' in result.output
+        assert 'shoot' in result.output
+
+        # help for mine
+        result = runner.invoke(cli, ['mine', '--help'])
+        assert result.exit_code == 0
+        assert 'set' in result.output
+        assert 'remove' in result.output
+
+        # ship new
+        result = runner.invoke(cli, ['ship', 'new', 'FireStorm'])
+        assert result.exit_code == 0
+        assert result.output == 'Created ship FireStorm\n'
+
+        # ship move
+        result = runner.invoke(
+            cli, ['ship', 'move', 'FireStorm', '12', '34', '--speed', '90']
+        )
+        assert result.exit_code == 0
+        assert result.output == ('Moving ship FireStorm to 12.0,34.0 with speed 90\n')
+
+        # ship shoot
+        result = runner.invoke(cli, ['ship', 'shoot', 'FireStorm', '30', '45'])
+        assert result.exit_code == 0
+        assert result.output == 'Ship FireStorm fires to 30.0,45.0\n'
+
+        # mine set
+        result = runner.invoke(cli, ['mine', 'set', '10', '20'])
+        assert result.exit_code == 0
+        assert result.output == 'Set moored mine at 10.0,20.0\n'
+
+        result = runner.invoke(cli, ['mine', 'set', '10', '20', '--drifting'])
+        assert result.exit_code == 0
+        assert result.output == 'Set drifting mine at 10.0,20.0\n'
+
+        # mine remove
+        result = runner.invoke(cli, ['mine', 'remove', '10', '20'])
+        assert result.exit_code == 0
+        assert result.output == 'Removed mine at 10.0,20.0\n'
+
+    @classmethod
+    def teardown_class(cls):
+        pass
