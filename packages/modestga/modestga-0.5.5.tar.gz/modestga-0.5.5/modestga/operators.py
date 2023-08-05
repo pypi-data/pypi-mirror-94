7c511da92b369331448754c54fab87831a1ff6f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:51 +0000

    Add package modestga/0.5.5/modestga-0.5.5.tar.gz

---
+++ packages/modestga/modestga-0.5.5.tar.gz/modestga-0.5.5/modestga/operators.py
@@ -0,0 +1,68 @@
+import logging
+import random
+import numpy as np
+
+
+def crossover(ind1, ind2, uniform=0.5):
+    # log = logging.getLogger('crossover')
+    child = ind1.copy()
+
+    rand = np.random.random(size=child.gen.size)
+    child.gen = np.where(rand > uniform, child.gen, ind2.gen)
+
+    return child
+
+
+def tournament(pop, size):
+    # log = logging.getLogger('tournament')
+
+    assert size * 2 < len(pop.ind), \
+        f"Tournament size ({size}) has to be lower than population ({len(pop.ind)}) // 2"
+
+    # Form groups
+    g1g2 = np.random.choice(pop.ind, size=size*2, replace=False)
+    group1 = g1g2[:size]
+    group2 = g1g2[size:]
+
+    # Pick and return fittest from each group
+    fit1 = np.argmin([x.val for x in group1])
+    fit2 = np.argmin([x.val for x in group2])
+
+    i1 = group1[fit1]
+    i2 = group2[fit2]
+
+    # log.debug("{}, {}".format(i1.id, i2.id))
+
+    return i1, i2
+
+
+def mutation(ind, rate, scale):
+    """
+    Mutate genes. `rate` controls how many genes are mutated.
+
+    :param ind: Individual
+    :param rate: float (0-1), mutation rate
+    :param scale: standard deviation of the normal distribution
+    :return: mutated Individual (copy)
+    """
+    # log = logging.getLogger('mutation')
+
+    # Draw random value to be compared with rate
+    mut = np.random.rand(ind.gen.size)
+
+    # Draw new random genes from a normal distribution
+    mut_gen = np.random.normal(loc=ind.gen, scale=scale)
+    # To 0-1 range
+    mut_gen = np.where(mut_gen > 1., 1., mut_gen)
+    mut_gen = np.where(mut_gen < 0., 0., mut_gen)
+    # Substitute genes where mut < rate
+    new_gen = np.where(mut < rate, mut_gen, ind.gen)
+
+    # log.debug(f"Old genes: {ind.gen}")
+    # log.debug(f"Candidate genes {mut_gen}")
+    # log.debug(f"New genes {new_gen}")
+
+    mutind = ind.copy()
+    mutind.set_genes(new_gen)
+
+    return mutind
---
+++ packages/modestga/modestga-0.5.5.tar.gz/modestga-0.5.5/modestga/parallel/full.py
@@ -0,0 +1,69 @@
+import os
+import cloudpickle
+import numpy as np
+import logging
+
+from modestga import operators
+from modestga import population
+from modestga import individual
+from modestga.ga import norm
+
+
+def parallel_pop(pipe,
+                 pickled_fun,
+                 args,
+                 bounds,
+                 pop_size,
+                 trm_size,
+                 xover_ratio,
+                 mut_rate,
+                 end_event):
+    """Subpopulation used in parallel GA."""
+    log = logging.getLogger(name=f'parallel_pop[PID={os.getpid()}]')
+    log.debug("Starting process")
+
+    # Unpickle function
+    fun = cloudpickle.loads(pickled_fun)
+
+    # Initialize population
+    pop = population.Population(pop_size, bounds, fun, evaluate=False)
+
+    while not end_event.is_set():
+        # Check if there's some data
+        if pipe.poll(0.01):
+            # Get data
+            try:
+                data = pipe.recv()
+            except EOFError:
+                break
+            scale = data['scale']
+            pop.set_genes(data['genes'])
+            pop.set_fx(data['fx'])
+
+            # Generate children
+            children = list()
+            fx = list()
+
+            while len(children) < pop_size:
+                #Cross-over
+                i1, i2 = operators.tournament(pop, trm_size)
+                child = operators.crossover(i1, i2, xover_ratio)
+
+                # Mutation
+                child = operators.mutation(child, mut_rate, scale)
+
+                # Evaluate f(x)
+                child.evaluate()
+
+                # Add to children
+                children.append(child)
+                fx.append(child.val)
+
+            # Return data (new genes) to the main process
+            pop.ind = children
+            data = dict()
+            data['genes'] = pop.get_genes()
+            data['fx'] = fx
+            pipe.send(data)
+
+    pipe.close()
