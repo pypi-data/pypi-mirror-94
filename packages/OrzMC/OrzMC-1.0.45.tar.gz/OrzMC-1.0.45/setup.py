285823ad704ec17a891813cd243dc325cc9b11fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:06 +0000

    Add package OrzMC/1.0.45/OrzMC-1.0.45.tar.gz

---
+++ packages/OrzMC/OrzMC-1.0.45.tar.gz/OrzMC-1.0.45/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import setuptools
+import sys
+
+setuptools.setup(
+    name="OrzMC",
+    version="1.0.45",
+    description="A project for deployment of minecraft game.",
+    long_description= open('README.md', encoding = 'UTF-8').read(),
+    long_description_content_type="text/markdown",
+    author="王志舟<wangzhizhou>",
+    author_email="824219521@qq.com",
+    maintainer='王志舟',
+    maintainer_email='824219521@qq.com',
+    url="https://github.com/OrzGeeker/OrzMC.git",
+    packages=setuptools.find_packages(),
+    platforms = ['all'],
+    keywords='minecraft python',
+    classifiers=(
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux"
+    ),
+    python_requires='>=3, <4',
+    install_requires=[
+        'pylint',
+        'requests',
+        'twine',
+        'beautifulsoup4'
+    ],
+    entry_points = {
+        'console_scripts': [
+            'orzmc = OrzMC:start',
+            'orzmc_jdk = OrzMC:install_jdk',
+            'orzmc_jdk_remove = OrzMC:uninstall_jdk'
+        ]
+    }
+)
