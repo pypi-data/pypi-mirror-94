057075751275e5014ffefa3f1bb345bd93a7f6ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:24 +0000

    Add package urbandictionarypy/0.1/urbandictionarypy-0.1.tar.gz

---
+++ packages/urbandictionarypy/urbandictionarypy-0.1.tar.gz/urbandictionarypy-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+from setuptools import find_packages
+setup(name='urbandictionarypy',
+      version='0.1',
+      description="A simple api wrapper for urban dictionary's api.",
+      author='Jacks0n9',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
