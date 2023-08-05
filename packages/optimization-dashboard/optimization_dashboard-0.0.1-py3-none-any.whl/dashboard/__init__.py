086cae55c3624d289b6b68757a86a1055590b019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:18 +0000

    Add package optimization-dashboard/0.0.1/optimization_dashboard-0.0.1-py3-none-any.whl

---
+++ packages/optimization-dashboard/optimization_dashboard-0.0.1-py3-none-any.whl/dashboard/__init__.py
@@ -0,0 +1,14 @@
+# Author: Simon Blanke
+# Email: simon.blanke@yahoo.com
+# License: MIT License
+
+__version__ = "0.0.1"
+__license__ = "MIT"
+
+
+from .dashboard import Dashboard
+
+
+__all__ = [
+    "Dashboard",
+]
