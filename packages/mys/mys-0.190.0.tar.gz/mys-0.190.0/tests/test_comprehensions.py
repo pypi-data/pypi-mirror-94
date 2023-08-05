58db70bfc2131efc72de18173995a03431c29d00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:46 +0000

    Add package mys/0.190.0/mys-0.190.0.tar.gz

---
+++ packages/mys/mys-0.190.0.tar.gz/mys-0.190.0/tests/test_comprehensions.py
@@ -0,0 +1,36 @@
+from .utils import TestCase
+from .utils import build_and_test_module
+
+
+class Test(TestCase):
+
+    def test_comprehensions(self):
+        build_and_test_module('comprehensions')
+
+    def test_list_comprehension_two_for_loops(self):
+        self.assert_transpile_raises(
+            'def foo() -> [i64]:\n'
+            "    return [x * y for x in [1] for y in [2]]\n",
+            '  File "", line 2\n'
+            '        return [x * y for x in [1] for y in [2]]\n'
+            "               ^\n"
+            'CompileError: only one for-loop allowed\n')
+
+    def test_comprehension_over_tuple(self):
+        self.assert_transpile_raises(
+            'def foo() -> [i64]:\n'
+            "    return [x for x in (1, True)]\n",
+            '  File "", line 2\n'
+            '        return [x for x in (1, True)]\n'
+            "                           ^\n"
+            'CompileError: iteration over tuples not allowed\n')
+
+    # ToDo - Remove once implemented.
+    def test_set_comprehension_not_implemented(self):
+        self.assert_transpile_raises(
+            'def foo():\n'
+            "    print({v for v in [1]})\n",
+            '  File "", line 2\n'
+            '        print({v for v in [1]})\n'
+            "              ^\n"
+            'CompileError: set comprehension is not implemented\n')
