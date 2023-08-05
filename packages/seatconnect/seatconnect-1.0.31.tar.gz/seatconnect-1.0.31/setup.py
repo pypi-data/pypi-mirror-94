8b6d3562735fee4cec5f0ea89a5c3605d5430d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:59 +0000

    Add package seatconnect/1.0.31/seatconnect-1.0.31.tar.gz

---
+++ packages/seatconnect/seatconnect-1.0.31.tar.gz/seatconnect-1.0.31/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+def local_scheme(version):
+    return ""
+
+setuptools.setup(
+    name='seatconnect',
+    version='1.0.30',
+    description='Communicate with Seat Connect',
+    author='Farfar',
+    author_email='faekie@hotmail.com',
+    url='https://github.com/farfar/seatconnect',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    provides=["seatconnect"],
+    install_requires=list(open("requirements.txt").read().strip().split("\n")),
+    #use_scm_version=True,
+    use_scm_version={"local_scheme": local_scheme},
+    setup_requires=[
+        'setuptools_scm',
+        'pytest>=5,<6',
+    ]
+)
