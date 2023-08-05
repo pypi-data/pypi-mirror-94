6acaaedef0ad4ddd61c704efcbbe00959ca7a234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:13 +0000

    Add package markdown-it-py/0.6.2/markdown_it_py-0.6.2-py3-none-any.whl

---
+++ packages/markdown-it-py/markdown_it_py-0.6.2-py3-none-any.whl/markdown_it/utils.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+from typing import TYPE_CHECKING, Any, List, Union
+
+
+if TYPE_CHECKING:
+    AttrDict = Any
+else:
+
+    class AttrDict(dict):
+        def __init__(self, *args, **kwargs):
+            super(AttrDict, self).__init__(*args, **kwargs)
+            self.__dict__ = self
+
+            # recursively apply to all nested dictionaries
+            for key, item in list(self.items()):
+                if isinstance(item, dict):
+                    self[key] = AttrDict(item)
+
+
+def read_fixture_file(path: Union[str, Path]) -> List[list]:
+    text = Path(path).read_text(encoding="utf-8")
+    tests = []
+    section = 0
+    last_pos = 0
+    lines = text.splitlines(keepends=True)
+    for i in range(len(lines)):
+        if lines[i].rstrip() == ".":
+            if section == 0:
+                tests.append([i, lines[i - 1].strip()])
+                section = 1
+            elif section == 1:
+                tests[-1].append("".join(lines[last_pos + 1 : i]))
+                section = 2
+            elif section == 2:
+                tests[-1].append("".join(lines[last_pos + 1 : i]))
+                section = 0
+
+            last_pos = i
+    return tests
