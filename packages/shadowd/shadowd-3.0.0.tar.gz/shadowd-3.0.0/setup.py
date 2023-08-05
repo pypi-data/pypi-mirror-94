f0d02caf4d63e00cc4c531955c08adbd58134353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:59 +0000

    Add package shadowd/3.0.0/shadowd-3.0.0.tar.gz

---
+++ packages/shadowd/shadowd-3.0.0.tar.gz/shadowd-3.0.0/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+#
+# Shadow Daemon -- Web Application Firewall
+#
+# Copyright (C) 2014-2021 Hendrik Buchwald <hb@zecure.org>
+#
+# This file is part of Shadow Daemon. Shadow Daemon is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import os
+from setuptools import setup, Command
+
+README = os.path.join(os.path.dirname(__file__), 'README.rst')
+long_description = open(README).read() + '\n\n'
+
+setup(
+    name='shadowd',
+    version='3.0.0',
+    description='Python connector for the Shadow Daemon web application firewall',
+    long_description=long_description,
+    url='http://github.com/zecure/shadowd_python',
+    author='Hendrik Buchwald',
+    author_email='hb@zecure.org',
+    license='GPLv2',
+    packages=['shadowd'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: System Administrators',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python :: 2',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: System :: Networking :: Firewalls',
+    ],
+    keywords='waf security shadowd',
+    test_suite = 'shadowd.tests.test_all',
+)
