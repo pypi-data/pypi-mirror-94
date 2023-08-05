8d9792da2d03aaa378458f6cf268b250bf3d28a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:47 +0000

    Add package iotc/1.1.0/iotc-1.1.0.tar.gz

---
+++ packages/iotc/iotc-1.1.0.tar.gz/iotc-1.1.0/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+import sys
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+version = "1.1.0"
+
+setuptools.setup(
+    name='iotc',
+    version=version,
+    author="Luca Druda",
+    author_email="ludruda@microsoft.com",
+    description="Azure IoT Central device client for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/iot-for-all/iotc-python-client",
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    license="MIT",
+    platform="OS Independent",
+    keywords="iot,azure,iotcentral",
+    classifiers=[
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 2',
+      'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.8',
+    ],
+    include_package_data=True,
+    install_requires=["azure-iot-device"]
+)
