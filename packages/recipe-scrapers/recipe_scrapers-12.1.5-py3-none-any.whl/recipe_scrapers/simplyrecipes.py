252cf188284cbef5a323660a3264dc8399a06ed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:54 +0000

    Add package recipe-scrapers/12.1.5/recipe_scrapers-12.1.5-py3-none-any.whl

---
+++ packages/recipe-scrapers/recipe_scrapers-12.1.5-py3-none-any.whl/recipe_scrapers/simplyrecipes.py
@@ -0,0 +1,46 @@
+from ._abstract import AbstractScraper
+from ._utils import get_minutes, normalize_string, get_yields
+
+
+class SimplyRecipes(AbstractScraper):
+    @classmethod
+    def host(cls):
+        return "simplyrecipes.com"
+
+    def title(self):
+        return self.soup.find("h1").get_text()
+
+    def total_time(self):
+        return sum(
+            [
+                get_minutes(self.soup.find("span", {"class": "preptime"})),
+                get_minutes(self.soup.find("span", {"class": "cooktime"})),
+            ]
+        )
+
+    def yields(self):
+        return get_yields(self.soup.find("span", {"class": "yield"}))
+
+    def ingredients(self):
+        ingredients = self.soup.find("div", {"class": "recipe-ingredients"}).findAll(
+            "li"
+        )
+
+        return [normalize_string(ingredient.get_text()) for ingredient in ingredients]
+
+    def instructions(self):
+        instructions_html = self.soup.find("div", {"class": "instructions"}).findAll(
+            "p"
+        )
+        instructions = [
+            normalize_string(instruction.get_text())
+            for instruction in instructions_html
+        ]
+
+        return "\n".join(
+            [
+                instruction
+                for instruction in instructions
+                if instruction and not instruction.isspace()
+            ]
+        )
