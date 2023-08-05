729e064ddc4e83b94f1a96008b7e13c6dd7673a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:16 +0000

    Add package discord-logger/1.2.2/discord-logger-1.2.2.tar.gz

---
+++ packages/discord-logger/discord-logger-1.2.2.tar.gz/discord-logger-1.2.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", mode="r") as fd:
+    long_description = fd.read()
+
+setuptools.setup(
+    name="discord-logger",
+    version="1.2.2",
+    author="Chaitanya Chinni",
+    description="Discord Logger is a custom message logger to Discord for Python 3",
+    license="MIT License",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chinnichaitanya/python-discord-logger",
+    packages=setuptools.find_packages(),
+    install_requires=["discord-webhook == 0.11.0", "pyyaml == 5.4.1"],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    python_requires=">=3.5",
+)
