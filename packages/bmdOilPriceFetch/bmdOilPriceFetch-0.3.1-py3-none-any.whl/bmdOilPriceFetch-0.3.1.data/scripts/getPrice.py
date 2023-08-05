af2d6708c6b38ad07b23584ce5d807128af4a239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:18 +0000

    Add package bmdOilPriceFetch/0.3.1/bmdOilPriceFetch-0.3.1-py3-none-any.whl

---
+++ packages/bmdOilPriceFetch/bmdOilPriceFetch-0.3.1-py3-none-any.whl/bmdOilPriceFetch-0.3.1.data/scripts/getPrice.py
@@ -0,0 +1,10 @@
+#!python
+import bmdOilPriceFetch
+
+#Get and Print WTI Oil Price
+def printPrice():
+    data = bmdOilPriceFetch.bmdPriceFetch()
+    outputString = 'The price of WTI is $' + str("%.2f" % data['regularMarketPrice'])
+    print(outputString)
+
+printPrice()
