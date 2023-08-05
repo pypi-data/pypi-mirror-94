2c04369cbf82a056bdfe37fa61b962200070cc2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:43 +0000

    Add package aa-srp/1.0.0/aa_srp-1.0.0-py3-none-any.whl

---
+++ packages/aa-srp/aa_srp-1.0.0-py3-none-any.whl/aasrp/app_settings.py
@@ -0,0 +1,46 @@
+# coding=utf-8
+
+"""
+our app setting
+"""
+
+import re
+
+from django.conf import settings
+
+from aasrp.utils import clean_setting
+
+# AA-GDPR
+AVOID_CDN = clean_setting("AVOID_CDN", False)
+
+
+def get_site_url() -> str:  # regex sso url
+    """
+    get the site url
+    :return: string
+    """
+    regex = r"^(.+)\/s.+"
+    matches = re.finditer(regex, settings.ESI_SSO_CALLBACK_URL, re.MULTILINE)
+    url = "http://"
+
+    for match in matches:
+        url = match.groups()[0]  # first match
+
+    return url
+
+
+def avoid_cdn() -> bool:
+    """
+    check if we should aviod CDN usage
+    :return: bool
+    """
+    return AVOID_CDN
+
+
+def discord_bot_active():
+    """
+    check if allianceauth-dicordbot is installed and active
+    :return:
+    """
+
+    return "aadiscordbot" in settings.INSTALLED_APPS
