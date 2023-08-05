db6f436e3c7e01bf2340e3377e06c218ab763a06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:23 +0000

    Add package deep-forest/0.1.1/deep_forest-0.1.1-cp37-cp37m-win_amd64.whl

---
+++ packages/deep-forest/deep_forest-0.1.1-cp37-cp37m-win_amd64.whl/deepforest/__init__.py
@@ -0,0 +1,14 @@
+from .cascade import CascadeForestClassifier
+from .forest import RandomForestClassifier
+from .forest import ExtraTreesClassifier
+from .tree import DecisionTreeClassifier
+from .tree import ExtraTreeClassifier
+
+
+__all__ = [
+    "CascadeForestClassifier",
+    "RandomForestClassifier",
+    "ExtraTreesClassifier",
+    "DecisionTreeClassifier",
+    "ExtraTreeClassifier",
+]
