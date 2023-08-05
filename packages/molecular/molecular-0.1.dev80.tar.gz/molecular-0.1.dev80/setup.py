9906ce9423934e5c5ec90a1ac89d84e9fba092f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:24 +0000

    Add package molecular/0.1.dev80/molecular-0.1.dev80.tar.gz

---
+++ packages/molecular/molecular-0.1.dev80.tar.gz/molecular-0.1.dev80/setup.py
@@ -0,0 +1,74 @@
+"""
+setup.py
+written in Python3
+author: C. Lockhart <chris@lockhartlab.org>
+"""
+
+# from setuptools import setup as _setup
+from setuptools import dist
+from numpy.distutils.core import Extension, setup
+
+# Make sure numpy is installed
+dist.Distribution().fetch_build_egg('numpy')
+
+# Read version
+with open('version.yml', 'r') as f:
+    data = f.read().splitlines()
+version_dict = dict([element.split(': ') for element in data])
+
+# Convert the version_data to a string
+version = '.'.join([str(version_dict[key]) for key in ['major', 'minor']])
+if version_dict['micro'] != 0:
+    version += '.' + version_dict['micro']
+print(version)
+
+# Read in requirements.txt
+with open('requirements.txt', 'r') as stream:
+    requirements = stream.read().splitlines()
+
+# Long description
+with open('README.rst', 'r') as stream:
+    long_description = stream.read()
+
+# First make sure numpy is installed
+# _setup(install_requires=['numpy'])
+
+# Then, install molecular
+setup(
+    name='molecular',
+    version=version,
+    author='C. Lockhart',
+    author_email='chris@lockhartlab.org',
+    description='A toolkit for molecular dynamics simulations',
+    long_description=long_description,
+    url="https://www.lockhartlab.org",
+    packages=[
+        'molecular',
+        'molecular.analysis',
+        'molecular.analysis.protein',
+        'molecular.bioinformatics',
+        'molecular.core',
+        'molecular.external',
+        'molecular.geometry',
+        'molecular.io',
+        'molecular.misc',
+        'molecular.simulations',
+        'molecular.statistics',
+        'molecular.transform',
+        'molecular.viz'
+    ],
+    # install_requires=[
+    #     'glovebox',
+    #     'numpy',
+    #     'pandas',
+    #     'privatize',
+    #     'typelike',
+    #     'hypothesis',
+    #     'numba'
+    # ],
+    # include_package_data=True,
+    # zip_safe=True,
+    ext_modules=[
+        Extension('molecular.io.fortran.read_dcd', ['molecular/io/src/read_dcd.f90'])
+    ]
+)
