a2e3e0b8ff2172d99a858d4a5f5dc9a27e8fe4c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:42 +0000

    Add package django-aws-s3-storage/1.0.0/django-aws-s3-storage-1.0.0.tar.gz

---
+++ packages/django-aws-s3-storage/django-aws-s3-storage-1.0.0.tar.gz/django-aws-s3-storage-1.0.0/setup.py
@@ -0,0 +1,24 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+with open('requirements.txt') as reqs_file:
+    requirements = reqs_file.readlines()
+
+with open('README.md') as desc_file:
+    long_description = desc_file.read()
+
+setup(
+    name='django-aws-s3-storage',
+    version='1.0.0',
+    description='AWS S3 Bucket file storage system for Django applications',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Gustavo Valentim',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    python_requires='>=3.6',
+    install_requires=requirements,
+    setup_requires=['wheel'],
+)
