4fca05ca3514c1bf2f4eb67e7ea0fd9630d25a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:46 +0000

    Add package python-gql-gen/0.1.2/python_gql_gen-0.1.2-py2.py3-none-any.whl

---
+++ packages/python-gql-gen/python_gql_gen-0.1.2-py2.py3-none-any.whl/python_gql_gen/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for python-gql-gen."""
+
+
+
+#from python_gql_gen.utils import get_schema, parse_schema_to_gql
+
+
+__author__ = """wujian"""
+__email__ = 'sonic3021@gmail.com'
+__version__ = '0.1.0'
+
+#__all__ = ['get_schema', 'parse_schema_to_gql']
---
+++ packages/python-gql-gen/python_gql_gen-0.1.2-py2.py3-none-any.whl/python_gql_gen/cli.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for python_gql_gen."""
+import sys
+import click
+from python_gql_gen.utils import get_schema, parse_schema_to_gql
+
+
+@click.command()
+@click.option('--url','-u', help='the graphql api url  ', required=True)
+@click.option('--directory', '-d', help='the output directory name ')
+def main(url,directory ):
+    """Console script for python_gql_gen."""
+    click.echo("get schema from url ".format(url))
+    schema_str=get_schema(url=url,directory=directory)
+
+    click.echo("parse the schema to query(mutation...) file to  ".format(directory))
+    parse_schema_to_gql(schema_str=schema_str,directory=directory)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
