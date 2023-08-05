26049a2a99291141ae384c5a4d65abb8c6a21a14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:43 +0000

    Add package moto-custom-solutions/0.0.1/moto_custom_solutions-0.0.1-py3-none-any.whl

---
+++ packages/moto-custom-solutions/moto_custom_solutions-0.0.1-py3-none-any.whl/customsol_pkg/errors.py
@@ -0,0 +1,30 @@
+class CustomError(Exception):
+    pass
+
+
+class DataError(CustomError):
+    pass
+
+
+class FTPConnectionError(CustomError):
+    pass
+
+
+class FTPDownloadError(CustomError):
+    pass
+
+
+class FTPUploadError(CustomError):
+    pass
+
+
+class FTPEncodingError(CustomError):
+    pass
+
+
+class RedashAPIError(CustomError):
+    pass
+
+
+class DuplicateFileError(CustomError):
+    pass
