4db584983f54961fc933d00c89c3b3188d809bbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:16 +0000

    Add package discord-logger/1.2.2/discord_logger-1.2.2-py3-none-any.whl

---
+++ packages/discord-logger/discord_logger-1.2.2-py3-none-any.whl/examples/success_message.py
@@ -0,0 +1,27 @@
+from discord_logger import DiscordLogger
+
+webhook_url = "your discord webhook url"
+options = {
+    "application_name": "My Server",
+    "service_name": "Backend API",
+    "service_icon_url": "your icon url",
+    "service_environment": "Production",
+    "default_level": "info",
+}
+
+logger = DiscordLogger(webhook_url=webhook_url, **options)
+
+logger.construct(
+    title="Celery Task Manager",
+    description="Successfully completed training job for model v1.3.3!",
+    level="success",
+    metadata={
+        "Metrics": {
+            "Accuracy": 78.9,
+            "Inference time": "0.8 sec",
+            "Model size": "32 MB",
+        },
+        "Deployment status": "progress",
+    },
+)
+response = logger.send()
