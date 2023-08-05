71190dc633a6e2a71a8eaef2679350d3fcfba236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:33 +0000

    Add package libshipkore/0.1.0/libshipkore-0.1.0-py2.py3-none-any.whl

---
+++ packages/libshipkore/libshipkore-0.1.0-py2.py3-none-any.whl/libshipkore/track/common/basetrackservice.py
@@ -0,0 +1,20 @@
+from libshipkore.common.baseservice import BaseService
+# from geopy.geocoders import Nominatim
+
+class BaseTrackService(BaseService):
+    # geolocator = Nominatim(user_agent="shipkore")
+
+    def __init__(self, waybill, provider, *args, **kwargs):
+        super().__init__(waybill, provider, *args, **kwargs)
+        self.waybill = waybill
+        self.provider = provider
+
+    def _transform(self):
+        raise NotImplementedError
+
+    def _save(self):
+
+        pass
+
+    def get(self):
+        raise NotImplementedError
