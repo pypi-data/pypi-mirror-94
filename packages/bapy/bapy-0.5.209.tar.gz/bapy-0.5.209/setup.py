0da38690b90439f723dcecab1b894f9b89b18876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:40 +0000

    Add package bapy/0.5.209/bapy-0.5.209.tar.gz

---
+++ packages/bapy/bapy-0.5.209.tar.gz/bapy-0.5.209/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3.9
+# -*- coding: utf-8 -*-
+"""The setup script."""
+from setuptools import setup, find_packages
+
+import bapy
+from bapy import User, Url, path, ic
+ic(path)
+ic(path.project.description())
+ic(path.project)
+ic(path.project.requirements)
+ic(path.scripts_relative)
+ic(path.scripts)
+setup(
+    author=User.gecos,
+    author_email=Url.email(),
+    description=path.project.description(),
+    entry_points={
+        'console_scripts': [
+            f'{path.repo} = {path.repo}:app',
+        ],
+    },
+    include_package_data=True,
+    install_requires=path.project.requirements['requirements'],
+    name=path.repo,
+    package_data={
+        path.repo: [f'{path.repo}/scripts/*', f'{path.repo}/templates/*'],
+    },
+    packages=find_packages(),
+    python_requires='>=3.9,<4',
+    scripts=path.scripts_relative,
+    setup_requires=path.project.requirements['requirements_setup'],
+    tests_require=path.project.requirements['requirements_test'],
+    url=Url.lumenbiomics(http=True, repo=path.repo).url,
+    use_scm_version=False,
+    version='0.5.209',
+    zip_safe=False,
+)
