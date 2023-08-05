d5254431e11c15c952e46527a8c46ba9b84393da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:13 +0000

    Add package circle-python/0.0.13/circle_python-0.0.13-py3-none-any.whl

---
+++ packages/circle-python/circle_python-0.0.13-py3-none-any.whl/circle/object_classes.py
@@ -0,0 +1,9 @@
+from circle import resources
+
+OBJECT_CLASSES = {
+    resources.Ach.OBJECT_NAME: resources.Ach,
+    resources.Card.OBJECT_NAME: resources.Card,
+    resources.Payment.OBJECT_NAME: resources.Payment,
+    resources.Payout.OBJECT_NAME: resources.Payout,
+    resources.Wire.OBJECT_NAME: resources.Wire,
+}
---
+++ packages/circle-python/circle_python-0.0.13-py3-none-any.whl/circle/resources/__init__.py
@@ -0,0 +1,7 @@
+from circle.resources.ach import Ach
+from circle.resources.card import Card
+from circle.resources.payment import Payment
+from circle.resources.payout import Payout
+from circle.resources.public_key import PublicKey
+from circle.resources.transfer import Transfer
+from circle.resources.wire import Wire
