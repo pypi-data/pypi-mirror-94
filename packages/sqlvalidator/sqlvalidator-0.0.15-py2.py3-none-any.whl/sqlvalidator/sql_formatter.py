4025124ef18e3fe2b9cf3e603e0e5dfe68c2bbeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:06 +0000

    Add package sqlvalidator/0.0.15/sqlvalidator-0.0.15-py2.py3-none-any.whl

---
+++ packages/sqlvalidator/sqlvalidator-0.0.15-py2.py3-none-any.whl/sqlvalidator/sql_formatter.py
@@ -0,0 +1,7 @@
+from sqlvalidator.grammar.lexer import SQLStatementParser
+from sqlvalidator.grammar.tokeniser import to_tokens
+
+
+def format_sql(sql_string: str) -> str:
+    # print(sql_string)
+    return SQLStatementParser.parse(to_tokens(sql_string)).transform()
