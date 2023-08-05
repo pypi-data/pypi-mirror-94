0b1423abe9ea2129c1404e116a2ef3a60e426b10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:14 +0000

    Add package minimal-example-project/1.0.3/minimal_example_project-1.0.3.tar.gz

---
+++ packages/minimal-example-project/minimal_example_project-1.0.3.tar.gz/minimal_example_project-1.0.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+install_reqs = [x.strip() for x in all_reqs if 'git+' not in x]
+dep_links = [x.strip().replace('git+', '') for x in all_reqs if 'git+' in x]
+
+# read the contents of your README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="minimal_example_project",
+    version="1.0.3",
+    author='Michel Lacle',
+    author_email='michel+pypi@f1kart.com',
+    url='https://github.com/michel-lacle/minimal_example_project',
+    description="a minimal Python project example with unit testing, command line interface, PyPi publishing capabilities",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(where='src', exclude=['env', 'tests', 'infrastructure']),
+    include_package_data=True,
+    install_requires=install_reqs,
+    package_dir={'': 'src'},
+    dependency_links=dep_links)
