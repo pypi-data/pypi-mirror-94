03e2518e4e4eea6718154a06e679308864342e50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:32 +0000

    Add package tamcolors/1.3.0/tamcolors-1.3.0-cp37-cp37m-win32.whl

---
+++ packages/tamcolors/tamcolors-1.3.0-cp37-cp37m-win32.whl/tamcolors/__init__.py
@@ -0,0 +1,32 @@
+from . import tam
+from . import tam_tools
+from . import tam_basic
+from .tam_basic.basic import printc, inputc, clear
+from . import tam_io
+from . import tam_c
+from . import examples
+from . import tests
+from . import utils
+
+
+__author__ = "Charles McMarrow"
+__version__ = "1.3.0"
+__email__ = "Charles.M.McMarrow@gmail.com"
+__license__ = "Apache Software License 2.0"
+
+
+__all__ = ("__author__",
+           "__version__",
+           "__email__",
+           "__license__",
+           "tam",
+           "tam_tools",
+           "tam_basic",
+           "tam_c",
+           "printc",
+           "inputc",
+           "clear",
+           "tam_io",
+           "examples",
+           "tests",
+           "utils")
---
+++ packages/tamcolors/tamcolors-1.3.0-cp37-cp37m-win32.whl/tamcolors/tam_c/_win_tam_c/win_tam.h
@@ -0,0 +1,37 @@
+#ifndef __WIN_TAM__H_
+#define __WIN_TAM__H_
+#include <exception>
+
+class ConsoleException : public std::exception {
+	private:
+		virtual const char* what() const throw();
+};
+
+
+class Dimensions {
+	private:
+		short width;
+		short height;
+	public:
+		Dimensions(short, short);
+		short get_width();
+		short get_height();
+};
+
+void get_console_info(HANDLE&, CONSOLE_SCREEN_BUFFER_INFOEX&);
+bool has_vaild_win_console();
+void init_default_color();
+int get_default_color();
+void show_console_cursor(bool);
+Dimensions get_dimensions();
+Dimensions get_buffer_dimensions();
+void set_buffer_dimensions(Dimensions);
+void clear(bool);
+void set_cursor_info(int, int, int);
+void set_console_color(int);
+void set_rgb_color(int, COLORREF);
+COLORREF get_rgb_color(int);
+int get_key();
+char* get_keyboard_name();
+int get_key_state(int key_code);
+#endif
