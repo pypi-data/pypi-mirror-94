3b370eeb2976b45f9819af9bbb1e61fc803e5772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:36 +0000

    Add package civicpy/1.1.3/civicpy-1.1.3-py3-none-any.whl

---
+++ packages/civicpy/civicpy-1.1.3-py3-none-any.whl/civicpy/__version__.py
@@ -0,0 +1,15 @@
+__title__ = 'civicpy'
+__description__ = 'CIViC variant knowledgebase analysis toolkit.'
+__url__ = 'http://civicpy.org'
+__major__ = '1'
+__minor__ = '1'
+__patch__ = '3'
+__meta_label__ = ''
+__short_version__ = "{}.{}".format(__major__, __minor__)
+__version__ = "{}.{}".format(__short_version__, __patch__)
+if __meta_label__:
+    __version__ += "-{}".format(__meta_label__)
+__authors__ = ['Alex H. Wagner', 'Susanna Kiwala']
+__author_email__ = 'help@civicpy.org'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2018-2020 The Griffith Lab'
