e61cc7bace503bc63515082eefa84c019984ee86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:44 +0000

    Add package sphinx-clutter/0.0.2/sphinx-clutter-0.0.2.tar.gz

---
+++ packages/sphinx-clutter/sphinx-clutter-0.0.2.tar.gz/sphinx-clutter-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sphinx-clutter",
+    version="0.0.2",
+    author="Itay Donanhirsh",
+    author_email="itay.donanhirsh@checkr.com",
+    description="Sphinx Clutter plugin",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cluttercode/sphinx-clutter",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
