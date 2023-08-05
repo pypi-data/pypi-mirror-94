6ee7d9d41a47b896fae528b1638f35eb1c8dc54d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:11 +0000

    Add package microdot/0.3.1/microdot-0.3.1.tar.gz

---
+++ packages/microdot/microdot-0.3.1.tar.gz/microdot-0.3.1/setup.py
@@ -0,0 +1,31 @@
+"""
+Microdot
+--------
+
+The impossibly small web framework for MicroPython.
+"""
+from setuptools import setup
+
+setup(
+    name='microdot',
+    version="0.3.1",
+    url='http://github.com/miguelgrinberg/microdot/',
+    license='MIT',
+    author='Miguel Grinberg',
+    author_email='miguel.grinberg@gmail.com',
+    description='The impossibly small web framework for MicroPython',
+    long_description=__doc__,
+    py_modules=['microdot'],
+    platforms='any',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: Implementation :: MicroPython',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
