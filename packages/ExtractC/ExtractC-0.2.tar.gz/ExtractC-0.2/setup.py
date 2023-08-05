07f1928551ebe893609069960220820099476475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:46 +0000

    Add package ExtractC/0.2/ExtractC-0.2.tar.gz

---
+++ packages/ExtractC/ExtractC-0.2.tar.gz/ExtractC-0.2/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'ExtractC',         # How you named your package folder (MyLib)
+  packages = ['ExtractC'],   # Chose the same as "name"
+  version = '0.2',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Extract Vietnamese Card',   # Give a short description about your library
+  author = 'TrongBui',                   # Type in your name
+  author_email = 'buiquoctrong110500@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/trongbui1105/ExtractCard.git',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/trongbui1105/ExtractCard/archive/0.1.5.tar.gz',    # I explain this later on
+  keywords = ['extract'],   # Keywords that define your package best
+  install_requires=['opencv-python', 'torch', 'torchvision', 'vietocr', 'pyyaml'],            # I get to this in a second
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which python versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9'
+  ],
+)
