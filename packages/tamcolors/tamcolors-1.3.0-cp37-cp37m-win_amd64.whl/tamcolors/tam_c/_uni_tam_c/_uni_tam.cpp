d6c0d70480093305058648d1a4ecab82f68dac05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:33 +0000

    Add package tamcolors/1.3.0/tamcolors-1.3.0-cp37-cp37m-win_amd64.whl

---
+++ packages/tamcolors/tamcolors-1.3.0-cp37-cp37m-win_amd64.whl/tamcolors/tam_c/_uni_tam_c/_uni_tam.cpp
@@ -0,0 +1,122 @@
+//python library
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+//tamcolors library
+#include "uni_tam.h"
+
+
+/*
+C++ API to Unix console
+can get dimension of console
+get key input in the background
+*/
+
+
+static PyObject* _get_dimensions(PyObject *self, PyObject *args) {
+    /*
+	info: will return Dimensions info width and height
+	return: Dimensions
+	*/
+	if (!PyArg_ParseTuple(args, "")) {
+		return NULL;
+	}
+	Dimensions dimensions = get_dimensions();
+	return Py_BuildValue("ii", dimensions.width, dimensions.height);
+}
+
+static PyObject* _enable_get_key(PyObject *self, PyObject *args) {
+    /*
+	info: will enable terminal for kbhit and get_key
+	turns off echo and other settings
+	*/
+	if (!PyArg_ParseTuple(args, "")) {
+		return NULL;
+	}
+	enable_get_key();
+	Py_RETURN_NONE;
+}
+
+static PyObject* _disable_get_key(PyObject *self, PyObject *args) {
+    /*
+	info: will reset terminal
+	turns on echo and other settings
+	*/
+	if (!PyArg_ParseTuple(args, "")) {
+		return NULL;
+	}
+	disable_get_key();
+	Py_RETURN_NONE;
+}
+
+static PyObject* _get_key(PyObject *self, PyObject *args) {
+    /*
+	return: int
+	-1: no key data to give
+	Not -1: value of key data
+	*/
+	if (!PyArg_ParseTuple(args, "")) {
+		return NULL;
+	}
+	return Py_BuildValue("i", get_key());
+}
+
+static PyObject* _get_key_state(PyObject* self, PyObject* args) {
+	/*
+	info: will check if applation has a vaild win console
+	retrun: bool
+	*/
+	int key;
+	if (!PyArg_ParseTuple(args, "i", &key)) {
+		return NULL;
+	}
+
+	if (get_key_state(key)) {
+		Py_INCREF(Py_True);
+		return Py_BuildValue("O", Py_True);
+	}
+	Py_INCREF(Py_False);
+	return Py_BuildValue("O", Py_False);
+}
+
+static PyMethodDef _uni_tam_methods[] = {
+	{
+		"_get_dimensions", _get_dimensions, METH_VARARGS,
+		"_get_dimensions"
+	},
+	{
+		"_enable_get_key", _enable_get_key, METH_VARARGS,
+		"_enable_get_key"
+	},
+	{
+		"_disable_get_key", _disable_get_key, METH_VARARGS,
+		"_disable_get_key"
+	},
+	{
+		"_get_key", _get_key, METH_VARARGS,
+		"_get_key"
+	},
+	{
+		"_get_key_state", _get_key_state, METH_VARARGS,
+		"_get_key_state"
+	},
+{ NULL, NULL, 0, NULL }
+};
+
+static struct PyModuleDef _uni_tam_definition = {
+	PyModuleDef_HEAD_INIT,
+	"_uin_tam",
+	"Can work with Unix terminal.",
+	-1,
+	_uni_tam_methods
+};
+
+PyMODINIT_FUNC PyInit__uni_tam(void) {
+    PyObject* py_module = PyModule_Create(&_uni_tam_definition);
+
+	if (py_module == NULL) {
+		return NULL;
+	}
+
+	return py_module;
+}
