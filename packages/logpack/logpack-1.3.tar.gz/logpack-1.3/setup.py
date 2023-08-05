24a48dd3a1580d76b159d8929b2b0d442bc23be0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:34 +0000

    Add package logpack/1.3/logpack-1.3.tar.gz

---
+++ packages/logpack/logpack-1.3.tar.gz/logpack-1.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="logpack",
+    version="1.3",
+    author="Joxos",
+    author_email="xujunhao61@163.com",
+    description="Log for Python!",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    url="https://github.com/Joxos/logpack",
+    packages=setuptools.find_packages(),
+    install_requires=["datetime"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
+# Note:
+# python setup.py sdist
+# twine upload ./dist/*
