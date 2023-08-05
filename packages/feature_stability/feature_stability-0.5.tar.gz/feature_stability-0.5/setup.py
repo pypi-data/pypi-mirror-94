ed5f419242024a9a35d0840f3d014916c7922f8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:49 +0000

    Add package feature_stability/0.5/feature_stability-0.5.tar.gz

---
+++ packages/feature_stability/feature_stability-0.5.tar.gz/feature_stability-0.5/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  2 15:34:52 2021
+
+@author: dujuan
+"""
+
+from setuptools import find_packages,setup
+setup(
+    name = 'feature_stability',
+    version = '0.5',
+    packages = find_packages(),
+    author='dujuan',
+)
