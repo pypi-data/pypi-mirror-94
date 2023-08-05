5a15ea48de59870c5bc436c022fc188917d8a06b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:18 +0000

    Add package osdk-manager/0.3.0/osdk-manager-0.3.0.tar.gz

---
+++ packages/osdk-manager/osdk-manager-0.3.0.tar.gz/osdk-manager-0.3.0/src/osdk_manager/__main__.py
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: BSD-2-Clause
+"""osdk-manager module.
+
+Manage osdk and opm binary installation, and help to scaffold, release, and
+version Operator SDK-based Kubernetes operators.
+"""
+
+from osdk_manager.cli import cli
+
+cli()
