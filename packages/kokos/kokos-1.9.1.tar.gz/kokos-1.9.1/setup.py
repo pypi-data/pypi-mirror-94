d7f7f40f3e75666b136a8a29ebab0587a94dfa4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:45 +0000

    Add package kokos/1.9.1/kokos-1.9.1.tar.gz

---
+++ packages/kokos/kokos-1.9.1.tar.gz/kokos-1.9.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='kokos',
+    description='KoKoS helper tool',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Vyacheslav Boben',
+    url='https://github.com/DarkKeks/kks',
+    version='1.9.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        'requests',
+        'configparser',
+        'colorama',
+        'tqdm',
+        'beautifulsoup4',
+        'html2text==2020.1.16',
+        'pyyaml'
+    ],
+    entry_points='''
+        [console_scripts]
+        kks=kks.cli:cli
+    ''',
+)
