dc76279ee73a35fd52fea3d217ba9f877edc996f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:46 +0000

    Add package jytestrunopt2/0.0.1/jytestrunopt2-0.0.1.tar.gz

---
+++ packages/jytestrunopt2/jytestrunopt2-0.0.1.tar.gz/jytestrunopt2-0.0.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='jytestrunopt2',
+    version='0.0.1',
+    author='zjy',
+    author_email='unknown@bgsn.com',
+    url='https://www.cnblogs.com/zhangjialu2015/p/5173313.html',
+    description='opt2',
+    py_modules = ['jytestrun'],
+    install_requires=[],
+
+)
