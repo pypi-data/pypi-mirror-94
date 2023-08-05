eaa9fa2f3d4388bfc33160fb886eaaba48f7cdec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:35 +0000

    Add package ANNZOO/2021.2.5.19.21.41/ANNZOO-2021.2.5.19.21.41.tar.gz

---
+++ packages/ANNZOO/ANNZOO-2021.2.5.19.21.41.tar.gz/ANNZOO-2021.2.5.19.21.41/annzoo/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : tql-ANN.
+# @File         : __init__.py
+# @Time         : 2019-12-04 20:05
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : 
+
