580adce0074780f62a837d1c3f61669abfec3d9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:57 +0000

    Add package nt-lib/0.1.4/nt-lib-0.1.4.tar.gz

---
+++ packages/nt-lib/nt-lib-0.1.4.tar.gz/nt-lib-0.1.4/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['nt', 'nt.lib']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['typer>=0.3.2,<0.4.0']
+
+entry_points = \
+{'console_scripts': ['nt-lib = nt.lib:app']}
+
+setup_kwargs = {
+    'name': 'nt-lib',
+    'version': '0.1.4',
+    'description': '',
+    'long_description': None,
+    'author': 'Uriel Mandujano',
+    'author_email': 'uriel.mandujano14@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
