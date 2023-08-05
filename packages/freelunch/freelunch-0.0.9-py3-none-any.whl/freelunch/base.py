7f960c5f3ed50a8b1c3d55c0d1486ef093cfe076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:15 +0000

    Add package freelunch/0.0.9/freelunch-0.0.9-py3-none-any.whl

---
+++ packages/freelunch/freelunch-0.0.9-py3-none-any.whl/freelunch/base.py
@@ -0,0 +1,97 @@
+'''
+Base classes for optimisers
+
+'''
+from os import error
+import numpy as np
+from freelunch import darwin
+from freelunch.adaptable import adaptable_set
+
+
+class optimiser:
+    '''
+    Base class for all optimisers
+    '''
+    name = 'optimiser'
+    tags = []
+    hyper_definitions = {}
+    hyper_defaults = {}
+    can_run_quick = False
+
+    def __init__(self, obj=None, hypers={}, bounds=None):
+        '''
+        Unlikely to instance the base class but idk what else goes here
+        '''
+        self.hypers = dict(self.hyper_defaults, **hypers) # Hyperparamters/ methods (dictionary of )
+        self.obj = obj # Objective function
+        self.bounds = bounds # Bounds / constraints
+        self.nfe = 0 # TODO: wrap obj so that these are counted automatically
+
+    def __call__(self, nruns=1, return_m=1, full_output=False):
+        '''
+        API for running the optimisation
+        '''
+        if self.obj is None:
+            raise NotImplementedError('No optimiser selected')
+        if nruns == 1 and return_m == 1 and not full_output and self.can_run_quick:
+            return self.run_quick()
+        if nruns > 1:
+            runs = [self.run() for i in range(nruns)]
+            if all([r == None for r in runs]):
+                print('No solutions returned, is this an instance of the base class?')
+                return np.array([])
+            sols = np.concatenate(runs)
+        else: 
+            sols = self.run()
+        sols = sorted(sols, key=lambda x: x.fitness)
+        if not full_output:
+            return np.array([sol.dna for sol in sols[:return_m]])
+        else:
+            out = {
+                'optimiser':self.name,
+                'hypers':self.hypers,
+                'bounds':self.bounds,
+                'nruns':nruns,
+                'nfe':self.nfe,
+                'solutions':[sol.dna for sol in sols],
+                'scores':[sol.fitness for sol in sols]
+            }
+            return out
+
+    def __repr__(self):
+        return self.name + ' optimisation object'    
+
+    def run(self):
+        if self.obj is None:
+            raise NotImplementedError('No objective function selected')
+
+    def run_quick(self):
+        if self.obj is None:
+            raise NotImplementedError('No objective function selected')
+
+    def parse_hyper(self, op):
+        if isinstance(op, list): # top 10 recursive gamer moments
+            strats = [self.parse_hyper(strat) for strat in op]
+            return adaptable_set(strats)
+        elif isinstance(op, darwin.genetic_operation):
+            return op()
+        elif isinstance(op, str):
+            try:
+                op = getattr(darwin,op)
+                return op()
+            except AttributeError:
+                raise AttributeError # TODO handle this properly
+
+
+# Subclasses for granularity
+
+class continuous_space_optimiser(optimiser):
+    '''
+    Base class for continuous space optimisers i.e DE, PSO, SA etc.
+    '''
+
+class discrete_space_optimiser(optimiser):
+    '''
+    Base class for discrete space optimisers i.e GA, GP etc.
+    '''
+
---
+++ packages/freelunch/freelunch-0.0.9-py3-none-any.whl/freelunch/tech.py
@@ -0,0 +1,73 @@
+'''
+Standard / common techniques that are used in several optimisers are abstracted to functions here. 
+'''
+
+# %% Imports
+
+import numpy as np
+from freelunch.zoo import animal
+
+# %% Custom exceptions
+
+
+class BadObjectiveFunctionScores(Exception):
+    '''Exception raised when both objective score comparisons evaluate false'''
+
+
+class ZeroLengthSolutionError(Exception):
+    '''Exception raised when an empty solution is passed to a benchmark'''
+
+
+class SolutionCollapseError(Exception):
+    '''Exception raised when all solutions are identical'''
+
+
+
+# %% Common methods
+
+def uniform_continuous_init(bounds, N):
+    out = np.empty((N,), dtype=object)
+    for i in range(N):
+        out[i] = animal(np.array([np.random.uniform(a, b)
+                                    for a, b in bounds]))
+    return out
+
+
+def compute_obj(pop, obj):
+    for sol in pop:
+        sol.fitness = obj(sol.dna)
+        if np.isnan(sol.fitness):
+            sol.fitness = None
+    return pop
+
+
+def apply_sticky_bounds(dna, bounds):
+    out = dna[:]
+    for i, bound in enumerate(bounds):
+        low, high = bound
+        if dna[i] > high:
+            out[i] = high
+        elif dna[i] < low:
+            out[i] = low
+    return out
+
+
+def bounds_as_mat(bounds):
+    bounds_mat = np.zeros((len(bounds), 2))
+    for i, bound in enumerate(bounds):
+        bounds_mat[i, 0], bounds_mat[i, 1] = bound
+    return bounds_mat
+
+
+def lin_reduce(lims, n, n_max):
+    # Linearly reduce with generations, e.g. inertia values
+    return lims[1] + (lims[0]-lims[1])*n/n_max
+
+
+def pdist(A,B=None):
+    '''
+    Pairwise Euclidean Distance inside array
+    '''
+    if B is None: B = A
+    return np.sqrt(np.sum((A[:,None]-B[None,:])**2,axis=-1))
+
