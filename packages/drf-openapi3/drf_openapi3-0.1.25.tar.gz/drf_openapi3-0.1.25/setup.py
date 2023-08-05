f046c0012eb32ab0e9e5d3d8ccb2a2c57f38c7f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:03 +0000

    Add package drf-openapi3/0.1.25/drf_openapi3-0.1.25.tar.gz

---
+++ packages/drf-openapi3/drf_openapi3-0.1.25.tar.gz/drf_openapi3-0.1.25/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='drf_openapi3',
+    description='Manage OpenApi documentation with DRF',
+    version='0.1.25',
+    author='Davide Pugliese',
+    author_email='davide.pglse@gmail.com',
+    license='GPL',
+    url='https://github.com/gungnir888/drf-openapi',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires='>3.6.0',
+    install_requires=[
+        'Django',
+        'djangorestframework',
+        'pyyaml'
+    ],
+    keywords=["openapi", "openapi-3.0", "django-rest-framework", "drf", "documentatation"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
