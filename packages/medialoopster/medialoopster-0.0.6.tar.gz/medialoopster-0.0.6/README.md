9f7dafdd005357505fa634b421f6ae6d6c93555e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:35 +0000

    Add package medialoopster/0.0.6/medialoopster-0.0.6.tar.gz

---
+++ packages/medialoopster/medialoopster-0.0.6.tar.gz/medialoopster-0.0.6/README.md
@@ -0,0 +1,23 @@
+# medialoopster
+
+**medialoopster API wrapper for Python**
+
+_medialoopster_ is a wrapper library for the [HTTP API](https://docs.medialoopster.com/api) of the
+[medialoopster](https://www.medialoopster.com/) Media Asset Management System.
+
+
+## Installation
+
+```bash
+pip install medialoopster
+```
+    
+## Usage
+
+You can start using _medialoopster_ as simply as:
+
+```python
+from medialoopster import Medialoopster
+
+
+```
---
+++ packages/medialoopster/medialoopster-0.0.6.tar.gz/medialoopster-0.0.6/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="medialoopster",
+    version="0.0.6",
+    author="Stéphane Ludwig",
+    author_email="gitlab@stephane-ludwig.net",
+    description="medialoopster API wrapper for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/medialoopster/medialoopster_python",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
