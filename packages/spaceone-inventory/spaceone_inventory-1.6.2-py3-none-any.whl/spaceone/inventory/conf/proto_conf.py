63cd4f3985005603bc9c2612a26ffd97c61d9b93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:28 +0000

    Add package spaceone-inventory/1.6.2/spaceone_inventory-1.6.2-py3-none-any.whl

---
+++ packages/spaceone-inventory/spaceone_inventory-1.6.2-py3-none-any.whl/spaceone/inventory/conf/proto_conf.py
@@ -0,0 +1,17 @@
+PROTO = {
+    'spaceone.inventory.api.v1.region': ['Region'],
+    # 'zone': ['Zone'],
+    # 'pool': ['Pool'],
+    # 'network_type': ['NetworkType'],
+    # 'network_policy': ['NetworkPolicy'],
+    # 'network': ['Network'],
+    # 'subnet': ['Subnet'],
+    # 'ip_address': ['IPAddress'],
+    'spaceone.inventory.api.v1.server': ['Server'],
+    'spaceone.inventory.api.v1.collector': ['Collector'],
+    'spaceone.inventory.api.v1.job': ['Job'],
+    'spaceone.inventory.api.v1.job_task': ['JobTask'],
+    'spaceone.inventory.api.v1.cloud_service_type': ['CloudServiceType'],
+    'spaceone.inventory.api.v1.cloud_service': ['CloudService'],
+    'spaceone.inventory.api.v1.resource_group': ['ResourceGroup'],
+}
