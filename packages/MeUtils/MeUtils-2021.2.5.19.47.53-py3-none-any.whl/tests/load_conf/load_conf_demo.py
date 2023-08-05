257b13182480aa4edffa10b4b06653ba1caba024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:49 +0000

    Add package MeUtils/2021.2.5.19.47.53/MeUtils-2021.2.5.19.47.53-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2021.2.5.19.47.53-py3-none-any.whl/tests/load_conf/load_conf_demo.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : MeUtils.
+# @File         : load_conf_demo
+# @Time         : 2021/1/27 8:38 下午
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : 
+
+
+from meutils.pipe import *
+
+
+class Config(BaseConfig):
+    name: str = ''
+    age: int = 666
+    betterme: List[str]=[]
+
+
+# Config.parse_zk('/mipush/myconf')
+print(Config.parse_yaml('./myconf.yaml').dict())
+#
+# print(Config.parse_zk('/mipush/bot').betterme)
+# print(Config.parse_zk('/mipush/bot').__getattribute__('betterme'))
+
+# os.environ['name'] = '123456789'
+# print(Config.parse_env().dict())
