43559630647c8ee70e1257f7e6188bb65717b949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:33 +0000

    Add package hydra-py/0.5.2/hydra-py-0.5.2.tar.gz

---
+++ packages/hydra-py/hydra-py-0.5.2.tar.gz/hydra-py-0.5.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as f:
+    install_requires = f.read().splitlines()
+
+with open("./hydrapy/VERSION") as f:
+    version = f.read().rstrip()
+
+setuptools.setup(
+    name="hydra-py",
+    version=version,
+    description="Hydra for Python. A light-weight library for building distributed applications such as microservices.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pnxtech/HydraPy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=install_requires
+)
