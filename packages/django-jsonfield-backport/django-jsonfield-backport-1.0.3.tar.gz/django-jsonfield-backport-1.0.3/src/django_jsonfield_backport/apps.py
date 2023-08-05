b5190c3d131e51decf98d05fa0b33362ba3aa9ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:35 +0000

    Add package django-jsonfield-backport/1.0.3/django-jsonfield-backport-1.0.3.tar.gz

---
+++ packages/django-jsonfield-backport/django-jsonfield-backport-1.0.3.tar.gz/django-jsonfield-backport-1.0.3/src/django_jsonfield_backport/apps.py
@@ -0,0 +1,17 @@
+import django
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
+from django_jsonfield_backport import features, forms, models
+
+
+class JSONFieldConfig(AppConfig):
+    name = "django_jsonfield_backport"
+    verbose_name = _("JSONField backport from Django 3.1")
+
+    def ready(self):
+        if django.VERSION >= (3, 1):
+            return
+        features.connect_signal_receivers()
+        forms.patch_admin()
+        models.register_lookups()
