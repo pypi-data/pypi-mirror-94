008e1cabc661bcde70dfa95508697c26b0e83412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:22 +0000

    Add package MimePost/1.0.0/MimePost-1.0.0.tar.gz

---
+++ packages/MimePost/MimePost-1.0.0.tar.gz/MimePost-1.0.0/MimePost/api/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from MimePost.api.accounts_api import AccountsApi
+from MimePost.api.domains_api import DomainsApi
+from MimePost.api.emails_api import EmailsApi
+from MimePost.api.stats_api import StatsApi
+from MimePost.api.webhooks_api import WebhooksApi
