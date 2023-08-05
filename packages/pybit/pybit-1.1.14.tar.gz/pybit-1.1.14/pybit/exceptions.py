c9fc08eb0dac715f92b99236b91a0cc4688dde15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:06 +0000

    Add package pybit/1.1.14/pybit-1.1.14.tar.gz

---
+++ packages/pybit/pybit-1.1.14.tar.gz/pybit-1.1.14/pybit/exceptions.py
@@ -0,0 +1,32 @@
+class FailedRequestError(Exception):
+    """
+    Exception raised for failed requests.
+
+    Attributes:
+        message -- Explanation of the error.
+        status_code -- The code number returned.
+    """
+    def __init__(self, message, status_code, time):
+        self.message = message
+        self.status_code = status_code
+        self.time = time
+        super().__init__((
+            f'{message} (ErrCode: {status_code}) (ErrTime: {time})'
+        ))
+
+
+class InvalidRequestError(Exception):
+    """
+    Exception raised for returned Bybit errors.
+
+    Attributes:
+        message -- Explanation of the error.
+        status_code -- The code number returned.
+    """
+    def __init__(self, message, status_code, time):
+        self.message = message
+        self.status_code = status_code
+        self.time = time
+        super().__init__((
+            f'{message} (ErrCode: {status_code}) (ErrTime: {time} UTC)'
+        ))
