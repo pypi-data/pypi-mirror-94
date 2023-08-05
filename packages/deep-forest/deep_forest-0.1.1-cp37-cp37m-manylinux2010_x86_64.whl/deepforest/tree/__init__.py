dbe9fce511fb323357339f5fc819c41fa8a9823e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:23 +0000

    Add package deep-forest/0.1.1/deep_forest-0.1.1-cp37-cp37m-manylinux2010_x86_64.whl

---
+++ packages/deep-forest/deep_forest-0.1.1-cp37-cp37m-manylinux2010_x86_64.whl/deepforest/tree/__init__.py
@@ -0,0 +1,6 @@
+from .tree import BaseDecisionTree
+from .tree import DecisionTreeClassifier
+from .tree import ExtraTreeClassifier
+
+
+__all__ = ["BaseDecisionTree", "DecisionTreeClassifier", "ExtraTreeClassifier"]
