aa9b6457030bb141f461f7a28d83d29ed93696d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:19 +0000

    Add package earwax/2026.2.5/earwax-2026.2.5-py3-none-any.whl

---
+++ packages/earwax/earwax-2026.2.5-py3-none-any.whl/earwax/__init__.py
@@ -0,0 +1,150 @@
+"""The Earwax game engine.
+
+Earwax
+------
+
+    This package is heavily inspired by `Flutter <https://flutter.dev/>`_.
+
+Usage
+=====
+
+* Begin with a :class:`~earwax.Game` object::
+
+    from earwax import Game, Level
+    g = Game()
+
+* Create a level::
+
+    l = Level()
+
+* Add actions to allow the player to do things::
+
+    @l.action(...)
+    def action():
+        pass
+
+* Create a Pyglet window::
+
+    from pyglet.window import Window
+    w = Window(caption='Earwax Game')
+
+* Run the game you have created::
+
+    g.run(w)
+
+There are ready made :class:`~earwax.Level` classes for creating :class:`menus
+<earwax.Menu>`, and :class:`editors <earwax.Editor>`.
+"""
+
+from typing import List
+
+try:
+    import pyglet
+    pyglet.options['shadow_window'] = False
+except (ImportError, TypeError):
+    pyglet = None  # Docs are building.
+
+from . import hat_directions, story, types, utils
+from .action import Action
+from .action_map import ActionMap
+from .ambiance import Ambiance
+from .config import Config, ConfigValue
+from .configuration import EarwaxConfig
+from .credit import Credit
+from .dialogue_tree import DialogueLine, DialogueTree
+from .die import Die
+from .editor import Editor
+from .event_matcher import EventMatcher
+from .game import Game, GameNotRunning
+from .game_board import GameBoard, NoSuchTile
+from .level import IntroLevel, Level
+from .mapping import (Box, BoxBounds, BoxLevel, BoxTypes, CurrentBox, Door,
+                      MapEditor, MapEditorContext, NearestBox, NotADoor,
+                      Portal)
+from .menus import (ActionMenu, ConfigMenu, FileMenu, Menu, MenuItem,
+                    ReverbEditor, TypeHandler, UnknownTypeError)
+from .mixins import DismissibleMixin, DumpLoadMixin, TitleMixin
+from .networking import (
+    AlreadyConnected, AlreadyConnecting, ConnectionStates, NetworkConnection,
+    NetworkingConnectionError, NotConnectedYet)
+from .point import Point, PointDirections
+from .promises import (Promise, PromiseStates, StaggeredPromise,
+                       ThreadedPromise, staggered_promise)
+from .reverb import Reverb
+from .rumble_effects import RumbleEffect, RumbleSequence, RumbleSequenceLine
+from .sound import (AlreadyDestroyed, BufferCache, BufferDirectory, NoCache,
+                    Sound, SoundError, SoundManager)
+from .speech import tts
+from .task import IntervalFunction, Task, TaskFunction
+from .track import Track, TrackTypes
+from .vault_file import IncorrectVaultKey, VaultFile
+from .walking_directions import walking_directions
+
+# This next import is intentionally separated from the rest, to prevent
+# circular imports.
+if True:
+    from . import cmd
+
+__all__: List[str] = [
+    # General modules:
+    'cmd', 'hat_directions', 'story', 'types', 'utils',
+    # action.py:
+    'Action',
+    # action_map.py:
+    'ActionMap',
+    # ambiance.py:
+    'Ambiance',
+    # config.py:
+    'Config', 'ConfigValue',
+    # configuration.py:
+    'EarwaxConfig',
+    # credit.py:
+    'Credit',
+    # dialogue_tree.py:
+    'DialogueLine', 'DialogueTree',
+    # die.py:
+    'Die',
+    # editor.py:
+    'Editor',
+    # event_matcher.py:
+    'EventMatcher',
+    # game.py:
+    'Game', 'GameNotRunning',
+    # game_board.py:
+    'GameBoard', 'NoSuchTile',
+    # level.py:
+    'IntroLevel', 'Level',
+    # mapping/__init__.py:
+    'Box', 'BoxBounds', 'BoxLevel', 'BoxTypes', 'CurrentBox', 'Door',
+    'MapEditor', 'MapEditorContext', 'NearestBox', 'NotADoor', 'Portal',
+    # menus/__init__.py:
+    'ActionMenu', 'ConfigMenu', 'FileMenu', 'Menu', 'MenuItem', 'ReverbEditor',
+    'TypeHandler', 'UnknownTypeError',
+    # mixins.py:
+    'DismissibleMixin', 'DumpLoadMixin', 'TitleMixin',
+    # networking.py:
+    'AlreadyConnected', 'AlreadyConnecting', 'ConnectionStates',
+    'NetworkConnection', 'NetworkingConnectionError', 'NotConnectedYet',
+    # point.py:
+    'Point', 'PointDirections',
+    # promises/__init__.py:
+    'Promise', 'PromiseStates', 'StaggeredPromise', 'ThreadedPromise',
+    'staggered_promise',
+    # reverb.py:
+    'Reverb',
+    # rumble_effects.py:
+    'RumbleEffect', 'RumbleSequence', 'RumbleSequenceLine',
+    # sound.py:
+    'AlreadyDestroyed', 'BufferCache', 'BufferDirectory',
+    'NoCache', 'Sound', 'SoundError', 'SoundManager',
+    # speech.py:
+    'tts',
+    # task.py:
+    'IntervalFunction', 'Task', 'TaskFunction',
+    # track.py:
+    'Track', 'TrackTypes',
+    # vault_file.py:
+    'IncorrectVaultKey', 'VaultFile',
+    # walking_directions.py:
+    'walking_directions',
+]
---
+++ packages/earwax/earwax-2026.2.5-py3-none-any.whl/earwax/menus/action_menu.py
@@ -0,0 +1,158 @@
+"""Provides the ActionMenu class."""
+
+from typing import Tuple
+
+from ..action import Action
+from ..hat_directions import DEFAULT, DOWN, LEFT, RIGHT, UP
+from ..pyglet import key, mouse
+from ..types import ActionFunctionType, OptionalGenerator
+from .menu import Menu
+
+
+class ActionMenu(Menu):
+    """A menu to show a list of actions and their associated triggers.
+
+    You can use this class with any game, like so::
+
+        from earwax import Game, Level, ActionMenu
+        from pyglet.window import Window, key
+        w = Window(caption='Test Game')
+        g = Game()
+        l = Level()
+        @l.action('Show actions', symbol=key.SLASH, modifiers=key.MOD_SHIFT)
+        def actions_menu():
+            '''Show an actions menu.'''
+            a = ActionMenu(g, 'Actions')
+            g.push_level(a)
+
+        g.push_level(l)
+        g.run(w)
+
+    Now, if you press shift and slash (a question mark on english keyboards),
+    you will get an action menu.
+
+    This code can be shortened to::
+
+        @l.action('Show actions', symbol=key.SLASH, modifiers=key.MOD_SHIFT)
+        def actions_menu():
+            '''Show an actions menu.'''
+            game.push_action_menu()
+
+    If you want to override how triggers appear in the menu, then you can
+    override :meth:`~ActionMenu.symbol_to_string` and
+    :meth:`~ActionMenu.mouse_to_string`.
+    """
+
+    def __attrs_post_init__(self) -> None:
+        """Add every action as an item."""
+        super().__attrs_post_init__()
+        if self.game.level is not None:
+            a: Action
+            for a in self.game.level.actions:
+                self.add_item(self.action_menu(a), title=a.title)
+
+    def symbol_to_string(self, action: Action) -> str:
+        """Describe how to trigger the given action with the keyboard.
+
+        Returns a string representing the symbol and modifiers needed to
+        trigger the provided action.
+
+        You can be certain that ``action.symbol is not None``.
+
+        Override this method to change how symbol triggers appear.
+
+        :param action: The action whose :attr:`~earwax.Action.symbol` attribute
+            this method will be working on.
+        """
+        s: str
+        mods: str = ''
+        if action.modifiers:
+            mods = key.modifiers_string(action.modifiers)
+        s = key.symbol_string(action.symbol)
+        if mods:
+            s = f'{mods} + {s}'
+        return s
+
+    def mouse_to_string(self, action: Action) -> str:
+        """Describe how to trigger the given action with the mouse.
+
+        Returns a string representing the mouse button and modifiers needed
+        to trigger the provided action.
+
+        You can be certain that ``action.mouse_button is not None``.
+
+        Override this method to change how mouse triggers appear.
+
+        :param action: The action whose :attr:`~earwax.Action.mouse_button`
+            attribute this method will be working on.
+        """
+        s: str
+        mods: str = ''
+        if action.modifiers:
+            mods = key.modifiers_string(action.modifiers)
+        s = mouse.buttons_string(action.mouse_button)
+        if mods:
+            s = f'{mods} + {s}'
+        return s
+
+    def hat_direction_to_string(self, direction: Tuple[int, int]) -> str:
+        """Return the given hat direction as a string."""
+        if direction == DEFAULT:
+            return 'Default'
+        elif direction == UP:
+            return 'Up'
+        elif direction == DOWN:
+            return 'Down'
+        elif direction == LEFT:
+            return 'Left'
+        elif direction == RIGHT:
+            return 'Right'
+        else:
+            return str(direction)
+
+    def action_menu(self, action: Action) -> ActionFunctionType:
+        """Show a submenu of triggers.
+
+        Override this method to change how the submenu for actions is
+        displayed.
+
+        :param action: The action to generate the menu for.
+        """
+
+        def inner() -> None:
+            func: ActionFunctionType = self.handle_action(action)
+            m: Menu = Menu(self.game, 'Triggers')
+            s: str
+            if action.symbol is not None:
+                s = self.symbol_to_string(action)
+                m.add_item(func, title='Keyboard: ' + s)
+            if action.mouse_button is not None:
+                s = self.mouse_to_string(action)
+                m.add_item(func, title='Mouse: ' + s)
+            if action.joystick_button is not None:
+                m.add_item(
+                    func, title=f'Joystick: Button {action.joystick_button}'
+                )
+            if action.hat_direction is not None:
+                d: str = self.hat_direction_to_string(action.hat_direction)
+                m.add_item(func, title=f'Joystick: {d} hat')
+            self.game.push_level(m)
+
+        return inner
+
+    def handle_action(self, action: Action) -> ActionFunctionType:
+        """Handle an action.
+
+        This method is used as the menu handler that is triggered when you
+        select a trigger to activate the current action.
+
+        :param action: The action to run.
+        """
+
+        def inner() -> OptionalGenerator:
+            """Pop the menu, and run the action."""
+            self.game.pop_level()
+            self.game.pop_level()
+            return action.run(None)
+
+        return inner
