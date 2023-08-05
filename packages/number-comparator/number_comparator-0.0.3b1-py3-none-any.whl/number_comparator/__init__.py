e84e5d8f854d5b863dfdfa958dbd9a06cd8e5f08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:14 +0000

    Add package number-comparator/0.0.3b1/number_comparator-0.0.3b1-py3-none-any.whl

---
+++ packages/number-comparator/number_comparator-0.0.3b1-py3-none-any.whl/number_comparator/__init__.py
@@ -0,0 +1,10 @@
+# The number_comparator package.
+
+# It can compare if a number is prime, pair, etc.
+
+from number_comparator.comparations import *
+from number_comparator.operations import *
+from number_comparator.data.first_prime import *
+
+__author__ = "Diego Ramirez <dr01191115@gmail.com> @DiddiLeija"
+__version__ = "0.0.3"
