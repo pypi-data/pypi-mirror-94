a0f35571d160b3de19c18c0271a10cd43321825f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:21 +0000

    Add package tgraphics/0.0.1/tgraphics-0.0.1.tar.gz

---
+++ packages/tgraphics/tgraphics-0.0.1.tar.gz/tgraphics-0.0.1/tgraphics/__init__.py
@@ -0,0 +1,5 @@
+from .screen import Screen
+from .chars import Chars
+from .sprite import Sprite
+
+from .readchar import wait_for_key
---
+++ packages/tgraphics/tgraphics-0.0.1.tar.gz/tgraphics-0.0.1/tgraphics/chars.py
@@ -0,0 +1,9 @@
+class Chars:
+    H_LINE = '─'
+    V_LINE = '│'
+    D_R_CORNER = '┘'
+    D_L_CORNER = '└'
+    U_R_CORNER = '┐'
+    U_L_CORNER = '┌'
+    FILLED_SPACE = '█'
+    SPACE = ' '
