45d0b11c4cf3f8ac8e770f3b2b98344987ce63be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:45 +0000

    Add package oncall/1.4.1/oncall-1.4.1-py3-none-any.whl

---
+++ packages/oncall/oncall-1.4.1-py3-none-any.whl/oncall/healthcheck.py
@@ -0,0 +1,43 @@
+# Copyright (c) LinkedIn Corporation. All rights reserved. Licensed under the BSD-2 Clause license.
+# See LICENSE in the project root for license information.
+
+from falcon import HTTPNotFound, HTTPInternalServerError
+from . import db
+
+
+class HealthCheck(object):
+
+    def __init__(self, config):
+        if config.get('debug') or config.get('auth').get('debug'):
+            self.dummy_status = 'GOOD'
+        else:
+            self.dummy_status = None
+            path = config.get('healthcheck_path')
+            if not path:
+                self.dummy_status = 'BAD'
+            else:
+                self.path = path
+
+    def on_get(self, req, resp):
+        if self.dummy_status:
+            status = self.dummy_status
+        else:
+            try:
+                connection = db.connect()
+                cursor = connection.cursor()
+                cursor.execute("SELECT VERSION();")
+                cursor.close()
+                connection.close()
+            except:
+                raise HTTPInternalServerError()
+            try:
+                with open(self.path) as f:
+                    status = f.readline().strip()
+            except:
+                raise HTTPNotFound()
+        resp.content_type = 'text/plain'
+        resp.body = status
+
+
+def init(application, config):
+    application.add_route('/healthcheck', HealthCheck(config))
