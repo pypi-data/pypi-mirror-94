cf96b39e4da28970c1c34c651927e9bd03b969e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:09 +0000

    Add package VideoScript/0.0.1/VideoScript-0.0.1.tar.gz

---
+++ packages/VideoScript/VideoScript-0.0.1.tar.gz/VideoScript-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='VideoScript',
+    version='0.0.1',
+    author="Joseph Ernest",
+    author_email="contact@videoscript.org",
+    description="VideoScript is a compiler for the VideoScript video-making language.",
+    long_description="No release on PyPI yet, see https://github.com/josephernest/VideoScript.",
+    url="https://github.com/josephernest/VideoScript",
+    platforms='any',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
