a7ac24ca8c5890f8fe400809228f2e80bcb4af40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:51 +0000

    Add package IslandPy/0.0.0.dev13/IslandPy-0.0.0.dev13-py3-none-any.whl

---
+++ packages/IslandPy/IslandPy-0.0.0.dev13-py3-none-any.whl/IslandPy/Render/UI/Indents.py
@@ -0,0 +1,6 @@
+class Indents:
+    def __init__(self, left: int = 0, top: int = 0, right: int = 0, bottom: int = 0) -> None:
+        self.left = left
+        self.top = top
+        self.right = right
+        self.bottom = bottom
