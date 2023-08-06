60d4169984316437decbaf0f6a2f4b269a1be622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:53 +0000

    Add package apphub/1.3.1/apphub-1.3.1-py2.py3-none-any.whl

---
+++ packages/apphub/apphub-1.3.1-py2.py3-none-any.whl/apphub/exceptions.py
@@ -0,0 +1,2 @@
+class AppHubException(Exception):
+    pass
---
+++ packages/apphub/apphub-1.3.1-py2.py3-none-any.whl/apphub/globals.py
@@ -0,0 +1,10 @@
+APPHUB_PROD_HOST = "https://apphub.swimlane.com"
+SEARCH_FIELDS_LIST = [
+    "name",
+    "version",
+    "family",
+    "owner",
+    "id",
+    "vendor",
+    "product"
+]
