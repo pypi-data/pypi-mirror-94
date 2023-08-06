03922175a11d3ef98e7c4fc7d875dc56f9e4c5db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:22 +0000

    Add package ExamplePack/0.1.0/ExamplePack-0.1.0.tar.gz

---
+++ packages/ExamplePack/ExamplePack-0.1.0.tar.gz/ExamplePack-0.1.0/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+setuptools.setup(
+    name="ExamplePack",
+    version="0.1.0",
+    author="TechGeeks",
+    author_email="whirldata@tgeeks.cf",
+    description="WhirlData",
+    url="https://github.com/pypa/whirdata",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
