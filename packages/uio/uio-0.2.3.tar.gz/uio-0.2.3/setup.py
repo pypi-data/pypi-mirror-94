9a8dfc061ee9d7a7e194dd7491de714e3a674c11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:31 +0000

    Add package uio/0.2.3/uio-0.2.3.tar.gz

---
+++ packages/uio/uio-0.2.3.tar.gz/uio-0.2.3/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['uio']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['fire>=0.4.0,<0.5.0', 'logless>=0.2.2,<0.3.0', 'runnow>=0.1.0,<0.2.0']
+
+extras_require = \
+{'AWS': ['boto3>=1.17.5,<2.0.0', 's3fs<0.5.2'],
+ 'Azure': ['azure-storage-blob>=12.7.1,<13.0.0',
+           'azure-storage-file-datalake>=12.2.3,<13.0.0']}
+
+entry_points = \
+{'console_scripts': ['uio = uio.uio:main']}
+
+setup_kwargs = {
+    'name': 'uio',
+    'version': '0.2.3',
+    'description': 'Universal IO (uio) library.',
+    'long_description': None,
+    'author': 'AJ Steers',
+    'author_email': 'aj.steers@slalom.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
