f934d8e4c58901bfe480ea157a6bbeefda6681a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:27 +0000

    Add package BlueWhale3/3.28.0/BlueWhale3-3.28.0-cp36-cp36m-macosx_10_9_x86_64.whl

---
+++ packages/BlueWhale3/BlueWhale3-3.28.0-cp36-cp36m-macosx_10_9_x86_64.whl/Orange/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM ORANGE SETUP.PY
+short_version = '3.28.0'
+version = '3.28.0'
+full_version = '3.28.0.dev0+28ac8e9'
+git_revision = '28ac8e99b102b9334cca0c5b01485981dcd0133c'
+release = False
+
+if not release:
+    version = full_version
+    short_version += ".dev"
