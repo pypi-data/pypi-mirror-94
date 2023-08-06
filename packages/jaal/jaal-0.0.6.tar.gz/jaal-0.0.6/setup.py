61563115073c513a551fdbc7fd3ab4494159c4da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:21 +0000

    Add package jaal/0.0.6/jaal-0.0.6.tar.gz

---
+++ packages/jaal/jaal-0.0.6.tar.gz/jaal-0.0.6/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jaal",
+    version="0.0.6",
+    author="Mohit Mayank",
+    author_email="mohitmayank1@gmail.com",
+    description="jaal - your interactive network visualizer dashboard",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/imohitmayank/jaal",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    package_data={'': ['datasets/*', 'assest/logo.png', 'datasets/got/*']},
+    include_package_data=True,
+    install_requires=['dash', 'visdcc', 'pandas', 'dash_core_components', 'dash_html_components', 'dash_bootstrap_components'],
+)
