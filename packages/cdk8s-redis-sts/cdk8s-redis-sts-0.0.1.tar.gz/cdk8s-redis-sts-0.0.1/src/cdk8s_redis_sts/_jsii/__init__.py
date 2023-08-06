e2b72fac5870d80734948934324d569c5a803eff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:18 +0000

    Add package cdk8s-redis-sts/0.0.1/cdk8s-redis-sts-0.0.1.tar.gz

---
+++ packages/cdk8s-redis-sts/cdk8s-redis-sts-0.0.1.tar.gz/cdk8s-redis-sts-0.0.1/src/cdk8s_redis_sts/_jsii/__init__.py
@@ -0,0 +1,23 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import cdk8s._jsii
+import cdk8s_plus_17._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk8s-redis-sts", "0.0.1", __name__[0:-6], "cdk8s-redis-sts@0.0.1.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
