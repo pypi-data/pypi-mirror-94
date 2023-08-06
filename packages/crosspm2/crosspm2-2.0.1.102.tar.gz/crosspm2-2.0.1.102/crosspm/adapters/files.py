377c05e301dd651e9ddc8c2c587f3dbc90fc5e4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:11 +0000

    Add package crosspm2/2.0.1.102/crosspm2-2.0.1.102.tar.gz

---
+++ packages/crosspm2/crosspm2-2.0.1.102.tar.gz/crosspm2-2.0.1.102/crosspm/adapters/files.py
@@ -0,0 +1,263 @@
+# -*- coding: utf-8 -*-
+import json
+import os
+import pathlib
+import shutil
+
+from crosspm.adapters.common import BaseAdapter
+from crosspm.helpers.exceptions import *  # noqa
+from crosspm.helpers.package import Package
+
+CHUNK_SIZE = 1024
+
+setup = {
+    "name": [
+        "files",
+    ],
+}
+
+
+# class PureFilesPath(pathlib.PurePath):
+#     """
+#     A class to work with Files paths that doesn't connect
+#     to any server. I.e. it supports only basic path
+#     operations.
+#     """
+#     _flavour = pathlib._posix_flavour
+#     __slots__ = ()
+#
+
+class FilesPath(pathlib.Path):  # , PureFilesPath):
+    def glob(self, pattern):
+        return super(FilesPath, self).glob(pattern)
+
+    def rglob(self, pattern):
+        return super(FilesPath, self).rglob(pattern)
+
+    def __new__(cls, *args, **kwargs):
+        cls = WindowsPath if os.name == 'nt' else PosixPath
+        obj = pathlib.Path.__new__(cls, *args, **kwargs)
+        return obj
+
+    @property
+    def properties(self):
+        """
+        Fetch artifact properties
+        """
+        return {}
+
+    @properties.setter
+    def properties(self, properties):
+        self.del_properties(self.properties, recursive=False)
+        self.set_properties(properties, recursive=False)
+
+    @properties.deleter
+    def properties(self):
+        self.del_properties(self.properties, recursive=False)
+
+    def set_properties(self, properties, recursive=True):
+        """
+        Adds new or modifies existing properties listed in properties
+
+        properties - is a dict which contains the property names and values to set.
+                     Property values can be a list or tuple to set multiple values
+                     for a key.
+        recursive  - on folders property attachment is recursive by default. It is
+                     possible to force recursive behavior.
+        """
+        if not properties:
+            return False
+
+        return True  # self._accessor.set_properties(self, properties, recursive)
+
+    def del_properties(self, properties, recursive=None):
+        """
+        Delete properties listed in properties
+
+        properties - iterable contains the property names to delete. If it is an
+                     str it will be casted to tuple.
+        recursive  - on folders property attachment is recursive by default. It is
+                     possible to force recursive behavior.
+        """
+        return True  # self._accessor.del_properties(self, properties, recursive)
+
+
+class PosixPath(FilesPath, pathlib.PurePosixPath):
+    __slots__ = ()
+
+
+class WindowsPath(FilesPath, pathlib.PureWindowsPath):
+    __slots__ = ()
+
+    def owner(self):
+        raise NotImplementedError("Path.owner() is unsupported on this system")
+
+    def group(self):
+        raise NotImplementedError("Path.group() is unsupported on this system")
+
+
+class Adapter(BaseAdapter):
+    def get_packages(self, source, parser, downloader, list_or_file_path):
+        _pkg_name_col = self._config.name_column
+        _packages_found = {}
+        _pkg_name_old = ""
+        for _paths in parser.get_paths(list_or_file_path, source):
+            _packages = []
+            _params_found = {}
+            _params_found_raw = {}
+            last_error = ''
+            _pkg_name = _paths['params'][_pkg_name_col]
+            if _pkg_name != _pkg_name_old:
+                _pkg_name_old = _pkg_name
+                self._log.info(
+                    '{}: {}'.format(_pkg_name,
+                                    {k: v for k, v in _paths['params'].items() if
+                                     k not in (_pkg_name_col, 'repo')}))
+            for _sub_paths in _paths['paths']:
+                self._log.info('repo: {}'.format(_sub_paths['repo']))
+                for _path in _sub_paths['paths']:
+                    _tmp_params = dict(_paths['params'])
+                    _tmp_params['repo'] = _sub_paths['repo']
+
+                    _path_fixed, _path_pattern = parser.split_fixed_pattern(_path)
+                    _repo_paths = FilesPath(_path_fixed)
+                    try:
+                        for _repo_path in _repo_paths.glob(_path_pattern):
+                            _mark = 'found'
+                            _matched, _params, _params_raw = parser.validate_path(str(_repo_path), _tmp_params)
+                            if _matched:
+                                _params_found[_repo_path] = {k: v for k, v in _params.items()}
+                                _params_found_raw[_repo_path] = {k: v for k, v in _params_raw.items()}
+                                _mark = 'match'
+                                _valid, _params = parser.validate(_repo_path.properties, 'properties', _tmp_params,
+                                                                  return_params=True)
+                                if _valid:
+                                    _mark = 'valid'
+                                    _packages += [_repo_path]
+                                    _params_found[_repo_path].update({k: v for k, v in _params.items()})
+                                    _params_found[_repo_path]['filename'] = str(_repo_path.name)
+                            self._log.debug('  {}: {}'.format(_mark, str(_repo_path)))
+                    except RuntimeError as e:
+                        try:
+                            err = json.loads(e.args[0])
+                        except Exception:
+                            err = {}
+                        if isinstance(err, dict):
+                            # TODO: Check errors
+                            # e.args[0] = '''{
+                            #                   "errors" : [ {
+                            #                     "status" : 404,
+                            #                     "message" : "Not Found"
+                            #                   } ]
+                            #                 }'''
+                            for error in err.get('errors', []):
+                                err_status = error.get('status', -1)
+                                err_msg = error.get('message', '')
+                                if err_status == 401:
+                                    msg = 'Authentication error[{}]{}'.format(err_status,
+                                                                              (': {}'.format(
+                                                                                  err_msg)) if err_msg else '')
+                                elif err_status == 404:
+                                    msg = last_error
+                                else:
+                                    msg = 'Error[{}]{}'.format(err_status,
+                                                               (': {}'.format(err_msg)) if err_msg else '')
+                                if last_error != msg:
+                                    self._log.error(msg)
+                                last_error = msg
+
+            _package = None
+            if _packages:
+                _packages = parser.filter_one(_packages, _paths['params'], _params_found)
+                if isinstance(_packages, dict):
+                    _packages = [_packages]
+
+                if len(_packages) == 1:
+                    _params_raw = _params_found_raw.get(_packages[0]['path'], {})
+                    _params_tmp = _params_found.get(_packages[0]['path'], {})
+                    _params_tmp.update({k: v for k, v in _packages[0]['params'].items() if k not in _params_tmp})
+                    _package = Package(_pkg_name, _packages[0]['path'], _paths['params'], downloader, self, parser,
+                                       _params_tmp, _params_raw)
+                    _mark = 'chosen'
+                    self._log.info('  {}: {}'.format(_mark, str(_packages[0]['path'])))
+
+                elif len(_packages) > 1:
+                    raise CrosspmException(
+                        CROSSPM_ERRORCODE_MULTIPLE_DEPS,
+                        'Multiple instances found for package [{}] not found.'.format(_pkg_name)
+                    )
+                else:
+                    # Package not found: may be error, but it could be in other source.
+                    pass
+            else:
+                # Package not found: may be error, but it could be in other source.
+                pass
+
+            if (_package is not None) or (not self._config.no_fails):
+                _added, _package = downloader.add_package(_pkg_name, _package)
+            else:
+                _added = False
+            if _package is not None:
+                _pkg_name = _package.name
+            if _added or (_package is not None):
+                if (_package is not None) or (not self._config.no_fails):
+                    if (_package is not None) or (_packages_found.get(_pkg_name, None) is None):
+                        _packages_found[_pkg_name] = _package
+
+            if _added and (_package is not None):
+                if downloader.do_load:
+                    _package.download()
+
+                    _deps_file = _package.get_file(self._config.deps_lock_file_name)
+                    if _deps_file:
+                        _package.find_dependencies(_deps_file)
+                    elif self._config.deps_file_name:
+                        _deps_file = _package.get_file(self._config.deps_file_name)
+                        if _deps_file and os.path.isfile(_deps_file):
+                            _package.find_dependencies(_deps_file)
+
+        return _packages_found
+
+    @staticmethod
+    def pkg_stat(package):
+        _stat_attr = {'ctime': 'st_atime',
+                      'mtime': 'st_mtime',
+                      'size': 'st_size'}
+        _stat_pkg = os.stat(str(package))
+        _stat_pkg = {k: getattr(_stat_pkg, v, None) for k, v in _stat_attr.items()}
+        return _stat_pkg
+
+    def download_package(self, package, dest_path):
+        dest_dir = os.path.dirname(dest_path)
+
+        if not os.path.exists(dest_dir):
+            os.makedirs(dest_dir)
+        elif os.path.exists(dest_path):
+            os.remove(dest_path)
+
+        try:
+            _stat_pkg = self.pkg_stat(package)
+            shutil.copyfile(str(package), dest_path)
+            os.utime(dest_path, (_stat_pkg['ctime'], _stat_pkg['mtime']))
+
+        except Exception as e:
+            code = CROSSPM_ERRORCODE_SERVER_CONNECT_ERROR
+            msg = 'FAILED to download package {} at url: [{}]'.format(
+                package.name,
+                str(package),
+            )
+            raise CrosspmException(code, msg) from e
+
+        return dest_path
+
+    @staticmethod
+    def get_package_filename(package):
+        if isinstance(package, FilesPath):
+            return package.name
+        return ''
+
+    @staticmethod
+    def get_package_path(package):
+        if isinstance(package, FilesPath):
+            return str(package)
+        return ''
---
+++ packages/crosspm2/crosspm2-2.0.1.102.tar.gz/crosspm2-2.0.1.102/crosspm/helpers/package.py
@@ -0,0 +1,223 @@
+# -*- coding: utf-8 -*-
+import fnmatch
+from collections import OrderedDict
+
+import hashlib
+import logging
+import os
+import shutil
+from artifactory import ArtifactoryPath
+
+# import only for TypeHint
+from dohq_common.deps_txt.deps_txt_simple import DepsTxtSimpleReader
+
+try:
+    from crosspm.helpers.downloader import Downloader  # noqa: F401
+except ImportError:
+    pass
+
+
+class Package:
+    def __init__(self, name, pkg, art_package, params, downloader, adapter, parser,
+                 params_found=None, params_found_raw=None, in_cache=False):
+        self.name = name
+        self.package_name = name
+        self.packed_path = ''
+        self.unpacked_path = ''
+        self.duplicated = False
+        self.packages = OrderedDict()
+
+        self.pkg = pkg  # type: ArtifactoryPath
+        # Someone use this internal object, do not remove  them :)
+        self._pkg = self.pkg
+
+        self.art_package = art_package
+
+        if isinstance(pkg, int):
+            if pkg == 0:
+                self._root = True
+
+        self._raw = []
+        self._root = False
+        self._params_found = {}
+        self._params_found_raw = {}
+        self._not_cached = True
+        self._log = logging.getLogger('crosspm')
+
+        self._params = params
+        self._adapter = adapter
+        self._parser = parser
+        self._downloader = downloader  # type: Downloader
+        self._in_cache = in_cache
+
+        if params_found:
+            self._params_found = params_found
+        if params_found_raw:
+            self._params_found_raw = params_found_raw
+
+    def download(self, force=False):
+        """
+        Download file containing this package.
+        :param force: Force download even if it seems file already exists
+        :return: Full path with filename of downloaded package file.
+        """
+        exists, dest_path = self._downloader.cache.exists_packed(package=self, pkg_path=self.packed_path,
+                                                                 check_stat=not self._in_cache)
+        unp_exists, unp_path = self._downloader.cache.exists_unpacked(package=self, pkg_path=self.unpacked_path)
+
+        # Если архива нет, то и кешу доверять не стоит
+        if not exists:
+            unp_exists = False
+
+        if exists and not self.packed_path:
+            self.packed_path = dest_path
+
+        if force or not exists:
+            # _packed_path = self._packed_path
+            dest_path_tmp = dest_path + ".tmp"
+            if os.path.exists(dest_path_tmp):
+                os.remove(dest_path_tmp)
+            self._adapter.download_package(self._pkg, dest_path_tmp)
+            os.rename(dest_path_tmp, dest_path)
+            self.packed_path = dest_path
+            # if not _packed_path:
+            self._not_cached = True
+        else:
+            if unp_exists and not self.unpacked_path:
+                self.unpacked_path = unp_path
+                self._not_cached = False
+
+        if self._not_cached and unp_exists:
+            shutil.rmtree(unp_path, ignore_errors=True)
+
+        return self.packed_path
+
+    def get_file(self, file_name, unpack_force=True):
+        if unpack_force:
+            self.unpack()
+        _dest_file = os.path.normpath(self.get_file_path(file_name))
+        _dest_file = _dest_file if os.path.isfile(_dest_file) else None
+        return _dest_file
+
+    def get_file_path(self, file_name):
+        _dest_file = os.path.join(self.unpacked_path, file_name)
+        return _dest_file
+
+    def find_dependencies(self, depslock_file_path, property_validate=True, deps_content=None):
+        """
+        Find all dependencies by package
+        :param depslock_file_path:
+        :param property_validate: for `root` packages we need check property, bad if we find packages from `lock` file,
+        :param deps_content: HACK for use --dependencies-content and existed dependencies.txt.lock file
+        we can skip validate part
+        :return:
+        """
+        self._raw = DepsTxtSimpleReader(depslock_file_path).package_matches
+
+        self.packages = self._downloader.get_dependency_packages(self._raw,
+                                                                 property_validate=property_validate)
+
+    def find_usedby(self, depslock_file_path, property_validate=True):
+        """
+        Find all dependencies by package
+        :param depslock_file_path:
+        :param property_validate: for `root` packages we need check property, bad if we find packages from `lock` file,
+        we can skip validate part
+        :return:
+        """
+        if depslock_file_path is None:
+            self._raw = [self._params]
+            self._raw[0]['repo'] = None
+            self._raw[0]['server'] = None
+        else:
+            self._raw = [x for x in self._downloader.common_parser.iter_packages_params(depslock_file_path)]
+        self.packages = self._downloader.get_usedby_packages({'raw': self._raw},
+                                                             property_validate=property_validate)
+
+    @property
+    def stat(self):
+        return self.art_package.pkg_stat()
+
+    def get_params(self, param_list=None, get_path=False, merged=False, raw=False):
+        """
+        Get Package params
+        :param param_list: name or list of parameters
+        :param get_path:
+        :param merged: if version splited, True return version in string
+        :param raw:
+        :return:
+        """
+        # Convert parameter name to list
+        if param_list and isinstance(param_list, str):
+            param_list = [param_list]
+        if param_list and isinstance(param_list, (list, tuple)):
+            result = {k: v for k, v in self._params_found.items() if k in param_list}
+            result.update({k: v for k, v in self._params.items() if (k in param_list and k not in result)})
+        else:
+            result = {k: v for k, v in self._params_found.items()}
+            result.update({k: v for k, v in self._params.items() if k not in result})
+        if get_path:
+            result['path'] = self.unpacked_path
+        if merged:
+            result.update(self._parser.merge_valued(result))
+        if raw:
+            result.update({k: v for k, v in self._params_found_raw.items()})
+        return result
+
+    def set_full_unique_name(self):
+        self.name = self._parser.get_full_package_name(self)
+        return self.name
+
+    def get_name_and_path(self, name_only=False):
+        if name_only:
+            return self.name
+        return self.name, self.unpacked_path
+
+    def get_none_packages(self):
+        """
+        Get packages with None (not founded), recursively
+        """
+        not_found = set()
+        for package_name, package in self.packages.items():
+            if package is None:
+                not_found.add(package_name)
+            else:
+                if package.packages:
+                    not_found = not_found | package.get_none_packages()
+        return not_found
+
+    @property
+    def all_packages(self):
+        packages = []
+        for package in self.packages.values():
+            if package:
+                packages.extend(package.all_packages)
+        packages.extend(self.packages.values())
+        return packages
+
+    def ext(self, check_ext):
+        if self._pkg:
+            if not isinstance(check_ext, (list, tuple)):
+                check_ext = [check_ext]
+            name = self._adapter.get_package_filename(self._pkg)
+            if any((fnmatch.fnmatch(name, x) or fnmatch.fnmatch(name, '*%s' % x)) for x in check_ext):
+                return True
+        return False
+
+    @property
+    def md5(self):
+        try:
+            return ArtifactoryPath.stat(self.pkg).md5
+        except AttributeError:
+            return md5sum(self.packed_path)
+
+
+def md5sum(filename):
+    """
+    Calculates md5 hash of a file
+    """
+    md5 = hashlib.md5()
+    with open(filename, 'rb') as f:
+        for chunk in iter(lambda: f.read(128 * md5.block_size), b''):
+            md5.update(chunk)
+    return md5.hexdigest()
