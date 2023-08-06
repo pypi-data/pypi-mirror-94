8b749028d37af6cbc95298805350f3fb4b76352f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:17 +0000

    Add package distkv/0.52.1/distkv-0.52.1.tar.gz

---
+++ packages/distkv/distkv-0.52.1.tar.gz/distkv-0.52.1/distkv/command/__init__.py
@@ -0,0 +1,30 @@
+# command line interface
+
+import sys
+import asyncclick as click
+
+from distkv.util import main_
+from distkv.exceptions import ClientError, ServerError
+
+
+def cmd():
+    """
+    The main command entry point, as declared in ``setup.py``.
+    """
+    try:
+        # pylint: disable=no-value-for-parameter,unexpected-keyword-arg
+        main_()
+    except (ClientError, ServerError) as err:
+        print(type(err).__name__ + ":", *err.args, file=sys.stderr)
+        sys.exit(1)
+
+
+@main_.command(
+    short_help="Import the debugger", help="Imports PDB and then continues to process arguments."
+)
+@click.argument("args", nargs=-1)
+async def pdb(args):  # safe
+    breakpoint()  # safe
+    if not args:
+        return
+    return await main.main(args)
