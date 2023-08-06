e17535f60f32b7391a510616413dd5cc187936b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:23 +0000

    Add package topaiza/0.0.5/topaiza-0.0.5.tar.gz

---
+++ packages/topaiza/topaiza-0.0.5.tar.gz/topaiza-0.0.5/topaiza/__init__.py
@@ -0,0 +1,50 @@
+import sys
+import pprint as pp
+import requests
+import json
+
+def get(url):
+    r = requests.get(url)
+    if r:
+        if(r.headers.get('content-type') == 'application/json'):
+            print(r.text)
+            # handleJson(r.text)
+        else:
+            print(r.text)
+            # handleResp(r.text)
+
+def post(url, data):
+    r = requests.post(url, data=data)
+    if r:
+        if(r.headers.get('content-type') == 'application/json'):
+            handleJson(r.text)
+        else:
+            handleResp(r.text)
+
+def massGet(urls):
+    for url in urls:
+        r = requests.get(url)
+        if r:
+            if(r.headers.get('content-type') == 'application/json'):
+                handleJson(r.text)
+            else:
+                handleResp(r.text)
+
+def massPost(urls, datas):
+    for url in urls:
+        for data in datas:
+            r = requests.post(url, data=data)
+            if r:
+                if(r.headers.get('content-type') == 'application/json'):
+                    handleJson(r.text)
+                else:
+                    handleResp(r.text)
+
+
+
+def handleJson(jsonResp):
+    parsedResponse = jsonResp.json()
+    return parsedResponse
+
+def handleResp(normalResp):
+    return normalResp
