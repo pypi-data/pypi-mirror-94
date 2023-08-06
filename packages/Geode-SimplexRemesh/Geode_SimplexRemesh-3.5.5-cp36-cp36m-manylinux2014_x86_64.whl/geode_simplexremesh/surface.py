7d4e6e269b722b3f61fbc4ac7eb31a693be8ea92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:20 +0000

    Add package Geode-SimplexRemesh/3.5.5/Geode_SimplexRemesh-3.5.5-cp36-cp36m-manylinux2014_x86_64.whl

---
+++ packages/Geode-SimplexRemesh/Geode_SimplexRemesh-3.5.5-cp36-cp36m-manylinux2014_x86_64.whl/geode_simplexremesh/surface.py
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2019 - 2021 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+from geode_common import geometry, helpers, modifier_surface, cutter_surface
+from geode_parameterization import surface
+
+from .geode_simplexremesh_py_surface import *
+
