523963b9ae716560b55a0667ef0716b990b2fd60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:34 +0000

    Add package mt-SetFile/0.0.1/mt_SetFile-0.0.1.tar.gz

---
+++ packages/mt-SetFile/mt_SetFile-0.0.1.tar.gz/mt_SetFile-0.0.1/README.md
@@ -0,0 +1,5 @@
+#mt_SetFile
+
+This is a package containing the configuration editing tools for MaxTrack.
+[Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/)
+to write content.
---
+++ packages/mt-SetFile/mt_SetFile-0.0.1.tar.gz/mt_SetFile-0.0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name="mt_SetFile",
+    version="0.0.1",
+    author="mole1000",
+    author_email="mole1000@protonmail.com",
+    description="A configuration tool to work with MetTrack and MetCal",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['PyQt5>=5', 'glob2', 'mt_UI'],
+    test_suite='tests',
+    extras_require={
+        'testing': ['pytest']
+    },
+)
+    
