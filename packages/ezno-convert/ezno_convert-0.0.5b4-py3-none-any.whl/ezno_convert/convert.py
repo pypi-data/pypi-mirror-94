53519cd4b221cddad03e166f6708cde844ad75f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:34 +0000

    Add package ezno-convert/0.0.5b4/ezno_convert-0.0.5b4-py3-none-any.whl

---
+++ packages/ezno-convert/ezno_convert-0.0.5b4-py3-none-any.whl/ezno_convert/convert.py
@@ -0,0 +1,170 @@
+import enum
+import logging
+from dataclasses import dataclass
+from os import PathLike
+from pathlib import Path
+from typing import Collection, Union, Optional
+
+from comtypes import COMError
+from comtypes.client import CreateObject
+
+from ezno_convert.common import validate_paths, multi_glob
+from ezno_convert.enums import PPT, WORD, XL, enum_types
+
+logger = logging.getLogger('NativeOfficeConverter')
+
+
+def word_convert(word_app, src: Path, dst: Path, target: WORD) -> Optional[Path]:
+    doc = word_app.Documents.Open(str(src))
+    try:
+        doc.SaveAs(str(dst), FileFormat=target.value)
+        return dst
+    finally:
+        doc.Close()
+
+
+def ppt_convert(ppt_app, src: Path, dst: Path, target: PPT) -> Optional[Path]:
+    doc = ppt_app.Presentations.Open(str(src))
+    try:
+        doc.SaveAs(str(dst), FileFormat=target.value)
+        return dst
+    finally:
+        doc.Close()
+
+
+def xl_convert(xl_app, src: Path, dst: Path, target: XL, sheets: Union[Collection, bool]) -> Optional[Path]:
+    doc = xl_app.Workbooks.Open(str(src))
+    try:
+        if sheets:
+            if sheets is True:  # Identical to True as opposed to evaluated as True - Meaning export all sheets
+                sheets = [sh.Name for sh in doc.Sheets]
+            for sheet in sheets:
+                sheet_object = doc.Sheets(sheet)
+                sheet_dst = dst.with_stem(f'{dst.stem}-{sheet_object.Name}')
+                sheet_object.ExportAsFixedFormat(target.value, str(sheet_dst))
+            return dst
+        else:
+            doc.ExportAsFixedFormat(target.value, str(dst))
+            return dst
+    finally:
+        doc.Close()
+
+
+def convert_one(
+        src: PathLike,
+        dst: Optional[PathLike] = None,
+        app_object=None,
+        target: Optional[enum_types] = None,
+        date_fmt: Optional[str] = None,
+        sheets: Union[Collection, bool] = False) -> Optional[Path]:
+    src, dst = validate_paths(src, dst, date_fmt)
+
+    app_opened_here = False
+    match = False
+    for app in (WORD, PPT, XL):
+        if src.suffix in app.extensions.value:
+            match = True
+            if app_object is None:
+                app_opened_here = True
+                app_object = CreateObject(app.app.value)
+            break
+    if not match:
+        raise ValueError(f'Unknown file extension {src.suffix} ({src})')
+
+    target = target if target else app.PDF
+
+    if app is WORD:
+        result = word_convert(app_object, src, dst, target)
+    elif app is PPT:
+        result = ppt_convert(app_object, src, dst, target)
+    elif app is XL:
+        result = xl_convert(app_object, src, dst, target, sheets)
+    else:
+        raise RuntimeError(f'Function ran without an app defined: {app} ({app_object})')
+
+    if app_opened_here:
+        try:
+            app_object.Quit()
+        finally:
+            pass
+    return next(result.parent.glob(result.name + '*'))
+
+
+@dataclass
+class BatchConverter:
+    src: Union[Collection[PathLike], PathLike]
+    dst: Optional[PathLike] = None
+    app: Optional[enum.EnumMeta] = None
+    target: Optional[enum_types] = None
+    recursive: bool = False
+    date_fmt: Optional[str] = None
+    sheets: Union[Collection, bool] = False
+
+    def __post_init__(self):
+        self.app_object = None
+
+        if isinstance(self.src, (str, PathLike)):
+            self.src = [self.src]
+
+        if self.app is None:
+            # Try to guess app if not supplied by the extension of the first src path
+            guess_apps = [app for app in (WORD, PPT, XL) if Path(tuple(self.src)[0]).suffix in app.extensions.value]
+            if guess_apps:
+                self.app = guess_apps[0]
+            else:
+                raise RuntimeError('Could not guess correct app and none was provided')
+
+        self.files = [Path(f) for f in self.src if Path(f).is_file()]
+        extensions = [x if '*' in x else '*' + x for x in self.app.extensions.value]
+        for d in (Path(f) for f in self.src if Path(f).is_dir()):
+            self.files += multi_glob(d, extensions, recursive=self.recursive)
+
+        self.files = {f for f in self.files if not f.name.startswith('~$')}
+
+        if self.dst is not None and len(self.files) > 1:
+            self.dst = Path(self.dst)
+            if not Path(self.dst).is_dir():
+                raise NotADirectoryError(f'Destination for batch conversion must be a folder (or empty) ({self.dst})')
+
+    def __len__(self):
+        return len(self.files)
+
+    def __iter__(self):
+        if self.files:
+            self.app_object = CreateObject(self.app.app.value)
+            for i, f in enumerate(self.files):
+                try:
+                    result = convert_one(f, self.dst, self.app_object, self.target, self.date_fmt, self.sheets)
+                except (COMError, FileNotFoundError, NotADirectoryError, ValueError):
+                    logger.exception(f'Failed to convert: {f}')
+                    yield None
+                else:
+                    yield result
+            self.app_object.Quit()
+
+    def execute_all(self, output: bool = False) -> list[Optional[Path]]:
+        # TODO - use wrap execution in progressbar
+        all_results = []
+        for result in self:
+            all_results.append(result)
+            if output:
+                print(f'Success: {result}' if result else 'Conversion failed, see logs for details')
+        return all_results
+
+
+@dataclass
+class WORDConverter(BatchConverter):
+    """ Alias for BatchConverter(WORD, ...) """
+    app: enum.EnumMeta = WORD
+
+
+@dataclass
+class PPTConverter(BatchConverter):
+    """ Alias for BatchConverter(PPT, ...) """
+    app: enum.EnumMeta = PPT
+
+
+@dataclass
+class XLConverter(BatchConverter):
+    """ Alias for BatchConverter(XL, ...) """
+    app: enum.EnumMeta = XL
---
+++ packages/ezno-convert/ezno_convert-0.0.5b4-py3-none-any.whl/ezno_convert/gui.py
@@ -0,0 +1,211 @@
+import logging
+from argparse import ArgumentParser
+from pathlib import Path
+from typing import Collection, Optional
+
+import wx
+
+from ezno_convert.common import DATE_FORMAT, VERSION, script_dir
+from ezno_convert.convert import WORD, PPT, XL, BatchConverter, WORDConverter, PPTConverter, XLConverter
+
+PDF = 'PDF'
+logger = logging.getLogger('NativeOfficeConverter')
+
+
+class WarningDialog(wx.MessageDialog):
+    def __init__(self, parent, message):
+        warning_style = wx.YES_NO | wx.CENTER | wx.ICON_WARNING
+        super().__init__(parent=parent, message=message, caption='Warning!', style=warning_style)
+        logger.warning(message)
+
+
+class ErrorDialog(wx.MessageDialog):
+    def __init__(self, parent, message):
+        super().__init__(parent=parent, message=message, caption='Error!', style=wx.OK | wx.CENTER | wx.ICON_ERROR)
+        logger.error(message)
+        self.ShowModal()
+
+
+class Progress(wx.ProgressDialog):
+    def __init__(self, converters: Collection[BatchConverter]):
+        super().__init__(
+            title=f'Easy Native Office Convert v{VERSION}',
+            message='Converting...',
+            maximum=len(max(converters, key=len)),
+            style=wx.PD_CAN_ABORT | wx.PD_ELAPSED_TIME | wx.PD_REMAINING_TIME | wx.PD_APP_MODAL | wx.PD_AUTO_HIDE
+        )
+        self.converters = converters
+        self.results = []  # type: list[tuple[int, Optional[Path]]]
+
+    def run(self) -> list[tuple[int, Optional[Path]]]:
+        self.Show()
+        for con_i, converter in enumerate(self.converters):
+            app_name = converter.app.app.value.split('.')[0]
+            for i, result in enumerate(converter):
+                self.results.append((i, result))
+                if isinstance(result, Path):
+                    self.Update(i, f'Running {app_name} converter... ({con_i}/{len(self.converters)})')
+                    if self.WasCancelled():
+                        self.Destroy()
+                        return self.results
+        return self.results
+
+
+class MainFrame(wx.Frame):
+    def __init__(self):
+        style = wx.MINIMIZE_BOX | wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX | wx.CLIP_CHILDREN
+        super().__init__(None, size=(550, 500), title=f'Easy Native Office Convert v{VERSION}', style=style)
+        word_choices = [item for item in dir(WORD) if not item.startswith('_')]
+        ppt_choices = [item for item in dir(PPT) if not item.startswith('_')]
+        xl_choices = [item for item in dir(XL) if not item.startswith('_')]
+        self.panel = wx.Panel(self)
+        self.path = wx.TextCtrl(self.panel)
+        self.path_select = wx.Button(self.panel, label='Browse...')
+        self.recursive = wx.CheckBox(self.panel, label='Recursively check in sub-folders')
+        self.save = wx.TextCtrl(self.panel, value=str(Path.home() / 'Documents'))
+        self.save_select = wx.Button(self.panel, label='Save to...')
+        self.use_location = wx.CheckBox(self.panel, label='Save output files in the same folder(s) as source files')
+        self.word_check = wx.CheckBox(self.panel, label=f'Word documents {WORD.extensions.value}')
+        self.ppt_check = wx.CheckBox(self.panel, label=f'Powerpoint Presentations {PPT.extensions.value}')
+        self.xl_check = wx.CheckBox(self.panel, label=f'Excel Workbooks {XL.extensions.value}')
+        self.word_fmt = wx.ComboBox(self.panel, style=wx.CB_READONLY, value=PDF, choices=word_choices)
+        self.ppt_fmt = wx.ComboBox(self.panel, style=wx.CB_READONLY, value=PDF, choices=ppt_choices)
+        self.xl_fmt = wx.ComboBox(self.panel, style=wx.CB_READONLY, value=PDF, choices=xl_choices)
+        self.date_fmt = wx.TextCtrl(self.panel, value=DATE_FORMAT)
+        self.reset_btn = wx.Button(self.panel, label='Reset settings')
+        self.execute_btn = wx.Button(self.panel, label='Start Converting...')
+        img_path = str(script_dir / 'images/ezno-banner-wide.png')
+        img = wx.Image(img_path, type=wx.BITMAP_TYPE_PNG).Rescale(500, 100).ConvertToBitmap()
+        img = wx.StaticBitmap(self.panel, bitmap=img)
+        grid = wx.GridBagSizer(10, 10)
+        grid.Add(img, pos=(0, 1), span=(1, 2), flag=wx.EXPAND)
+        grid.Add(wx.StaticText(self.panel, label='Select a folder:'), pos=(1, 1))
+        grid.Add(self.path, pos=(2, 1), flag=wx.EXPAND)
+        grid.Add(self.path_select, pos=(2, 2))
+        grid.Add(self.recursive, pos=(3, 1))
+        grid.Add(self.use_location, pos=(4, 1))
+        grid.Add(wx.StaticText(self.panel, label='Select destination folder:'), pos=(5, 1))
+        grid.Add(self.save, pos=(6, 1), flag=wx.EXPAND)
+        grid.Add(self.save_select, pos=(6, 2))
+        grid.Add(self.word_check, pos=(7, 1))
+        grid.Add(self.word_fmt, pos=(7, 2), flag=wx.EXPAND)
+        grid.Add(self.ppt_check, pos=(8, 1))
+        grid.Add(self.ppt_fmt, pos=(8, 2), flag=wx.EXPAND)
+        grid.Add(self.xl_check, pos=(9, 1))
+        grid.Add(self.xl_fmt, pos=(9, 2), flag=wx.EXPAND)
+        grid.Add(wx.StaticText(self.panel, label='Date/Time format on filenames:'), pos=(10, 1))
+        grid.Add(self.date_fmt, pos=(10, 2), flag=wx.EXPAND)
+        grid.Add(self.execute_btn, pos=(11, 1), flag=wx.EXPAND)
+        grid.Add(self.reset_btn, pos=(11, 2), flag=wx.EXPAND)
+        self.panel.SetSizer(grid)
+        self.use_location.Bind(wx.EVT_CHECKBOX, self.use_location_handler)
+        self.path_select.Bind(wx.EVT_BUTTON, self.select_path_location)
+        self.save_select.Bind(wx.EVT_BUTTON, self.select_save_location)
+        self.reset_btn.Bind(wx.EVT_BUTTON, self.reset)
+        self.execute_btn.Bind(wx.EVT_BUTTON, self.validate)
+        self.SetIcon(wx.Icon(str(script_dir / 'images/ezno-icon.png')))
+        self.reset()
+
+    def reset(self, event: Optional[wx.Event] = None):
+        if event:
+            event.Skip()
+        self.use_location.SetValue(True)
+        self.save.Disable()
+        self.save_select.Disable()
+        self.word_check.SetValue(True)
+        self.ppt_check.SetValue(True)
+        self.xl_check.SetValue(True)
+        self.date_fmt.SetValue(DATE_FORMAT)
+
+    def select_path_location(self, event: wx.Event):
+        event.Skip()
+        self.select_folder(self.path, 'Select a source folder')
+
+    def select_save_location(self, event: wx.Event):
+        event.Skip()
+        self.select_folder(self.save, 'Select a destination folder')
+
+    def select_folder(self, text_ctrl: wx.TextCtrl, message: str):
+        start = text_ctrl.GetValue()
+        start = start if start and Path(start).is_dir() else str(Path.home())
+        dir_dlg = wx.DirDialog(self, defaultPath=start, message=message)
+        if dir_dlg.ShowModal() == wx.ID_OK:
+            text_ctrl.SetValue(dir_dlg.GetPath())
+
+    def use_location_handler(self, event: wx.Event):
+        event.Skip()
+        self.save.Enable(not self.use_location.IsChecked())
+        self.save_select.Enable(not self.use_location.IsChecked())
+
+    def validate(self, event: Optional[wx.Event] = None):
+        if event:
+            event.Skip()
+
+        date_fmt = self.date_fmt.GetValue()
+        if DATE_FORMAT != date_fmt:
+            warning = 'Changing the timestamp format field could result in unintended overwrites of files!\n'
+            warning += 'Are you sure you want to continue?'
+            result = WarningDialog(self, warning).ShowModal()
+            if result != wx.ID_YES:
+                return
+
+        src = self.path.GetValue()
+        if src:
+            src = Path(src)
+        else:
+            ErrorDialog(self, 'Source path cannot be empty')
+            return
+        if not src.is_dir() and not src.is_file():
+            ErrorDialog(self, 'Source path provided is invalid')
+            return
+
+        dst = None if self.use_location.GetValue() else Path(self.save.GetValue())
+        if isinstance(dst, Path) and not dst.is_dir():
+            ErrorDialog(self, 'Destination path must be a valid directory')
+            return
+
+        kwargs = dict(src=src, dst=dst, recursive=self.recursive.GetValue(), date_fmt=date_fmt)
+
+        do_word = (self.word_check.GetValue() and src.is_dir) or (src.is_file() and src.suffix in WORD.extensions.value)
+        do_ppt = (self.ppt_check.GetValue() and src.is_dir) or (src.is_file() and src.suffix in PPT.extensions.value)
+        do_xl = (self.xl_check.GetValue() and src.is_dir) or (src.is_file() and src.suffix in XL.extensions.value)
+        converters = []
+        if do_word:
+            converters.append(WORDConverter(target=getattr(WORD, self.word_fmt.GetValue(), None), **kwargs))
+        if do_ppt:
+            converters.append(PPTConverter(target=getattr(PPT, self.ppt_fmt.GetValue(), None), **kwargs))
+        if do_xl:
+            converters.append(XLConverter(target=getattr(XL, self.xl_fmt.GetValue(), None), **kwargs))
+
+        if converters:
+            self.Destroy()
+            progress = Progress(converters)
+            results = progress.run()
+            failed = sum(1 for i, p in results if p is None)
+            success = sum(len(c) for c in progress.converters) - failed
+            message = f'Finished converting!\n{failed} item(s) failed\n{success} items converted'
+            wx.MessageDialog(progress, message, 'Done').ShowModal()
+        else:
+            ErrorDialog(self, 'Could not find any files to convert. Check your settings.')
+            return
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--path', type=Path, metavar='PATH')
+    parser.add_argument('--pdf', action='store_true')
+    args, _ = parser.parse_known_args()
+    app = wx.App()
+    frame = MainFrame()
+    if args.path and (args.path.is_file() or args.path.is_dir()):
+        frame.path.SetValue(str(args.path))
+    if args.pdf:
+        frame.validate()
+    else:
+        frame.Show()
+        frame.Center()
+    app.MainLoop()
+
+
+if __name__ == '__main__':
+    main()
