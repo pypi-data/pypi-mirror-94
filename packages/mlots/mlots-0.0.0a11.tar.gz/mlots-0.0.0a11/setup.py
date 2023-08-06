6fa43cbd250e5547e675727fdf3a475ab1532c74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:05 +0000

    Add package mlots/0.0.0a11/mlots-0.0.0a11.tar.gz

---
+++ packages/mlots/mlots-0.0.0a11.tar.gz/mlots-0.0.0a11/setup.py
@@ -0,0 +1,30 @@
+import os
+
+import setuptools
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(ROOT, 'README.md'), encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='mlots',
+    version='0.0.0a11',
+    author="Vivek Mahato",
+    author_email="vivek.mahato@ucdconnect.ie",
+    description="Machine Learning Over Time-Series: A toolkit for time-series analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/vivekmahato/mlots',
+    packages=setuptools.find_packages(),
+    py_modules=['mlots'],
+    install_requires=[
+        'tslearn', 'numpy', 'scikit-learn', 'annoy', 'hnswlib', 'sortedcollections',
+        'tqdm'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+)
