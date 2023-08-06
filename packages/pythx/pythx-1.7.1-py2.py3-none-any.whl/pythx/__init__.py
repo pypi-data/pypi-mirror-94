c6456576697d8ad9ecfe081ef2989f6263c25cd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:31 +0000

    Add package pythx/1.7.1/pythx-1.7.1-py2.py3-none-any.whl

---
+++ packages/pythx/pythx-1.7.1-py2.py3-none-any.whl/pythx/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for pythx."""
+
+__author__ = """Dominik Muhs"""
+__email__ = "dominik.muhs@consensys.net"
+__version__ = "1.7.1"
+
+from mythx_models.exceptions import MythXAPIError
+from pythx.api.client import Client
