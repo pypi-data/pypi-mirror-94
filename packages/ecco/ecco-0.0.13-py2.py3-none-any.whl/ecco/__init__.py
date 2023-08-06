8249f7eaffc605076f467486cb33cfcf4df57dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:28 +0000

    Add package ecco/0.0.13/ecco-0.0.13-py2.py3-none-any.whl

---
+++ packages/ecco/ecco-0.0.13-py2.py3-none-any.whl/ecco/__init__.py
@@ -0,0 +1,24 @@
+__version__ = '0.0.13'
+from ecco.lm import LM, MockGPT, MockGPTTokenizer
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+def from_pretrained(hf_model_id,
+                    activations=False,
+                    attention=False,
+                    hidden_states=True,
+                    activations_layer_nums=None,
+                    ):
+    if hf_model_id == "mockGPT":
+        tokenizer = MockGPTTokenizer()
+        model = MockGPT()
+    else:
+        tokenizer = AutoTokenizer.from_pretrained(hf_model_id)
+        model = AutoModelForCausalLM.from_pretrained(hf_model_id,
+                                                     output_hidden_states=hidden_states,
+                                                     output_attentions=attention)
+
+    lm_kwargs = {
+        'collect_activations_flag': activations,
+        'collect_activations_layer_nums': activations_layer_nums}
+    lm = LM(model, tokenizer, **lm_kwargs)
+    return lm
