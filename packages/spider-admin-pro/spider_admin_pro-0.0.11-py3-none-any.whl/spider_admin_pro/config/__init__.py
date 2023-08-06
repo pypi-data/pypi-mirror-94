5d0ab4867ea68873d6c1fce58afbef3280bb8f23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:44 +0000

    Add package spider-admin-pro/0.0.11/spider_admin_pro-0.0.11-py3-none-any.whl

---
+++ packages/spider-admin-pro/spider_admin_pro-0.0.11-py3-none-any.whl/spider_admin_pro/config/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+配置优先级：默认 < env环境变量 < yaml配置文件
+"""
+
+from .yaml_config import *
+
+if not os.path.exists(LOG_DIR):
+    os.mkdir(LOG_DIR)
+
+
+def resolve_log_file(filename):
+    return os.path.join(LOG_DIR, filename)
