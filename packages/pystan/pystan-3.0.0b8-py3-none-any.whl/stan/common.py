dc5a6bbbb6da9969acb42181a24b4770a78dae8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:07 +0000

    Add package pystan/3.0.0b8/pystan-3.0.0b8-py3-none-any.whl

---
+++ packages/pystan/pystan-3.0.0b8-py3-none-any.whl/stan/common.py
@@ -0,0 +1,27 @@
+"""Common routines"""
+import contextlib
+import socket
+
+import aiohttp.web
+import httpstan.app
+
+
+def unused_tcp_port():
+    s = socket.socket()
+    s.bind(("127.0.0.1", 0))
+    port = s.getsockname()[1]
+    s.close()
+    return port
+
+
+@contextlib.asynccontextmanager
+async def httpstan_server():
+    """Manage starting and stopping the httpstan HTTP server."""
+    host, port = "127.0.0.1", unused_tcp_port()
+    app = httpstan.app.make_app()
+    runner = aiohttp.web.AppRunner(app)
+    await runner.setup()
+    site = aiohttp.web.TCPSite(runner, host, port)
+    await site.start()
+    yield (host, port)
+    await runner.cleanup()
