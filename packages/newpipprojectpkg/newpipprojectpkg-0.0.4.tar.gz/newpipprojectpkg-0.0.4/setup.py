d81d4e01cf8deacef69c09667b95cdb5ad58c9d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:01 +0000

    Add package newpipprojectpkg/0.0.4/newpipprojectpkg-0.0.4.tar.gz

---
+++ packages/newpipprojectpkg/newpipprojectpkg-0.0.4.tar.gz/newpipprojectpkg-0.0.4/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['newpipprojectpkg',
+ 'newpipprojectpkg.newpipprojectpkg',
+ 'newpipprojectpkg.tests']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'newpipprojectpkg',
+    'version': '0.0.4',
+    'description': '',
+    'long_description': None,
+    'author': 'basma',
+    'author_email': 'basmaelsaify@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
