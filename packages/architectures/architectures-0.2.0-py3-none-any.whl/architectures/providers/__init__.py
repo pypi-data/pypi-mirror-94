c89ea21da25616204f45c744459aeb229af97c49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:23 +0000

    Add package architectures/0.2.0/architectures-0.2.0-py3-none-any.whl

---
+++ packages/architectures/architectures-0.2.0-py3-none-any.whl/architectures/providers/__init__.py
@@ -0,0 +1,23 @@
+# Do not modify this file directly. It is auto-generated with Python.
+
+from architectures.core import Node
+
+class _Azure(Node):
+    _provider = "azure"
+    _icon_dir = "icons/azure"
+
+class _General(Node):
+    _provider = "general"
+    _icon_dir = "icons/general"
+
+class _Gcp(Node):
+    _provider = "gcp"
+    _icon_dir = "icons/gcp"
+
+class _Aws(Node):
+    _provider = "aws"
+    _icon_dir = "icons/aws"
+
+class _Kubernetes(Node):
+    _provider = "kubernetes"
+    _icon_dir = "icons/kubernetes"
