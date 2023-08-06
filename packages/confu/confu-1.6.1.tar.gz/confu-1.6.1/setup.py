e36f6b4ef08957e838e11b9c72d52245cf893965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:41 +0000

    Add package confu/1.6.1/confu-1.6.1.tar.gz

---
+++ packages/confu/confu-1.6.1.tar.gz/confu-1.6.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['confu', 'confu.schema']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'confu',
+    'version': '1.6.1',
+    'description': 'Configuration file validation and generation',
+    'long_description': None,
+    'author': '20C',
+    'author_email': 'code@20c.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/20c/confu',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
