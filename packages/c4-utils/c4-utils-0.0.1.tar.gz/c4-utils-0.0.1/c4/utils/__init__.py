2ca1d382bdee3562ba337c165151bd4f2a4fcf65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:02 +0000

    Add package c4-utils/0.0.1/c4-utils-0.0.1.tar.gz

---
+++ packages/c4-utils/c4-utils-0.0.1.tar.gz/c4-utils-0.0.1/c4/utils/__init__.py
@@ -0,0 +1,9 @@
+"""
+Part of c4, providing test authors with re-usable set of functions.
+
+Needs to be explicitly imported as:
+
+    import c4.utils
+"""
+
+print("Placeholder for project to be opensourced soon")
---
+++ packages/c4-utils/c4-utils-0.0.1.tar.gz/c4-utils-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='c4-utils',
+    description='c4.utils',
+    author='QA Automation',
+    version='0.0.1',
+    packages=['c4.utils'],
+    classifiers=['Programming Language :: Python :: 3'],
+)
