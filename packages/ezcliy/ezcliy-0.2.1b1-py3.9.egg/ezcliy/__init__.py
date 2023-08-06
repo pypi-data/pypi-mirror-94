dbb32ea86cd124e2227cbc7258cd7c2a94f3b25a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:44 +0000

    Add package ezcliy/0.2.1b1/ezcliy-0.2.1b1-py3.9.egg

---
+++ packages/ezcliy/ezcliy-0.2.1b1-py3.9.egg/ezcliy/__init__.py
@@ -0,0 +1,7 @@
+"""Framework for creating cli tools."""
+
+from ezcliy.commands import Command
+from ezcliy.parameters import Flag, KeyVal
+from ezcliy.positional import Positional
+
+__version__ = '0.2.1b1'  # https://stackoverflow.com/a/24293364/9256726
