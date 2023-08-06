d68a5590cb3ec5a9b7a1e995868bcabde2f14c44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:32 +0000

    Add package video-to-transcript/0.0.2/video_to_transcript-0.0.2.tar.gz

---
+++ packages/video-to-transcript/video_to_transcript-0.0.2.tar.gz/video_to_transcript-0.0.2/video_to_transcript/video_to_transcript.py
@@ -0,0 +1,19 @@
+import speech_recognition as sr
+import moviepy.editor as mp
+import os
+
+
+# Function to convert
+# Pass in the path to the file
+def extracter(file_name):                     
+    r=sr.Recognizer()
+    clip=mp.VideoFileClip(file_name)
+    conv_file="audio"
+    clip.audio.write_audiofile("audio.wav")
+    with sr.AudioFile("audio.wav") as file:
+        audio_data=r.record(file)
+        text=r.recognize_google(audio_data)
+        os.remove("audio.wav")
+        return text
+    
+
