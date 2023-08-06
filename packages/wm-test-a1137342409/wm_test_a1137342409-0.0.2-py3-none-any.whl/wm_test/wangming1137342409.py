2626c05c9271a682d35649878789eb6e22d1b607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:48 +0000

    Add package wm-test-a1137342409/0.0.2/wm_test_a1137342409-0.0.2-py3-none-any.whl

---
+++ packages/wm-test-a1137342409/wm_test_a1137342409-0.0.2-py3-none-any.whl/wm_test/wangming1137342409.py
@@ -0,0 +1,14 @@
+"""
+我是注释行
+"""
+def print_lists(this_list):
+    """
+    把多重列表零散化
+    :param this_list:11
+    :return:
+    """
+    for lists in this_list:
+        if isinstance(lists,list):
+            print_lol(lists)
+        else:
+            print(lists)
