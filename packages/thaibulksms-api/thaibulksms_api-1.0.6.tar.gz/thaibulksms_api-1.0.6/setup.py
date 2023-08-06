5abc0cc10fca2f5634dbc7e8adcd33e16538acfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:01 +0000

    Add package thaibulksms-api/1.0.6/thaibulksms_api-1.0.6.tar.gz

---
+++ packages/thaibulksms-api/thaibulksms_api-1.0.6.tar.gz/thaibulksms_api-1.0.6/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='thaibulksms_api',
+    version='1.0.6',
+    description='Thaibulksms API for send sms',
+    author='thaibulksms_api',
+    author_email='apichart@1moby.com',
+    packages=['thaibulksms_api'],
+    license="thaibulksms",
+    #   url='https://github.com/morangesolution/bahttext',
+    #   download_url='https://github.com/morangesolution/bahttext/tarball/master',
+)
