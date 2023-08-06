38736f0420b8a054c26eb0dd1da8d63047e27d80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:36 +0000

    Add package aoa/3.1/aoa-3.1.tar.gz

---
+++ packages/aoa/aoa-3.1.tar.gz/aoa-3.1/aoa/model_utility/model_templates/python/sklearn/model_modules/evaluation.py
@@ -0,0 +1,53 @@
+import pandas as pd
+from sklearn import metrics
+from sklearn.externals import joblib
+import json
+
+
+def save_plot(title):
+    import matplotlib.pyplot as plt
+
+    plt.title(title)
+    fig = plt.gcf()
+    filename = title.replace(" ", "_").lower()
+    fig.savefig('artifacts/output/{}'.format(filename), dpi=500)
+
+
+def evaluate(data_conf, model_conf, **kwargs):
+
+    predict_df = pd.read_csv(data_conf['location'])
+    features = 'sepallength,sepalwidth,petallength,petalwidth'.split(',')
+    X_test = predict_df.loc[:, features]
+    y_test = predict_df['class']
+
+    model = joblib.load('artifacts/input/iris_knn.joblib')
+
+    y_pred = model.predict(X_test)
+
+    evaluation = {
+        'Accuracy': '{:.2f}'.format(metrics.accuracy_score(y_test, y_pred)),
+        'Recall': '{:.2f}'.format(metrics.recall_score(y_test, y_pred)),
+        'Precision': '{:.2f}'.format(metrics.precision_score(y_test, y_pred)),
+        'f1-score': '{:.2f}'.format(metrics.f1_score(y_test, y_pred))
+    }
+
+    with open("artifacts/output/metrics.json", "w+") as f:
+        json.dump(evaluation, f)
+
+    metrics.plot_confusion_matrix(model, X_test, y_test)
+    save_plot('Confusion Matrix')
+
+    metrics.plot_roc_curve(model, X_test, y_test)
+    save_plot('ROC Curve')
+
+    print("Evaluation complete...")
+
+
+# Uncomment this code if you want to deploy your model as a Web Service (Real-time / Interactive usage)
+# class ModelScorer(object):
+#    def __init__(self, config=None):
+#        self.model = joblib.load('artifacts/input/iris_knn.joblib')
+#
+#    def predict(self, data):
+#        return self.model.predict([data])
+#
