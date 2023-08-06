628daf896d472b69fdf43ab82b593078cacf9aba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:26 +0000

    Add package pdflab/0.0.1/pdflab-0.0.1.tar.gz

---
+++ packages/pdflab/pdflab-0.0.1.tar.gz/pdflab-0.0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="pdflab",
+    version="0.0.1",
+    author="polyhedral",
+    url="https://github.com/principia1910/pdflab",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
