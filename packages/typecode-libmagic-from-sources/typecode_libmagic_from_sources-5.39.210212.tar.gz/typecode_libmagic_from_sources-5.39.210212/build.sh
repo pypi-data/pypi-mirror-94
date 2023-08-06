5b352176c43388202953b2d08dbbca8280b1707f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:39 +0000

    Add package typecode-libmagic-from-sources/5.39.210212/typecode_libmagic_from_sources-5.39.210212.tar.gz

---
+++ packages/typecode-libmagic-from-sources/typecode_libmagic_from_sources-5.39.210212.tar.gz/typecode_libmagic_from_sources-5.39.210212/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) nexB Inc. http://www.nexb.com/ - All rights reserved.
+#
+
+set -e
+cd lib-src/
+rm -rf file-5.39/
+tar -xf file-5.39.tar.gz
+cd file-5.39/
+./configure
+make
+cp src/.libs/libmagic.so.1.0.0  ../../src/typecode_libmagic_from_sources/lib/libmagic.so
+cp magic/magic.mgc ../../src/typecode_libmagic_from_sources/data/
+cd ..
+rm -rf file-5.39/
