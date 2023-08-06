fac606f1ef7c08d7e42cd21b7e4c98e97fb8b96f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:28 +0000

    Add package pdrepr/0.1.0/pdrepr-0.1.0-py3-none-any.whl

---
+++ packages/pdrepr/pdrepr-0.1.0-py3-none-any.whl/pdrepr/__init__.py
@@ -0,0 +1,13 @@
+"""
+Create DataFrame string representations that can be used to create new DataFrames.
+"""
+
+
+from ._pdrepr import pdrepr
+
+
+__author__ = 'Daniel Hjertholm'
+__version__ = '0.1.0'
+
+
+__all__ = [pdrepr]
