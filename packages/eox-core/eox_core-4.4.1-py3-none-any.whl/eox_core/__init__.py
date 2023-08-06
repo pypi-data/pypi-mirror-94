264f762b044d1d10c7963ae20dfbee0854f5bbcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:08 +0000

    Add package eox-core/4.4.1/eox_core-4.4.1-py3-none-any.whl

---
+++ packages/eox-core/eox_core-4.4.1-py3-none-any.whl/eox_core/__init__.py
@@ -0,0 +1,4 @@
+"""
+Init for main eox-core app
+"""
+__version__ = '4.4.1'
---
+++ packages/eox-core/eox_core-4.4.1-py3-none-any.whl/eox_core/urls.py
@@ -0,0 +1,16 @@
+""" urls.py """
+
+from django.conf.urls import include, url
+
+from eox_core import views
+from eox_core.api_schema import docs_ui_view
+
+app_name = 'eox_core'  # pylint: disable=invalid-name
+
+urlpatterns = [  # pylint: disable=invalid-name
+    url(r'^eox-info$', views.info_view),
+    url(r'^api/', include('eox_core.api.urls', namespace='eox-api')),
+    url(r'^data-api/', include('eox_core.api.data.v1.urls', namespace='eox-data-api')),
+    url(r'^management/', include('eox_core.cms.urls', namespace='eox-course-management')),
+    url(r'^api-docs/$', docs_ui_view, name='apidocs-ui'),
+]
