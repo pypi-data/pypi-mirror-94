9edf02b326b99c0b6ac51ce80f665fc979be26ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:19 +0000

    Add package mlm-task-for-contextual-embedding/0.12.0/mlm-task-for-contextual-embedding-0.12.0.tar.gz

---
+++ packages/mlm-task-for-contextual-embedding/mlm-task-for-contextual-embedding-0.12.0.tar.gz/mlm-task-for-contextual-embedding-0.12.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='mlm-task-for-contextual-embedding',
+    packages=find_packages(),
+    version='0.12.0',
+    description='a machine learning project for mlm task for contextual embedding',
+    author='Simon Meoni',
+    license='MIT',
+    install_requires=requirements,
+    entry_points='''
+    [console_scripts]
+    camembert_mlm_ft=src.models.camembert_mlm_ft:main
+    make_dataset=src.data.make_dataset:main
+    ''',
+    package_data={
+        "packagename": [
+            "log_config.ini"
+        ]},
+)
