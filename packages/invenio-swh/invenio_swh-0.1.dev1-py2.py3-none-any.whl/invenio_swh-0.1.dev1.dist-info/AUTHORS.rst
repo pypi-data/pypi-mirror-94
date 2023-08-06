6ab4fc2811bfe5a7d9f916b85c4358e4b406abab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:46 +0000

    Add package invenio-swh/0.1.dev1/invenio_swh-0.1.dev1-py2.py3-none-any.whl

---
+++ packages/invenio-swh/invenio_swh-0.1.dev1-py2.py3-none-any.whl/invenio_swh-0.1.dev1.dist-info/AUTHORS.rst
@@ -0,0 +1,13 @@
+..
+    Copyright (C) 2020 CERN
+    Copyright (C) 2020 Cottage Labs LLP.
+
+    invenio-swh is free software; you can redistribute it and/or modify it
+    under the terms of the MIT License; see LICENSE file for more details.
+
+Authors
+=======
+
+Support for onward deposit of software artifacts to Software Heritage
+
+- Cottage Labs LLP <info@inveniosoftware.org>
---
+++ packages/invenio-swh/invenio_swh-0.1.dev1-py2.py3-none-any.whl/invenio_swh/version.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN
+# Copyright (C) 2020 Cottage Labs LLP.
+#
+# invenio-swh is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Version information for invenio-swh.
+
+This file is imported by ``invenio_swh.__init__``,
+and parsed by ``setup.py``.
+"""
+
+__version__ = '0.1.dev1'
