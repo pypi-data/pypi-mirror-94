2e7700fd843f8923d45ef30f1c068c698738ba4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:28 +0000

    Add package edecs/1.0/edecs-1.0.tar.gz

---
+++ packages/edecs/edecs-1.0.tar.gz/edecs-1.0/edecs/models/__init__.py
@@ -0,0 +1,10 @@
+from .event import Event
+from .message import Message
+
+from .entity import Entity
+from .component import Component
+from .system import System
+
+from .world import World
+
+from .exceptions import *
