84d26f1bc54b91fb782a32666222f2cb81765915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:44 +0000

    Add package koinput/0.2.0a1/koinput-0.2.0a1-py3-none-any.whl

---
+++ packages/koinput/koinput-0.2.0a1-py3-none-any.whl/koinput/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '0.2.0a1'
+__title__ = 'koinput'
+__author__ = 'k0per'
+__copyright__ = 'Copyright 2021-present k0per'
+
+from koinput.menu import Menu
+from koinput.inputs import int_input, float_input
+
+__all__ = [
+    'int_input',
+    'float_input',
+    'Menu'
+]
