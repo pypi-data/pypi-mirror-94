14121b24210d33cb7c672a0f142eabbdf4e3ef73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:55 +0000

    Add package pyarma/0.500.0/pyarma-0.500.0-cp38-cp38-manylinux2014_x86_64.whl

---
+++ packages/pyarma/pyarma-0.500.0-cp38-cp38-manylinux2014_x86_64.whl/pyarma/share/Armadillo/CMake/ArmadilloConfig.cmake
@@ -0,0 +1,16 @@
+# - Config file for the Armadillo package
+# It defines the following variables
+#  ARMADILLO_INCLUDE_DIRS - include directories for Armadillo
+#  ARMADILLO_LIBRARY_DIRS - library directories for Armadillo (normally not used!)
+#  ARMADILLO_LIBRARIES    - libraries to link against
+
+# Tell the user project where to find our headers and libraries
+set(ARMADILLO_INCLUDE_DIRS "/tmp/pip-req-build-1grf8p1r/_skbuild/linux-x86_64-3.8/cmake-install/src/pyarma/include")
+set(ARMADILLO_LIBRARY_DIRS "/tmp/pip-req-build-1grf8p1r/_skbuild/linux-x86_64-3.8/cmake-install/src/pyarma/lib64")
+
+# Our library dependencies (contains definitions for IMPORTED targets)
+include("/tmp/pip-req-build-1grf8p1r/_skbuild/linux-x86_64-3.8/cmake-install/src/pyarma/share/Armadillo/CMake/ArmadilloLibraryDepends.cmake")
+
+# These are IMPORTED targets created by ArmadilloLibraryDepends.cmake
+set(ARMADILLO_LIBRARIES armadillo)
+
