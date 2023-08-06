f7b7b3a0954c08daf80a297570c2b6a42d850920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:57 +0000

    Add package bdata/6.2.0/bdata-6.2.0.tar.gz

---
+++ packages/bdata/bdata-6.2.0.tar.gz/bdata-6.2.0/bdata/__init__.py
@@ -0,0 +1,10 @@
+from bdata.bdata import bdata,life
+from bdata.bjoined import bjoined
+from bdata.bmerged import bmerged
+
+import os
+
+__all__ = ['bdata','bjoined','bmerged']
+__version__ = '6.2.0'
+__author__ = 'Derek Fujimoto'
+_mud_data = os.path.join(os.environ['HOME'],'.bdata')
