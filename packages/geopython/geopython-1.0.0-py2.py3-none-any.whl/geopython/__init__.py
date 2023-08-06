8443c6fafeba57f0bd671b947f355399b357dba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:08 +0000

    Add package geopython/1.0.0/geopython-1.0.0-py2.py3-none-any.whl

---
+++ packages/geopython/geopython-1.0.0-py2.py3-none-any.whl/geopython/__init__.py
@@ -0,0 +1,61 @@
+# =================================================================
+#
+# Authors: Tom Kralidis <tomkralidis@gmail.com>
+#
+# Copyright (c) 2021 Tom Kralidis
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# =================================================================
+
+__version__ = '1.0.0'
+
+s = """
+Website: https://geopython.github.io
+
+GitHub: https://github.com/geopython
+
+Chat: https://gitter.im/geopython/Lobby
+
+Workshops
+---------
+
+The Geopython workshop: https://geopython.github.io/geopython-workshop
+pycsw workshop: https://github.com/geopython/pycsw-workshop
+
+Projects
+--------
+
+pycsw: https://pycsw.org
+PyWPS: https://pywps.org
+pygeoapi: https://pygeoapi.io
+OWSLib: https://geopython.github.io/OWSLib
+GeoHealthCheck: https://geohealthcheck.org
+Stetl: https://www.stetl.org
+pygeometa: https://geopython.github.io/pygeometa
+GeoUsage: https://github.com/geopython/GeoUsage
+pycql: https://github.com/geopython/pycql
+CadTools: https://github.com/geopthon/CadTools
+geolinks: https://github.com/geopython/geolinks
+"""
+
+print(s)
