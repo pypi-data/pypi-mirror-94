e16bcae526d080367914dfb701c295827e1adb0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:36 +0000

    Add package pobm/1.0.4/pobm-1.0.4-py2.py3-none-any.whl

---
+++ packages/pobm/pobm-1.0.4-py2.py3-none-any.whl/pobm/obm/complex.py
@@ -0,0 +1,233 @@
+import numpy as np
+from lempel_ziv_complexity import lempel_ziv_complexity
+from scipy import integrate, stats
+import warnings
+
+from pobm._ErrorHandler import _check_shape_, _check_len_ApEn_, WrongParameter
+from pobm._ResultsClasses import ComplexityMeasuresResults
+
+
+class ComplexityMeasures:
+    """
+    Class that calculates complexity features from SpO2 time series.
+    """
+
+    def __init__(self, CTM_Threshold: float = 0.25, DFA_Window: int = 20, M_Sampen: int = 3, R_Sampen: float = 0.2,
+                 M_ApEn: int = 2, R_ApEn: float = 0.25):
+        """
+        :param CTM_Threshold: Radius of Central Tendency Measure.
+        :type CTM_Threshold: float, optional
+        :param DFA_Window: Length of window to calculate DFA biomarker.
+        :type DFA_Window: int, optional
+        :param M_Sampen: Embedding dimension to compute SampEn.
+        :type M_Sampen: int, optional
+        :param R_Sampen: Tolerance to compute SampEn.
+        :type R_Sampen: float, optional
+        :param M_ApEn: Embedding dimension to compute ApEn.
+        :type M_ApEn: int, optional
+        :param R_ApEn: Tolerance to compute ApEn.
+        :type R_ApEn: float, optional
+        """
+
+        if DFA_Window <= 0:
+            raise WrongParameter("DFA_Window should be strictly positive")
+        if M_Sampen <= 0:
+            raise WrongParameter("M_Sampen should be strictly positive")
+        if R_Sampen <= 0:
+            raise WrongParameter("R_Sampen should be strictly positive")
+        if M_ApEn <= 0:
+            raise WrongParameter("DFA_Window should be strictly positive")
+        if R_ApEn <= 0:
+            raise WrongParameter("R_ApEn should be strictly positive")
+
+        self.CTM_Threshold = CTM_Threshold
+        self.DFA_Window = DFA_Window
+        self.M_Sampen = M_Sampen
+        self.R_Sampen = R_Sampen
+        self.M_ApEn = M_ApEn
+        self.R_ApEn = R_ApEn
+
+    def compute(self, signal) -> ComplexityMeasuresResults:
+        """
+        Computes all the biomarkers of this category.
+        :param signal: 1-d array, of shape (N,) where N is the length of the signal
+        :return: ComplexityMeasuresResults class containing the following features:
+            * ApEn: Approximate Entropy.
+            * LZ: Lempel-Ziv complexity.
+            * CTM: Central Tendency Measure.
+            * SampEn: Sample Entropy.
+            * DFA: Detrended Fluctuation Analysis.
+        Example:
+        
+        .. code-block:: python
+            from pobm.obm.complex import ComplexityMeasures
+            # Initialize the class with the desired parameters
+            complexity_class = ComplexityMeasures(CTM_Threshold=0.25, DFA_Window=20, M_Sampen=3, R_Sampen=0.2, M_ApEn=2, R_ApEn=0.25)
+            
+            # Compute the biomarkers
+            results_complexity = complexity_class.compute(spo2_signal)
+        """
+        _check_shape_(signal)
+        warnings.filterwarnings("ignore", category=RuntimeWarning)
+        return ComplexityMeasuresResults(self.comp_apen(signal), self.comp_lz(signal),
+                                         self.comp_ctm(signal),
+                                         self.comp_sampen(signal),
+                                         self.comp_dfa(signal))
+
+    def comp_apen(self, signal):
+        """
+        Compute the approximate entropy, according to [1]_
+        :param signal: 1-d array, of shape (N,) where N is the length of the signal
+        :return: ApEn (float)
+        .. [1] Pincus, S. M. Approximate entropy as a measure of system complexity. Proc. Natl. Acad. Sci. U. S. A. 88, 2297–2301 (1991).
+        """
+        signal = np.array(signal)
+        signal = signal[np.logical_not(np.isnan(signal))]
+
+        phi_m = self.__apen(self.M_ApEn, signal)
+        phi_m1 = self.__apen(self.M_ApEn + 1, signal)
+        with np.errstate(invalid='ignore'):
+            res = phi_m - phi_m1
+        return res
+
+    def __apen(self, m, signal):
+        """
+        Help function of CompApEn
+        """
+        N = len(signal)
+        r = self.R_ApEn
+        _check_len_ApEn_(N, m)
+        C = np.zeros(shape=(N - m + 1))
+
+        res = [signal[i:i + m] for i in range(0, N - m + 1)]
+        for i in range(0, N - m + 1):
+            C[i] = np.nansum(self.__dist(res[i], res, r)) / (N - m + 1)
+
+        phi_m = np.nansum(np.log(C)) / (N - m + 1)
+        return phi_m
+
+    def __dist(self, window1, window2, r):
+        """
+        Help function of CompApEn
+        """
+        window1 = np.array(window1)
+        window2 = np.array(window2)
+
+        with np.errstate(invalid='ignore'):
+            return np.nanmax(abs(window1 - window2), axis=1) < r
+
+    def comp_lz(self, signal, dual_quantization=True):
+        """
+        Compute Lempel-Ziv, according to [2]_
+        :param signal: 1-d array, of shape (N,) where N is the length of the signal
+        :param dual_quantization: if True, the signal is converted into a binary signal, according to the original algorithm  [2]_. If False, the signal is quantized in 4 levels.
+        :return: LZ (float)
+        .. [2] Lempel, A. & Ziv, J. On the Complexity of Finite Sequences. IEEE Trans. Inf. Theory 22, 75–81 (1976).
+        """
+        signal = np.array(signal)
+
+        if dual_quantization is True:
+            median = np.nanmedian(signal)
+            bool_median = signal > median
+            byte = [str(int(b == np.bool(True))) for b in bool_median]
+        else:
+            quantile1, median, quantile3 = np.quantile(signal, 0.25), np.quantile(signal, 0.5), np.quantile(signal, 0.75)
+            byte = np.empty(shape=signal.shape, dtype=str)
+            byte[signal <= quantile1] = "0"
+            byte[(signal > quantile1) & (signal <= median)] = "1"
+            byte[(signal > median) & (signal <= quantile3)] = "2"
+            byte[signal > quantile3] = "3"
+
+        sequence = ''.join(byte)
+
+        sub_strings = set()
+
+        ind = 0
+        inc = 1
+        while True:
+            if ind + inc > len(sequence):
+                break
+            sub_str = sequence[ind: ind + inc]
+            if sub_str in sub_strings:
+                inc += 1
+            else:
+                sub_strings.add(sub_str)
+                ind += inc
+                inc = 1
+        return len(sub_strings)
+
+    def comp_ctm(self, signal):
+        """
+        Compute CTM, according to [3]_
+        :param signal: 1-d array, of shape (N,) where N is the length of the signal
+        :return: CTM (float)
+        .. [3] Cohen, M. E., Hudson, D. L. & Deedwania, P. C. Applying continuous chaotic modeling to cardiac signal analysis. IEEE Eng. Med. Biol. Mag. 15, 97–102 (1996).
+        """
+        res = 0
+        for i in range(len(signal) - 2):
+            res += self.__d_ctm(i, self.CTM_Threshold, signal)
+        return res / (len(signal) - 2)
+
+    def __d_ctm(self, i, p, signal):
+        """
+        Help function of CompCTM
+        """
+        if np.sqrt(((signal[i + 2] - signal[i + 1]) ** 2) + ((signal[i + 1] - signal[i]) ** 2)) < p:
+            return 1
+        return 0
+
+    def comp_sampen(self, signal):
+        """
+        Compute the sample entropy, according to [4]_
+        :param signal: 1-d array, of shape (N,) where N is the length of the signal
+        :return: SampEn (float)
+        .. [4] Richman, J. S. & Moorman, J. R. Physiological time-series analysis using approximate entropy and sample entropy. Am J Physiol-Heart C 278, H2039–H2049 (2000).
+        """
+        N = len(signal)
+        m = self.M_Sampen
+        r = self.R_Sampen
+
+        # Split time series and save all templates of length m
+        xmi = np.array([signal[i: i + m] for i in range(N - m)])
+        xmj = np.array([signal[i: i + m] for i in range(N - m + 1)])
+
+        # Save all matches minus the self-match, compute B
+        with np.errstate(invalid='ignore'):
+            B = np.sum([np.sum(np.abs(xmii - xmj).max(axis=1) <= r) - 1 for xmii in xmi])
+
+        # Similar for computing A
+        m += 1
+        xm = np.array([signal[i: i + m] for i in range(N - m + 1)])
+
+        with np.errstate(invalid='ignore'):
+            A = np.sum([np.sum(np.abs(xmi - xm).max(axis=1) <= r) - 1 for xmi in xm])
+
+        # Return SampEn
+        return -np.log(A / B)
+
+    def comp_dfa(self, signal):
+        """
+        Compute DFA, Detrended Fluctuation Analysis according to [5]_
+        :param signal: 1-d array, of shape (N,) where N is the length of the signal
+        :return: DFA (float)
+        .. [5] Peng, C. ‐K., Havlin, S., Stanley, H. E. & Goldberger, A. L. Quantification of scaling exponents and crossover phenomena in nonstationary heartbeat time series. Chaos An Interdiscip. J. Nonlinear Sci. 5, 82–87 (1995).
+        """
+        n = self.DFA_Window
+        y = integrate.cumtrapz(signal - np.nanmean(signal))
+        least_square = np.zeros(len(y))
+
+        i = 0
+        while i < len(y):
+            if i + n > len(y):
+                n = len(y) - i
+            if n == 1:
+                least_square[-1] = y[-1]
+                break
+            x = np.array(range(0, n))
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore", category=RuntimeWarning)
+                slope, intercept, _, _, _ = stats.linregress(x, y[i:i + n])
+            least_square[i:i + n] = slope * x + intercept
+            i += n
+
+        return np.sqrt(np.nansum((y - least_square) ** 2) / len(y))
---
+++ packages/pobm/pobm-1.0.4-py2.py3-none-any.whl/pobm/obm/periodicity.py
@@ -0,0 +1,195 @@
+import numpy as np
+from scipy.signal import hamming, welch
+import warnings
+
+from pobm._ErrorHandler import _check_shape_, _check_fragment_PRSA_, WrongParameter
+from pobm._ResultsClasses import PRSAResults, PSDResults
+
+
+class PRSAMeasures:
+    """
+    Class that calculates PRSA features from SpO2 time series.
+    """
+
+    def __init__(self, PRSA_Window: int = 10, K_AC: int = 2):
+        """
+
+        :param PRSA_Window: Fragment duration of PRSA.
+        :type PRSA_Window: int, optional
+        :param K_AC: Number of values to shift when computing autocorrelation
+        :type K_AC: int, optional
+        """
+
+        if PRSA_Window <= 0:
+            raise WrongParameter("DI_Window should be strictly positive")
+
+        self.PRSA_Window = PRSA_Window
+        self.K_AC = K_AC
+
+    def compute(self, signal) -> PRSAResults:
+        """
+        Computes all the biomarkers of this category.
+
+        :param signal: 1-d array, of shape (N,) where N is the length of the signal
+        :return: PRSAResults class containing the following features:
+
+            * PRSAc: PRSA capacity.
+            * PRSAad: PRSA amplitude difference.
+            * PRSAos: PRSA overall slope.
+            * PRSAsb: PRSA slope before the anchor point.
+            * PRSAsa: PRSA slope after the anchor point.
+            * AC: Autocorrelation.
+
+        Example:
+        
+        .. code-block:: python
+        
+            from pobm.obm.periodicity import PRSAMeasures
+
+            # Initialize the class with the desired parameters
+            prsa_class = PRSAMeasures(PRSA_Window=10, K_AC=2)
+        
+            # Compute the biomarkers
+            results_PRSA = prsa_class.compute(spo2_signal)
+
+        """
+        _check_shape_(signal)
+
+        warnings.filterwarnings("ignore", category=RuntimeWarning)
+        d = self.PRSA_Window
+        _check_fragment_PRSA_(d)
+        anchor_points = []
+        anchor_found = False
+
+        for i in range(len(signal)):
+            if i < d:
+                continue
+            if len(signal) - i < d:
+                continue
+            if signal[i - 1] > signal[i]:
+                anchor_found = True
+                anchor_points.append(signal[i - d:i + d])
+
+        if anchor_found is False:
+            PRSA_features = PRSAResults(0, 0, 0, 0, 0, np.correlate(signal, signal, "same")[self.K_AC])
+        else:
+            anchor_points = np.array(anchor_points)
+            windows = np.zeros(2 * d)
+
+            for i in range(2 * d):
+                windows[i] = np.nansum(anchor_points[:, i]) / len(anchor_points)
+
+            PRSA_features = PRSAResults((windows[d] + windows[d + 1] - windows[d - 1] - windows[d - 2]) / 4,
+                                        np.nanmax(windows) - np.nanmin(windows),
+                                        float(np.polyfit(range(2 * d), windows, 1)[0]),
+                                        float(np.polyfit(range(d), windows[0:d], 1)[0]),
+                                        float(np.polyfit(range(d), windows[d:], 1)[0]),
+                                        np.correlate(windows, windows, "same")[self.K_AC])
+
+        return PRSA_features
+
+
+class PSDMeasures:
+    """
+    Class that calculates PSD features from SpO2 time series.
+    """
+
+    def __init__(self, frequency_low_threshold: float = 0.014, frequency_high_threshold: float = 0.033):
+        """
+
+        :param frequency_low_threshold: Low threshold for the PSD_band biomarker.
+        :type frequency_low_threshold: float, optional
+        :param frequency_high_threshold: High threshold for the PSD_band biomarker.
+        :type frequency_high_threshold: float, optional
+        """
+
+        if frequency_low_threshold >= frequency_high_threshold:
+            raise WrongParameter("frequency_low_threshold should be lower than frequency_high_threshold")
+
+        self.frequency_low = frequency_low_threshold
+        self.frequency_high = frequency_high_threshold
+
+    def compute(self, signal) -> PSDResults:
+        """
+        Computes all the biomarkers of this category.
+
+        :param signal: The SpO2 signal, of shape (N,)
+
+        :return: PSDResults class containing the following features:
+        
+            * PSD_total: The amplitude of the spectral signal.
+            * PSD_band: The amplitude of the signal multiplied by a band-pass filter in the desired band.
+            * PSD_ratio: The ratio between PSD_total and PSD_band.
+            * PDS_peak: The max value of the FFT into the desired band.
+
+
+        Example:
+        
+        .. code-block:: python
+
+            from pobm.obm.periodicity import PSDMeasures
+
+            # Initialize the class with the desired parameters
+            psd_class = PSDMeasures()
+            
+            # Compute the biomarkers
+            results_PSD = psd_class.compute(spo2_signal)
+
+        """
+        _check_shape_(signal)
+
+        signal = np.array(signal)
+        signal = signal[np.logical_not(np.isnan(signal))]
+
+        freq, signal_fft = self.__get_psd(signal)
+        amplitude_signal = np.sqrt((signal_fft.real ** 2) + (signal_fft.imag ** 2))
+
+        # taking only positive frequencies, since the signal is real.
+        freq = freq[0:int(len(freq) / 2)]
+        amplitude_signal = amplitude_signal[0:int(len(amplitude_signal) / 2)]
+
+        # Taking the spectral signal in the relevant band
+        amplitude_bp = self.__get_bandpass(amplitude_signal, freq, self.frequency_low, self.frequency_high)
+
+        if len(amplitude_bp) == 0:
+            return PSDResults(np.nansum(amplitude_signal), 0, 0, 0)
+        return PSDResults(np.nansum(amplitude_signal), np.nansum(amplitude_bp),
+                          np.nansum(amplitude_bp) / np.nansum(amplitude_signal),
+                          float(np.nanmax(amplitude_bp)))
+
+    @staticmethod
+    def __get_bandpass(signal, freq, lower_f, higher_f):
+        """
+        Helper function, to get the amplitude within the desired band.
+
+        :param signal: The amplitude signal, of shape (L,)
+        :param freq: Array of frequencies, of shape (L,)
+        :param lower_f: The lower frequency of the band
+        :param higher_f: The higher frequency of the band
+        :return: The amplitude within the band
+        """
+        amplitude_bp = signal[lower_f < freq]
+        freq_bp = freq[lower_f < freq]
+
+        amplitude_bp = amplitude_bp[freq_bp < higher_f]
+        return amplitude_bp
+
+    @staticmethod
+    def __get_psd(signal):
+        """
+        Helper function, compute the PSD
+        
+        :param signal: The SpO2 signal, of shape (N,)
+        :return:
+        freq: array of frequencies, of shape (L,)
+        signal_fft: The PSD of the signal, of shape (L,)
+        """
+        # N = len(signal)
+        # w = hamming(N)
+        # signal_fft = np.fft.fft(signal * w) / N
+        # freq = np.fft.fftfreq(signal.shape[-1])
+
+        freq, signal_fft = welch(signal, window="hamming")
+        signal_fft = signal_fft / len(signal)
+
+        return freq, signal_fft
