25cb655f974512c6d93325ffe0116de6564a6cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:38 +0000

    Add package json-easy/1.1/json_easy-1.1.tar.gz

---
+++ packages/json-easy/json_easy-1.1.tar.gz/json_easy-1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='json_easy',
+    version='1.1',
+    description='Json DB\'s made easy',
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url='https://github.com/ConnorTippets/json-easy/',
+    author='meiscool466',
+    license='MIT',
+    packages=["json_easy"],
+    python_requires=">=3.8",
+    keywords=["json", "db", "easy"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3 :: Only",
+        
+    ],
+)
