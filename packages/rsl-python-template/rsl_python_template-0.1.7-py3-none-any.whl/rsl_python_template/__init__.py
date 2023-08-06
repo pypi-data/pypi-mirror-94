83e93dbe01cd59a3405e5204422fecdfc0449a20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:38 +0000

    Add package rsl-python-template/0.1.7/rsl_python_template-0.1.7-py3-none-any.whl

---
+++ packages/rsl-python-template/rsl_python_template-0.1.7-py3-none-any.whl/rsl_python_template/__init__.py
@@ -0,0 +1,4 @@
+raise UserWarning(
+    "Using PyPi with pip is not a supported installation method for this project yet. In the meantime"
+    "time please refer to the project README.md for details on how to install this project."
+)
