a86c50e5d0393ed581060adda1d43d2c9b3bb205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:53 +0000

    Add package alexapy/1.24.0/AlexaPy-1.24.0-py3-none-any.whl

---
+++ packages/alexapy/AlexaPy-1.24.0-py3-none-any.whl/alexapy/__init__.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#  SPDX-License-Identifier: Apache-2.0
+"""
+Python Package for controlling Alexa devices (echo dot, etc) programmatically.
+
+For more details about this api, please refer to the documentation at
+https://gitlab.com/keatontaylor/alexapy
+"""
+try:
+    from importlib_metadata import version
+except ModuleNotFoundError:
+    from importlib.metadata import version
+from .alexaapi import AlexaAPI
+from .alexalogin import AlexaLogin
+from .alexawebsocket import WebsocketEchoClient
+from .alexaproxy import AlexaProxy
+from .errors import (
+    AlexapyConnectionError,
+    AlexapyLoginCloseRequested,
+    AlexapyLoginError,
+    AlexapyPyotpInvalidKey,
+)
+from .helpers import hide_email, hide_serial, obfuscate
+
+__version__ = version("alexapy")
+
+__all__ = [
+    "AlexaLogin",
+    "AlexaAPI",
+    "AlexapyConnectionError",
+    "AlexapyLoginCloseRequested",
+    "AlexapyLoginError",
+    "AlexaProxy",
+    "AlexapyPyotpInvalidKey",
+    "WebsocketEchoClient",
+    "hide_email",
+    "hide_serial",
+    "obfuscate",
+    "__version__",
+]
