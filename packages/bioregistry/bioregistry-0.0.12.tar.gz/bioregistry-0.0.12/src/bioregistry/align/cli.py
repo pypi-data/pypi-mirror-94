feb2b3f1f35c8618db39089c942e00e6b86fe92a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:11 +0000

    Add package bioregistry/0.0.12/bioregistry-0.0.12.tar.gz

---
+++ packages/bioregistry/bioregistry-0.0.12.tar.gz/bioregistry-0.0.12/src/bioregistry/align/cli.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+"""CLI for alignment."""
+
+import click
+
+from .bioportal import BioPortalAligner
+from .go import GoAligner
+from .miriam import align_miriam
+from .n2t import align_n2t
+from .obofoundry import align_obofoundry
+from .ols import align_ols
+from .wikidata import align_wikidata
+from ..utils import secho
+
+__all__ = [
+    'align',
+]
+
+
+@click.command()
+def align():
+    """Align all external registries."""
+    secho('Aligning MIRIAM')
+    align_miriam()
+
+    secho('Aligning N2T')
+    align_n2t()
+
+    secho('Aligning OBO Foundry')
+    align_obofoundry()
+
+    secho('Aligning OLS')
+    align_ols()
+
+    secho('Aligning Wikidata')
+    align_wikidata()
+
+    secho('Aligning GO')
+    GoAligner.align()
+
+    secho('Aligning BioPortal')
+    BioPortalAligner.align()
+
+
+if __name__ == '__main__':
+    align()
---
+++ packages/bioregistry/bioregistry-0.0.12.tar.gz/bioregistry-0.0.12/src/bioregistry/external/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Workflows for downloading and aligning various external registries."""
+
+from .bioportal import get_bioportal  # noqa:F401
+from .go import get_go  # noqa:F401
+from .miriam import get_miriam, get_miriam_df  # noqa:F401
+from .n2t import get_n2t  # noqa:F401
+from .obofoundry import get_obofoundry, get_obofoundry_df  # noqa:F401
+from .ols import get_ols, get_ols_df  # noqa:F401
+from .wikidata import get_wikidata_registry  # noqa:F401
