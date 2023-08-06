68641093715e9edc86a97c8c1cf1673a1afa41ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:07 +0000

    Add package omniblack.email-to-chat/0.2.1/omniblack.email_to_chat-0.2.1-py3-none-any.whl

---
+++ packages/omniblack.email-to-chat/omniblack.email_to_chat-0.2.1-py3-none-any.whl/omniblack/email_to_chat/internal_sink.py
@@ -0,0 +1,36 @@
+from .abc import InputSink, MessageType, LocalMessage, Message
+from textwrap import dedent
+
+message_body = dedent("""
+    An unhandled execution occurred while trying to send a message.
+    A log message has been written to standard out on this host.
+""").strip()
+
+
+class InternalMessage(LocalMessage):
+    pass
+
+
+class Internal(InputSink):
+    def report_exception(self, exc: Exception, failed_msg: Message):
+        print('Exception reviced')
+        msg = LocalMessage(
+            type=MessageType.error,
+            program='Email To Chat',
+            body=(
+                ('Message', message_body),
+                ('Original User', failed_msg.user),
+                ('Original Host', failed_msg.host),
+                ('Original Program', failed_msg.program),
+                ('Error Message', f'`{repr(exc)}`'),
+            ),
+        )
+
+        self.queue.put_nowait(msg)
+
+    # There is no setup work needed for the Internal Message queue
+    async def start(self):
+        pass
+
+    async def stop(self):
+        pass
