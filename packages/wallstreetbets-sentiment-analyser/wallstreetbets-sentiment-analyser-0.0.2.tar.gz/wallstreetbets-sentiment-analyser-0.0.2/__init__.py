dbd33d5386fa29e31f8bb687c8024b88efef2dbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:42 +0000

    Add package wallstreetbets-sentiment-analyser/0.0.2/wallstreetbets-sentiment-analyser-0.0.2.tar.gz

---
+++ packages/wallstreetbets-sentiment-analyser/wallstreetbets-sentiment-analyser-0.0.2.tar.gz/wallstreetbets-sentiment-analyser-0.0.2/__init__.py
@@ -0,0 +1,6 @@
+def run(number_of_posts: int = 100, 
+        reddit_sub: str = "wallstreetbets"):
+    print(f"Starting extraction of latest {number_of_posts} {reddit_sub} posts")
+    return {}
+
+
