fd8c11b9b3d6d5685eb9bf0b0547de07bb2cf09a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:23 +0000

    Add package alphazeta.warden/0.9.0.0/alphazeta.warden-0.9.0.0.tar.gz

---
+++ packages/alphazeta.warden/alphazeta.warden-0.9.0.0.tar.gz/alphazeta.warden-0.9.0.0/setup.py
@@ -0,0 +1,49 @@
+
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+current_version = '0.9.0.0'
+
+with open("src/warden/static/config/version.txt", "w") as text_file:
+    print(f"{current_version}", file=text_file)
+
+reqs = [
+    'Flask-Login',
+    'flask_apscheduler',
+    'flask_mail',
+    'libusb1',
+    'pandas',
+    'numpy',
+    'PySocks',
+    'requests',
+    'urllib3',
+    'simplejson',
+    'jsonify'
+]
+
+setup(
+    name="alphazeta.warden",
+    version=current_version,
+    author="Alpha Zeta",
+    author_email="alphaazeta@protonmail.com",
+    description="Private Portfolio Tool - Specter Server Edition",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pxsocs/specter_warden",
+    include_package_data=True,
+    package_dir={"": "src"},
+    packages=find_packages(),
+    install_requires=reqs,
+    setup_requires=reqs,
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Framework :: Flask",
+    ],
+    python_requires=">=3.6",
+)
