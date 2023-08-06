0147ff85cd1428667b7c4ec7386b9606916d0c3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:45 +0000

    Add package rio-cogeo/2.1.2/rio-cogeo-2.1.2.tar.gz

---
+++ packages/rio-cogeo/rio-cogeo-2.1.2.tar.gz/rio-cogeo-2.1.2/rio_cogeo/errors.py
@@ -0,0 +1,13 @@
+"""Rio-Cogeo Errors and Warnings."""
+
+
+class DeprecationWarning(UserWarning):
+    """Rio-cogeo module deprecations warning."""
+
+
+class LossyCompression(UserWarning):
+    """Rio-cogeo module Lossy compression warning."""
+
+
+class IncompatibleBlockRasterSize(UserWarning):
+    """Rio-cogeo module incompatible raster block/size warning."""
