e0544401ae1d06b46431e9a9c5673947230009df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:06 +0000

    Add package pyQuASAR-genotype/0.1.29/pyQuASAR_genotype-0.1.29.tar.gz

---
+++ packages/pyQuASAR-genotype/pyQuASAR_genotype-0.1.29.tar.gz/pyQuASAR_genotype-0.1.29/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pyQuASAR_genotype',
+    version='0.1.29',
+    author='Anthony Aylward',
+    author_email='aaylward@eng.ucsd.edu',
+    description='Command line tool for genotyping with QuASAR',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/anthony-aylward/pyQuASAR_genotype.git',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    install_requires=['pyQuASAR', 'seqalign', 'wasp_map'],
+    entry_points={
+        'console_scripts': ['pyQuASAR-genotype=pyQuASAR_genotype.genotype:main']
+    }
+)
