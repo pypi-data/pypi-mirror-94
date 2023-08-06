6c24c0efb7416fb4f33449acecee694dbe5b8104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:30 +0000

    Add package cogdb/2.0.1a0/cogdb-2.0.1a0.tar.gz

---
+++ packages/cogdb/cogdb-2.0.1a0.tar.gz/cogdb-2.0.1a0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cogdb',
+      version='2.0.1-alpha',
+      description='Embedded Graph Database',
+      url='http://github.com/arun1729/cog',
+      author='Arun Mahendra',
+      author_email='arunm3.141@gmail.com',
+      license='MIT',
+      packages=['cog'],
+      zip_safe=False)
