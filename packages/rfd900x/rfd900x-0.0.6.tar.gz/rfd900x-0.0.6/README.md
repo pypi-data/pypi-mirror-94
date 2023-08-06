c224eb979b7a705db7b7b964454033fa76137ce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:57 +0000

    Add package rfd900x/0.0.6/rfd900x-0.0.6.tar.gz

---
+++ packages/rfd900x/rfd900x-0.0.6.tar.gz/rfd900x-0.0.6/README.md
@@ -0,0 +1,2 @@
+# rfd900x
+Python module for configuring and controlling RFD-900x series radios
---
+++ packages/rfd900x/rfd900x-0.0.6.tar.gz/rfd900x-0.0.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name             = 'rfd900x',
+    packages         = ['rfd900x'],
+    version          = '0.0.6',
+    description      = 'Python module for configuring RFD900x series radios',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    author           = 'Power_Broker',
+    author_email     = 'gitstuff2@gmail.com',
+    url              = 'https://github.com/PowerBroker2/rfd900x',
+    download_url     = 'https://github.com/PowerBroker2/rfd900x/archive/0.0.6.tar.gz',
+    keywords         = ['RFD', 'RFD-900', 'RFD-900x', 'RFD-900+', 'RFD 900', 'RFD 900x', 'RFD 900+'],
+    classifiers      = [],
+    install_requires = ['pyserial']
+)
