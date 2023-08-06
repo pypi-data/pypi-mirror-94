a72e8f66921d76eefd91c406385537d9a3f67949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:42 +0000

    Add package pytailor/0.3.1/pytailor-0.3.1-py3-none-any.whl

---
+++ packages/pytailor/pytailor-0.3.1-py3-none-any.whl/pytailor/exceptions.py
@@ -0,0 +1,22 @@
+class PytailorBaseError(Exception):
+    pass
+
+
+class BackendResponseError(PytailorBaseError):
+    pass
+
+
+class BackendResourceError(PytailorBaseError):
+    pass
+
+
+class DAGError(PytailorBaseError):
+    pass
+
+
+class ParameterizationError(PytailorBaseError):
+    pass
+
+
+class AuthenticationError(PytailorBaseError):
+    pass
