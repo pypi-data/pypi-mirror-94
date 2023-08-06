d02e9a11e7e392232b76a64ac87d912c14641c2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:34 +0000

    Add package grpc-extensions/0.0.4/grpc-extensions-0.0.4.tar.gz

---
+++ packages/grpc-extensions/grpc-extensions-0.0.4.tar.gz/grpc-extensions-0.0.4/README.md
@@ -0,0 +1,19 @@
+# Python Grpc库
+
+## install
+
+### pip
+
+```shell
+pip install python-grpc
+```
+
+## source code
+
+```shell
+git clone https://github.com/go-zs/python-grpc
+or
+git@github.com:go-zs/python-grpc.git
+
+python setup.py install
+```
---
+++ packages/grpc-extensions/grpc-extensions-0.0.4.tar.gz/grpc-extensions-0.0.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="grpc-extensions",
+    version="0.0.4",
+    python_requires=">=3.7.9",
+    description="Python Grpc Library",
+    long_description="python grpc library, provide various tools and middlewares",
+    license="MIT Licence",
+    url="https://github.com/go-zs/python-grpc",
+    author="haoyinqianzui",
+    author_email="810909753@qq.com",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[
+        "grpcio>=1.32.0",
+        "protobuf>=3.14.0",
+        "grpcio-opentracing>=1.1.4",
+        "grpc-interceptor>=0.13.0",
+    ],
+)
