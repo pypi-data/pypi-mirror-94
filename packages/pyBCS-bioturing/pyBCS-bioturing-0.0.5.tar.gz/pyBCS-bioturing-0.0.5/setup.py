284abdb45cec04520fc85c04cef3ad5100ac86c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:31 +0000

    Add package pyBCS-bioturing/0.0.5/pyBCS-bioturing-0.0.5.tar.gz

---
+++ packages/pyBCS-bioturing/pyBCS-bioturing-0.0.5.tar.gz/pyBCS-bioturing-0.0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyBCS-bioturing",
+    version="0.0.5",
+    author="BioTuring",
+    author_email="support@bioturing.com",
+    description="Create BioTuring Compressed Study (bcs) file",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.bioturing.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
