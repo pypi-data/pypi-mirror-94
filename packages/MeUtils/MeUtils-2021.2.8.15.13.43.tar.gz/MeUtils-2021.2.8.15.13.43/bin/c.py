5ef1ab49288b9a97624b0e345ecdb769abb01c80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:00 +0000

    Add package MeUtils/2021.2.8.15.13.43/MeUtils-2021.2.8.15.13.43.tar.gz

---
+++ packages/MeUtils/MeUtils-2021.2.8.15.13.43.tar.gz/MeUtils-2021.2.8.15.13.43/bin/c.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : MeUtils.
+# @File         : c
+# @Time         : 2021/2/7 9:19 下午
+# @Author       : yuanjie
+# @WeChat       : 313303303
+# @Software     : PyCharm
+# @Description  : 
+
+
+def f1():
+    print('c')
+
+import b
+import a
