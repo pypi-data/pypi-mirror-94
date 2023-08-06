08fa1abe9117f57ba5a869ce65a853727ec60169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:54 +0000

    Add package autosegl3/0.9.0/autosegl3-0.9.0-py2.py3-none-any.whl

---
+++ packages/autosegl3/autosegl3-0.9.0-py2.py3-none-any.whl/autosegl3/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for L3 Auto-segmentation Tool."""
+
+__author__ = """Ralph Brecheisen"""
+__email__ = 'ralph.brecheisen@gmail.com'
+__version__ = '0.9.0'
+
+from .train.train import TrainL3
+from . import models
+from . import utils
