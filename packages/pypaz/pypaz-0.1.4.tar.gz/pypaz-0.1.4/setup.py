4fe15a81ce391b9cfdc99cff9f777d9be789c017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:44 +0000

    Add package pypaz/0.1.4/pypaz-0.1.4.tar.gz

---
+++ packages/pypaz/pypaz-0.1.4.tar.gz/pypaz-0.1.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools import find_packages
+import paz
+
+setup(name='pypaz',
+      version=paz.__version__,
+      description='Perception for Autonomous Systems',
+      author='Octavio Arriaga',
+      author_email='octavio.arriaga@dfki.de',
+      license='MIT',
+      install_requires=['opencv-python', 'tensorflow', 'numpy'],
+      packages=find_packages()
+      )
