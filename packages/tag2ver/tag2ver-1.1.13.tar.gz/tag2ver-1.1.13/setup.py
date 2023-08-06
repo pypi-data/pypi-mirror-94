9d9aba716f7a3d2799b70868f2001658b76338de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:18 +0000

    Add package tag2ver/1.1.13/tag2ver-1.1.13.tar.gz

---
+++ packages/tag2ver/tag2ver-1.1.13.tar.gz/tag2ver-1.1.13/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+import tag2ver
+
+
+def read_text(file_name: str):
+    with open(file_name, "r") as fh:
+        return fh.read()
+
+
+setuptools.setup(
+    name='tag2ver',
+    version='1.1.13',
+    url=tag2ver.__repository__,
+    license=read_text('LICENSE'),
+    author=tag2ver.__author__,
+    author_email='howard.lovatt@gmail.com',
+    description=tag2ver.HELP_TEXT,
+    # read_text('README.md') doesn't work because PyPi can't render README.md - PyPI bug.
+    long_description=f'See <{tag2ver.__repository__}> for detailed description.\n',  # read_text('README.md'),
+    long_description_content_type='text/markdown',
+    py_modules=['tag2ver'],
+    platforms=['any'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
