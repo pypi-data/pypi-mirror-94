46f93ece92faf62b51574340bcd01ce9e3fe22d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:28 +0000

    Add package legacystamps/1.0.4/legacystamps-1.0.4.tar.gz

---
+++ packages/legacystamps/legacystamps-1.0.4.tar.gz/legacystamps-1.0.4/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_desc = f.read()
+
+setuptools.setup(
+    name='legacystamps',
+    version='1.0.4',
+    author='Frits Sweijen',
+    author_email='frits.sweijen@gmail.com',
+    description='Small module to retrieve cutouts from the Legacy Survey.',
+    scripts=['legacystamps/legacystamps.py'],
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    url='https://github.com/tikk3r/legacystamps',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=2.7.5',
+    install_requires=[
+        'requests',
+        'tqdm',
+    ],
+)
