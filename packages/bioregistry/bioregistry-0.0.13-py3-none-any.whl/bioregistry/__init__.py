02e3d65ce9134ff799d176a5fc838b584e386f90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:11 +0000

    Add package bioregistry/0.0.13/bioregistry-0.0.13-py3-none-any.whl

---
+++ packages/bioregistry/bioregistry-0.0.13-py3-none-any.whl/bioregistry/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Extract registry information."""
+
+from .resolve import (  # noqa
+    get, get_email, get_example, get_format, get_name, get_pattern, get_pattern_re, get_version, get_versions,
+    is_deprecated, namespace_in_lui, normalize_prefix, validate,
+)
+from .utils import read_bioregistry  # noqa
