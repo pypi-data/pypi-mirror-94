5497c83ce68c28e78d587339054561e257437b16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:56 +0000

    Add package scoredb-sdk/0.1.1/scoredb_sdk-0.1.1-py3-none-any.whl

---
+++ packages/scoredb-sdk/scoredb_sdk-0.1.1-py3-none-any.whl/scoredb/client/__init__.py
@@ -0,0 +1,36 @@
+from typing import List
+
+from requests import Session
+
+from ._baseapi import BaseAPI
+from ._pagination import Pagination
+from .studentdb import StudentDBAPI
+from .users import UsersAPI
+from ..models import User
+
+
+class Client:
+    user: User
+
+    def __init__(self, token: str, base_url='https://api.scoredb.tech'):
+        if not token:
+            raise ValueError('An API token is required.')
+        self.session = Session()
+        self.session.headers.update({
+            'Accept': 'application/json',
+            'Authorization': f'Bearer {token}',
+            'User-Agent': 'sdk/python'
+        })
+        self.base_url = base_url
+        while self.base_url[-1] == '/':
+            self.base_url = self.base_url[:-1]
+
+        # Register APIs
+        self.users = UsersAPI(self.session, self.base_url)
+        self.studentdb = StudentDBAPI(self.session, self.base_url)
+
+        # Get current user
+        self.user = self.users.get_current_user()
+
+    def apis(self) -> List[BaseAPI]:
+        return [self.users, self.studentdb]
