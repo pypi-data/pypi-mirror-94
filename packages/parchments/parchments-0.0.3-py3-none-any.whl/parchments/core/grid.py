5452c80bbe3b978cf9cb38591739cbb2949d4bbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:28 +0000

    Add package parchments/0.0.3/parchments-0.0.3-py3-none-any.whl

---
+++ packages/parchments/parchments-0.0.3-py3-none-any.whl/parchments/core/grid.py
@@ -0,0 +1,40 @@
+from parchments.core.row import Row
+from parchments.core.period import Period
+from parchments.core.choices import PERIOD_ITERATION_CHOICES, OVER_PERIOD_ITERATION_CHOICES
+
+
+class Grid:
+
+    def __init__(self, row_index, period_iteration='month', over_period_iteration='year'):
+        if period_iteration in PERIOD_ITERATION_CHOICES:
+            self.period_iteration = period_iteration
+        else:
+            raise SyntaxError('Invalid period iteration choices %s' % PERIOD_ITERATION_CHOICES)
+
+        if over_period_iteration in OVER_PERIOD_ITERATION_CHOICES:
+            self.over_period_iteration = over_period_iteration
+        else:
+            raise SyntaxError('Invalid layer iteration choices %s' % OVER_PERIOD_ITERATION_CHOICES)
+
+        self.row_index = row_index
+
+        self.row_dict = dict()
+        for row in self.row_index:
+            self.row_dict[row[0]] = Row(row[0], row[1], row[2], self.period_iteration, self.over_period_iteration)
+
+    def add_period(self, datetime, value_list):
+        period = Period(datetime, self.period_iteration)
+        if type(value_list) is list:
+            for loop_index, row in enumerate(self.row_index):
+                self.row_dict[row[0]].create_block(period, value_list[loop_index])
+                self.row_dict[row[0]].update_row()
+
+    def to_dict(self):
+        grid_dict = dict()
+        grid_dict['row_data'] = dict()
+        for row in self.row_index:
+            # Todo: This is a bit lazy for assignment of block order list
+            grid_dict['row_data'][row[0]] = self.row_dict[row[0]].as_dict()
+            grid_dict['column_index'] = self.row_dict[row[0]].block_order_list
+        return grid_dict
+
---
+++ packages/parchments/parchments-0.0.3-py3-none-any.whl/parchments/tests/grid_tests.py
@@ -0,0 +1,35 @@
+import unittest
+import parchments
+import datetime
+import decimal
+
+test_index = (
+    ('goats', 'int', 0),
+    ('price', 'dollar', 2),
+    ('value', 'percentage', 4),
+    ('names', 'string', 0),
+)
+
+test_grid = parchments.Grid(test_index)
+
+
+class TestGrid(unittest.TestCase):
+
+    def test_grid_add_period(self, period=datetime.datetime.now(), index=[1, 22.2, 0.70, 'bob']):
+        try:
+            test_grid.add_period(datetime.datetime.now(), [1, 22.2, 0.70, 'bob'])
+            self.assertTrue(True)
+        except:
+            self.assertTrue(False)
+
+    def test_add_period_with_datetime_input(self):
+        self.test_grid_add_period(period=datetime.datetime.now())
+
+    def test_add_period_with_date_input(self):
+        self.test_grid_add_period(period=datetime.date.today())
+
+    def test_add_period_with_decimal_input(self):
+        self.test_grid_add_period(index=[1, decimal.Decimal(22.2), 0.70, 'bob'])
+
+    def test_add_period_with_float_input(self):
+        self.test_grid_add_period(index=[1, float(22.2), 0.70, 'bob'])
