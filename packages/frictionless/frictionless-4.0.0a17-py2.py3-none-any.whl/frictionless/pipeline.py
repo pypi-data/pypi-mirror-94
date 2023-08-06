efab989b8484ea17a0ca5e8ce7b0717233cbcf12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:38 +0000

    Add package frictionless/4.0.0a17/frictionless-4.0.0a17-py2.py3-none-any.whl

---
+++ packages/frictionless/frictionless-4.0.0a17-py2.py3-none-any.whl/frictionless/pipeline.py
@@ -0,0 +1,125 @@
+from importlib import import_module
+from .exception import FrictionlessException
+from .errors import PipelineError, TaskError
+from .status import Status, StatusTask
+from .metadata import Metadata
+from .resource import Resource
+from .package import Package
+from . import helpers
+from . import config
+
+
+class Pipeline(Metadata):
+    """Pipeline representation.
+
+    Parameters:
+        descriptor? (str|dict): pipeline descriptor
+
+    Raises:
+        FrictionlessException: raise any error that occurs during the process
+
+    """
+
+    def __init__(self, descriptor, tasks=None):
+        self.setinitial("tasks", tasks)
+        super().__init__(descriptor)
+
+    @property
+    def tasks(self):
+        """
+        Returns:
+            dict[]: tasks
+        """
+        tasks = self.get("tasks", [])
+        return self.metadata_attach("tasks", tasks)
+
+    # Run
+
+    def run(self, *, parallel=False):
+        """Run the pipeline"""
+        tasks = []
+        timer = helpers.Timer()
+        for task in self.tasks:
+            errors = []
+            target = None
+            try:
+                target = task.run()
+            except Exception as exception:
+                errors.append(TaskError(note=str(exception)))
+            tasks.append(StatusTask(errors=errors, target=target, type=task.type))
+        return Status(tasks=tasks, time=timer.time, errors=[])
+
+    # Metadata
+
+    metadata_strict = True
+    metadata_Error = PipelineError
+    metadata_profile = config.PIPELINE_PROFILE
+
+    def metadata_process(self):
+
+        # Tasks
+        tasks = self.get("tasks")
+        if isinstance(tasks, list):
+            for index, task in enumerate(tasks):
+                if not isinstance(task, PipelineTask):
+                    task = PipelineTask(task)
+                    list.__setitem__(tasks, index, task)
+            if not isinstance(tasks, helpers.ControlledList):
+                tasks = helpers.ControlledList(tasks)
+                tasks.__onchange__(self.metadata_process)
+                dict.__setitem__(self, "tasks", tasks)
+
+
+# NOTE: we can use metadata_process to task.source -> Resource/Package?
+class PipelineTask(Metadata):
+    """Pipeline task representation.
+
+    Parameters:
+        descriptor? (str|dict): pipeline task descriptor
+
+    Raises:
+        FrictionlessException: raise any error that occurs during the process
+
+    """
+
+    def __init__(self, descriptor=None, *, source=None, type=None, steps=None):
+        self.setinitial("source", source)
+        self.setinitial("type", type)
+        self.setinitial("steps", steps)
+        super().__init__(descriptor)
+
+    @property
+    def source(self):
+        return self["source"]
+
+    @property
+    def type(self):
+        return self["type"]
+
+    @property
+    def steps(self):
+        return self["steps"]
+
+    def run(self):
+        """Run the task"""
+        transforms = import_module("frictionless.transform")
+
+        # Resource transform
+        if self.type == "resource":
+            source = Resource(self.source)
+            return transforms.transform_resource(source, steps=self.steps)
+
+        # Package transform
+        elif self.type == "package":
+            source = Package(self.source)
+            return transforms.transform_package(source, steps=self.steps)
+
+        # Not supported transform
+        note = f'Transform type "{self.type}" is not supported'
+        raise FrictionlessException(PipelineError(note=note))
+
+    # Metadata
+
+    metadata_strict = True
+    metadata_Error = PipelineError
+    metadata_profile = config.PIPELINE_PROFILE["properties"]["tasks"]["items"]
---
+++ packages/frictionless/frictionless-4.0.0a17-py2.py3-none-any.whl/frictionless/report.py
@@ -0,0 +1,308 @@
+import functools
+from copy import deepcopy
+from .metadata import Metadata
+from .errors import Error, TaskError, ReportError
+from .exception import FrictionlessException
+from . import config
+from . import helpers
+
+
+# NOTE:
+# We can allow some Report/ReportTask constructor kwargs be None
+# We need to review how we validate Report/ReportTask (strict mode is disabled)
+
+
+class Report(Metadata):
+    """Report representation.
+
+    API      | Usage
+    -------- | --------
+    Public   | `from frictionless import Report`
+
+    Parameters:
+        descriptor? (str|dict): report descriptor
+        time (float): validation time
+        errors (Error[]): validation errors
+        tasks (ReportTask[]): validation tasks
+
+    Raises:
+        FrictionlessException: raise any error that occurs during the process
+
+    """
+
+    def __init__(self, descriptor=None, *, time, errors, tasks):
+        error_count = len(errors) + sum(tab["stats"]["errors"] for tab in tasks)
+        self.setinitial("version", config.VERSION)
+        self.setinitial("time", time)
+        self.setinitial("valid", not errors and all(task["valid"] for task in tasks))
+        self.setinitial("stats", {"errors": error_count, "tasks": len(tasks)})
+        self.setinitial("errors", errors)
+        self.setinitial("tasks", tasks)
+        super().__init__(descriptor)
+
+    @property
+    def version(self):
+        """
+        Returns:
+            str: frictionless version
+        """
+        return self["version"]
+
+    @property
+    def time(self):
+        """
+        Returns:
+            float: validation time
+        """
+        return self["time"]
+
+    @property
+    def valid(self):
+        """
+        Returns:
+            bool: validation result
+        """
+        return self["valid"]
+
+    @property
+    def stats(self):
+        """
+        Returns:
+            dict: validation stats
+        """
+        return self["stats"]
+
+    @property
+    def errors(self):
+        """
+        Returns:
+            Error[]: validation errors
+        """
+        return self["errors"]
+
+    @property
+    def tasks(self):
+        """
+        Returns:
+            ReportTask[]: validation tasks
+        """
+        return self["tasks"]
+
+    @property
+    def task(self):
+        """
+        Returns:
+            ReportTask: validation task (if there is only one)
+
+        Raises:
+            FrictionlessException: if there are more that 1 task
+        """
+        if len(self.tasks) != 1:
+            error = Error(note='The "report.task" is available for single task reports')
+            raise FrictionlessException(error)
+        return self.tasks[0]
+
+    # Expand
+
+    def expand(self):
+        """Expand metadata"""
+        for task in self.tasks:
+            task.expand()
+
+    # Flatten
+
+    def flatten(self, spec=["taskPosition", "rowPosition", "fieldPosition", "code"]):
+        """Flatten the report
+
+        Parameters
+            spec (any[]): flatten specification
+
+        Returns:
+            any[]: flatten report
+        """
+        result = []
+        for error in self.errors:
+            context = {}
+            context.update(error)
+            result.append([context.get(prop) for prop in spec])
+        for count, task in enumerate(self.tasks, start=1):
+            for error in task.errors:
+                context = {"taskNumber": count, "taskPosition": count}
+                context.update(error)
+                result.append([context.get(prop) for prop in spec])
+        return result
+
+    # Import/Export
+
+    @staticmethod
+    def from_validate(validate):
+        """Validate function wrapper
+
+        Parameters:
+            validate (func): validate
+
+        Returns:
+            func: wrapped validate
+        """
+
+        @functools.wraps(validate)
+        def wrapper(*args, **kwargs):
+            timer = helpers.Timer()
+            try:
+                return validate(*args, **kwargs)
+            except Exception as exception:
+                error = TaskError(note=str(exception))
+                if isinstance(exception, FrictionlessException):
+                    error = exception.error
+                return Report(time=timer.time, errors=[error], tasks=[])
+
+        return wrapper
+
+    # Metadata
+
+    #  metadata_strict = True
+    metadata_Error = ReportError
+    metadata_profile = deepcopy(config.REPORT_PROFILE)
+    metadata_profile["properties"]["tasks"] = {
+        "type": "array",
+        "items": {"type": "object"},
+    }
+
+    def metadata_validate(self):
+        yield from super().metadata_validate()
+
+        # Tasks
+        for task in self.tasks:
+            yield from task.metadata_errors
+
+
+class ReportTask(Metadata):
+    """Report task representation.
+
+    API      | Usage
+    -------- | --------
+    Public   | `from frictionless import ReportTask`
+
+    Parameters:
+        descriptor? (str|dict): schema descriptor
+        time (float): validation time
+        scope (str[]): validation scope
+        partial (bool): wehter validation was partial
+        errors (Error[]): validation errors
+        task (Task): validation task
+
+    # Raises
+        FrictionlessException: raise any error that occurs during the process
+
+    """
+
+    def __init__(self, descriptor=None, *, resource, time, scope, partial, errors):
+        self.setinitial("resource", resource)
+        self.setinitial("time", time)
+        self.setinitial("valid", not errors)
+        self.setinitial("scope", scope)
+        self.setinitial("partial", partial)
+        self.setinitial("stats", {"errors": len(errors)})
+        self.setinitial("errors", errors)
+        super().__init__(descriptor)
+
+    @property
+    def resource(self):
+        """
+        Returns:
+            Resource: resource
+        """
+        return self["resource"]
+
+    @property
+    def time(self):
+        """
+        Returns:
+            float: validation time
+        """
+        return self["time"]
+
+    @property
+    def valid(self):
+        """
+        Returns:
+            bool: validation result
+        """
+        return self["valid"]
+
+    @property
+    def scope(self):
+        """
+        Returns:
+            str[]: validation scope
+        """
+        return self["scope"]
+
+    @property
+    def partial(self):
+        """
+        Returns:
+            bool: if validation partial
+        """
+        return self["partial"]
+
+    @property
+    def stats(self):
+        """
+        Returns:
+            dict: validation stats
+        """
+        return self["stats"]
+
+    @property
+    def errors(self):
+        """
+        Returns:
+            Error[]: validation errors
+        """
+        return self["errors"]
+
+    @property
+    def error(self):
+        """
+        Returns:
+            Error: validation error if there is only one
+
+        Raises:
+            FrictionlessException: if more than one errors
+        """
+        if len(self.errors) != 1:
+            error = Error(note='The "task.error" is available for single error tasks')
+            raise FrictionlessException(error)
+        return self.errors[0]
+
+    # Expand
+
+    def expand(self):
+        """Expand metadata"""
+        self.resource.expand()
+
+    # Flatten
+
+    def flatten(self, spec=["rowPosition", "fieldPosition", "code"]):
+        """Flatten the report
+
+        Parameters
+            spec (any[]): flatten specification
+
+        Returns:
+            any[]: flatten task report
+        """
+        result = []
+        for error in self.errors:
+            context = {}
+            context.update(error)
+            result.append([context.get(prop) for prop in spec])
+        return result
+
+    # Metadata
+
+    #  metadata_strict = True
+    metadata_Error = ReportError
+    metadata_profile = config.REPORT_PROFILE["properties"]["tasks"]["items"]
+    #  del metadata_profile["properties"]["errors"]
