30cf2aef3223e8930df7ce09a79bd3928f57fdf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:16 +0000

    Add package spider-admin-pro/0.0.7/spider_admin_pro-0.0.7-py3-none-any.whl

---
+++ packages/spider-admin-pro/spider_admin_pro-0.0.7-py3-none-any.whl/spider_admin_pro/model/base.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from peewee import *
+from playhouse.db_url import connect, register_database
+from playhouse.shortcuts import ReconnectMixin
+from playhouse.sqliteq import SqliteQueueDatabase
+
+from spider_admin_pro.config import SCHEDULE_HISTORY_DATABASE_URL
+from spider_admin_pro.utils.sqlite_util import make_sqlite_dir
+
+# 显示查询日志
+logger = logging.getLogger('peewee')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
+
+
+class ReconnectSqliteDatabase(ReconnectMixin, SqliteQueueDatabase):
+    pass
+
+
+class ReconnectMySQLDatabase(ReconnectMixin, MySQLDatabase):
+    pass
+
+
+register_database(ReconnectSqliteDatabase, 'sqlite')
+register_database(ReconnectMySQLDatabase, 'mysql')
+
+make_sqlite_dir(SCHEDULE_HISTORY_DATABASE_URL)
+db = connect(url=SCHEDULE_HISTORY_DATABASE_URL)
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
