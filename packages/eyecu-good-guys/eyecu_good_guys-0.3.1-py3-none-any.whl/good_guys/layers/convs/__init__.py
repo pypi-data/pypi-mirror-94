a231cee6153d1ece84607b643a3c6a8b4e596f73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:36 +0000

    Add package eyecu-good-guys/0.3.1/eyecu_good_guys-0.3.1-py3-none-any.whl

---
+++ packages/eyecu-good-guys/eyecu_good_guys-0.3.1-py3-none-any.whl/good_guys/layers/convs/__init__.py
@@ -0,0 +1,7 @@
+from .DepthwiseSeparableConv2d import DepthwiseSeparableConv2d
+from .DepthwiseSeparableConv3d import DepthwiseSeparableConv3d
+
+from .BatchwiseConv2d import BatchwiseConv2d
+from .BatchwiseSeparableConv2d import BatchwiseSeparableConv2d
+
+from .DepthwiseSeparableUpsample2d import DepthwiseSeparableUpsample2d
---
+++ packages/eyecu-good-guys/eyecu_good_guys-0.3.1-py3-none-any.whl/good_guys/layers/pooling/FPN.py
@@ -0,0 +1,46 @@
+import torch.nn as N
+import torch
+
+from good_guys.misc import AttrProxy
+
+
+class FPN(N.Module):
+
+    def __init__(self, filters: [int], stride=2):
+        super().__init__()
+
+        self.convs = AttrProxy(self, "convs_")
+        self.upsamples = AttrProxy(self, "upsamples_")
+
+        for ind, in_channel in enumerate(reversed(filters)):
+            self.add_module(
+                self.convs(ind),
+                N.Conv2d(in_channel, in_channel, 1)
+            )
+
+        self.add_module(
+            self.upsamples(0),
+            N.Identity()
+        )
+        for ind, (in_channel, out_channel) in enumerate(reversed(list(zip(filters[1:], filters[:-1])))):
+            self.add_module(
+                self.upsamples(ind + 1),
+                N.ConvTranspose2d(in_channel, out_channel, 4, stride, 1)
+            )
+
+    def forward(self, features: [torch.Tensor]):
+
+        results = []
+        prev = features[-1]
+
+        for ind, x in enumerate(reversed(features)):
+            up_module = self.upsamples[ind]
+            conv_module = self.convs[ind]
+
+            prev = up_module(prev)
+
+            x = conv_module(x)
+            prev = x + prev
+            results.append(prev)
+
+        return reversed(results)
