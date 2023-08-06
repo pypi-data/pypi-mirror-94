6a40e612d76a337fde1c8154ff1da3f454aca42a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:37 +0000

    Add package dupecheck/0.7.1/dupecheck-0.7.1.tar.gz

---
+++ packages/dupecheck/dupecheck-0.7.1.tar.gz/dupecheck-0.7.1/setup.py
@@ -0,0 +1,26 @@
+import pathlib
+from setuptools import find_packages, setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="dupecheck",
+    version="0.7.1",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Jakob Anderson",
+    author_email="fictionisfiction@gmail.com",
+    license="MIT",
+    url="https://github.com/spacerockzero/dupecheck-py",
+    description="Check for duplicate word series between generated text and a dataset",
+    packages=find_packages(exclude=["tests"]),
+    include_package_data=True,
+    install_requires=["tqdm", "nltk", "autocorrect"],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest==4.4.1", "numpy", "nltk", "autocorrect"],
+    test_suite="tests",
+)
