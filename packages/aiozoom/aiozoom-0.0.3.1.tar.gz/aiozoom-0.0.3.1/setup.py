5b8b7461e72a4d258f065a6aec0f754a13f9e2cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:38 +0000

    Add package aiozoom/0.0.3.1/aiozoom-0.0.3.1.tar.gz

---
+++ packages/aiozoom/aiozoom-0.0.3.1.tar.gz/aiozoom-0.0.3.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3.1' 
+DESCRIPTION = 'Async library for interaction with Zoom API'
+LONG_DESCRIPTION = 'Async library for interaction with Zoom API.\n' \
+                    'It allows you to create, get, stop meetings ' \
+                    'and you can get statistics of past meeting'
+
+# Setting up
+setup(
+        name="aiozoom", 
+        version=VERSION,
+        author="Vladislav Isakov",
+        author_email="vladisa88@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['aiohttp'],
+        keywords=['python', 'zoom', 'async'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
