7adbe87be6d3b5bd3ec37e991db7274d9f6812f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:04 +0000

    Add package wifi-password/1.1.0/wifi-password-1.1.0.tar.gz

---
+++ packages/wifi-password/wifi-password-1.1.0.tar.gz/wifi-password-1.1.0/README.md
@@ -0,0 +1,45 @@
+<h1 align="center">
+wifi-password
+</h1>
+<p align="center">
+Quickly fetch your WiFi password and if needed, generate a QR code of your WiFi to allow phones to easily connect.
+<br>
+Works on <b>macOS</b> and <b>Linux</b> and <b>Windows</b>
+<img src="images/demo.gif" align="center">
+</p>
+
+## Installation
+
+### Install using `pip`
+```console
+$ python3 -m pip install --user wifi-password
+```
+
+### Install using `git`
+```
+$ git clone https://github.com/sdushantha/wifi-password
+$ cd wifi-password
+$ python3 setup.py install
+```
+
+### Install using the [AUR](https://aur.archlinux.org/packages/wifi-password/)
+```console
+$ sudo pamac build wifi-password
+```
+
+## Usage
+```console
+$ wifi-password
+usage: wifi_password [options]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --qrcode, -q          Generate a QR code
+  --image, -i           Create the QR code as image instead of showing it on the terminal (must be useed along with --qrcode)
+  --ssid SSID, -s SSID  Specify a SSID that you have previously connected to
+  --version             Show version number
+```
+
+## Reference
+- This project is an improvement of [wifi-password](https://github.com/rauchg/wifi-password) created by [@rauchg](https://github.com/rauchg), where I have added support for multiple platforms and have added the feature for generating QR codes.
+- [wifiPassword](https://github.com/ankitjain28may/wifiPassword) created by [@ankitjain28may](https://github.com/ankitjain28may) was frequently used as reference throughout the development of this project.
---
+++ packages/wifi-password/wifi-password-1.1.0.tar.gz/wifi-password-1.1.0/wifi_password/wifi_password.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+#
+# by Siddharth Dushantha
+#
+import pathlib
+import sys
+import subprocess
+import argparse
+from shutil import which
+import re
+import os
+import qrcode
+import colorama
+
+__version__ = "1.1.0"
+
+def run_command(command):
+    env = os.environ.copy()
+    env["LANG"] = "C"
+    output, _ = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, shell=True, env=env).communicate()
+    return output.decode("utf-8").rstrip('\r\n')
+
+
+def print_error(text):
+    print(f"ERROR: {text}", file=sys.stderr)
+    sys.exit(1)
+
+
+def get_ssid():
+    if sys.platform == "darwin":
+        airport = pathlib.Path("/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport")
+        if not airport.is_file():
+            print_error(f"Can't find 'airport' command at {airport}")
+
+        ssid = run_command(f"{airport} -I | awk '/ SSID/ {{print substr($0, index($0, $2))}}'")
+
+    elif sys.platform == "linux":
+        if which("nmcli") is None:
+            print_error("Network Manager is required to run this program on Linux.")
+
+        ssid = run_command("nmcli -t -f active,ssid dev wifi | egrep '^yes:' | sed 's/^yes://'")
+
+    elif sys.platform == "win32":
+        ssid = run_command("netsh wlan show interfaces | findstr SSID")
+        if ssid == "":
+            print_error("SSID was not found")
+
+        ssid = re.findall(r"[^B]SSID\s+:\s(.*)", ssid)[0]
+
+    return ssid
+
+
+def get_password(ssid):
+    if ssid == "":
+        print_error("SSID is not defined")
+
+    if sys.platform == "darwin":
+        password = run_command(f"security find-generic-password -l \"{ssid}\" -D 'AirPort network password' -w")
+
+    elif sys.platform == "linux":
+        # Check if the user is running with super user privilages
+        if os.geteuid() != 0:
+            password = run_command(f"sudo nmcli -s -g 802-11-wireless-security.psk connection show '{ssid}'")
+        else:
+            password = run_command(f"nmcli -s -g 802-11-wireless-security.psk connection show '{ssid}'")
+
+    elif sys.platform == "win32":
+        password = run_command(f"netsh wlan show profile name=\"{ssid}\" key=clear | findstr Key")
+        password = re.findall(r"Key Content\s+:\s(.*)", password)[0]
+
+    if password == "":
+        print_error("Could not find password")
+
+    return password
+
+
+def generate_qr_code(ssid, password, image=False):
+    # Source: https://git.io/JtLIv
+    text = f"WIFI:T:WPA;S:{ssid};P:{password};;"
+
+    qr = qrcode.QRCode(version=1,
+                       error_correction=qrcode.constants.ERROR_CORRECT_L,
+                       box_size=10,
+                       border=4)
+    qr.add_data(text)
+
+    if image:
+        file_name = ssid.replace(" ", "_") + ".png"
+        img = qr.make_image()
+        img.save(file_name)
+        print(f"QR code has been saved to {file_name}")
+    else:
+        # This will emulate support for ANSI escape sequences, which is needed
+        # in order to display the QR code on Windows
+        colorama.init()
+        qr.make()
+        qr.print_tty()
+
+
+def main():
+    parser = argparse.ArgumentParser(usage="%(prog)s [options]")
+    parser.add_argument("--qrcode", "-q",
+            action="store_true",
+            default=False,
+            help="Generate a QR code")
+
+    parser.add_argument("--image", "-i",
+            action="store_true",
+            default=False,
+            help="Create the QR code as image instead of showing it on the terminal (must be used along with --qrcode)")
+
+    parser.add_argument("--ssid", "-s",
+            help="Specify a SSID that you have previously connected to")
+
+    parser.add_argument("--version",
+            action="store_true",
+            help="Show version number")
+
+    args = parser.parse_args()
+
+    if args.version:
+        print(__version__)
+        sys.exit()
+
+    if args.ssid is None:
+        args.ssid = get_ssid()
+
+    password = get_password(args.ssid)
+
+    if args.qrcode:
+        generate_qr_code(args.ssid, password, image=args.image)
+        return
+
+    print(password)
+
+if __name__ == "__main__":
+    main()
