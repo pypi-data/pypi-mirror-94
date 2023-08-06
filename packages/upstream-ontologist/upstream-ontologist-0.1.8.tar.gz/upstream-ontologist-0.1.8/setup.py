cffed824866d7ff390c630bb057457cfe618a6d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:23 +0000

    Add package upstream-ontologist/0.1.8/upstream-ontologist-0.1.8.tar.gz

---
+++ packages/upstream-ontologist/upstream-ontologist-0.1.8.tar.gz/upstream-ontologist-0.1.8/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+from setuptools import setup
+
+setup(
+    name="upstream-ontologist",
+    packages=[
+        "upstream_ontologist",
+        "upstream_ontologist.debian",
+        "upstream_ontologist.tests",
+    ],
+    version="0.1.8",
+    author="Jelmer Vernooij",
+    author_email="jelmer@debian.org",
+    url="https://github.com/jelmer/upstream-ontologist",
+    description="tracking of upstream project metadata",
+    project_urls={
+        "Repository": "https://github.com/jelmer/upstream-ontologist.git",
+    },
+    entry_points={
+        'console_scripts': [
+            ('guess-upstream-metadata='
+             'upstream_ontologist.__main__:main'),
+        ],
+    },
+    requires=['debian', 'debmutate'],
+    test_suite="upstream_ontologist.tests.test_suite",
+)
