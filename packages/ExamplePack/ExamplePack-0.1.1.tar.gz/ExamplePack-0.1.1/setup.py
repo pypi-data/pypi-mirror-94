e4955ae23c53a1e230cfc90152855fc638853833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:37 +0000

    Add package ExamplePack/0.1.1/ExamplePack-0.1.1.tar.gz

---
+++ packages/ExamplePack/ExamplePack-0.1.1.tar.gz/ExamplePack-0.1.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+setuptools.setup(
+    name="ExamplePack",
+    version="0.1.1",
+    author="TechGeeks",
+    author_email="expack@tgeeks.cf",
+    description="A Example Python Package",
+    url="https://tgeeks.cf/projects/ExamplePack",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
