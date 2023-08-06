978687c36c51407a6e0b4814879d1fe1c6a8189a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:58 +0000

    Add package pkgcheck/0.8.2/pkgcheck-0.8.2-py3-none-any.whl

---
+++ packages/pkgcheck/pkgcheck-0.8.2-py3-none-any.whl/pkgcheck/api.py
@@ -0,0 +1,34 @@
+"""Implements pkgcheck API to be exported."""
+
+from snakeoil.contexts import patch
+
+from .base import PkgcheckException
+
+
+def scan(args=None, /, *, base_args=None):
+    """Run ``pkgcheck scan`` using given arguments.
+
+    Args:
+        args (:obj:`list`, optional): command-line args for ``pkgcheck scan``
+        base_args (:obj:`list`, optional): pkgcore-specific command-line args for ``pkgcheck``
+    Raises:
+        PkgcheckException on failure
+    Returns:
+        iterator of Result objects
+    """
+    # avoid circular imports
+    from .pipeline import Pipeline
+    from .scripts import pkgcheck
+
+    def parser_exit(parser, status, message):
+        """Stub function to handle argparse errors."""
+        raise PkgcheckException(message.strip())
+
+    if args is None:
+        args = []
+    if base_args is None:
+        base_args = []
+
+    with patch('argparse.ArgumentParser.exit', parser_exit):
+        options = pkgcheck.argparser.parse_args(base_args + ['scan'] + args)
+    return Pipeline(options)
