6c94f28de24df4f2d5cf22d26073868bff2a3fb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:47 +0000

    Add package spellwise/0.7.0/spellwise-0.7.0.tar.gz

---
+++ packages/spellwise/spellwise-0.7.0.tar.gz/spellwise-0.7.0/spellwise/__init__.py
@@ -0,0 +1,12 @@
+# flake8: noqa
+
+__all__ = ["CaverphoneOne", "CaverphoneTwo", "Editex", "Levenshtein", "Soundex" "Typox"]
+
+from .algorithms import (
+    CaverphoneOne,
+    CaverphoneTwo,
+    Editex,
+    Levenshtein,
+    Soundex,
+    Typox,
+)
