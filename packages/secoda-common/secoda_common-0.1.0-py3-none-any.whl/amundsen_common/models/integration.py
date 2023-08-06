4c4ea9e1c7577c75c0a698b98b6d20adeac95f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:09 +0000

    Add package secoda-common/0.1.0/secoda_common-0.1.0-py3-none-any.whl

---
+++ packages/secoda-common/secoda_common-0.1.0-py3-none-any.whl/amundsen_common/models/integration.py
@@ -0,0 +1,22 @@
+
+import attr
+from marshmallow_annotations.ext.attrs import AttrsSchema
+
+@attr.s(auto_attribs=True, kw_only=True)
+class Integration:
+  integration_id: Optional[str] = None
+  unique_id: Optional[str] = None
+  name: Optional[str] = None
+  owner_name: Optional[str] = None
+  picture: Optional[str] = None
+  region: Optional[str] = None
+  connection_url: Optional[str] = None
+  port: Optional[int] = None
+  user: Optional[str] = None
+  password: Optional[str] = None
+  deleted: Optional[bool] = None
+
+class IntegrationSchema(AttrsSchema):
+  class Meta:
+    target = Integration
+    register_as_scheme = True
