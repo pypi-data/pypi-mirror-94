ed7f03ffcc8646d03fd472dc7b8eac371dc648ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:27 +0000

    Add package e4/0.0.0/e4-0.0.0.tar.gz

---
+++ packages/e4/e4-0.0.0.tar.gz/e4-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'e4',
+    version = '0.0.0',
+    author = 'Trevor Bayless',
+    url = 'https://github.com/trevorbayless/e4',
+    description = 'e4 is in early development',
+    py_modules = ['main'],
+    package_dir = {'': 'e4'},
+)
+
