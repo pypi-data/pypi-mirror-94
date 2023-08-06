a14ab8cbe63f0930735dc111aacd87d1cdbf5339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:15 +0000

    Add package pyarubaswitch/0.0.3/pyarubaswitch-0.0.3-py3-none-any.whl

---
+++ packages/pyarubaswitch/pyarubaswitch-0.0.3-py3-none-any.whl/pyarubaswitch/get_loop_protect.py
@@ -0,0 +1,34 @@
+
+
+from pyarubaswitch.api_engine import APIuser
+
+
+class LoopProtect(APIuser):
+
+    def get_protected_ports(self):
+        jsondata = self.api_client.get("loop_protect/ports")
+        if self.api_passed == False:
+            self.api_client.logout()
+
+        ports = []
+
+        for port in jsondata["loop_protect_port_element"]:
+
+            if port["is_loop_protection_enabled"] == True:
+                ports.append(port["port_id"])
+
+        return ports
+
+    def get_unprotected_ports(self):
+        jsondata = self.api_client.get("loop_protect/ports")
+        if self.api_passed == False:
+            self.api_client.logout()
+
+        ports = []
+
+        for port in jsondata["loop_protect_port_element"]:
+
+            if port["is_loop_protection_enabled"] == False:
+                ports.append(port["port_id"])
+
+        return ports
