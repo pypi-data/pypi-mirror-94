56c1c923ce0b68aaac42b366c4fe7687e66b9083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:18 +0000

    Add package petfinder-client/0.0.2/petfinder_client-0.0.2-py3.8.egg

---
+++ packages/petfinder-client/petfinder_client-0.0.2-py3.8.egg/petfinder/__init__.py
@@ -0,0 +1,10 @@
+from petfinder.client import Client, AsyncClient
+from petfinder.models import (
+    AnimalQuery,
+    AnimalType,
+    AnimalSize,
+    AnimalGender,
+    AnimalStatus,
+    AnimalAge,
+    AnimalCoat,
+)
