de5774a9e8052b9981451a90786474d70b4a2ba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:59 +0000

    Add package QualtricsAPI/0.5.1/QualtricsAPI-0.5.1.tar.gz

---
+++ packages/QualtricsAPI/QualtricsAPI-0.5.1.tar.gz/QualtricsAPI-0.5.1/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='QualtricsAPI',
+    version='0.5.1',
+    author='Jeremy A. Seibert',
+    author_email='jeremy@seibert.industries',
+    description="QualtricsAPI is a lightweight Python library for the Qualtrics Web API. ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Jaseibert/QualtricsAPI",
+    license='MIT',
+    packages=setuptools.find_packages(),
+    keywords='qualtrics api python research survey',
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          'pandas',
+          'numpy',
+          'requests', 
+          'python-dateutil'
+      ],
+)
