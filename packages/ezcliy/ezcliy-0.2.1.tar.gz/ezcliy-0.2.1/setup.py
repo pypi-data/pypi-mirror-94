65a23fa2197933d4be45675b7ffc0c1cd6156a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:44 +0000

    Add package ezcliy/0.2.1/ezcliy-0.2.1.tar.gz

---
+++ packages/ezcliy/ezcliy-0.2.1.tar.gz/ezcliy-0.2.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+
+setup(
+    install_requires=[
+        "colorama>=0.4.0", "pyyaml>=5.0.0"
+    ],
+    name="ezcliy",
+    version="0.2.1",
+    packages=["ezcliy"],
+    project_urls={
+        "Repository": "https://github.com/kpostekk/ezcliy/",
+        "Docs": "https://ezcliy.readthedocs.io/en/latest/",
+    },
+    url="https://github.com/kpostekk/ezcliy",
+    license="Apache",
+    author="Krystian Postek",
+    author_email="krystian@postek.eu",
+    description="Framework for creating CLI tools",
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    python_requires=">=3.9",
+)
