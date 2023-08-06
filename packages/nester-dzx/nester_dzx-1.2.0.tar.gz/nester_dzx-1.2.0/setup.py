68f425a722f0993e95bfef9c9e6154c93dacbe63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:54 +0000

    Add package nester-dzx/1.2.0/nester_dzx-1.2.0.tar.gz

---
+++ packages/nester-dzx/nester_dzx-1.2.0.tar.gz/nester_dzx-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name = 'nester_dzx',
+version = '1.2.0',
+py_modules = ['nester'],
+author = 'hfpython',
+author_email = 'hfpython@headfirstlabs.com',
+url = 'http://www.headfirstlabs.com',
+description = 'A simple printer of nested lists',
+)
