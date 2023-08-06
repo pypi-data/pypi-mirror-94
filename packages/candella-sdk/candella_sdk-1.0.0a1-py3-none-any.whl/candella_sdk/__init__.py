9d25d96268b8cd6e1a989a1434f1417bb7ef83c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:10 +0000

    Add package candella-sdk/1.0.0a1/candella_sdk-1.0.0a1-py3-none-any.whl

---
+++ packages/candella-sdk/candella_sdk-1.0.0a1-py3-none-any.whl/candella_sdk/__init__.py
@@ -0,0 +1,14 @@
+#
+# __init__.py
+# Candella SDK
+#
+# (C) 2021 Marquis Kurt.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+
+from .cli import main
+from . import sdk
+
+__version__ = '1.0.0-alpha1'
---
+++ packages/candella-sdk/candella_sdk-1.0.0a1-py3-none-any.whl/candella_sdk/__main__.py
@@ -0,0 +1,15 @@
+# 
+# __main__.py
+# candella-sdk
+# 
+# Created by Marquis Kurt on 02/09/21.
+# Copyright © 2021 Marquis Kurt. All rights reserved.
+# 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+
+from .cli import main
+
+main()
