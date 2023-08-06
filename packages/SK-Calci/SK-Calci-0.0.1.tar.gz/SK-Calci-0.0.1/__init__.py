f9e71c54f3c17fe74c7658cc288638b91df42452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:13 +0000

    Add package SK-Calci/0.0.1/SK-Calci-0.0.1.tar.gz

---
+++ packages/SK-Calci/SK-Calci-0.0.1.tar.gz/SK-Calci-0.0.1/__init__.py
@@ -0,0 +1,12 @@
+def add_num(num1, num2):
+    return num1 + num2 
+
+def sub_num(num1, num2):
+    return num1 - num2 
+
+def mul_num(num1, num2):
+    return num1 * num2 
+
+def div_num(num1, num2):
+    return num1 / num2 
+
