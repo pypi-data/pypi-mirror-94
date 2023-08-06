c5026991ca8ea9f94e524f950d7ddff867eef4b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:38 +0000

    Add package fbl/0.7.0/fbl-0.7.0.tar.gz

---
+++ packages/fbl/fbl-0.7.0.tar.gz/fbl-0.7.0/README.md
@@ -0,0 +1,46 @@
+# FBL(Find/File broken links)
+
+FBL is tool to find broken links in articles and files
+
+
+
+## how it works
+
+FBL finds all the links and checks them all. Any broken link will be displayed to you
+
+
+
+## How to run
+
+Run this command in your terminal or CMD :
+
+```bash 
+pip3 install fbl
+
+fbl --file_location my.pdf
+```
+
+**or**
+
+1. Install `python3` and `git` on your system
+
+```bash
+git clone https://github.com/mr-tafreshi/fbl
+cd fbl
+pip3 install -e . 
+fbl ---file_location my.pdf
+```
+
+
+
+### TODO
+
+- [x] .txt support
+
+- [x] .pdf support
+- [x] Mark Down Support
+- [x] docx support
+- [ ] .odt support
+- [x] .htm and .html support
+- [ ] .XLS and XLSX support
+- [ ] .PPT and .PPTX support
---
+++ packages/fbl/fbl-0.7.0.tar.gz/fbl-0.7.0/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fbl",
+    version="0.7.0",
+    author="Amirmahdi Tafreshi",
+    install_requires=["certifi==2020.12.5",
+                     "chardet==4.0.0",
+                     "idna==2.10",
+                     "lxml==4.6.2",
+                     "PyMuPDF==1.18.6",
+                     "python-docx==0.8.10",
+                     "requests==2.25.1",
+                     "urllib3==1.26.2"],
+    author_email="a.tafreshi440@gmail.com",
+    description="FBL is tool to find broken links in articles and files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mr-tafreshi/fbl",
+    packages=setuptools.find_packages(),
+    entry_points={
+        "console_scripts": ["fbl = fbl.__main__:main"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
