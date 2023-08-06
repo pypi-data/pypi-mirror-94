bbccdf9a54d157cbd384bd630e0c7acc3455fb15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:36 +0000

    Add package xdgconfig/0.2.0/xdgconfig-0.2.0.tar.gz

---
+++ packages/xdgconfig/xdgconfig-0.2.0.tar.gz/xdgconfig-0.2.0/xdgconfig/__init__.py
@@ -0,0 +1,58 @@
+# flake8: noqa
+from platform import system
+
+import xdgconfig.mixins as mixins
+
+
+__version__ = '0.2.0'
+
+
+if system() == 'Windows':
+    from xdgconfig.config_win import WinConfig as Config
+elif system() in ('Darwin', 'Linux') or system().startswith('CYGWIN'):
+    from xdgconfig.config_unix import UnixConfig as Config
+else:
+    raise ImportError(
+        "xdgconfig is not available on this platform : %s" % system()
+    )
+
+
+class JsonConfig(mixins.JsonMixin, Config):
+    ...
+
+
+if hasattr(mixins, 'IniMixin'):
+    class IniConfig(mixins.IniMixin, Config):
+        ...
+else:
+    print((
+        'mergedeep is not installed. '
+        'Run pip install xdgconfig[ini] to install it.'
+    ))
+
+if hasattr(mixins, 'XmlMixin'):
+    class XmlConfig(mixins.XmlMixin, Config):
+        ...
+else:
+    print((
+        'xmltodict is not installed. '
+        'Run pip install xdgconfig[xml] to install it.'
+    ))
+
+if hasattr(mixins, 'YamlMixin'):
+    class YamlConfig(mixins.YamlMixin, Config):
+        ...
+else:
+    print((
+        'PyYAML is not installed. '
+        'Run pip install xdgconfig[yaml] to install it.'
+    ))
+
+if hasattr(mixins, 'TomlMixin'):
+    class TomlConfig(mixins.TomlMixin, Config):
+        ...
+else:
+    print((
+        'TOML is not installed. '
+        'Run pip install xdgconfig[toml] to install it.'
+    ))
