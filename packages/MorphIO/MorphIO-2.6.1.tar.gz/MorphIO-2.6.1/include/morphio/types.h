96e2a7683560baf20caa776d316222ccb43af61d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:28 +0000

    Add package MorphIO/2.6.1/MorphIO-2.6.1.tar.gz

---
+++ packages/MorphIO/MorphIO-2.6.1.tar.gz/MorphIO-2.6.1/include/morphio/types.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <memory>  // std::shared_ptr
+#include <string>  // std::string
+#include <vector>  // std::vector
+
+#include <gsl/gsl>
+#include <morphio/enums.h>
+#include <morphio/exceptions.h>
+#include <morphio/vector_types.h>
+
+
+/** @namespace morphio Blue Brain File IO classes */
+namespace morphio {
+
+using namespace enums;
+class EndoplasmicReticulum;
+class MitoSection;
+class Mitochondria;
+class Morphology;
+class Section;
+template <class T>
+class SectionBase;
+class Soma;
+
+namespace Property {
+struct Properties;
+}
+
+namespace vasculature {
+class Section;
+class Vasculature;
+}  // namespace vasculature
+
+namespace readers {
+struct DebugInfo;
+class ErrorMessages;
+}  // namespace readers
+
+namespace mut {
+class EndoplasmicReticulum;
+class MitoSection;
+class Mitochondria;
+class Morphology;
+class Section;
+class Soma;
+}  // namespace mut
+
+using SectionRange = std::pair<size_t, size_t>;
+
+template <typename T>
+using range = gsl::span<T>;
+
+}  // namespace morphio
