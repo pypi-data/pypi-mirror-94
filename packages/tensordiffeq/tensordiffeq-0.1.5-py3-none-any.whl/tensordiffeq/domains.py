f866cd65ef6a4f228711fc8ca2cd205464a80c73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:00 +0000

    Add package tensordiffeq/0.1.5/tensordiffeq-0.1.5-py3-none-any.whl

---
+++ packages/tensordiffeq/tensordiffeq-0.1.5-py3-none-any.whl/tensordiffeq/domains.py
@@ -0,0 +1,29 @@
+import numpy as np
+from .utils import LatinHypercubeSample
+
+
+class DomainND:
+    def __init__(self, var, time_var=None):
+        self.vars = var
+        self.domaindict = []
+        self.domain_ids = []
+        self.time_var = time_var
+
+    def generate_collocation_points(self, N_f):
+        range_list = []
+        for dict_ in self.domaindict:
+            range_list.append([val for key, val in dict_.items() if "range" in key][0])
+        limits = np.array(range_list)  # x,t domain
+        X_f = LatinHypercubeSample(N_f, limits)
+        self.X_f = X_f
+
+    def add(self, token, vals, fidel):
+        self.domain_ids.append(token)
+        self.domaindict.append({
+            "identifier": token,
+            "range": vals,
+            (token + "fidelity"): fidel,
+            (token + "linspace"): np.linspace(vals[0], vals[1], fidel),
+            (token + "upper"): vals[1],
+            (token + "lower"): vals[0]
+        })
