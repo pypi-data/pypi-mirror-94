91bd24142fe465aa3a2cbad9d4033562971041d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:12 +0000

    Add package graphene-types/0.15.1/graphene-types-0.15.1.tar.gz

---
+++ packages/graphene-types/graphene-types-0.15.1.tar.gz/graphene-types-0.15.1/graphene-stubs/types/interface.pyi
@@ -0,0 +1,20 @@
+from .base import BaseOptions as BaseOptions, BaseType as BaseType
+from .field import Field as Field
+from .utils import yank_fields_from_attrs as yank_fields_from_attrs
+from typing import Any, Dict, Optional, TypeVar, Generic
+
+MYPY: bool
+
+RT = TypeVar("RT")
+
+class InterfaceOptions(BaseOptions):
+    fields: Dict[str, Field] = ...
+
+class Interface(Generic[RT], BaseType):
+    @classmethod
+    def __init_subclass_with_meta__(  # type: ignore[override]
+        cls, _meta: Optional[Any] = ..., **options: Any
+    ) -> None: ...
+    @classmethod
+    def resolve_type(cls, instance: Any, info: Any) -> Any: ...
+    def __init__(self, *args: Any, **kwargs: Any) -> None: ...
