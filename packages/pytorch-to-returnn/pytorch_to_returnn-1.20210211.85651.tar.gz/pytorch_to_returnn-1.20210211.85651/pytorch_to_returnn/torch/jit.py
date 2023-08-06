e37a0c7b91e8831c009c6d82bd277c0f22f66720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:46 +0000

    Add package pytorch-to-returnn/1.20210211.85651/pytorch_to_returnn-1.20210211.85651.tar.gz

---
+++ packages/pytorch-to-returnn/pytorch_to_returnn-1.20210211.85651.tar.gz/pytorch_to_returnn-1.20210211.85651/pytorch_to_returnn/torch/jit.py
@@ -0,0 +1,12 @@
+def export(fn):
+  """Dummy function to avoid import errors"""
+  pass
+
+
+def unused(fn):
+  """Dummy function to avoid import errors"""
+  pass
+
+
+def is_scripting() -> bool:
+  return False  # stub
