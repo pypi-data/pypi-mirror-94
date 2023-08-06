b3d5e2007b552a15526ee95d7baf0831867e4021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:33 +0000

    Add package pyppackageexample/0.0.1/pyppackageexample-0.0.1.tar.gz

---
+++ packages/pyppackageexample/pyppackageexample-0.0.1.tar.gz/pyppackageexample-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name= 'pyppackageexample',
+    version= '0.0.1',
+    license = 'MIT',
+    description= 'Paquete de prueba para una clase de Udemy',
+    author= 'Borja Martín Medina',
+    packages= find_packages(),
+    url= 'https://github.com/brjmrtn/pyppackageexample'
+)
