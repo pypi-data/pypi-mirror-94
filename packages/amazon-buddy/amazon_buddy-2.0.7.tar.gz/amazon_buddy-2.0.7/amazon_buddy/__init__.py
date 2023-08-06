405ded4b2ae940be329a3697e02acbc5a90e906b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:37 +0000

    Add package amazon-buddy/2.0.7/amazon_buddy-2.0.7.tar.gz

---
+++ packages/amazon-buddy/amazon_buddy-2.0.7.tar.gz/amazon_buddy-2.0.7/amazon_buddy/__init__.py
@@ -0,0 +1,9 @@
+from .amazon_buddy import AmazonBuddy
+
+from .enums.category import Category
+from .enums.sort_type import SortType
+
+from .models.search_result_product import SearchResultProduct
+from .models.product import Product, ProductImageSet
+from .models.review import Review
+from .models.review_image import ReviewImage
---
+++ packages/amazon-buddy/amazon_buddy-2.0.7.tar.gz/amazon_buddy-2.0.7/setup.py
@@ -0,0 +1,33 @@
+import setuptools, os
+
+readme_path = os.path.join(os.getcwd(), "README.md")
+if os.path.exists(readme_path):
+    with open(readme_path, "r") as f:
+        long_description = f.read()
+else:
+    long_description = 'amazon_buddy'
+
+setuptools.setup(
+    name="amazon_buddy",
+    version="2.0.7",
+    author="Kristof",
+    description="python wrapper for the amazon_buddy npm package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kkristof200/py_amazon_buddy",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'beautifulsoup4',
+        'jsoncodable',
+        'kcu',
+        'ksimpleapi',
+        'requests',
+        'Unidecode'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
