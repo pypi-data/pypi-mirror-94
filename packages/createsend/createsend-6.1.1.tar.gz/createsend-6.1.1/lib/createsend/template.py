9ff246fd7655a8af4b5475589563a96403a452b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:42 +0000

    Add package createsend/6.1.1/createsend-6.1.1.tar.gz

---
+++ packages/createsend/createsend-6.1.1.tar.gz/createsend-6.1.1/lib/createsend/template.py
@@ -0,0 +1,43 @@
+from __future__ import absolute_import
+
+import json
+
+from createsend.createsend import CreateSendBase
+from createsend.utils import json_to_py
+
+
+class Template(CreateSendBase):
+    """Represents an email template and associated functionality."""
+
+    def __init__(self, auth=None, template_id=None):
+        self.template_id = template_id
+        super(Template, self).__init__(auth)
+
+    def create(self, client_id, name, html_url, zip_url):
+        """Creates a new email template."""
+        body = {
+            "Name": name,
+            "HtmlPageURL": html_url,
+            "ZipFileURL": zip_url}
+        response = self._post("/templates/%s.json" %
+                              client_id, json.dumps(body))
+        self.template_id = json_to_py(response)
+        return self.template_id
+
+    def details(self):
+        """Gets the details of this email template."""
+        response = self._get("/templates/%s.json" % self.template_id)
+        return json_to_py(response)
+
+    def update(self, name, html_url, zip_url):
+        """Updates this email template."""
+        body = {
+            "Name": name,
+            "HtmlPageURL": html_url,
+            "ZipFileURL": zip_url}
+        response = self._put("/templates/%s.json" %
+                             self.template_id, json.dumps(body))
+
+    def delete(self):
+        """Deletes this email template."""
+        response = self._delete("/templates/%s.json" % self.template_id)
---
+++ packages/createsend/createsend-6.1.1.tar.gz/createsend-6.1.1/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="createsend",
+    version='6.1.1',
+    description="A library which implements the complete functionality of the Campaign Monitor API.",
+    author='Campaign Monitor',
+    author_email='support@campaignmonitor.com',
+    url="http://campaignmonitor.github.io/createsend-python/",
+    license="MIT",
+    keywords="createsend campaign monitor email",
+    install_requires=[
+        'six>=1.10',
+    ],
+    test_suite='test',
+    packages=find_packages('lib'),
+    package_dir={'': 'lib'},
+    package_data={'': ['cacert.pem']},
+
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+
+        # Who and what the project is for
+        "Intended Audience :: Developers",
+        "Topic :: Communications",
+        "Topic :: Communications :: Email",
+        "Topic :: Communications :: Email :: Mailing List Servers",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries",
+
+        # License classifiers
+        "License :: OSI Approved :: MIT License",
+        "License :: DFSG approved",
+        "License :: OSI Approved",
+
+        # Generally, we support the following.
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+
+        # Specifically, we support the following releases.
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ]
+)
