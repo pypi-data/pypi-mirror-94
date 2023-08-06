cd429e9269828909768ae5236f47d6e309b36b3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:07 +0000

    Add package sergeant/0.19.0/sergeant-0.19.0.tar.gz

---
+++ packages/sergeant/sergeant-0.19.0.tar.gz/sergeant-0.19.0/sergeant/slave.py
@@ -0,0 +1,79 @@
+import argparse
+import importlib
+import multiprocessing.connection
+import sys
+import traceback
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser(
+        description='Sergeant Supervisor',
+    )
+    parser.add_argument(
+        '--child-pipe',
+        help='Pipe fileno to return the potential exception',
+        type=int,
+        required=True,
+        dest='child_pipe',
+    )
+    parser.add_argument(
+        '--worker-class',
+        help='Class name of the worker to spawn',
+        type=str,
+        required=True,
+        dest='worker_class',
+    )
+    parser.add_argument(
+        '--worker-module',
+        help='Module of the worker class',
+        type=str,
+        required=True,
+        dest='worker_module',
+    )
+    args = parser.parse_args()
+    pipe_obj = multiprocessing.connection.Connection(
+        handle=args.child_pipe,
+    )
+
+    try:
+        worker_module = importlib.import_module(
+            name=args.worker_module,
+        )
+    except ModuleNotFoundError:
+        return 2
+
+    try:
+        worker_class = getattr(worker_module, args.worker_class)
+    except AttributeError:
+        return 3
+
+    try:
+        worker_obj = worker_class()
+        worker_obj.init_worker()
+        summary = worker_obj.work_loop()
+        pipe_obj.send(summary)
+
+        if summary['respawn']:
+            return 4
+        elif summary['stop']:
+            return 5
+        else:
+            return 0
+    except KeyboardInterrupt:
+        return 0
+    except Exception as exception:
+        pipe_obj.send(
+            {
+                'message': str(exception),
+                'stacktrace': traceback.format_exc(),
+                'type': exception.__class__.__name__,
+            },
+        )
+
+        return 1
+    finally:
+        pipe_obj.close()
+
+
+if __name__ == '__main__':
+    sys.exit(main())
