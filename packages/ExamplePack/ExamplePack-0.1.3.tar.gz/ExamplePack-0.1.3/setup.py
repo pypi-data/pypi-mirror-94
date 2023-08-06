ab94995c2b8826736f6a1856c38855579d5c8f04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:48 +0000

    Add package ExamplePack/0.1.3/ExamplePack-0.1.3.tar.gz

---
+++ packages/ExamplePack/ExamplePack-0.1.3.tar.gz/ExamplePack-0.1.3/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="ExamplePack",
+    version="0.1.3",
+    author="TechGeeks",
+    author_email="expack@tgeeks.cf",
+    description="A Example Python Package",
+     long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TechGeeks-Rajdeep/ExamplePack",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
