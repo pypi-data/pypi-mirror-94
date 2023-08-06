ae2fb0167fd032f06873be171a708f5714d69f7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:36 +0000

    Add package torchpq/0.1.3.4/torchpq-0.1.3.4.tar.gz

---
+++ packages/torchpq/torchpq-0.1.3.4.tar.gz/torchpq-0.1.3.4/torchpq/__init__.py
@@ -0,0 +1,11 @@
+from . import kmeans
+from . import kernels
+from .IVFPQBase import IVFPQBase
+from .IVFPQ import IVFPQ
+from .IVFPQR import IVFPQR
+from .PQ import PQ
+
+try:
+  import cupy
+except ModuleNotFoundError:
+  raise ModuleNotFoundError("cupy is not installed, please visit https://pypi.org/project/cupy/")
