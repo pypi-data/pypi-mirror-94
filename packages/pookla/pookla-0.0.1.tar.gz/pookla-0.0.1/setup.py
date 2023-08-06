0dc6b09722fac5daa487001217363590ceedfa9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:37 +0000

    Add package pookla/0.0.1/pookla-0.0.1.tar.gz

---
+++ packages/pookla/pookla-0.0.1.tar.gz/pookla-0.0.1/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""pookla"""
+
+from setuptools import find_packages, setup
+
+setup(
+    name="pookla",
+    version="0.0.1",
+    author="Jeremy Jacobs",
+    author_email="jeremy.jacobs@ookla.com",
+    classifiers=[],
+    description="Pookla",
+    long_description="# Pookla",
+    long_description_content_type="text/markdown",
+    url="https://github.com/teamookla",
+    packages=find_packages(),
+    python_requires=">=3.8.3",
+    install_requires=[],
+    include_package_data=False,
+    project_urls={},
+    entry_points={},
+)
