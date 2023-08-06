0b417c0a87a2b537a8ef075f098f9a30014c1c2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:06 +0000

    Add package c4/0.0.2/c4-0.0.2.tar.gz

---
+++ packages/c4/c4-0.0.2.tar.gz/c4-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='c4',
+    description='c4 metapackage',
+    author='QA Automation',
+    version='0.0.2',
+    install_requires=['c4-framework','c4-utils'],
+    classifiers=['Programming Language :: Python :: 3'],
+)
