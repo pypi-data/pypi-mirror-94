40c7dabd0e14b1a6328b33dc03595b8de980bad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:44 +0000

    Add package kernreg/0.0.6/kernreg-0.0.6-py3-none-any.whl

---
+++ packages/kernreg/kernreg-0.0.6-py3-none-any.whl/kernreg/__init__.py
@@ -0,0 +1,16 @@
+"""This is the entry-point to the kernreg package.
+
+Include only imports which should be available using
+
+.. code-block::
+
+    import kernreg as kr
+
+    kr.<func>
+"""
+from kernreg.bandwidth_selection import get_bandwidth_rsc  # noqa: F401
+from kernreg.smooth import locpoly  # noqa: F401
+from kernreg.utils import get_example_data, sort_by_x  # noqa: F401
+from kernreg.visualize import plot  # noqa: F401
+
+__version__ = "0.0.6"
