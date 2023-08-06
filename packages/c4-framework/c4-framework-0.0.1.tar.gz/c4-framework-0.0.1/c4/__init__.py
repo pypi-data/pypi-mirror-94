e6cbd5bc9b03afb4861f5ce830d79c640ad035d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:00 +0000

    Add package c4-framework/0.0.1/c4-framework-0.0.1.tar.gz

---
+++ packages/c4-framework/c4-framework-0.0.1.tar.gz/c4-framework-0.0.1/c4/__init__.py
@@ -0,0 +1,11 @@
+"""
+c4 - automation framework which will blow your mind!
+
+Extension of python's unittest. Adds XML output, parameters, logging and more.
+
+Simple usage:
+
+    import c4
+"""
+
+print("Placeholder for project to be opensourced soon")
---
+++ packages/c4-framework/c4-framework-0.0.1.tar.gz/c4-framework-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='c4-framework',
+    description='C4 - Test automation framework',
+    author='QA Automation',
+    version='0.0.1',
+    packages=['c4'],
+    classifiers=['Programming Language :: Python :: 3'],
+)
