53e551dd35748dc5019f2497c941c49dd8811ffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:15 +0000

    Add package Finicky/0.0.3/Finicky-0.0.3.tar.gz

---
+++ packages/Finicky/Finicky-0.0.3.tar.gz/Finicky-0.0.3/setup.py
@@ -0,0 +1,30 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="Finicky",
+    version="0.0.3",
+    description="Easy Data Validation",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yaaminu/finicky",
+    author="Aminu Abdul Manaf",
+    author_email="afolanyaaminu@gmai.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    python_requires=">=3.6",  # requires f-strings
+    packages=["finicky"],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+        ]
+    },
+)
