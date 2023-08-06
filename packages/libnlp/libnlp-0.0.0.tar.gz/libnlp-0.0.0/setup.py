81056c8c5c863a57de0d6c8361aa7e6ec744393a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:10 +0000

    Add package libnlp/0.0.0/libnlp-0.0.0.tar.gz

---
+++ packages/libnlp/libnlp-0.0.0.tar.gz/libnlp-0.0.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='libnlp',
+    version='0.0.0',
+    author='Mario Corchero',
+    description='Reserved',
+    packages=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+    ]
+    )
