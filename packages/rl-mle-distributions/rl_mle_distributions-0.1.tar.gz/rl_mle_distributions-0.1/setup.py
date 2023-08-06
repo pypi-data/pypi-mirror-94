d78d59f455aba2795425040d6361d48d8e5b388f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:32 +0000

    Add package rl-mle-distributions/0.1/rl_mle_distributions-0.1.tar.gz

---
+++ packages/rl-mle-distributions/rl_mle_distributions-0.1.tar.gz/rl_mle_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='rl_mle_distributions',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['rl_mle_distributions'],
+      zip_safe=False)
