551d61887d173bbcdea4781cc88e947afc8e837f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:41 +0000

    Add package pymee/1.5.3/pymee-1.5.3.tar.gz

---
+++ packages/pymee/pymee-1.5.3.tar.gz/pymee-1.5.3/CHANGELOG.md
@@ -0,0 +1,98 @@
+# Changelog
+
+<!--next-version-placeholder-->
+
+## v1.5.3 (2021-02-08)
+### Fix
+* Implement reconnects iteratively to avoid recursion problems ([`52c2a6b`](https://github.com/FreshlyBrewedCode/pymee/commit/52c2a6b37cd30a9c3c939c110744222187663d1e))
+
+## v1.5.2 (2021-02-07)
+### Fix
+* Turn `on_reconnect` callback into coroutine ([`4fded37`](https://github.com/FreshlyBrewedCode/pymee/commit/4fded37bf7502f7e3d95fb9625506f990585e67d))
+
+## v1.5.1 (2021-02-06)
+### Fix
+* Reuse existing node, group and relationship instances after reconnect ([`ead401b`](https://github.com/FreshlyBrewedCode/pymee/commit/ead401b484e30f7030059bda8538f3436d403411))
+
+## v1.5.0 (2021-02-04)
+### Feature
+* Add working reconnection logic during authentication ([`235ea6f`](https://github.com/FreshlyBrewedCode/pymee/commit/235ea6f33b6f87536c486d794e274e4b8a22dfeb))
+
+### Fix
+* Improve ping handler shutdown ([`5d69574`](https://github.com/FreshlyBrewedCode/pymee/commit/5d695743615ea7cffaf143939afb4b693b692f88))
+* Fix syntax and logic errors during reconnection ([`b199f85`](https://github.com/FreshlyBrewedCode/pymee/commit/b199f85ad1f76f1bd1fe38033ee3d5c7b06f7dc2))
+* Handle all exception types during authentication ([`0da9005`](https://github.com/FreshlyBrewedCode/pymee/commit/0da9005bbd6b109c2dacfaec8610236eee1de012))
+* Remove legacy reconnection logic in `get_access_token` ([`52dd77a`](https://github.com/FreshlyBrewedCode/pymee/commit/52dd77ab38d95f3ed154947926fcd981335873f5))
+* Fix unclosed client session error during authentication ([`d7b5ca5`](https://github.com/FreshlyBrewedCode/pymee/commit/d7b5ca5b96b6da14e9e34764c233e118135de6b5))
+
+## v1.4.0 (2021-02-03)
+### Feature
+* Enable automatic reconnection attempts ([`ee3fb00`](https://github.com/FreshlyBrewedCode/pymee/commit/ee3fb001067bb3ca90c1221584d1893cc7ebe085))
+
+## v1.3.0 (2021-02-03)
+### Feature
+* Add manual websocket ping handler ([`773caa0`](https://github.com/FreshlyBrewedCode/pymee/commit/773caa0451d6b9353a1dd97ce8a37ad2e609a127))
+
+### Documentation
+* Use large badges in README ([`370006b`](https://github.com/FreshlyBrewedCode/pymee/commit/370006b58fb7a793ba130b8f5998b6c941c41bba))
+
+## v1.2.0 (2020-11-10)
+### Feature
+* Handle `groups`, `nodes`, `group` and `relationship` message types ([`fddac28`](https://github.com/FreshlyBrewedCode/pymee/commit/fddac2831e0185464d2b9991ad04cc52dd28ef5b))
+* Manage groups, settings and relationships using model ([`5650375`](https://github.com/FreshlyBrewedCode/pymee/commit/56503756a0ccd006aa57b0bebdbee5d6764173b9))
+* Add `HomeeRelationship` model ([`21f2db0`](https://github.com/FreshlyBrewedCode/pymee/commit/21f2db01267bc917a2af568aa109eafa55bea071))
+* Add `HomeeOptions` model ([`45efe54`](https://github.com/FreshlyBrewedCode/pymee/commit/45efe54709d2849f56d59fd26d8fb0a2a19f98e4))
+* Add `HomeeGroup` model ([`9d3759f`](https://github.com/FreshlyBrewedCode/pymee/commit/9d3759fde70dfe8e9beea83e92f5437e90ea183e))
+
+### Fix
+* Decode strings in `HomeeGroup` ([`9db565e`](https://github.com/FreshlyBrewedCode/pymee/commit/9db565e7268f513d4ffd8b974af5116e3da9e6a7))
+
+### Documentation
+* Mention Home Assistant homee integration in README ([`d44c6b6`](https://github.com/FreshlyBrewedCode/pymee/commit/d44c6b6caa7ee82a76dff387805f5b7e7294b549))
+
+## v1.1.2 (2020-11-01)
+### Fix
+* Fix exception handling bug that causes connection to close instantly ([`050d1d0`](https://github.com/FreshlyBrewedCode/pymee/commit/050d1d04c2b8bca243de1c8d50886ecce580aa67))
+
+## v1.1.1 (2020-11-01)
+### Fix
+* Fix websocket exceptions not getting handled correctly causing websocket to stay open ([`624b383`](https://github.com/FreshlyBrewedCode/pymee/commit/624b383736371d44dfade05fdc803fb6f78c8bcd))
+
+### Documentation
+* Add link to websockets library in README ([`5f42af6`](https://github.com/FreshlyBrewedCode/pymee/commit/5f42af6367bea667dca275009cda2010e9e4712e))
+* Update examples in README ([`0177066`](https://github.com/FreshlyBrewedCode/pymee/commit/01770668ecdd43ca364c96d6341f47080fc9ced3))
+
+## v1.1.0 (2020-10-24)
+### Feature
+* Add `add_on_changed_listener()` to `HomeeNode` to support better update handling ([`12cc3d1`](https://github.com/FreshlyBrewedCode/pymee/commit/12cc3d17ea6aa4cc3354b76f97e1833ac7b029a5))
+
+### Fix
+* Add error handling to websocket receive and send handlers ([`6e9f021`](https://github.com/FreshlyBrewedCode/pymee/commit/6e9f02191ea2d107563d962a42681d1f18f54c5b))
+* Change value type in `set_value()` from `int` to `float` ([`0974bac`](https://github.com/FreshlyBrewedCode/pymee/commit/0974bacd8a78b0103d222d7357b502e6fb63e042))
+* Use module relative imports ([`4f33a6f`](https://github.com/FreshlyBrewedCode/pymee/commit/4f33a6f6ae8cde8ac788e1d9e02a2d4ec0d9b666))
+
+## v1.0.1 (2020-10-23)
+### Fix
+* Bump to v1.0.1 to avoid PyPI conflict ([`573e700`](https://github.com/FreshlyBrewedCode/pymee/commit/573e70054d3711196b3e39d1cc9ba80b4794a13a))
+
+## v1.0.0 (2020-10-23)
+### Feature
+* Add utility methods for getting nodes and atributes by id ([`f43014c`](https://github.com/FreshlyBrewedCode/pymee/commit/f43014cb77da7a4059695663a093a4acfb3b5d02))
+* Support updating/adding nodes after receiving a 'nodes' message ([`535602b`](https://github.com/FreshlyBrewedCode/pymee/commit/535602b8b43696b29f0e1e9d86bcde9e40223dbb))
+* Provide async disconnected event ([`c019ef5`](https://github.com/FreshlyBrewedCode/pymee/commit/c019ef5d55306007f98ee2d0154ccee138d2cb25))
+* Use coroutines for Homee callbacks ([`99296e6`](https://github.com/FreshlyBrewedCode/pymee/commit/99296e650268467df6370c034d544c10c81530a1))
+* Port to websockets package ([`1b578bc`](https://github.com/FreshlyBrewedCode/pymee/commit/1b578bc166cc0d9c6238980aca1a889a2e8ddef2))
+
+### Breaking
+* Homee callbacks need to be awaitable, i.e. async functions. Handleing callbacks in the event loop should provide a better development experience since most functions in the Homee api are async now.  ([`99296e6`](https://github.com/FreshlyBrewedCode/pymee/commit/99296e650268467df6370c034d544c10c81530a1))
+
+### Documentation
+* Add badges ♥ ([`a7e3a28`](https://github.com/FreshlyBrewedCode/pymee/commit/a7e3a28e7d6d8116c893214cd6e6b24ed5758c0f))
+
+## v0.2.0 (2020-10-23)
+### Feature
+* Raise unique exceptions while acquiring access token ([`edac67b`](https://github.com/FreshlyBrewedCode/pymee/commit/edac67bad668349ced6d96f8306836f884d05937))
+
+### Documentation
+* Add install instructions and bump version ([`f0208a5`](https://github.com/FreshlyBrewedCode/pymee/commit/f0208a565523e63ff0ecf976c5288ffc0d4fc71a))
+* Add README ([`8701f0a`](https://github.com/FreshlyBrewedCode/pymee/commit/8701f0ad47120d8291eb5b4f4c441cc151c468b2))
---
+++ packages/pymee/pymee-1.5.3.tar.gz/pymee-1.5.3/pymee/__init__.py
@@ -0,0 +1,484 @@
+import asyncio
+import hashlib
+import json
+from typing import List
+import aiohttp
+from aiohttp.helpers import BasicAuth
+import websockets
+from datetime import datetime
+from .const import DeviceApp, DeviceOS, DeviceType
+from .model import (
+    HomeeAttribute,
+    HomeeGroup,
+    HomeeNode,
+    HomeeRelationship,
+    HomeeSettings,
+)
+import re
+import logging
+
+
+class Homee:
+    def __init__(
+        self,
+        host: str,
+        user: str,
+        password: str,
+        device: str = "pymee",
+        pingInterval: int = 30,
+        reconnectInterval: int = 5,
+        reconnect: bool = True,
+        maxRetries: int = 5,
+        loop: asyncio.AbstractEventLoop = None,
+    ) -> None:
+
+        self.host = host
+        self.user = user
+        self.password = password
+
+        self.device = device
+        self.pingInterval = pingInterval
+        self.shouldReconnect = reconnect
+        self.reconnectInterval = reconnectInterval
+        self.maxRetries = maxRetries
+
+        self.deviceId = str(device).lower().replace(" ", "-")
+
+        self.settings: HomeeSettings = None
+        self.nodes: List[HomeeNode] = []
+        self.groups: List[HomeeGroup] = []
+        self.relationships: List[HomeeRelationship] = []
+        self.token = ""
+        self.expires = 0
+        self.connected = False
+        self.retries = 0
+        self.shouldClose = False
+
+        self._message_queue = asyncio.Queue(loop=loop)
+        self._connected_event = asyncio.Event(loop=loop)
+        self._disconnected_event = asyncio.Event(loop=loop)
+
+    async def get_access_token(self):
+        """Asynchronously attempts to get an access token from the homee host using username and password."""
+
+        # Check if current token is still valid
+        if self.token != None and self.expires > datetime.now().timestamp():
+            return self.token
+
+        client = aiohttp.ClientSession()
+        auth = BasicAuth(
+            self.user, hashlib.sha512(self.password.encode("utf-8")).hexdigest()
+        )
+        url = f"{self.url}/access_token"
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        data = {
+            "device_name": self.device,
+            "device_hardware_id": self.deviceId,
+            "device_os": DeviceOS.LINUX,
+            "device_type": DeviceType.NONE,
+            "device_app": DeviceApp.HOMEE,
+        }
+
+        try:
+            req = await client.post(
+                url, auth=auth, data=data, headers=headers, timeout=5
+            )
+        except Exception as e:
+            await client.close()
+            raise e
+
+        try:
+            reqText = await req.text()
+            regex = r"^access_token=([0-z]+)&.*&expires=(\d+)$"
+            matches = re.match(regex, reqText)
+
+            self.token = matches[1]
+            self.expires = datetime.now().timestamp() + int(matches[2])
+
+            self.retries = 0
+
+        except:
+            await client.close()
+            raise AuthenticationFailedException
+
+        await client.close()
+        return self.token
+
+    async def run(self):
+        """Connects to homee after acquiring an access token and runs until the connection is closed. Should be used in combination with asyncio.create_task."""
+
+        self.shouldClose = False
+        initial_connect = True
+
+        # Reconnect loop to avoid recursive reconnects
+        while initial_connect or (
+            not self.shouldClose
+            and self.shouldReconnect
+            and self.retries < self.maxRetries
+        ):
+            initial_connect = False
+
+            # Sleep after reconnect
+            if self.retries > 0:
+                await asyncio.sleep(self.reconnectInterval * self.retries)
+
+            try:
+                await self.get_access_token()
+            except:
+                # Reconnect
+                self.retries += 1
+                continue
+
+            await self.open_ws()
+
+        # Handle max retries
+        if self.retries >= self.maxRetries:
+            await self.on_max_retries()
+
+    def start(self):
+        """Wraps run() with asyncio.create_task() and returns the resulting task."""
+        return asyncio.create_task(self.run())
+
+    async def open_ws(self):
+        """Opens the websocket connection assuming an access token was already received. Runs until connection is closed again."""
+
+        self._log("Opening websocket...")
+
+        if self.retries > 0:
+            await self.on_reconnect()
+
+        try:
+            async with websockets.connect(
+                uri=f"{self.ws_url}/connection?access_token={self.token}",
+                subprotocols=["v2"],
+            ) as ws:
+                await self._ws_on_open()
+
+                # Start Ping
+                asyncio.create_task(self._ws_ping_handler(ws))
+
+                while (not self.shouldClose) and self.connected:
+                    try:
+                        receive_task = asyncio.ensure_future(
+                            self._ws_receive_handler(ws)
+                        )
+                        send_task = asyncio.ensure_future(self._ws_send_handler(ws))
+                        done, pending = await asyncio.wait(
+                            [receive_task, send_task],
+                            return_when=asyncio.FIRST_COMPLETED,
+                        )
+
+                        exceptions = []
+
+                        # Kill pending tasks
+                        for task in pending:
+                            task.cancel()
+
+                        # Check if we finished with an exception
+                        for task in done:
+                            exceptions.append(task.exception())
+
+                        if exceptions and exceptions[0] is not None:
+                            raise exceptions[0]
+
+                    except websockets.exceptions.ConnectionClosed as e:
+                        self.connected = False
+                        ws.abort_pings()
+                        await self.on_disconnected()
+        except Exception as e:
+            await self._ws_on_error(e)
+
+        self.retries += 1
+        await self._ws_on_close()
+
+    async def _ws_receive_handler(self, ws: websockets.WebSocketClientProtocol):
+        try:
+            msg = await ws.recv()
+            await self._ws_on_message(msg)
+        except websockets.exceptions.ConnectionClosedOK:
+            return
+        except Exception as e:
+            if not self.shouldClose:
+                raise e
+
+    async def _ws_send_handler(self, ws: websockets.WebSocketClientProtocol):
+        try:
+            msg = await self._message_queue.get()
+            if self.connected and not self.shouldClose:
+                await ws.send(msg)
+        except Exception as e:
+            if not self.shouldClose:
+                raise e
+
+    async def _ws_ping_handler(self, ws: websockets.WebSocketClientProtocol):
+        if self.pingInterval <= 0:
+            return
+
+        while self.connected and not self.shouldClose and ws.open:
+            await ws.ping()
+            self._log("PING!")
+            await asyncio.sleep(self.pingInterval)
+
+    async def _ws_on_open(self):
+        """Websocket on_open callback."""
+
+        self._log("Connection to websocket successfull")
+
+        self.connected = True
+
+        await self.on_connected()
+        self.retries = 0
+
+        await self.send("GET:all")
+
+    async def _ws_on_message(self, msg: str):
+        """Websocket on_message callback."""
+
+        await self._handle_message(json.loads(msg))
+
+    async def _ws_on_close(self):
+        """Websocket on_close callback."""
+        # if not self.shouldClose and self.retries <= 1:
+
+        self.connected = False
+        self._disconnected_event.set()
+
+        await self.on_disconnected()
+
+    async def _ws_on_error(self, error):
+        """Websocket on_error callback."""
+
+        self._log(f"An error occurred: {error}")
+        await self.on_error(error)
+
+    async def send(self, msg: str):
+        """Send a raw string message to homee."""
+
+        if not self.connected or self.shouldClose:
+            return
+
+        await self._message_queue.put(msg)
+
+    async def reconnect(self):
+        """Start a reconnection attempt."""
+
+        self._log(
+            f"Attempting to reconnect in {self.reconnectInterval * self.retries} seconds..."
+        )
+
+        await asyncio.sleep(self.reconnectInterval * self.retries)
+        await self.run()
+
+    def disconnect(self):
+        """Disconnect from homee by closing the websocket connection."""
+
+        self.shouldClose = True
+
+    async def _handle_message(self, msg: dict):
+        """Internal handleing of incoming homee messages."""
+
+        msgType = None
+
+        try:
+            msgType = list(msg)[0]
+        except:
+            self._log(f"Invalid message: {msg}")
+            await self.on_error()
+            return
+
+        self._log(msg)
+
+        if msgType == "all":
+            self.settings = HomeeSettings(msg["all"]["settings"])
+
+            # Create / Update nodes
+            if len(self.nodes) <= 0:
+                self.nodes = list(map(lambda n: HomeeNode(n), msg["all"]["nodes"]))
+            else:
+                for node_data in msg["all"]["nodes"]:
+                    self._update_or_create_node(node_data)
+
+            # Update / Create groups
+            if len(self.groups) <= 0:
+                self.groups = list(map(lambda g: HomeeGroup(g), msg["all"]["groups"]))
+            else:
+                for group_data in msg["all"]["groups"]:
+                    self._update_or_create_group(group_data)
+
+            self._update_or_create_relationships(msg["all"]["relationships"])
+
+            self._remap_relationships()
+            self._connected_event.set()
+
+        elif msgType == "attribute":
+            await self._handle_attribute_change(msg["attribute"])
+        elif msgType == "groups":
+            for data in msg["groups"]:
+                self._update_or_create_group(data)
+        elif msgType == "node":
+            self._update_or_create_node(msg["node"])
+        elif msgType == "nodes":
+            for data in msg["nodes"]:
+                self._update_or_create_node(data)
+        elif msgType == "relationships":
+            self._update_or_create_relationships(msg["relationships"])
+            self._remap_relationships()
+        elif msgType == "group":
+            self._update_or_create_group(msg["group"])
+        elif msgType == "relationship":
+            self._update_or_create_relationship(msg["relationship"])
+        else:
+            self._log(f"Unknown/Unsupported message type: {msgType}")
+
+        await self.on_message(msg)
+
+    async def _handle_attribute_change(self, attribute_data: dict):
+        """Internal handleling of an attribute changed message."""
+
+        self._log(f"Updating attribute {attribute_data['id']}")
+
+        # try:
+        attrNodeId = attribute_data["node_id"]
+        node = self.get_node_by_id(attrNodeId)
+        if node != None:
+            node._update_attribute(attribute_data)
+            await self.on_attribute_updated(attribute_data, node)
+        # except:
+        #     self._log("Unable to update attribute")
+
+    def _update_or_create_node(self, node_data: dict):
+        existingNode = self.get_node_by_id(node_data["id"])
+        if existingNode != None:
+            existingNode._data = node_data
+            existingNode._update_attributes(node_data["attributes"])
+        else:
+            self.nodes.append(HomeeNode(node_data))
+            self._remap_relationships()
+
+    def _update_or_create_group(self, data: dict):
+        group = self.get_group_by_id(data["id"])
+        if group is not None:
+            group._data = data
+        else:
+            self.groups.append(HomeeGroup(data))
+            self._remap_relationships()
+
+    def _update_or_create_relationship(self, data: dict):
+        relationship: HomeeRelationship = next(
+            [r for r in self.relationships if r.id == data["id"]], None
+        )
+
+        if relationship is not None:
+            relationship._data = data
+        else:
+            self.relationships.append(HomeeRelationship(data))
+        self._remap_relationships()
+
+    def _update_or_create_relationships(self, data: dict):
+        if len(self.relationships) <= 0:
+            self.relationships = list(map(lambda r: HomeeRelationship(r), data))
+        else:
+            for relationship_data in data:
+                self._update_or_create_relationship(relationship_data)
+
+    def _remap_relationships(self):
+        """Remap the relationships between nodes and groups defined by the relationships list."""
+
+        # Clear existing relationships
+        for n in self.nodes:
+            n.groups.clear()
+        for g in self.groups:
+            g.nodes.clear()
+
+        for r in self.relationships:
+            node = self.get_node_by_id(r.node_id)
+            group = self.get_group_by_id(r.group_id)
+
+            if node is not None and group is not None:
+                node.groups.append(group)
+                group.nodes.append(node)
+
+    def get_node_index(self, nodeId: int) -> int:
+        """Returns the index of the node with the given id or -1 if no node with the given id exists."""
+        return next((i for i, node in enumerate(self.nodes) if node.id == nodeId), -1)
+
+    def get_node_by_id(self, nodeId: int) -> HomeeNode:
+        """Returns the node with the given id or `None` if no node with the given id exists."""
+        index = self.get_node_index(nodeId)
+        return self.nodes[index] if index != -1 else None
+
+    def get_group_index(self, groupId: int) -> int:
+        """Returns the index of the group with the given id or -1 if no group with the given id exists."""
+        return next(
+            (i for i, group in enumerate(self.groups) if group.id == groupId), -1
+        )
+
+    def get_group_by_id(self, groupId: int) -> HomeeGroup:
+        """Returns the group with the given id or `None` if no group with the given id exists."""
+        index = self.get_group_index(groupId)
+        return self.groups[index] if index != -1 else None
+
+    async def set_value(self, deviceId: int, attributeId: int, value: float):
+        """Set the target value of an attribute of a device."""
+
+        self._log(f"Set value: Device: {deviceId} Attribute: {attributeId} To: {value}")
+        await self.send(
+            f"PUT:/nodes/{deviceId}/attributes/{attributeId}?target_value={value}"
+        )
+
+    async def play_homeegram(self, homeegramId: int):
+        """Invoke a homeegram."""
+
+        await self.send(f"PUT:homeegrams/{homeegramId}?play=1")
+
+    @property
+    def url(self):
+        """Local homee url."""
+
+        return f"http://{self.host}:7681"
+
+    @property
+    def ws_url(self):
+        """Local homee websocket url."""
+
+        return f"ws://{self.host}:7681"
+
+    def wait_until_connected(self):
+        """"Returns a coroutine that runs until a connection has been established and the initial data has been received."""
+        return self._connected_event.wait()
+
+    def wait_until_disconnected(self):
+        """Returns a coroutine that runs until the connection has been closed."""
+        return self._disconnected_event.wait()
+
+    async def on_reconnect(self):
+        """Called right before a reconnection attempt is started."""
+
+    async def on_max_retries(self):
+        """Called if the maximum amount of retries was reached."""
+
+    async def on_connected(self):
+        """Called once the websocket connection has been established."""
+
+    async def on_disconnected(self):
+        """Called after the websocket connection has been closed."""
+
+    async def on_error(self, error: str = None):
+        """Called after an error has occurred."""
+
+    async def on_message(self, msg: dict):
+        """Called when the websocket receives a message. The message is automatically parsed from json into a dictionary."""
+
+    async def on_attribute_updated(self, attribute_data: dict, node: HomeeNode):
+        """Called when an 'attribute' message was received and an attribute was updated. Contains the parsed json attribute data and the corresponding node instance."""
+
+    def _log(self, msg: str):
+        logging.debug(msg)
+
+
+class HomeeException(Exception):
+    """Base class for all errors thrown by this library"""
+
+
+class AuthenticationFailedException(HomeeException):
+    """Raised if no valid access token could be acquired."""
