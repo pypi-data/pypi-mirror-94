cfaa02cbc36a9dd808d0e02fb6df006bd1357f53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:10 +0000

    Add package kiwi-naomi-dev/0.0.0/kiwi-naomi-dev-0.0.0.tar.gz

---
+++ packages/kiwi-naomi-dev/kiwi-naomi-dev-0.0.0.tar.gz/kiwi-naomi-dev-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = 'kiwi-naomi-dev',
+    version = '0.0.0',
+    description = 'This is a dummy package.',
+)
+
+raise RuntimeError("This is a dummy package that was never ment to be actually installed. Please contact platform(at)kiwi.com.")
