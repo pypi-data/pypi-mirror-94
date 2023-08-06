8c431dc900825c72402d418848409ef10f077d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:18 +0000

    Add package distributions-probability-dyy/1.1/distributions_probability_dyy-1.1.tar.gz

---
+++ packages/distributions-probability-dyy/distributions_probability_dyy-1.1.tar.gz/distributions_probability_dyy-1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name = 'distributions_probability_dyy',
+		version = '1.1',
+		description = 'Gaussian distribution',
+		packages = ['distributions_probability_dyy'],
+		author = 'Daniela Yassuda Yamashita',
+		zip_safe = False)
