68398576b4765cea2703f555938d174991e98ffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:42 +0000

    Add package createsend/6.1.1/createsend-6.1.1-py2.py3-none-any.whl

---
+++ packages/createsend/createsend-6.1.1-py2.py3-none-any.whl/createsend/__init__.py
@@ -0,0 +1,37 @@
+from __future__ import absolute_import
+
+# -*- coding: utf-8 -*-
+__title__ = 'createsend-python'
+__author__ = 'Campaign Monitor'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017'
+
+
+from createsend.utils import (
+    CertificateError,
+    VerifiedHTTPSConnection,
+    json_to_py,
+    dict_to_object,
+    get_faker)
+from createsend.administrator import Administrator
+from createsend.campaign import Campaign
+from createsend.client import Client
+from createsend.createsend import (
+    CreateSendError,
+    ClientError,
+    ServerError,
+    BadRequest,
+    Unauthorized,
+    NotFound,
+    Unavailable,
+    ExpiredOAuthToken,
+    CreateSendBase,
+    CreateSend)
+from createsend.journey_email import JourneyEmail
+from createsend.journey import Journey
+from createsend.list import List
+from createsend.person import Person
+from createsend.segment import Segment
+from createsend.subscriber import Subscriber
+from createsend.template import Template
+from createsend.transactional import Transactional
