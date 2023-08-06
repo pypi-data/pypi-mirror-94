a1d85e8d9b4eaeacdcf33d0abbdec8508c88a39e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:56 +0000

    Add package evpy/1.0.6/evpy-1.0.6.tar.gz

---
+++ packages/evpy/evpy-1.0.6.tar.gz/evpy-1.0.6/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+setup(
+      name='evpy',
+      version='1.0.6',
+      description='A phython package to predict the efficiency/size of electronic powertrain components',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      author='Dalton Chancellor',
+      license='MIT',
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.7',
+          ],
+      packages=["evpy_"],
+      include_package_data=True,
+      install_requires=['scipy','numpy'],
+      entry_points={
+          'console_scripts': [
+                'GUI3=GUI3.py']})
