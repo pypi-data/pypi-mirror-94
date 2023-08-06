39d1394fc364be9f06c5a7cf293d352c3d1b3931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:07 +0000

    Add package StrEnum/0.4.4/StrEnum-0.4.4.tar.gz

---
+++ packages/StrEnum/StrEnum-0.4.4.tar.gz/StrEnum-0.4.4/strenum/__init__.py
@@ -0,0 +1,64 @@
+try:
+    import aenum as enum
+except ImportError:
+    import enum
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+__version_info__ = tuple(int(n) for n in __version__.partition("+")[0].split("."))
+del get_versions
+
+
+class StrEnum(str, enum.Enum):
+    """
+    StrEnum is a Python `enum.Enum` that inherits from `str` to complement
+    `enum.IntEnum` in the standard library.
+    """
+
+    def __new__(cls, value, *args, **kwargs):
+        if not isinstance(value, (str, enum.auto)):
+            raise TypeError(
+                "Values of StrEnums must be strings: {} is a {}".format(
+                    repr(value), type(value)
+                )
+            )
+        return super().__new__(cls, value, *args, **kwargs)
+
+    def __str__(self):
+        return str(self.value)
+
+    # pylint: disable=no-self-argument
+    # The first argument to this function is documented to be the name of the
+    # enum member, not `self`:
+    # https://docs.python.org/3.6/library/enum.html#using-automatic-values
+    def _generate_next_value_(name, *_):
+        return name
+
+
+class LowercaseStrEnum(StrEnum):
+    """
+    A subclass of `StrEnum` that the folds the member name to lowercase for the
+    `auto()` value.
+    """
+
+    # pylint: disable=no-self-argument
+    # The first argument to this function is documented to be the name of the
+    # enum member, not `self`:
+    # https://docs.python.org/3.6/library/enum.html#using-automatic-values
+    def _generate_next_value_(name, *_):
+        return name.lower()
+
+
+class UppercaseStrEnum(StrEnum):
+    """
+    A subclass of `StrEnum` that the folds the member name to uppercase for the
+    `auto()` value.
+    """
+
+    # pylint: disable=no-self-argument
+    # The first argument to this function is documented to be the name of the
+    # enum member, not `self`:
+    # https://docs.python.org/3.6/library/enum.html#using-automatic-values
+    def _generate_next_value_(name, *_):
+        return name.upper()
