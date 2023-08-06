c6c2769b38b23245ba46a912423f3af06f2ef04a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:30 +0000

    Add package pyqec/0.3.0/pyqec-0.3.0-cp39-cp39-manylinux2010_x86_64.whl

---
+++ packages/pyqec/pyqec-0.3.0-cp39-cp39-manylinux2010_x86_64.whl/pyqec/experiments.py
@@ -0,0 +1,54 @@
+from math import sqrt
+
+class Statistics:
+    def __init__(self):
+        self.number_of_successes = 0
+        self.number_of_failures = 0
+
+    def add_failure(self):
+        self.number_of_failures += 1
+
+    def add_success(self):
+        self.number_of_successes += 1
+
+    def sample_size(self):
+        return self.number_of_failures + self.number_of_successes
+
+    def failure_rate(self):
+        return self.number_of_failures / self.sample_size()
+
+    def success_rate(self):
+        return self.number_of_successes / self.sample_size()
+
+    def uncertainty(self):
+        assert(self.sample_size != 0)
+        return sqrt(
+            self.failure_rate() 
+            * self.success_rate() 
+            / self.sample_size()
+        )
+
+
+class ClassicalExperiment:
+    def __init__(self, code, decoder, noise):
+        self.code = code
+        self.decoder = decoder
+        self.noise = noise
+
+    def run_once(self):
+        error = self.noise.sample_error_of_length(self.code.block_size())
+        correction = self.decoder.decode(error)
+        return error == correction
+
+    def run_while(self, condition):
+        stats = Statistics()
+        while condition(stats):
+            if self.run_once():
+                stats.add_success()
+            else:
+                stats.add_failure()
+        return stats
+
+    def run_n_times(self, number_of_iterations):
+        self.run_while(lambda s: s.sample_size() < number_of_iterations)
+
