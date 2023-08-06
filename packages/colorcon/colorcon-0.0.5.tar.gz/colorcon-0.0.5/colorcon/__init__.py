95736b739bb68d6fbe20bb830305c6401c256d29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:26 +0000

    Add package colorcon/0.0.5/colorcon-0.0.5.tar.gz

---
+++ packages/colorcon/colorcon-0.0.5.tar.gz/colorcon-0.0.5/colorcon/__init__.py
@@ -0,0 +1,12 @@
+"""
+Colored consoles(Ansi formatting),
+Written in c++ by 'Arman Ahmadi' with (armanagha6@gmail.com) email!
+
+Ansi mode will enable automatically
+
+Supported OS's:
+- windows(MS-DOS Console)
+- unix base(bash)
+"""
+from .colored import *
+from colorcon._infs import *
