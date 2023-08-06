c3c9128ed95dd9090c183dcd25b4c90939528ee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:34 +0000

    Add package mud-py/1.1.0/mud-py-1.1.0.tar.gz

---
+++ packages/mud-py/mud-py-1.1.0.tar.gz/mud-py-1.1.0/mudpy/__init__.py
@@ -0,0 +1,9 @@
+from mudpy import containers
+from mudpy import mud_friendly
+from mudpy.mdata import mdata
+
+import os
+
+__all__ = ['mdata','containers','mud_friendly']
+__version__ = '1.1.0'
+__author__ = 'Derek Fujimoto'
