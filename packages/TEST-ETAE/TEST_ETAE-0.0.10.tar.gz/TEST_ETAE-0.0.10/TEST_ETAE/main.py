8a75d1b19a62c96a5937f46c72a1da6bc5405b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:45 +0000

    Add package TEST-ETAE/0.0.10/TEST_ETAE-0.0.10.tar.gz

---
+++ packages/TEST-ETAE/TEST_ETAE-0.0.10.tar.gz/TEST_ETAE-0.0.10/TEST_ETAE/main.py
@@ -0,0 +1,57 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*- 
+# @File     :main.py
+# @Time     :2021/2/7
+# @Author   :jiawei.li
+# @Software :PyCharm
+# @Desc     :None
+
+
+# from TEST_ETAE.main import infer
+import pandas as pd
+import json
+import sys
+from ETAES.operations import excute_pipeline, build_configurations
+
+#
+# config_path = 'TEST_ETAE/cfgs/config.yaml'
+# waybills_df = pd.read_csv('TEST_ETAE/data/waybills.csv')
+# waybills_df.drop(['Unnamed: 0', 'delivery_time'], axis=1, inplace=True)
+# waybills_str = waybills_df[:3].to_json(orient="records")
+# inputs = json.loads(waybills_str)
+# kwargs = build_configurations(config_path)
+# kwargs['basic']['task_id'] = '1612750425'
+
+
+def infer(kwargs, inputs):
+    excute_pipeline(kwargs, inputs)
+
+# infer()
+# if __name__ == '__main__':
+#     waybills_df = pd.read_csv('TEST_ETAE/data/waybills.csv')
+#     waybills_df.drop(['Unnamed: 0', 'delivery_time'], axis=1, inplace=True)
+#     waybills_str = waybills_df[:3].to_json(orient="records")
+#     waybills_js = json.loads(waybills_str)
+#
+#     # def find_config_from_taskid(task_id):
+#     #     config_path = 'cfgs/config.yaml'
+#     #     return build_configurations(config_path)
+#     #
+#     # task_id = '1612691303'
+#     # kwargs = find_config_from_taskid(task_id)
+#     # kwargs['basic']['task_id'] = task_id
+#     #
+#     # result = infer(kwargs, waybills_js)
+#     # print(result)
+#
+#     _, config_path = sys.argv
+#     kwargs = build_configurations(config_path)
+#     if kwargs['basic']['status'] == 'online':
+#         kwargs['basic']['task_id'] = '1612750425'
+#         waybills_df = pd.read_csv('TEST_ETAE/data/waybills.csv')
+#         waybills_df.drop(['Unnamed: 0'], axis=1, inplace=True)
+#         waybills_str = waybills_df[:2].to_json(orient="records")
+#         waybills_js = json.loads(waybills_str)
+#         excute_pipeline(kwargs, waybills_js)
+#     else:
+#         excute_pipeline(kwargs, js=None)
