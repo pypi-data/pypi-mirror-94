bc5db006d74ba0f2a3edc222318ea930b696898c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:31 +0000

    Add package trainml/0.1.1/trainml-0.1.1.tar.gz

---
+++ packages/trainml/trainml-0.1.1.tar.gz/trainml-0.1.1/setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+
+setup(
+    name="trainml",
+    version="0.1.1",
+    description="trainML client SDK and command line utilities",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/trainML/trainml-cli",
+    author="trainML",
+    author_email="support@trainml.ai",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=True,
+    python_requires=">=3.8",
+    install_requires=[
+        "aiohttp",
+        "boto3",
+        "python-jose[cryptography]",
+        "requests",
+    ],
+)
