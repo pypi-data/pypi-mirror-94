e64a1846492bde799ef35b5fcc36dfe8fb3f328b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:41 +0000

    Add package furhat-remote-api/0.3.1/furhat-remote-api-0.3.1.tar.gz

---
+++ packages/furhat-remote-api/furhat-remote-api-0.3.1.tar.gz/furhat-remote-api-0.3.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="furhat-remote-api",
+    version="0.3.1",
+    install_requires=[
+            "certifi>=2017.4.17",
+            "python-dateutil>=2.1",
+            "six>=1.10",
+            "urllib3>=1.23"
+        ],
+    description="Furhat Remote API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://docs.furhat.io/remote-api",
+    packages=find_packages(),
+    include_package_data=True,
+    author="Furhat Robotics",
+    author_email="support@furhatrobotics.com"
+)
