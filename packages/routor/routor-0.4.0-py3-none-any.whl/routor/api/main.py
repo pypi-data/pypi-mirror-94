53d432d361a2792df97d21056a0c4481af4c68eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:49 +0000

    Add package routor/0.4.0/routor-0.4.0-py3-none-any.whl

---
+++ packages/routor/routor-0.4.0-py3-none-any.whl/routor/api/main.py
@@ -0,0 +1,60 @@
+import logging
+from functools import lru_cache
+from typing import List
+
+from fastapi import Depends, FastAPI
+
+from .. import engine
+from .. import models as engine_models
+from .. import weights
+from . import config, models
+
+logger = logging.getLogger()
+app = FastAPI()
+
+
+@lru_cache()
+def get_settings() -> config.Settings:
+    """
+    Return application settings.
+    """
+    logger.debug("initialise settings")
+    return config.Settings()
+
+
+def get_engine(
+    settings: config.Settings = Depends(get_settings),  # noqa: B008
+) -> engine.Engine:
+    """
+    Return an initialised routing engine.
+
+    This is a singletone and the engine is only initialised once.
+    """
+    cached_value = getattr(get_engine, "__cache", None)
+    if not cached_value:
+        logger.debug("initialise engine")
+        cached_value = engine.Engine(settings.map_path)
+        get_engine.__cache = cached_value  # type: ignore
+    return cached_value
+
+
+@app.get("/weights", response_model=List[str])
+def read_weights() -> List[str]:
+    """
+    Return all possible weight functions.
+    """
+    return weights.get_function_names()
+
+
+@app.get("/route", response_model=engine_models.Route)
+def read_route(
+    data: models.RouteRequest,
+    engine: engine.Engine = Depends(get_engine),  # noqa: B008
+) -> engine_models.Route:
+    """
+    Calculate a route from A to B.
+    """
+    weight_func = weights.get_function(data.weight)
+
+    route = engine.route(data.origin, data.destination, weight_func)
+    return route
---
+++ packages/routor/routor-0.4.0-py3-none-any.whl/routor/engine.py
@@ -0,0 +1,141 @@
+import logging
+from pathlib import Path
+from typing import Any, List, Optional, Tuple
+
+import networkx
+import osmnx
+from more_itertools import pairwise
+from networkx_astar_path import astar_path
+
+from . import exceptions, models
+from .utils.debug import timeit
+from .utils.graph import load_map
+from .weights import WeightFunction
+
+logger = logging.getLogger()
+
+
+class Engine:
+    graph: networkx.DiGraph
+
+    @timeit
+    def __init__(self, map_path: Path) -> None:
+        logger.info("Initialise engine")
+        self.graph = load_map(map_path)
+        logger.info(
+            f"Map loaded (edges: {len(self.graph.edges)}, nodes: {len(self.graph.nodes)})"
+        )
+
+    @timeit
+    def find_path(
+        self, origin: models.Node, destination: models.Node, weight: WeightFunction
+    ) -> List[models.Node]:
+        """
+        Calculate a route using the given weight.
+        """
+        if origin.node_id not in self.graph.nodes:
+            raise exceptions.NodeDoesNotExist(f"{origin} does not exists.")
+        if origin.node_id not in self.graph.nodes:
+            raise exceptions.NodeDoesNotExist(f"{origin} does not exists.")
+
+        def _weight_wrapper(
+            graph: networkx.DiGraph,
+            prev_edge_nodes: Optional[Tuple[Any, Any]],
+            edge_nodes: Tuple[Any, Any],
+        ) -> float:
+            prev_edge: Optional[models.Edge] = None
+            if prev_edge_nodes:
+                prev_edge = models.Edge.from_graph(self.graph, *prev_edge_nodes)
+            edge = models.Edge.from_graph(self.graph, *edge_nodes)
+
+            return weight(prev_edge, edge)
+
+        logger.info(
+            f"Calculating path from {origin.osm_id} to {destination.osm_id} with {weight}"
+        )
+        path = astar_path(
+            self.graph, origin.node_id, destination.node_id, weight=_weight_wrapper
+        )
+        logger.info(f"Found path with {len(path)} items.")
+        return [models.Node.from_graph(self.graph, node_id) for node_id in path]
+
+    @timeit
+    def route(
+        self,
+        origin: models.Location,
+        destination: models.Location,
+        weight: WeightFunction,
+    ) -> models.Route:
+        """
+        Calculate a shortest path.
+        """
+        origin_node = self.get_closest_node(origin)
+        destination_node = self.get_closest_node(destination)
+
+        path = self.find_path(origin_node, destination_node, weight=weight)
+        costs = self.costs_for_path(path, weight=weight)
+        length = self.length_of_path(path)
+        travel_time = self.travel_time_of_path(path)
+
+        route = models.Route(
+            costs=round(costs, 2),
+            length=round(length, 2),
+            travel_time=round(travel_time, 2),
+            path=[models.Location(**node.dict()) for node in path],
+        )
+        return route
+
+    @timeit
+    def costs_for_path(self, path: List[models.Node], weight: WeightFunction) -> float:
+        """
+        Calculate the costs for a given path.
+        """
+        edges = (
+            models.Edge.from_nodes(self.graph, start, end)
+            for start, end in pairwise(path)
+        )
+
+        costs: float = 0
+        for index, (prev_edge, edge) in enumerate(pairwise(edges)):
+            if index == 0:
+                costs = weight(None, prev_edge)
+
+            costs += weight(prev_edge, edge)
+
+        return costs
+
+    @timeit
+    def length_of_path(self, path: List[models.Node]) -> float:
+        """
+        Calculate the length of a given path.
+        """
+        edges = (
+            models.Edge.from_nodes(self.graph, start, end)
+            for start, end in pairwise(path)
+        )
+
+        return sum(edge.length for edge in edges)
+
+    @timeit
+    def travel_time_of_path(self, path: List[models.Node]) -> float:
+        """
+        Calculate the travel time of a given path.
+        """
+        edges = (
+            models.Edge.from_nodes(self.graph, start, end)
+            for start, end in pairwise(path)
+        )
+
+        return sum(edge.travel_time for edge in edges)
+
+    @timeit
+    def get_closest_node(self, location: models.Location) -> models.Node:
+        """
+        Get the closest node to a GPS location.
+        """
+        node_id = osmnx.get_nearest_node(
+            self.graph, (location.latitude, location.longitude)
+        )
+        node = models.Node.from_graph(self.graph, node_id)
+        logger.info(f"Found closest node for {location} is {node.osm_id}")
+        return node
