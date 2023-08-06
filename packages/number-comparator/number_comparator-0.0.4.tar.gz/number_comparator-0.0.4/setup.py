a6863ac630cdc1fa0507b41f7f74ec7235d1b7f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:57 +0000

    Add package number-comparator/0.0.4/number_comparator-0.0.4.tar.gz

---
+++ packages/number-comparator/number_comparator-0.0.4.tar.gz/number_comparator-0.0.4/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="number_comparator",
+    version="0.0.4",
+    author="Diego Ramirez",
+    author_email="dr01191115@gmail.com",
+    description="A numeric comparator package.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Education",
+        "Intended Audience :: Developers",
+        'Topic :: Software Development :: Build Tools',
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development"
+    ],
+    keywords="numbers number comparations operations python",
+    python_requires='>=3.7',
+)
