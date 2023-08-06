56044922921901dca91ee14bc0877039c23e6766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:39 +0000

    Add package tn-radio/0.0.3/tn_radio-0.0.3.tar.gz

---
+++ packages/tn-radio/tn_radio-0.0.3.tar.gz/tn_radio-0.0.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tn_radio", # Replace with your own username
+    version="0.0.3",
+    author="THAVASIGTI",
+    author_email="ganeshanthavasigti1032000@gmail.com",
+    description="A small names package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+    install_requires=["python-vlc"]
+)
