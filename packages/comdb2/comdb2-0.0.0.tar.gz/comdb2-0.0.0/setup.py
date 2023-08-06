cbedb2dae27642273a37e5fb831ad6c08d524515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:06 +0000

    Add package comdb2/0.0.0/comdb2-0.0.0.tar.gz

---
+++ packages/comdb2/comdb2-0.0.0.tar.gz/comdb2-0.0.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='comdb2',
+    version='0.0.0',
+    author='Mario Corchero',
+    description='Reserved',
+    packages=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+    ]
+    )
