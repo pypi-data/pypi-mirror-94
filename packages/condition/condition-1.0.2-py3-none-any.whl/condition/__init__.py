3f2c5c113a10bcc411b78d5a4399c5c331a9fc39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:31 +0000

    Add package condition/1.0.2/condition-1.0.2-py3-none-any.whl

---
+++ packages/condition/condition-1.0.2-py3-none-any.whl/condition/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "1.0.2"
+
+from ._condition import (
+    Field,
+    FieldList,
+    FieldCondition,
+    CompositeCondition,
+    Condition,
+    And,
+    Or,
+    Operator,
+    get_test_df,
+)
