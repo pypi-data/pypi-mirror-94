bd25415b0aec37f9d192419c71babe1911a0b71a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:43 +0000

    Add package koinput/0.2.0a0/koinput-0.2.0a0.tar.gz

---
+++ packages/koinput/koinput-0.2.0a0.tar.gz/koinput-0.2.0a0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['koinput']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['colorama>=0.4.4,<0.5.0']
+
+setup_kwargs = {
+    'name': 'koinput',
+    'version': '0.2.0a0',
+    'description': 'Maximum simplification of Input / Output for text programs.',
+    'long_description': None,
+    'author': 'Николай Перминов',
+    'author_email': 'kolya-perminov@ya.ru',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
