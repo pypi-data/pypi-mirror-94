3ea71cea2872e5d1ec7504f15ceadd104a551ece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:54 +0000

    Add package fictive-sqlalchemy/0.0.1/fictive-sqlalchemy-0.0.1.tar.gz

---
+++ packages/fictive-sqlalchemy/fictive-sqlalchemy-0.0.1.tar.gz/fictive-sqlalchemy-0.0.1/setup.py
@@ -0,0 +1,29 @@
+
+import os
+import setuptools
+
+name = 'fictive-sqlalchemy'
+version = '0.0.1'
+author = 'Fictive Kin LLC'
+email = 'hello@fictivekin.com'
+classifiers = [
+    'Development Status :: 1 - Planning',
+    'Topic :: Software Development',
+]
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name=name,
+        version=version,
+        description='',
+        long_description='',
+        classifiers=classifiers,
+        url='',
+        author=author,
+        author_email=email,
+        maintainer=author,
+        maintainer_email=email,
+        packages=[],
+        install_requires=[],
+        scripts=[],
+    )
