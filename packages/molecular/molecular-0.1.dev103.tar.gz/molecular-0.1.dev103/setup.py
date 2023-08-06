f524d902a59b294c0540df4177598ad12dadf69c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:30 +0000

    Add package molecular/0.1.dev103/molecular-0.1.dev103.tar.gz

---
+++ packages/molecular/molecular-0.1.dev103.tar.gz/molecular-0.1.dev103/setup.py
@@ -0,0 +1,66 @@
+"""
+setup.py
+written in Python3
+author: C. Lockhart <chris@lockhartlab.org>
+"""
+
+import setuptools # noqa
+from numpy.distutils.core import Extension, setup
+
+# Read version
+with open('version.yml', 'r') as f:
+    data = f.read().splitlines()
+version_dict = dict([element.split(': ') for element in data])
+
+# Convert the version_data to a string
+version = '.'.join([str(version_dict[key]) for key in ['major', 'minor']])
+if version_dict['micro'] != 0:
+    version += '.' + version_dict['micro']
+print(version)
+
+# Read in requirements.txt
+with open('requirements.txt', 'r') as buffer:
+    requirements = buffer.read().splitlines()
+
+# Long description
+with open('README.rst', 'r') as buffer:
+    long_description = buffer.read()
+
+# First make sure numpy is installed
+# _setup(install_requires=['numpy'])
+
+# Then, install molecular
+setup(
+    name='molecular',
+    version=version,
+    author='C. Lockhart',
+    author_email='chris@lockhartlab.org',
+    description='A toolkit for molecular dynamics simulations',
+    long_description=long_description,
+    url="https://www.lockhartlab.org",
+    packages=[
+        'molecular',
+        'molecular.analysis',
+        'molecular.analysis.protein',
+        'molecular.bioinformatics',
+        'molecular.core',
+        'molecular.external',
+        'molecular.geometry',
+        'molecular.io',
+        'molecular.io.fortran',
+        'molecular.misc',
+        'molecular.simulations',
+        'molecular.statistics',
+        'molecular.transform',
+        'molecular.viz'
+    ],
+    install_requires=requirements,
+    include_package_data=True,
+    zip_safe=True,
+    # ext_modules=[
+    #     Extension('molecular.io.fortran.read_dcd', [
+    #         'molecular/io/fortran/read_dcd.f90',
+    #         'molecular/io/fortran/read_dcd.pyf'
+    #     ]),
+    # ]
+)
