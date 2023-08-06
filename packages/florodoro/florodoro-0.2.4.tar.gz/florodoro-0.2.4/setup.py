674964056098017e6d8396e80625830e8f112dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:52 +0000

    Add package florodoro/0.2.4/florodoro-0.2.4.tar.gz

---
+++ packages/florodoro/florodoro-0.2.4.tar.gz/florodoro-0.2.4/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from glob import glob
+from os import path
+
+script_location = path.abspath(path.dirname(__file__))
+
+setup(
+    # information about the package
+    name="florodoro",
+    version="0.2.4",
+    author="Tomáš Sláma",
+    author_email="tomas@slama.dev",
+    keywords="education pyqt5 plants pomodoro",
+    url="https://github.com/xiaoxiae/Florodoro",
+    description="A pomodoro timer that grows procedurally generated trees and flowers while you're studying.",
+    long_description=open(path.join(script_location, "README.md"), "r").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+    # where to look for files
+    packages=["florodoro"],
+    include_package_data = True,
+    package_data = {'florodoro': ["sounds/*", "images/*"]},
+    data_files=[("", ["LICENSE.txt", "README.md"])],
+
+    entry_points={'console_scripts': ['florodoro=florodoro.__init__:run']},
+
+    # requirements
+    install_requires=["pyqt5", "plyer"],
+    python_requires='>=3.7.1',
+)
