4852f317b5cc798acfd3453339975db9acd5099f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:18 +0000

    Add package METER/0.4.4/METER-0.4.4.tar.gz

---
+++ packages/METER/METER-0.4.4.tar.gz/METER-0.4.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='METER',
+    url='https://github.com/MR236/METER',
+    author='Malcolm Risk',
+    author_email='mr236@st-andrews.ac.uk',
+    packages=['METER'],
+    install_requires=['numpy', 'pandas', 'matplotlib'],
+    version='0.4.4',
+    license='CC0',
+    description='Multi-state Estimates for Time-to-Event Research',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    project_urls={'Documentation': 'https://meter1.readthedocs.io/en/latest/'}
+)
