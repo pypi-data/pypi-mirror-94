82a0f3098139d519bd273124a25385b6ff5b6e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:53 +0000

    Add package mxtheme/0.3.14/mxtheme-0.3.14.tar.gz

---
+++ packages/mxtheme/mxtheme-0.3.14.tar.gz/mxtheme-0.3.14/mxtheme/__init__.py
@@ -0,0 +1,14 @@
+from os import path
+from .card import CardDirective
+
+__version__ = '0.3.14'
+__version_full__ = __version__
+
+package_dir = path.dirname(path.abspath(__file__))
+
+def get_path():
+    return package_dir
+
+def setup(app):
+    app.add_html_theme('mxtheme', package_dir)
+    return {'version': __version__, 'parallel_read_safe': True}
