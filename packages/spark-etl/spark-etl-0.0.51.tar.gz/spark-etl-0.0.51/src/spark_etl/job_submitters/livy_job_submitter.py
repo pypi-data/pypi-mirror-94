ee2a8fd90295e99181603fcad433519743febc38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:03 +0000

    Add package spark-etl/0.0.51/spark-etl-0.0.51.tar.gz

---
+++ packages/spark-etl/spark-etl-0.0.51.tar.gz/spark-etl-0.0.51/src/spark_etl/job_submitters/livy_job_submitter.py
@@ -0,0 +1,139 @@
+import os
+import json
+import time
+import subprocess
+from urllib.parse import urlparse
+import uuid
+import tempfile
+
+from requests.auth import HTTPBasicAuth
+import requests
+
+from .abstract_job_submitter import AbstractJobSubmitter
+
+def _execute(host, cmd, error_ok=False):
+    r = subprocess.call(["ssh", "-q", host, cmd], shell=False)
+    if not error_ok and r != 0:
+        raise Exception(f"command {cmd} failed with exit code {r}")
+
+class LivyJobSubmitter(AbstractJobSubmitter):
+    def __init__(self, config):
+        super(LivyJobSubmitter, self).__init__(config)
+
+    # options is vendor specific arguments
+    # args.conf is the spark job config
+    # args is arguments need to pass to the main entry
+    def run(self, deployment_location, options={}, args={}, handlers=[], on_job_submitted=None):
+        bridge = self.config["bridge"]
+        stage_dir = self.config['stage_dir']
+        run_dir = self.config['run_dir']
+
+        # create run dir
+        run_id = str(uuid.uuid4())
+        _execute(bridge, f"mkdir -p {stage_dir}/{run_id}")
+        _execute(bridge, f"hdfs dfs -mkdir -p {run_dir}/{run_id}")
+
+        # generate input.json
+        tf = tempfile.NamedTemporaryFile(mode="wt", delete=False)
+        json.dump(args, tf)
+        tf.close()
+        subprocess.check_call([
+            'scp', '-q', tf.name, f"{bridge}:{stage_dir}/{run_id}/input.json"
+        ])
+        _execute(bridge, f"hdfs dfs -copyFromLocal {stage_dir}/{run_id}/input.json {run_dir}/{run_id}/input.json")
+        os.remove(tf.name)
+
+        o = urlparse(deployment_location)
+        if o.scheme != 'hdfs':
+            raise SparkETLDeploymentFailure("deployment_location must be in hdfs")
+
+        headers = {
+            "Content-Type": "application/json",
+            "X-Requested-By": "root",
+            'proxyUser': 'root'
+        }
+
+        config = {
+            'file': f'{deployment_location}/job_loader.py',
+            'pyFiles': [f'{deployment_location}/app.zip'],
+            'args': [
+                '--run-id', run_id,
+                '--run-dir', f"{run_dir}/{run_id}",
+                '--lib-url', f'{deployment_location}/lib.zip'
+            ]
+        }
+        config.update(options)
+        config.pop("display_name", None)  # livy job submitter does not support display_name
+
+        service_url = self.config['service_url']
+        if service_url.endswith("/"):
+            service_url = service_url[:-1]
+
+        username = self.config['username']
+        password = self.config['password']
+
+        print(json.dumps(config))
+        r = requests.post(
+            "{}/batches".format(service_url),
+            data=json.dumps(config),
+            headers=headers,
+            auth=HTTPBasicAuth(username, password),
+            verify=False
+        )
+        if r.status_code not in [200, 201]:
+            msg = "Failed to submit the job, status: {}, error message: \"{}\"".format(
+                r.status_code,
+                r.content
+            )
+            raise Exception(msg)
+
+        print('job submitted')
+        ret = json.loads(r.content.decode("utf8"))
+        if on_job_submitted is not None:
+            on_job_submitted(run_id, vendor_info=ret)
+        job_id = ret['id']
+        print('job id: {}'.format(job_id))
+        print(ret)
+        print('logs:')
+        for log in ret.get('log', []):
+            print(log)
+            
+        # pull the job status
+        while True:
+            r = requests.get(
+                "{}/batches/{}".format(service_url, job_id),
+                headers=headers,
+                auth=HTTPBasicAuth(username, password),
+                verify=False
+            )
+            if r.status_code != 200:
+                msg = "Failed to get job status, status: {}, error message: \"{}\"".format(
+                    r.status_code,
+                    r.content
+                )
+                raise Exception(msg)
+            ret = json.loads(r.content.decode("utf8"))
+            job_state = ret['state']
+            appId = ret.get('appId')
+            print('job_state: {}, applicationId: {}'.format(
+                job_state, appId
+
+            ))
+            if job_state in ['success', 'dead']:
+                # cmd = f"yarn logs -applicationId {appId}"
+                # host = self.config['bridge']
+                # subprocess.call(["ssh", "-q", "-t", host, cmd], shell=False)
+                print(f"job_state={job_state}")
+                if job_state == 'success':
+                    # get the result.json
+                    _execute(bridge, f"hdfs dfs -copyToLocal {run_dir}/{run_id}/result.json {stage_dir}/{run_id}/result.json")
+                    rf = tempfile.NamedTemporaryFile(mode="w", delete=False)
+                    rf.close()
+                    subprocess.check_call([
+                        'scp', '-q', f"{bridge}:{stage_dir}/{run_id}/result.json", rf.name
+                    ])
+
+                    with open(rf.name, "r") as f:
+                        return json.load(f)
+                raise Exception("Job failed")
+            time.sleep(5)
---
+++ packages/spark-etl/spark-etl-0.0.51.tar.gz/spark-etl-0.0.51/src/spark_etl/vendors/oracle/job_loader.py
@@ -0,0 +1,362 @@
+import argparse
+import importlib
+import os
+import io
+import errno
+import subprocess
+import sys
+import uuid
+import json
+from urllib.parse import urlparse
+import tempfile
+import time
+from datetime import datetime, timedelta
+from contextlib import redirect_stdout, redirect_stderr
+import code
+
+from pyspark.sql import SparkSession, SQLContext, Row
+
+{% if use_instance_principle %}
+USE_INSTANCE_PRINCIPLE = True
+{% else %}
+USE_INSTANCE_PRINCIPLE = False
+OCI_CONFIG = json.loads("""{{ oci_config_str }}""")
+OCI_KEY = """{{ oci_key }}"""
+{% endif %}
+
+class Channel:
+    def __init__(self, region, run_dir):
+        self.spark = None
+        self.region = region
+        self.run_dir = run_dir
+
+        o = urlparse(run_dir)
+        self.namespace = o.netloc.split('@')[1]
+        self.bucket = o.netloc.split('@')[0]
+        self.object_name_prefix = o.path[1:]  # drop the leading /
+
+    def bind(self, spark):
+        self.spark = spark
+    
+    def read_json(self, name):
+        if self.spark is None:
+            raise Exception("Channel: please bind first")
+        from oci_core import os_download_json
+        os_client = get_os_client_ex(self.spark, self.region)
+        return os_download_json(
+            os_client, 
+            self.namespace, self.bucket, 
+            f"{self.object_name_prefix}/{name}"
+        )
+
+    def has_json(self, name):
+        if self.spark is None:
+            raise Exception("Channel: please bind first")
+        from oci_core import os_has_object
+        os_client = get_os_client_ex(self.spark, self.region)
+        return os_has_object(
+            os_client, 
+            self.namespace, self.bucket, 
+            f"{self.object_name_prefix}/{name}"
+        )
+    
+    def write_json(self, name, payload):
+        if self.spark is None:
+            raise Exception("Channel: please bind first")
+        from oci_core import os_upload_json
+        os_client = get_os_client_ex(self.spark, self.region)
+        os_upload_json(
+            os_client, 
+            payload, 
+            self.namespace, self.bucket, 
+            f"{self.object_name_prefix}/{name}"
+        )
+    
+    def delete_json(self, name):
+        if self.spark is None:
+            raise Exception("Channel: please bind first")
+        from oci_core import os_delete_object
+        os_client = get_os_client_ex(self.spark, self.region)
+        os_delete_object(
+            os_client, 
+            self.namespace, self.bucket, 
+            f"{self.object_name_prefix}/{name}"
+        )
+    
+# lib installer
+def _install_libs(lib_url, run_id):
+    current_dir = os.getcwd()
+    base_dir    = os.path.join(current_dir, run_id)
+    lib_dir     = os.path.join(base_dir, 'python_libs')
+    lib_zip     = os.path.join(base_dir, 'lib.zip')
+    lock_name   = os.path.join(base_dir, '__lock__')
+
+    os.makedirs(base_dir, exist_ok=True)
+
+    for i in range(0, 100):
+        try:
+            lock_fh = os.open(lock_name, os.O_CREAT | os.O_EXCL | os.O_WRONLY)
+            os.close(lock_fh)
+            try:
+                if not os.path.isdir(lib_dir):
+                    print("_install_libs: install lib starts")
+                    os.makedirs(lib_dir)
+                    subprocess.check_call(['wget', "-O", lib_zip, lib_url])
+                    subprocess.check_call(['unzip', lib_zip, "-d", lib_dir])
+                    print("_install_libs: install lib done")
+                if lib_dir not in sys.path:
+                    print(f"_install_libs: add {lib_dir} path")
+                    sys.path.insert(0, lib_dir)
+                return 
+            finally:
+                os.remove(lock_name)
+        except OSError as e:
+            if e.errno == errno.EEXIST:
+                time.sleep(10)
+                continue
+            raise
+    
+    raise Exception("Failed to install libraries!")
+
+class Asker:
+    def __init__(self, run_dir, app_region):
+        self.spark = None
+        self.run_dir = run_dir
+        self.app_region = app_region
+    
+    def initialize(self, spark):
+        from oci_core import dfapp_get_os_client, get_delegation_token, get_os_client
+
+        if USE_INSTANCE_PRINCIPLE:
+            delegation_token = get_delegation_token(spark)
+            self.os_client = dfapp_get_os_client(self.app_region, delegation_token)
+        else:
+            with tempfile.NamedTemporaryFile(mode='w+t', delete=False) as key_f:
+                key_f.write(OCI_KEY)
+            _oci_config = dict(OCI_CONFIG)
+            _oci_config['key_file'] = key_f.name
+            self.os_client = get_os_client(None, config=_oci_config)
+
+    # place a request to the launcher
+    def __call__(self, content, timeout=timedelta(minutes=10)):
+        from oci_core import os_upload_json, os_download_json
+
+        request_id = str(uuid.uuid4())
+
+        o = urlparse(self.run_dir)
+        namespace = o.netloc.split('@')[1]
+        bucket = o.netloc.split('@')[0]
+        object_name_prefix = o.path[1:]  # drop the leading /
+
+        os_upload_json(self.os_client, content, namespace, bucket, f"{object_name_prefix}/to_submitter/ask_{request_id}.json")
+        answer_name = f"{object_name_prefix}/to_submitter/answer_{request_id}.json"
+        start_time = datetime.utcnow()
+        while True:
+            r = self.os_client.list_objects(
+                namespace, 
+                bucket,
+                prefix = answer_name,
+                limit = 1
+            )
+            if len(r.data.objects) == 0:
+                if (datetime.utcnow() - start_time) >= timeout:
+                    raise Exception("Ask is not answered: timed out")
+                time.sleep(5)
+                continue
+
+            return os_download_json(self.os_client, namespace, bucket, answer_name)
+
+
+def _save_result(os_client, run_dir, result):
+    from oci_core import os_upload_json
+    o = urlparse(run_dir)
+
+    namespace = o.netloc.split('@')[1]
+    bucket = o.netloc.split('@')[0]
+    object_name_prefix = o.path[1:]  # drop the leading /
+
+    os_upload_json(os_client, result, namespace, bucket, f"{object_name_prefix}/result.json")
+
+def get_os_client_ex(spark, region):
+    from oci_core import dfapp_get_os_client, get_delegation_token, get_os_client
+    if USE_INSTANCE_PRINCIPLE:
+        delegation_token = get_delegation_token(spark)
+        os_client = dfapp_get_os_client(region, delegation_token)
+    else:
+        with tempfile.NamedTemporaryFile(mode='w+t', delete=False) as key_f:
+            key_f.write(OCI_KEY)
+        _oci_config = dict(OCI_CONFIG)
+        _oci_config['key_file'] = key_f.name
+        os_client = get_os_client(None, config=_oci_config)
+    return os_client
+
+
+
+def _get_args(os_client, run_dir):
+    from oci_core import os_download_json
+    o = urlparse(run_dir)
+
+    namespace = o.netloc.split('@')[1]
+    bucket = o.netloc.split('@')[0]
+    object_name_prefix = o.path[1:]  # drop the leading /
+
+    return os_download_json(os_client, namespace, bucket, f"{object_name_prefix}/args.json")
+
+
+def _bootstrap():
+    parser = argparse.ArgumentParser(description='job')
+    parser.add_argument(
+        "--deployment-location", type=str, required=True, help="Deployment Location",
+    )
+    parser.add_argument(
+        "--run-id", type=str, required=True, help="Run ID",
+    )
+    parser.add_argument(
+        "--run-dir", type=str, required=True, help="Run Directory",
+    )
+    parser.add_argument(
+        "--lib-url", type=str, required=True, help="Library URL",
+    )
+    parser.add_argument(
+        "--app-region", type=str, required=True, help="Application Region",
+    )
+    args = parser.parse_args()
+
+    o = urlparse(args.run_dir)
+    if o.scheme != 'oci':
+        raise Exception("run_dir MUST be in OCI")
+
+    # Load application
+    spark = SparkSession.builder.appName("RunJob").getOrCreate()
+    sc = spark.sparkContext
+    sc.addPyFile(f"{args.deployment_location}/app.zip")
+    print("Application loaded")
+
+    subprocess.check_call(['wget', args.lib_url])
+
+    # The archive file goes into /opt/dataflow
+    # Load python libraries
+    current_dir = os.getcwd()
+    lib_dir = os.path.join(current_dir, 'python_libs')
+    os.mkdir(lib_dir)
+    subprocess.call([
+        'unzip', "lib.zip", "-d", lib_dir
+    ])
+    sys.path.insert(0, lib_dir)
+
+    run_id = args.run_id
+    run_dir = args.run_dir
+
+    # The entry is in a python file "main.py"
+    from oci_core import dfapp_get_os_client, get_delegation_token, get_os_client
+    if USE_INSTANCE_PRINCIPLE:
+        delegation_token = get_delegation_token(spark)
+        os_client = dfapp_get_os_client(args.app_region, delegation_token)
+    else:
+        with tempfile.NamedTemporaryFile(mode='w+t', delete=False) as key_f:
+            key_f.write(OCI_KEY)
+        _oci_config = dict(OCI_CONFIG)
+        _oci_config['key_file'] = key_f.name
+        os_client = get_os_client(None, config=_oci_config)
+    xargs = _get_args(os_client, run_dir)
+    entry = importlib.import_module("main")
+    result = entry.main(spark, xargs, sysops={
+        "install_libs": lambda : _install_libs(args.lib_url, run_id),
+        "ask": Asker(run_dir, args.app_region),
+        "cli_main": cli_main,
+        "channel": Channel(args.app_region, run_dir)
+    })
+
+    # user need to initialize ask with ask.initialize(spark) before using it
+
+    _save_result(os_client, run_dir, result)
+
+    # TODO: make a copy of the log file
+
+class PySparkConsole(code.InteractiveInterpreter):
+    def __init__(self, locals=None):
+        super(PySparkConsole, self).__init__(locals=locals)
+
+def handle_pwd(user_input, channel):
+    channel.write_json(
+        "cli-response.json", 
+        {
+            "status": "ok",
+            "output": os.getcwd()
+        }
+    )
+
+def handle_bash(user_input, channel):
+    cmd_buffer = '\n'.join(user_input['lines'])
+    f = io.StringIO()
+    with redirect_stdout(f):
+        p = subprocess.run(cmd_buffer, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    
+    channel.write_json(
+        "cli-response.json", 
+        {
+            "status": "ok",
+            "exit_code": p.returncode,
+            "output": p.stdout.decode('utf-8'),
+        }
+    )
+
+def handle_python(user_input, console, channel):
+    source = '\n'.join(user_input['lines'])
+    stdout_f = io.StringIO()
+    stderr_f = io.StringIO()
+    with redirect_stdout(stdout_f):
+        with redirect_stderr(stderr_f):
+            console.runsource(source, symbol="exec")
+    
+    channel.write_json(
+        "cli-response.json", 
+        {
+            "status": "ok",
+            "output": stdout_f.getvalue() + "\n" + stderr_f.getvalue() ,
+        }
+    )
+
+def cli_main(spark, args, sysops={}):
+    channel = sysops['channel']
+    channel.bind(spark)
+    console = PySparkConsole(locals={'spark': spark})
+
+    channel.write_json(
+        "cli-response.json", 
+        {
+            "status": "ok",
+            "output": "Welcome to OCI Spark-CLI Interface",
+        }
+    )
+
+    while True:
+        if not channel.has_json('cli-request.json'):
+            time.sleep(1)
+            continue
+
+        user_input = channel.read_json('cli-request.json')
+        channel.delete_json('cli-request.json')
+
+        if user_input["type"] == "@@quit":
+            channel.write_json(
+                "cli-response.json", 
+                {
+                    "status": "ok",
+                    "output": "Server quit gracefully",
+                }
+            )
+            break
+        if user_input["type"] == "@@pwd":
+            handle_pwd(user_input, channel)
+            continue
+        if user_input["type"] == "@@bash":
+            handle_bash(user_input, channel)
+            continue
+        if user_input["type"] == "@@python":
+            handle_python(user_input, console, channel)
+            continue
+    return {"status": "ok"}
+
+
+_bootstrap()
