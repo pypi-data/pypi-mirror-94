b8882250e98d1e8bebc8a5b438c49b7029714cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:04 +0000

    Add package dodfminer/1.3.2/dodfminer-1.3.2.tar.gz

---
+++ packages/dodfminer/dodfminer-1.3.2.tar.gz/dodfminer-1.3.2/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+from dodfminer.__version__ import __version__
+
+def requirements_list():
+    list_of_req = []
+    with open('requirements.txt') as req:
+        for line in req:
+            list_of_req.append(line)
+
+    return list_of_req
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+print(requirements_list())
+
+setuptools.setup(
+    name="dodfminer",
+    version=__version__,
+    author="KnEDLe",
+    author_email="author@example.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/UnB-KnEDLe/DODFMiner",
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': ['dodfminer=dodfminer.run:run'],
+    },
+    install_requires=requirements_list(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    package_data={'dodfminer': ['extract/polished/acts/models/*.pkl', 'extract/polished/segmentation/model/*.pkl', 'extract/polished/segmentation/model/*.pkl']},
+    include_package_data = True,
+)
