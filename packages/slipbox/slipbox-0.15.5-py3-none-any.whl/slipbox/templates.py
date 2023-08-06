fe551561a4c2366f5c6887b30a689b7e47ddea9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:55 +0000

    Add package slipbox/0.15.5/slipbox-0.15.5-py3-none-any.whl

---
+++ packages/slipbox/slipbox-0.15.5-py3-none-any.whl/slipbox/templates.py
@@ -0,0 +1,34 @@
+"""HTML elements."""
+
+from __future__ import annotations
+from textwrap import indent
+from typing import Any, Union
+
+
+class Elem:
+    """Represents an HTML element."""
+    def __init__(self,
+                 tag: str,
+                 *children: Union[str, Elem],
+                 **attributes: Any):
+        self.tag = tag
+        self.children = list(children)
+        self.attributes = attributes.copy()
+
+
+def render(elem: Union[str, Elem], prefix: str = "") -> str:
+    """Render HTML from Elem or str."""
+    if isinstance(elem, str):
+        return prefix + elem
+    assert isinstance(elem, Elem)
+    attrs = ""
+    for key, val in elem.attributes.items():
+        attrs += f" {key}={val!r}"
+    if not elem.children:
+        return "<{tag}{attrs}></{tag}>".format(tag=elem.tag, attrs=attrs)
+    children = indent("\n".join(render(child) for child in elem.children),
+                      prefix + "  ")
+    return "<{tag}{attrs}>\n{children}\n</{tag}>".format(
+        tag=elem.tag,
+        attrs=attrs,
+        children=children)
