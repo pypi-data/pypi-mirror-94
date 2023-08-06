8c1b9000bffb2d5d5141b324813cb60cfae263d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:34 +0000

    Add package MACS3/3.0.0a6/MACS3-3.0.0a6.tar.gz

---
+++ packages/MACS3/MACS3-3.0.0a6.tar.gz/MACS3-3.0.0a6/docs/INSTALL.md
@@ -0,0 +1,158 @@
+# INSTALL Guide For MACS3
+Time-stamp: <2020-12-05 16:35:26 Tao Liu>
+
+Please check the following instructions to complete your installation.
+
+## Prerequisites
+
+### Python3
+
+MACS v3.x.x requires Python3. We have tested MACS in Python3.6, 3.7 and 3.8. 
+
+### NumPy
+
+MACS also requires [Numpy](http://www.scipy.org/Download) (>=1.17).
+
+### Cython
+
+[Cython](http://cython.org/) is required to translate .pyx codes to .c
+code. The version of Cython has to be >=0.29.
+
+### cykhash
+
+[cykhash](https://github.com/realead/cykhash) is a fast and efficient
+hash implementation in Cython. It is used to replace python dictionary
+in MACS3 codes. Since it requires Cython, make sure you install Cython
+first, then install cykhash. 
+
+### fermi-lite and simde
+
+A newly added `callvar` subcommand in MACS3 uses
+[fermi-lite](https://github.com/lh3/fermi-lite) to assemble the DNA
+sequence in a peak region while necessary. A modified fermi-lite has
+been included in MACS3 package. Since fermi-lite was implemented using
+intel SSE2 intrinsics for x86 CPUs, we added
+[simde](https://github.com/simd-everywhere/simde) as submodule to
+solve the compatibility issues on non-x86 architectures. Note that, we
+may remove this submodule and add simde in *dependencies* of MACS3
+later.
+
+### GCC and Python-dev 
+
+GCC is required to compile `.c` codes in MACS v3 package, and python 
+header files are needed. If you are using Mac OSX, I recommend you 
+install Xcode; if you are using Linux, you need to make sure 
+`python-dev` package is installed -- the actual package name depends 
+on the Linux OS distribution, you are using. 
+
+
+## Prepare a virtual Python environment 
+
+We strongly recommend installing your MACS program in a virtual
+environment, so that you have full control of your installation and
+won't mess up with your system libraries. To learn about virtual
+environment, read [this
+article](https://docs.python.org/3/library/venv.html). A simple way to
+create a virtual environment of Python3 is
+
+`$ python3 -m venv MyPythonEnv/`
+
+Then active it by
+
+`$ source MyPythonEnv/bin/active`
+
+## Install through PyPI
+
+The easiest way to install MACS is through PyPI system. Get `pip` if
+it's not available in your system. If you create a virtual environment
+as described before, your `pip` command will install everything under
+the folder you specified previously through `python3 -m env` command.
+
+Then under the command line, type `pip install macs3`. PyPI will
+install Numpy automatically if it is absent.
+
+To upgrade MACS3, type `pip install --upgrade macs3`. It will check
+currently installed MACS3, compare the version with the one on PyPI
+repository, download and install a newer version while necessary.
+
+If you plan to install MACS in your own user directory, use `pip
+install macs3 --user`.
+
+## Install from source
+
+MACS uses Python's [setuptools](https://setuptools.readthedocs.io) for
+source code installations. To install a source distribution of MACS,
+unpack the distribution tarball, or clone Git repository with `git
+clone git@github.com:taoliu/MACS.git`. Go to the directory where you
+unpacked MACS, and simply run the install script:
+
+ `$ python setup.py install`
+
+By default, the script will install python library and executable
+codes according to the environment. When you run the command under
+virtualenv, the script will install to the virtual environment
+instead. When you run it without virtual environment, you may need to
+be root or administrator of the machine so as to complete the
+installation. Please contact the system administrator if you want
+their help. If you need to provide a nonstandard install prefix, or
+any other nonstandard options, you can provide many command line
+options to the install script. Use the `--help` option to see a brief
+list of available options:
+
+ `$ python setup.py --help`
+
+For example, if I want to install everything under my own HOME
+directory, use this command:
+
+ `$ python setup.py install --prefix /home/myaccount/`
+
+As mentioned in *Prerequisites*, you don't need to install Cython in
+order to install MACS. When Cython is available, this setup script
+will regenerate C codes from Pyx codes when necessary. When Cython is
+not available, this setup script will just use the C codes included in
+the release package (or your Github clone) for installation.
+
+## Configure environment variables
+
+*Note*, if you are using a virtual environment, you should skip this
+section since all the corresponding environment variables have been
+correctly set while you `activate` the environment.
+
+After running the setup script, you might need to add the install
+location to your `PYTHONPATH` and `PATH` environment variables. The
+process for doing this varies on each platform, but the general
+concept is the same across platforms.
+
+### PYTHONPATH
+
+To set up your `PYTHONPATH` environment variable, you'll need to add
+the value `PREFIX/lib/pythonX.Y/site-packages` to your existing
+`PYTHONPATH`. In this value, X.Y stands for the major–minor version of
+Python you are using (such as 3.7; you can find this with
+`sys.version[:3]` from a Python command line). `PREFIX` is the install
+prefix where you installed MACS. If you did not specify a prefix on
+the command line, MACS will be installed using Python's sys.prefix
+value.
+
+On Linux, using bash, I include the new value in my `PYTHONPATH` by
+adding this line to my `~/.bashrc`::
+
+ `$ export
+ PYTHONPATH=/home/taoliu/lib/python3.7/site-packages:$PYTHONPATH`
+
+Using Windows, you need to open up the system properties dialog and
+locate the tab labeled Environment. Add your value to the `PYTHONPATH`
+variable, or create a new `PYTHONPATH` variable if there isn't one
+already.
+
+### PATH
+
+Just like your `PYTHONPATH`, you'll also need to add a new value to
+your PATH environment variable so that you can use the MACS command
+line directly. Unlike the `PYTHONPATH` value, however, this time
+you'll need to add `PREFIX/bin` to your PATH environment variable. The
+process for updating this is the same as described above for the
+`PYTHONPATH` variable::
+
+ `$ export PATH=/home/myaccount/bin:$PATH`
+
---
+++ packages/MACS3/MACS3-3.0.0a6.tar.gz/MACS3-3.0.0a6/setup.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+"""Description:
+
+Setup script for MACS -- Model Based Analysis for ChIP-Seq data
+
+This code is free software; you can redistribute it and/or modify it
+under the terms of the BSD License (see the file LICENSE included with
+the distribution).
+"""
+
+import sys
+import os
+import re
+from setuptools import setup, Extension
+import subprocess
+import sysconfig
+import numpy
+
+# get MACS version
+exec(open("MACS3/Utilities/Constants.py").read())
+
+# classifiers
+classifiers =[\
+              'Development Status :: 3 - Alpha',
+              'Environment :: Console',
+              'Intended Audience :: Developers',
+              'Intended Audience :: Science/Research',
+              'License :: OSI Approved :: BSD License',
+              'Operating System :: MacOS :: MacOS X',
+              'Operating System :: POSIX',
+              'Operating System :: Unix',
+              'Topic :: Scientific/Engineering :: Bio-Informatics',
+              'Programming Language :: Python :: 3.6',
+              'Programming Language :: Python :: 3.7',
+              'Programming Language :: Python :: 3.8',
+              'Programming Language :: Cython', ]
+
+install_requires = [ "numpy>=1.17",
+                     "cykhash>=1.0.2",
+                     "Cython>=0.29" ]
+
+tests_requires = [ 'pytest' ]
+
+
+def main():
+    if float(sys.version[:3])<3.6:
+        sys.stderr.write("CRITICAL: Python version must >= 3.6!\n")
+        sys.exit(1)
+
+    # NumPy include dir
+    numpy_include_dir = [ numpy.get_include() ]
+
+    # CFLAG
+    # I intend to use -Ofast, however if gcc version < 4.6, this option is unavailable so...
+    # should I care about old gcc compiler?...
+    extra_c_args = ["-w","-Ofast", "-g0"] # for C, -Ofast implies -O3 and -ffast-math
+
+    # CFLAG for fermi-lite related codes
+    clang = False
+    icc = False
+    new_gcc = False
+    try:
+        if os.environ['CC'] == "clang":
+            clang = True
+    except KeyError:
+        pass
+
+    if not clang:
+        try:
+            gcc_version_check = subprocess.check_output( ["gcc", "--version"], universal_newlines=True)
+            if gcc_version_check.find("clang") != -1:
+                clang = True
+            else:
+                gcc_version_check = gcc_version_check.split('\n')[0] # get the first line
+                m = re.search( "\s+(\d+\.\d+)\.\d+", gcc_version_check )
+                if m:
+                    gcc_version = float( m[1] )
+                    if gcc_version > 4.8:
+                        new_gcc = True
+        except subprocess.CalledProcessError:
+            pass
+
+    try:
+        if os.environ['CC'] == "icc":
+            icc = True
+    except KeyError:
+        pass
+
+    extra_c_args_for_fermi = ["-std=gnu99","-DUSE_SIMDE", "-DSIMDE_ENABLE_NATIVE_ALIASES"]
+    if icc or sysconfig.get_config_vars()['CC'] == 'icc':
+        extra_c_args_for_fermi.extend(['-qopenmp-simd', '-DSIMDE_ENABLE_OPENMP'])
+    elif new_gcc or clang or sysconfig.get_config_vars()['CC'] == 'clang':
+        extra_c_args_for_fermi.extend(['-fopenmp-simd', '-DSIMDE_ENABLE_OPENMP'])
+
+    # extensions, those has to be processed by Cython
+    ext_modules = [ \
+                    # Signal
+                    Extension("MACS3.Signal.Prob", ["MACS3/Signal/Prob.pyx"], libraries=["m"], include_dirs=numpy_include_dir, extra_compile_args=extra_c_args ),
+                    Extension("MACS3.Signal.Pileup", ["MACS3/Signal/Pileup.pyx","MACS3/Signal/cPosValCalculation.c"], include_dirs=numpy_include_dir, extra_compile_args=extra_c_args ),
+                    Extension("MACS3.Signal.PeakModel", ["MACS3/Signal/PeakModel.pyx"], include_dirs=numpy_include_dir, extra_compile_args=extra_c_args),
+                    Extension("MACS3.Signal.PeakDetect", ["MACS3/Signal/PeakDetect.pyx"], extra_compile_args=extra_c_args),
+                    Extension("MACS3.Signal.SignalProcessing", ["MACS3/Signal/SignalProcessing.pyx"], include_dirs=numpy_include_dir, extra_compile_args=extra_c_args),
+                    Extension("MACS3.Signal.FixWidthTrack", ["MACS3/Signal/FixWidthTrack.pyx"], include_dirs=numpy_include_dir, extra_compile_args=extra_c_args),
+                    Extension("MACS3.Signal.PairedEndTrack", ["MACS3/Signal/PairedEndTrack.pyx"], include_dirs=numpy_include_dir, extra_compile_args=extra_c_args),
+                    Extension("MACS3.Signal.BedGraph", ["MACS3/Signal/BedGraph.pyx"], libraries=["m"], include_dirs=numpy_include_dir, extra_compile_args=extra_c_args),
+                    Extension("MACS3.Signal.ScoreTrack", ["MACS3/Signal/ScoreTrack.pyx"], include_dirs=numpy_include_dir, extra_compile_args=extra_c_args ),
+                    Extension("MACS3.Signal.CallPeakUnit", ["MACS3/Signal/CallPeakUnit.pyx"], include_dirs=numpy_include_dir, extra_compile_args=extra_c_args),
+                    Extension("MACS3.Signal.VariantStat",["MACS3/Signal/VariantStat.pyx",],libraries=["m"], include_dirs=numpy_include_dir, extra_compile_args=extra_c_args),
+                    Extension("MACS3.Signal.ReadAlignment",["MACS3/Signal/ReadAlignment.pyx",],libraries=["m"],include_dirs=numpy_include_dir, extra_compile_args=extra_c_args),
+                    Extension("MACS3.Signal.RACollection",["MACS3/Signal/RACollection.pyx","MACS3/fermi-lite/bfc.c","MACS3/fermi-lite/bseq.c",\
+                                                           "MACS3/fermi-lite/bubble.c","MACS3/fermi-lite/htab.c","MACS3/fermi-lite/ksw.c","MACS3/fermi-lite/kthread.c",\
+                                                           "MACS3/fermi-lite/mag.c","MACS3/fermi-lite/misc.c","MACS3/fermi-lite/mrope.c","MACS3/fermi-lite/rld0.c",\
+                                                           "MACS3/fermi-lite/rle.c","MACS3/fermi-lite/rope.c","MACS3/fermi-lite/unitig.c", "MACS3/Signal/swalign.c" ], \
+                              libraries=["m","z"], include_dirs=numpy_include_dir+["./","./MACS3/fermi-lite/","./MACS3/Signal/"], extra_compile_args=extra_c_args+extra_c_args_for_fermi),
+                    Extension("MACS3.Signal.UnitigRACollection",["MACS3/Signal/UnitigRACollection.pyx"],libraries=["m"], include_dirs=numpy_include_dir, extra_compile_args=extra_c_args),
+                    Extension("MACS3.Signal.PosReadsInfo",["MACS3/Signal/PosReadsInfo.pyx",],libraries=["m"], include_dirs=numpy_include_dir, extra_compile_args=extra_c_args),
+                    Extension("MACS3.Signal.PeakVariants",["MACS3/Signal/PeakVariants.pyx",],libraries=["m"], include_dirs=numpy_include_dir, extra_compile_args=extra_c_args),
+                    # IO
+                    Extension("MACS3.IO.Parser",["MACS3/IO/Parser.pyx"], include_dirs=numpy_include_dir, extra_compile_args=extra_c_args),
+                    Extension("MACS3.IO.PeakIO", ["MACS3/IO/PeakIO.pyx"], extra_compile_args=extra_c_args),
+                    Extension("MACS3.IO.BedGraphIO", ["MACS3/IO/BedGraphIO.pyx"], extra_compile_args=extra_c_args),
+                    Extension("MACS3.IO.BAM",["MACS3/IO/BAM.pyx",],libraries=["m"], include_dirs=numpy_include_dir, extra_compile_args=extra_c_args) ]
+
+
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+
+    setup( name = "MACS3",
+           version = MACS_VERSION,
+           description = "Model Based Analysis for ChIP-Seq data",
+           long_description = long_description,
+           long_description_content_type = "text/markdown",
+           author = 'Tao Liu',
+           author_email = 'vladimir.liu@gmail.com',
+           url = 'http://github.com/macs3-project/MACS/',
+           package_dir = {'MACS3' : 'MACS3'},
+           packages = ['MACS3', 'MACS3.IO', 'MACS3.Signal', 'MACS3.Commands','MACS3.Utilities'],
+           package_data = {'MACS3':['*.pxd']},
+           scripts = ['bin/macs3', ],
+           classifiers = classifiers,
+           install_requires = install_requires,
+           setup_requires = install_requires,
+           tests_require = tests_requires,
+           python_requires = '>=3.6',
+           ext_modules = ext_modules )
+
+if __name__ == '__main__':
+    main()
