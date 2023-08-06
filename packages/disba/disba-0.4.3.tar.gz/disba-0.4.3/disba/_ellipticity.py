70b34919054d796c6434fb0c277754b7799eb94f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:30 +0000

    Add package disba/0.4.3/disba-0.4.3.tar.gz

---
+++ packages/disba/disba-0.4.3.tar.gz/disba-0.4.3/disba/_ellipticity.py
@@ -0,0 +1,89 @@
+from collections import namedtuple
+
+import numpy
+
+from ._base import Base
+from ._common import ifunc
+from ._cps import swegn96
+from ._exception import DispersionError
+
+__all__ = [
+    "RayleighEllipticity",
+    "Ellipticity",
+]
+
+
+RayleighEllipticity = namedtuple(
+    "RayleighEllipticity", ("period", "ellipticity", "mode")
+)
+
+
+class Ellipticity(Base):
+    def __init__(
+        self,
+        thickness,
+        velocity_p,
+        velocity_s,
+        density,
+        algorithm="dunkin",
+        dc=0.005,
+    ):
+        """
+        Ellipticity class (only Rayleigh-wave).
+
+        Parameters
+        ----------
+        thickness : array_like
+            Layer thickness (in km).
+        velocity_p : array_like
+            Layer P-wave velocity (in km/s).
+        velocity_s : array_like
+            Layer S-wave velocity (in km/s).
+        density : array_like
+            Layer density (in g/cm3).
+        algorithm : str {'dunkin', 'fast-delta'}, optional, default 'dunkin'
+            Algorithm to use for computation of Rayleigh-wave dispersion:
+             - 'dunkin': Dunkin's matrix (adapted from surf96),
+             - 'fast-delta': fast delta matrix (after Buchen and Ben-Hador, 1996).
+        dc : scalar, optional, default 0.005
+            Phase velocity increment for root finding.
+
+        """
+        super().__init__(thickness, velocity_p, velocity_s, density, algorithm, dc)
+
+    def __call__(self, t, mode=0):
+        """
+        Compute Rayleigh-wave ellipticity for input period axis and mode.
+
+        Parameters
+        ----------
+        t : array_like
+            Periods (in s).
+        mode : int, optional, default 0
+            Mode number (0 if fundamental).
+
+        Returns
+        -------
+        :class:`disba.RayleighEllipticity`
+            Rayleigh-wave ellipticity as a namedtuple (period, ellipticity, mode).
+
+        """
+        ell = []
+        for i, tt in enumerate(t):
+            try:
+                eig = swegn96(
+                    tt,
+                    self._thickness,
+                    self._velocity_p,
+                    self._velocity_s,
+                    self._density,
+                    mode,
+                    ifunc[self._algorithm]["rayleigh"],
+                    self._dc,
+                )[:, :2]
+                ell.append(eig[0, 0] / eig[0, 1])
+            except DispersionError:
+                i -= 1
+                break
+
+        return RayleighEllipticity(t[: i + 1], numpy.array(ell), mode)
---
+++ packages/disba/disba-0.4.3.tar.gz/disba-0.4.3/test/test_dispersion.py
@@ -0,0 +1,65 @@
+import helpers
+import numpy
+import pytest
+
+import disba
+
+
+@pytest.mark.parametrize(
+    "mode, wave, algorithm, cref",
+    [
+        (0, "rayleigh", "dunkin", 11.788),
+        (0, "rayleigh", "fast-delta", 11.782),
+        (1, "rayleigh", "dunkin", 8.032),
+        (1, "rayleigh", "fast-delta", 8.819),
+        (0, "love", "dunkin", 12.900),
+        (1, "love", "dunkin", 7.295),
+    ],
+)
+def test_phase(mode, wave, algorithm, cref):
+    velocity_model = helpers.velocity_model(5)
+    t = numpy.logspace(0.0, 1.0, 20)
+
+    pd = disba.PhaseDispersion(*velocity_model, algorithm=algorithm)
+    cp = pd(t, mode, wave)
+
+    assert numpy.allclose(cref, cp.velocity.sum(), atol=0.001)
+
+
+@pytest.mark.parametrize(
+    "mode, wave, algorithm, cref",
+    [
+        (0, "rayleigh", "dunkin", 10.667),
+        (0, "rayleigh", "fast-delta", 10.705),
+        (1, "rayleigh", "dunkin", 6.931),
+        (1, "rayleigh", "fast-delta", 6.914),
+        (0, "love", "dunkin", 11.766),
+        (1, "love", "dunkin", 6.255),
+    ],
+)
+def test_group(mode, wave, algorithm, cref):
+    velocity_model = helpers.velocity_model(5)
+    t = numpy.logspace(0.0, 1.0, 20)
+
+    gd = disba.GroupDispersion(*velocity_model, algorithm=algorithm)
+    cg = gd(t, mode, wave)
+
+    assert numpy.allclose(cref, cg.velocity.sum(), atol=0.001)
+
+
+@pytest.mark.parametrize(
+    "wave, algorithm, cref",
+    [
+        ("rayleigh", "dunkin", 11.596),
+        ("rayleigh", "fast-delta", 11.581),
+        ("love", "dunkin", 14.108),
+    ],
+)
+def test_water_layer(wave, algorithm, cref):
+    velocity_model = helpers.velocity_model(5, water_layer=True)
+    t = numpy.logspace(0.0, 1.0, 20)
+
+    pd = disba.PhaseDispersion(*velocity_model, algorithm=algorithm)
+    cp = pd(t, wave=wave)
+
+    assert numpy.allclose(cref, cp.velocity.sum(), atol=0.001)
