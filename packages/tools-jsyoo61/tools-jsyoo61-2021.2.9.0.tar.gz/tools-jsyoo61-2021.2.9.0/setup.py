8acd68b107eef3504a6b8ca00f90b533cae3d7d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:16 +0000

    Add package tools-jsyoo61/2021.2.9.0/tools-jsyoo61-2021.2.9.0.tar.gz

---
+++ packages/tools-jsyoo61/tools-jsyoo61-2021.2.9.0.tar.gz/tools-jsyoo61-2021.2.9.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tools-jsyoo61", # Replace with your own username
+    version="2021.02.09.0",
+    author="JaeSung Yoo",
+    author_email="jsyoo61@korea.ac.kr",
+    description="personal syntax tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jsyoo61/tools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['numpy','pandas','matplotlib']
+)
