18417bab20461cd242ce26335518cb5293a92e95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:54 +0000

    Add package METER/0.3.2/METER-0.3.2.tar.gz

---
+++ packages/METER/METER-0.3.2.tar.gz/METER-0.3.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='METER',
+    url='https://github.com/MR236/METER',
+    author='Malcolm Risk',
+    author_email='mr236@st-andrews.ac.uk',
+    packages=['METER'],
+    install_requires=['numpy', 'pandas', 'matplotlib'],
+    version='0.3.2',
+    license='CC0',
+    description='Python package for multi-state modelling',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
+)
