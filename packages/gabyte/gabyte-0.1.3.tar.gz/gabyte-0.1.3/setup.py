8fe2e1d63e2ae3b4acd5ebe8813e95ce475e14c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:06 +0000

    Add package gabyte/0.1.3/gabyte-0.1.3.tar.gz

---
+++ packages/gabyte/gabyte-0.1.3.tar.gz/gabyte-0.1.3/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name="gabyte",
+    version="0.1.3",
+    author="Francis B. Lavoie",
+    author_email="francis.b.lavoie@usherbrooke.ca",
+    description="Genetic algorithms with bytes",
+    long_description="Genetic algorithms with bytes",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ),
+)
