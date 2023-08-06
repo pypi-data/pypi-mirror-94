b7fdd5e4a006eee467e776a830e9e87aff3f7cb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:37 +0000

    Add package pyfuck/0.5/pyfuck-0.5.tar.gz

---
+++ packages/pyfuck/pyfuck-0.5.tar.gz/pyfuck-0.5/setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+from distutils.command.install import INSTALL_SCHEMES
+
+def pnt():
+  print("Hello World")
+  return 'pyfuck'
+
+setup(
+  name = 'pyfuck',
+  packages = ['pyfuck'],
+  version = '0.5',
+  license='MIT',
+  description = 'Python Brainfuck Implementation to Execute as well as Convert Brainfuck Code',
+  author = 'Jaysmito Mukherjee',
+  author_email = 'jaysmito101@gmail.com',
+  url = 'https://github.com/Jaysmito101/PyFuck',
+  download_url = 'https://github.com/Jaysmito101/PyFuck/archive/v_03.tar.gz',
+  keywords = ['brainfuck', 'interpreter', 'code-converter'],
+  install_requires=[
+          'numpy',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
