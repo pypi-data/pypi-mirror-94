97595a1630551665388e9a974b18c019affc65c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:33 +0000

    Add package pylambdex/0.2.0/pylambdex-0.2.0-py3-none-any.whl

---
+++ packages/pylambdex/pylambdex-0.2.0-py3-none-any.whl/lambdex/__init__.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+f = sys._getframe(1)
+while f.f_code is not None and f.f_code.co_filename.startswith('<'):
+    f = f.f_back
+
+if f and f.f_code and f.f_code.co_filename.startswith(os.path.dirname(os.__file__)):
+    if sys.path and sys.path[0] == os.getcwd():
+        sys.path = sys.path[1:]
+else:
+    __all__ = ['def_']
+    from .keywords import def_
+
+del os, sys, f
+
+__version__ = "0.2.0"
