f4ee8b32f9d49c84c37e502a25d0e03ff62baa71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:30 +0000

    Add package csutils/0.1.9/csutils-0.1.9.tar.gz

---
+++ packages/csutils/csutils-0.1.9.tar.gz/csutils-0.1.9/setup.py
@@ -0,0 +1,45 @@
+"""
+setup.py - For Pypi installer.
+
+Update package with the following commands:
+
+$ python setup.py sdist bdist_wheel
+$ twine check dist/*
+$ twine upload --config-file .pypirc dist/*
+
+If the "twine" command fails (after you're sure you installed it), use
+$ python -m twine check dist/*
+...etc...
+"""
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.1.9'
+PACKAGE_NAME = 'csutils'
+AUTHOR = 'Thomas J. Daley, J.D.'
+AUTHOR_EMAIL = 'tom@powerdaley.com'
+URL = 'https://github.com/tjdaley/csutils'
+
+LICENSE = 'BSD-3-Clause License'
+DESCRIPTION = 'Child Support Utilities'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'python_dateutil'
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages()
+      )
