da2206e3de7167e112af85ea5c1c2f0fbc1dc3dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:45 +0000

    Add package controlpackage/1.0.1/controlpackage-1.0.1.tar.gz

---
+++ packages/controlpackage/controlpackage-1.0.1.tar.gz/controlpackage-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+    name='controlpackage',
+    packages=['controlpackage'],
+    version='1.0.1',
+    license='MIT',
+    description='Control Package to verify PyPI source',
+    author='Daniel Beneyto',
+    author_email='danfaizer@gmail.com',
+    url='https://github.com/danfaizer/controlpackage',
+    install_requires=[],
+)
