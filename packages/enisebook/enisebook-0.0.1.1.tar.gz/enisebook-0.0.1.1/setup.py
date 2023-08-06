1d1a54ab8a94cca856fcbbdd195f7a80aae85ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:38 +0000

    Add package enisebook/0.0.1.1/enisebook-0.0.1.1.tar.gz

---
+++ packages/enisebook/enisebook-0.0.1.1.tar.gz/enisebook-0.0.1.1/setup.py
@@ -0,0 +1,14 @@
+#import setuptools
+from distutils.core import setup
+setup(
+      name = 'enisebook',
+      packages = ['enisebook'],
+      version = '0.0.1.1',
+      description = 'A lib for making interactive books',
+      author = 'Greg',
+      author_email = 'gregory.debono@enise.fr',
+      keywords = ['interactive', 'book'], # arbitrary keywords
+      install_requires=[
+      ],
+      classifiers = [],
+)
