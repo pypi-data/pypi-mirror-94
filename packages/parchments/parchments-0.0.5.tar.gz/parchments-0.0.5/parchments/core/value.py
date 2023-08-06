a6751f87ff8dc703c7d14647873c1a14f213fdbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:42 +0000

    Add package parchments/0.0.5/parchments-0.0.5.tar.gz

---
+++ packages/parchments/parchments-0.0.5.tar.gz/parchments-0.0.5/parchments/core/value.py
@@ -0,0 +1,33 @@
+from decimal import Decimal
+
+class Value:
+
+    def __init__(self, value, value_type, decimals=2):
+        self.data_dict = {
+            'raw': value,
+            'clean': value,
+            'verbose': self.verbose(value, value_type),
+            'type': value_type,
+            'decimals': decimals,
+        }
+
+    def verbose(self, value, value_type):
+        if value == 0 or value == 0.0 or value == Decimal(0.0):
+            return '-'
+        elif value_type == 'dollar':
+            return '${:,.2f}'.format(value)
+        elif value_type == 'percentage':
+            return '{:,.4f}%'.format((value * 100))
+        elif value_type == 'int':
+            return '{:,.0f}'.format(value)
+        else:
+            return value
+
+    def as_dict(self):
+        return self.data_dict
+
+    def as_list(self):
+        value_list = list()
+        for key, val in self.data_dict.items():
+            value_list.append(val)
+        return value_list
---
+++ packages/parchments/parchments-0.0.5.tar.gz/parchments-0.0.5/parchments/tests/grid_tests.py
@@ -0,0 +1,65 @@
+import unittest
+import parchments
+import datetime
+import decimal
+
+test_index = (
+    ('goats', 'int', 0),
+    ('price', 'dollar', 2),
+    ('value', 'percentage', 4),
+    ('names', 'string', 0),
+)
+
+test_grid = parchments.Grid(test_index)
+
+
+class TestGrid(unittest.TestCase):
+
+    def test_grid_add_period(self, period=datetime.datetime.now(), index=[1, 22.2, 0.70, 'bob']):
+        try:
+            test_grid.add_period(datetime.datetime.now(), [1, 22.2, 0.70, 'bob'])
+            self.assertTrue(True)
+        except:
+            self.assertTrue(False)
+
+    def test_add_period_with_datetime_input(self):
+        self.test_grid_add_period(period=datetime.datetime.now())
+
+    def test_add_period_with_date_input(self):
+        self.test_grid_add_period(period=datetime.date.today())
+
+    def test_add_period_with_decimal_input(self):
+        self.test_grid_add_period(index=[1, decimal.Decimal(22.2), 0.70, 'bob'])
+
+    def test_add_period_with_float_input(self):
+        self.test_grid_add_period(index=[1, float(22.2), 0.70, 'bob'])
+
+    def test_add_period_with_zero_division_input(self):
+        self.test_grid_add_period(index=[1, 22.0, 0.70, 'bob'])
+        self.test_grid_add_period(index=[1, 0.0, 0.70, 'bob'])
+
+    def test_grid_as_dict(self):
+        try:
+            self.assertTrue(type(test_grid.as_dict()) is dict)
+        except:
+            self.assertTrue(False)
+
+    def test_grid_as_list(self):
+        try:
+            self.assertTrue(type(test_grid.as_list()) is list)
+        except:
+            self.assertTrue(False)
+
+    def test_grid_get_row(self):
+        try:
+            test_grid.get_row('goats')
+            self.assertTrue(True)
+        except:
+            self.assertTrue(False)
+
+    def test_grid_get_block(self):
+        try:
+            test_grid.get_block('goats', datetime.datetime.now())
+            self.assertTrue(True)
+        except:
+            self.assertTrue(False)
