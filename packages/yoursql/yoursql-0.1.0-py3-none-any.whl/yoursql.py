5c2c6447ac0c0b8f884a646e2ff0e4dde8e22f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:30 +0000

    Add package yoursql/0.1.0/yoursql-0.1.0-py3-none-any.whl

---
+++ packages/yoursql/yoursql-0.1.0-py3-none-any.whl/yoursql.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import os
+
+for root, dirs, files in os.walk("."):
+    path = root.split(os.sep)
+    print((len(path) - 1) * '------', os.path.basename(root))
+    for file in files:
+        print(len(path) * '---', file)
