e234cdc619b85971928187f5b60c9838118c3197
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:41 +0000

    Add package phoebusgen/0.0.11/phoebusgen-0.0.11.tar.gz

---
+++ packages/phoebusgen/phoebusgen-0.0.11.tar.gz/phoebusgen-0.0.11/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="phoebusgen",
+    version="0.0.11",
+    author="Tynan Ford",
+    author_email="tford@lbl.gov",
+    description="Control screen generator for Phoebus",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tynanford/phoebusgen",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Topic :: Software Development :: Code Generators"
+    ],
+    python_requires='>=3.5',
+    install_requires=['pyyaml>=3'],
+)
+
