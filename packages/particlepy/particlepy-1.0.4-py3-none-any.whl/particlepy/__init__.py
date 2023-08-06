33c922dadb0091498263ba602fa41d733e8f6acb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:49 +0000

    Add package particlepy/1.0.4/particlepy-1.0.4-py3-none-any.whl

---
+++ packages/particlepy/particlepy-1.0.4-py3-none-any.whl/particlepy/__init__.py
@@ -0,0 +1,10 @@
+# __init__.py
+# -*- coding: utf-8 -*-
+
+__author__ = "grimmigerFuchs"
+__credits__ = __author__
+__version__ = "1.0.4"
+
+import particlepy.particle
+import particlepy.shape
+import particlepy.math
