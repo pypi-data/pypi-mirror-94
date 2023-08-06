5141d7d29e8c89c688b7b4a852116f7b7237d48b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:34 +0000

    Add package myownpypipackage/0.0.2/myownpypipackage-0.0.2.tar.gz

---
+++ packages/myownpypipackage/myownpypipackage-0.0.2.tar.gz/myownpypipackage-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'myownpypipackage',
+    version = '0.0.2',
+    license = 'MIT',
+    description = 'paquete de prueba',
+    author = "Roberto Carro Gastélum",
+    packages = find_packages(),
+    url = "https://github.com/robcg1102/pypipackage"
+)
