35ff44ed63d4b8eaa1acc309cb3a77cb1ff13b7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:35 +0000

    Add package funing/0.2.0/funing-0.2.0.tar.gz

---
+++ packages/funing/funing-0.2.0.tar.gz/funing-0.2.0/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="funing",
+    version="0.2.0",
+    author="larryw3i",
+    author_email="larryw3i@163.com",
+    description="A face recognition gui",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/larryw3i/Funing",
+    scripts=[
+        'funing.py',
+        'setup.py',
+        'setting.py'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    data_files=[
+        ('setting',['setting.yml.example']),
+        ('requirements',['requirements.txt']),
+        ('snapcraft',['snapcraft.yaml']),
+        ('readme',['README.md']),
+        ('LICENSE',['LICENSE']),
+    ],
+    install_requires=[
+        'dlib',
+        'face-recognition',
+        'opencv-python',
+        'language_data',
+        'langcodes',
+        'pony',
+        'pyyaml',
+    ]
+)
