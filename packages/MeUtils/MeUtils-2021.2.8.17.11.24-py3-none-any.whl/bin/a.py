c05d9d463bd6c7e06da0afa7e24b5384604ee51f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:16 +0000

    Add package MeUtils/2021.2.8.17.11.24/MeUtils-2021.2.8.17.11.24-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2021.2.8.17.11.24-py3-none-any.whl/bin/a.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : MeUtils.
+# @File         : a
+# @Time         : 2021/2/7 9:19 下午
+# @Author       : yuanjie
+# @WeChat       : 313303303
+# @Software     : PyCharm
+# @Description  : 
+
+
+import b
+
+def f1():
+    print('a')
