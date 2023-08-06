b46a290693ff06040a0531f974c9b5e4f6ec38cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:41 +0000

    Add package mt2/0.2.0/mt2-0.2.0.tar.gz

---
+++ packages/mt2/mt2-0.2.0.tar.gz/mt2-0.2.0/README.rst
@@ -0,0 +1,52 @@
+===
+mt2
+===
+
+.. image:: https://img.shields.io/pypi/v/mt2.svg
+        :target: https://pypi.python.org/pypi/mt2
+
+.. image:: https://github.com/tpgillam/mt2/workflows/Build/badge.svg?branch=master
+        :target: https://github.com/tpgillam/mt2/actions?query=workflow%3ABuild
+
+MT2, Asymmetric MT2, and the Stransverse Mass
+---------------------------------------------
+
+This package may be used to evaluate MT2 in all its variants.
+
+Specifically, it provides a `numpy ufunc <https://numpy.org/doc/stable/reference/ufuncs.html>`_ interface to the bisection algorithm and C++ implementation of http://arxiv.org/abs/1411.4312.
+The variable MT2 itself is described `here <http://arxiv.org/abs/hep-ph/9906349>`__.
+Related information may be found in papers relating to MT2 linked from `here <https://www.hep.phy.cam.ac.uk/~lester/mt2/index.html>`__.
+
+Example Usage
+-------------
+
+Single calculation of MT2:
+
+.. code-block:: python
+
+    from mt2 import mt2
+
+    # The units of all quantities are the same, e.g. GeV
+    val = mt2(
+        100, 410, 20,  # Visible 1: mass, px, py
+        150, -210, -300,  # Visible 2: mass, px, py
+        -200, 280,  # Missing transverse momentum: x, y
+        100, 100)  # Invisible 1 mass, invisible 2 mass
+    print("Expected mT2 = 412.628.  Computed mT2 = ", val)
+
+An example using broadcasting:
+
+.. code-block:: python
+
+    # to appear soon ....
+
+License
+-------
+
+Please cite:
+
+* http://arxiv.org/abs/hep-ph/9906349, if you use MT2 in an academic paper, and
+* http://arxiv.org/abs/1411.4312 if you use this particular calculator.
+
+All files other than ``src/lester_mt2_bisect_v7.h`` are released under the MIT license.
+
---
+++ packages/mt2/mt2-0.2.0.tar.gz/mt2-0.2.0/setup.py
@@ -0,0 +1,62 @@
+from setuptools import setup, Extension
+
+import numpy
+
+__version__ = '0.2.0'
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+ext_modules = [
+    Extension(
+        "_mt2",
+        ["src/main.cpp"],
+        # Pass in the version info so we can expose it in the extension.
+        # Also, for reasons explained in lester_mt2_bisect_v7.h, we need to manually enable some inlining optimisations.
+        # Copyright printing is disabled here, since we include the necessary citation information elsewhere.
+        define_macros=[
+            ('VERSION_INFO', __version__),
+            ('ENABLE_INLINING', 1),
+            ('DISABLE_COPYRIGHT_PRINTING', 1),
+        ],
+        include_dirs=[numpy.get_include()],
+        language='c++'
+    ),
+]
+
+setup_requirements = ['pytest-runner', ]
+test_requirements = ['pytest>=3', ]
+
+setup(
+    author="Thomas Gillam",
+    author_email='tpgillam@googlemail.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    description="Stransverse mass computation as a numpy ufunc.",
+    install_requires=["numpy"],
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='mt2',
+    name='mt2',
+    packages=['mt2'],
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/tpgillam/mt2',
+    version=__version__,
+    ext_modules=ext_modules,
+    zip_safe=False,
+)
