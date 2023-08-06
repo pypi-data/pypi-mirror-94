5a7831fbe9c55905a2ef621194c24b0d771faa2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:17 +0000

    Add package nonebot-plugin-bam/0.1.1/nonebot_plugin_bam-0.1.1.tar.gz

---
+++ packages/nonebot-plugin-bam/nonebot_plugin_bam-0.1.1.tar.gz/nonebot_plugin_bam-0.1.1/nonebot_plugin_bam/config.py
@@ -0,0 +1,12 @@
+import nonebot
+from pydantic import BaseSettings
+
+
+class Config(BaseSettings):
+    bam_db_file: str = ":memory:"
+    bam_on_startup_clean_live_status: bool = False
+    bam_monitor_task_interval: int = 5
+    bam_live_api: int = 2
+
+    class Config:
+        extra = "ignore"
