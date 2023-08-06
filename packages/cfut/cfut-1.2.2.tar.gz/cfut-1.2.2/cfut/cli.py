3ef4c9026af8e9a6e5bf436289bf6087484203a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:21 +0000

    Add package cfut/1.2.2/cfut-1.2.2.tar.gz

---
+++ packages/cfut/cfut-1.2.2.tar.gz/cfut-1.2.2/cfut/cli.py
@@ -0,0 +1,129 @@
+import os
+import sys
+from typing import Optional
+
+import argp
+
+from pathlib import Path
+
+from cfut import commands
+from cfut.commands import CONFIG_FILE, get_config, run_cf, OutputFormat, DEFAULT_OUTPUT_FORMAT
+from cfut.models import IniFile, CfnTemplate
+
+
+def do_init(args):
+    """ initialize cfut.json"""
+
+    template_files = Path(".").glob("**/*.y*ml")
+    templates = {
+        t.stem: CfnTemplate(name=t.stem, path=str(t).replace("\\", "/"))
+        for t in template_files
+    }
+
+    if len(templates) == 1:
+        templates = {"default": list(templates.values())[0]}
+
+    ini = IniFile(profile="default", templates=templates)
+    cont = ini.json(indent=2)
+    open(CONFIG_FILE, "w").write(cont)
+
+
+def lint(args):
+    config = get_config()
+    for t in config.templates.values():
+        os.system("cfn-lint " + t.path)
+
+
+def add_alias(fr: str, to: str, output: Optional[OutputFormat] = None):
+    out = output if output else DEFAULT_OUTPUT_FORMAT
+
+    def alias_handler(args):
+        cmd = to
+        if args.other_args:
+            cmd += " " + " ".join(args.other_args)
+        run_cf(cmd, output)
+
+    sp = argp.sub(fr, alias_handler, help="Alias: " + to)
+    sp.add_argument("other_args", nargs="*")
+
+
+def add_id_cmd(fr: str, to: str, query: Optional[str] = None):
+    output = None
+    if query:
+        output = OutputFormat("table", query)
+
+    def id_cmd_handler(args):
+        idd = args.id if args.id else "default"
+
+        commands.run_command(idd, to, output)
+
+    sp = argp.sub(fr, id_cmd_handler, help="Call: " + to)
+    sp.add_argument("id", help="Nickname of stack", nargs="?")
+
+
+def add_template_cmd(fr: str, to: str, with_params=False):
+    def template_cmd_handler(args):
+        idd = args.id if args.id else "default"
+        commands.run_command_with_file(idd, to, with_params)
+
+    sp = argp.sub(fr, template_cmd_handler, help=f"Call with template: {to}")
+    sp.add_argument("id", help="Alias of stack", nargs="?")
+
+
+def find_in_parents(fname: str) -> Optional[Path]:
+    cur = Path(".").absolute()
+    while 1:
+        trie = cur / fname
+        if trie.exists():
+            return trie
+        parent = cur.parent
+        if parent == cur:
+            return None
+        cur = parent
+
+
+def change_to_root_dir():
+    found = find_in_parents(commands.CONFIG_FILE)
+    if found:
+        os.chdir(found.parent)
+        return
+    resp = input(
+        f"Config file {commands.CONFIG_FILE} not found, create it in {os.getcwd()} [y/n]? "
+    )
+    if resp.startswith("y"):
+        do_init(None)
+
+
+def print_stacks():
+    config = get_config()
+    for k, v in config.templates.items():
+        print(f"{k}: {v.path} => {v.name}")
+
+
+def main():
+    os.environ["AWS_PAGER"] = "less"
+    change_to_root_dir()
+    if len(sys.argv) == 1:
+        print("Run cfut -h to get help.")
+        print("Workspace:", os.getcwd())
+        print_stacks()
+        return
+    argp.init()
+    argp.sub("init", do_init, help="Initialize working directory")
+    argp.sub("lint", lint, help="Lint templates")
+
+    add_template_cmd("update", "update-stack", True)
+    add_template_cmd("create", "create-stack", True)
+    add_id_cmd("describe", "describe-stacks")
+    add_id_cmd("events", "describe-stack-events", 'StackEvents[*].[LogicalResourceId,ResourceType,ResourceStatus,Timestamp]')
+    add_id_cmd("res", "describe-stack-resources",
+               'StackResources[*].[LogicalResourceId,ResourceType,PhysicalResourceId]')
+    add_id_cmd("delete", "delete-stack")
+
+    add_alias("ls", "describe-stacks", OutputFormat("table", "Stacks[*].[StackName,StackStatus,CreationTime]"))
+    commands.set_profile_from_config()
+    argp.parse()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/cfut/cfut-1.2.2.tar.gz/cfut-1.2.2/cfut/commands.py
@@ -0,0 +1,113 @@
+import os
+import subprocess
+import sys
+from dataclasses import dataclass
+from enum import Enum
+from typing import Optional, Dict
+
+from cfut.models import IniFile
+
+CONFIG_FILE = "cfut.json"
+
+
+class OutputStyle(str, Enum):
+    yaml = "yaml"
+    table = "table"
+
+
+@dataclass
+class OutputFormat:
+    style: OutputStyle
+    query: Optional[str]
+
+    def as_arg(self):
+        parts = ["--output", str(self.style)]
+        if self.query:
+            parts.extend(["--query", self.query])
+        return " ".join(parts)
+
+
+DEFAULT_OUTPUT_FORMAT = OutputFormat("yaml", None)
+
+
+def run_cf(cmd: str, output: Optional[OutputFormat] = None):
+    out = (output if output else DEFAULT_OUTPUT_FORMAT).as_arg()
+
+    profile_arg = "--profile " + current_profile
+
+    cmd = f"aws cloudformation {out} {profile_arg} {cmd}"
+    print("> " + cmd)
+
+    subprocess.call(cmd)
+
+
+current_profile = "default"
+
+
+def set_profile(profile: str):
+    global current_profile
+    current_profile = profile
+
+
+def set_profile_from_config():
+    config = get_config()
+    set_profile(config.profile)
+
+
+current_config: Optional[IniFile] = None
+
+
+def get_config():
+    global current_config
+    if current_config:
+        return current_config
+    if not os.path.isfile(CONFIG_FILE):
+        print(
+            f"Config file '{CONFIG_FILE}' not found, please run 'cfut init' to create it"
+        )
+        sys.exit(1)
+
+    current_config = IniFile.parse_raw(open(CONFIG_FILE).read())
+
+    return current_config
+
+
+def make_param_arg(d: Dict[str, str]):
+    params = " ".join(
+        f"ParameterKey={k},ParameterValue={v}" for (k, v) in d.items()
+    )
+    return f'--parameters {params}'
+
+
+def stack_args(stack_name: str, template_file: Optional[str]):
+    parts = [f"--stack-name {stack_name}"]
+    if template_file:
+        parts.append(f"--template-body file://{template_file}")
+    return " ".join(parts)
+
+
+def base_command(command_name: str, stack_name: str, template_file: Optional[str]):
+    return f"{command_name} " + stack_args(stack_name, template_file)
+
+
+def run_command(stack_id: str, command_name: str, output: OutputFormat):
+    inifile = get_config()
+    # if no alias exists, just pass it through
+    stack = inifile.templates.get(stack_id)
+    stack_name = stack.name if stack else stack_id
+    cmd = base_command(command_name, stack_name, None)
+    run_cf(cmd, output)
+
+
+def run_command_with_file(stack_id: str, command_name: str, with_params: False):
+    inifile = get_config()
+    stack = inifile.templates[stack_id]
+    cmd = base_command(command_name, stack.name, stack.path)
+    if stack.capabilities:
+        caps = " --capabilities " + " ".join(c.name for c in stack.capabilities)
+        cmd += caps
+    if with_params and stack.parameters:
+        params = make_param_arg(stack.parameters)
+        cmd += " " + params
+
+    run_cf(cmd)
