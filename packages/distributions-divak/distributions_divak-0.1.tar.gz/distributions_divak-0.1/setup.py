b753cad712e0849e9c7c37c6b86d918e4141d787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:07 +0000

    Add package distributions-divak/0.1/distributions_divak-0.1.tar.gz

---
+++ packages/distributions-divak/distributions_divak-0.1.tar.gz/distributions_divak-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_divak',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_divak'],
+      zip_safe=False)
