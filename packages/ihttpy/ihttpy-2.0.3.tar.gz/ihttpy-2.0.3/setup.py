8e6159fc73b299ee9c8de151002ad038434bba3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:27 +0000

    Add package ihttpy/2.0.3/ihttpy-2.0.3.tar.gz

---
+++ packages/ihttpy/ihttpy-2.0.3.tar.gz/ihttpy-2.0.3/setup.py
@@ -0,0 +1,52 @@
+import os
+import sys
+
+import setuptools
+from setuptools.command.install import install
+
+
+VERSION = '2.0.3'
+
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}"\
+                .format(tag, VERSION)
+            sys.exit(info)
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+
+setuptools.setup(
+    name="ihttpy",
+    version=VERSION,
+    author="irusland",
+    author_email="ruslansir@icloud.com",
+    description="A http server with fluent interface decorators, and plain "
+                "text configuration",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/irusland/httpserver",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+    install_requires=required,
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    }
+)
