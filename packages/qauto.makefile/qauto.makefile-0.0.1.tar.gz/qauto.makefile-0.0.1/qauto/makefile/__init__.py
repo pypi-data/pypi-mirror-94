d4e90f281ac1b45c5535fa6265c756a0e7c834cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:12 +0000

    Add package qauto.makefile/0.0.1/qauto.makefile-0.0.1.tar.gz

---
+++ packages/qauto.makefile/qauto.makefile-0.0.1.tar.gz/qauto.makefile-0.0.1/qauto/makefile/__init__.py
@@ -0,0 +1,9 @@
+"""
+The ultimate test automation framework
+
+Needs to be explicitly imported as:
+
+    import qauto.makefile
+"""
+
+print("Placeholder for qauto.makefile project to be opensourced soon")
---
+++ packages/qauto.makefile/qauto.makefile-0.0.1.tar.gz/qauto.makefile-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='qauto.makefile',
+    description='Makefile - test runner',
+    author='QA Automation',
+    version='0.0.1',
+    packages=['qauto.makefile'],
+    classifiers=['Programming Language :: Python :: 3'],
+)
