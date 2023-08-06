3c2815c28ef8b2db0dce72e47cdc668e556c569e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:36 +0000

    Add package pypi-test-castellani-davide-01-01-castellani-davide/1.0/pypi_test_castellani_davide_01_01_castellani_davide-1.0-py3-none-any.whl

---
+++ packages/pypi-test-castellani-davide-01-01-castellani-davide/pypi_test_castellani_davide_01_01_castellani_davide-1.0-py3-none-any.whl/pypi_test_castellani_davide_01_01/test_pypi_test_castellani_davide_01_01.py
@@ -0,0 +1,17 @@
+"""Test pypi_test_castellani_davide_01_01 file
+"""
+from pypi_test_castellani_davide_01_01 import *
+
+__author__ = "help@castellanidavide.it"
+__version__ = "2.0 2020-08-18"
+
+def test():
+	"""Tests the pypi_test_castellani_davide_01_01 function in the pypi_test_castellani_davide_01_01 class
+	Write here all test you want to do.
+	REMEMBER to test your programm you can't use __init__ function
+	"""
+	assert pypi_test_castellani_davide_01_01.pypi_test_castellani_davide_01_01() == "pypi_test_castellani_davide_01_01", "test failed"
+	#assert pypi_test_castellani_davide_01_01.<function>(<values>) == <the result(s) you would like to have>, "<the fail message>"
+	
+if __name__ == "__main__":
+	test()
