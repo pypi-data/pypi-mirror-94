6a13ee3168d7f0f39f41a0fbfda19bb905d52a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:10 +0000

    Add package ChemE/0.0.3/ChemE-0.0.3-py3-none-any.whl

---
+++ packages/ChemE/ChemE-0.0.3-py3-none-any.whl/ChemE/Boiler.py
@@ -0,0 +1,10 @@
+from scipy.optimize import fsolve, curve_fit
+from numpy import log as ln
+from scipy.integrate import odeint, quad
+import numpy as np
+from numpy import sin,cos,tan
+from numpy import sinh, cosh,tanh
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+import os
+import pandas as pd
