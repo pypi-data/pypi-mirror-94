71910a62ab8e622172a83d391e47651fa0ce3b4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:19 +0000

    Add package ninadcalc/0.0.1/ninadcalc-0.0.1.tar.gz

---
+++ packages/ninadcalc/ninadcalc-0.0.1.tar.gz/ninadcalc-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup,find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name = 'ninadcalc',
+    version = '0.0.1',
+    description = 'Our package will give u a basic calculator function such as add, sub, multiply and divide.',
+    Long_description = open('README.txt').read() + '\n\n\n' + open('CHANGELOG.txt').read(),
+    url = '',
+    author = 'Ninad Patil',
+    author_email = 'npradeeppatil2001@gmail.com',
+    license = 'MIT',
+    classifiers = classifiers,
+    keywords = ('add','sub','mul','div'),
+    packages = find_packages(),
+    install_require = ['']
+)
