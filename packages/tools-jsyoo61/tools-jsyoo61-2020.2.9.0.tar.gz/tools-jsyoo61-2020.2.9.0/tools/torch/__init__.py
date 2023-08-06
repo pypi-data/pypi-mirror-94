9422207e31ae692eaea369856e9928487f6d3b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:12 +0000

    Add package tools-jsyoo61/2020.2.9.0/tools-jsyoo61-2020.2.9.0.tar.gz

---
+++ packages/tools-jsyoo61/tools-jsyoo61-2020.2.9.0.tar.gz/tools-jsyoo61-2020.2.9.0/tools/torch/__init__.py
@@ -0,0 +1,19 @@
+
+from . import federated_learning, model
+from ._pandas import *
+
+def device_of(nn_module):
+    return next(nn_module.parameters()).device
+
+def device_of_optimizer(optimizer):
+    return optimizer.param_groups[0]['params'][0].device
+
+def to_onehot(x, max_dim):
+    '''
+    x: list of int
+
+    max_dim: maximum dimension
+    '''
+    onehot = torch.zeros(len(x),max_dim)
+    onehot[range(len(x)), x] = 1
+    return onehot
