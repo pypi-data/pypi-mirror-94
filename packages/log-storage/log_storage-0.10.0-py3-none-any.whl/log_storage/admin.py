e603c9f1d843ad7c8455a561ed829a97d787c677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:55 +0000

    Add package log-storage/0.10.0/log_storage-0.10.0-py3-none-any.whl

---
+++ packages/log-storage/log_storage-0.10.0-py3-none-any.whl/log_storage/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Log
+
+
+class LogAdmin(admin.ModelAdmin):
+    readonly_fields = ("log_data", "db_log_data", "filename", "save_file")
+    list_display = ("filename", "created", "save_file")
+    list_filter = ("created", "save_file")
+    search_fields = ("filename",)
+
+
+admin.site.register(Log, LogAdmin)
