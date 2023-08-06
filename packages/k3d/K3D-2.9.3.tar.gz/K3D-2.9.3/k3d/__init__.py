58ba38ee9b3f2ce712eb3786450aa5bb2897db1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:14 +0000

    Add package k3d/2.9.3/K3D-2.9.3.tar.gz

---
+++ packages/k3d/K3D-2.9.3.tar.gz/K3D-2.9.3/k3d/__init__.py
@@ -0,0 +1,43 @@
+from ._version import version_info, __version__
+
+from .colormaps import paraview_color_maps
+from .colormaps import basic_color_maps
+from .colormaps import matplotlib_color_maps
+
+from .factory import (plot,
+                      nice_colors,
+                      line,
+                      marching_cubes,
+                      mesh,
+                      points,
+                      stl,
+                      surface,
+                      text,
+                      text2d,
+                      texture,
+                      texture_text,
+                      vector_field,
+                      vectors,
+                      voxels,
+                      voxels_group,
+                      sparse_voxels,
+                      volume,
+                      mip,
+                      label,
+                      vtk_poly_data,
+                      voxel_chunk)
+
+from .plot import Plot
+
+from .transfer_function_editor import transfer_function_editor
+
+from .transform import transform
+
+
+def _jupyter_nbextension_paths():
+    return [{
+        'section': 'notebook',
+        'src': 'static',
+        'dest': 'k3d',
+        'require': 'k3d/extension'
+    }]
