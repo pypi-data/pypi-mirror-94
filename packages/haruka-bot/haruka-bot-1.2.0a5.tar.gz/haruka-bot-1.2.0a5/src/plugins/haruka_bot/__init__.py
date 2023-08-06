3f8d962dfd2b5e12ba5ba1c130152dde6c12e443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:04 +0000

    Add package haruka-bot/1.2.0a5/haruka-bot-1.2.0a5.tar.gz

---
+++ packages/haruka-bot/haruka-bot-1.2.0a5.tar.gz/haruka-bot-1.2.0a5/src/plugins/haruka_bot/__init__.py
@@ -0,0 +1,12 @@
+import nonebot
+
+try:
+    nonebot.get_driver()
+    from . import config_manager
+    from . import live_pusher
+    from . import dynamic_pusher
+    from . import auto_agree
+except ValueError:
+    pass
+
+from .version import __version__, VERSION
