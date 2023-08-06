a5eddfa4d296562737fc334c5edd0f4a83198683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:44 +0000

    Add package dl202121/1.6/dl202121-1.6.tar.gz

---
+++ packages/dl202121/dl202121-1.6.tar.gz/dl202121-1.6/mylib.py
@@ -0,0 +1,17 @@
+def add(x,y):
+    return x+y
+
+def division(x,y):
+    return x/y
+
+def mutiply(x,y):
+    return x*y
+
+def subtract(x,y):
+    return x-y
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ packages/dl202121/dl202121-1.6.tar.gz/dl202121-1.6/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(
+name='dl202121',
+version='1.6',
+author= "lhb" ,
+author_email= "1850801419@qq.com" ,
+py_modules=['mylib'],
+)
