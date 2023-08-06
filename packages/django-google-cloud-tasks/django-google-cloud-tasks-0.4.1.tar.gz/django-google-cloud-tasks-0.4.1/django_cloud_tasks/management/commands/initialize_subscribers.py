6d957329df12943885d4623aa1db6f93b9507adb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:12 +0000

    Add package django-google-cloud-tasks/0.4.1/django-google-cloud-tasks-0.4.1.tar.gz

---
+++ packages/django-google-cloud-tasks/django-google-cloud-tasks-0.4.1.tar.gz/django-google-cloud-tasks-0.4.1/django_cloud_tasks/management/commands/initialize_subscribers.py
@@ -0,0 +1,10 @@
+from typing import List
+
+from django_cloud_tasks.management.commands import BaseInitCommand
+
+
+class Command(BaseInitCommand):
+    name = 'subscribers'
+
+    def perform_init(self, app_config, *args, **options) -> List[str]:
+        return app_config.initialize_subscribers()
