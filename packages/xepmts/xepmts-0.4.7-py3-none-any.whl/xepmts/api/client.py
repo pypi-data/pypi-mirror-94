935e4793277f944dcbf109ba0cb0a5223061ab62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:31 +0000

    Add package xepmts/0.4.7/xepmts-0.4.7-py3-none-any.whl

---
+++ packages/xepmts/xepmts-0.4.7-py3-none-any.whl/xepmts/api/client.py
@@ -0,0 +1,16 @@
+import eve_panel
+from xepmts.api import server
+import os
+import pkg_resources
+
+
+def make_client(version):
+    make_app = getattr(server, version).app.make_app
+    app = make_app()
+    client = eve_panel.EveClient.from_app(app, name="xepmts", sort_by_url=True)
+    client._http_client.auth.set_auth_by_name("Bearer")
+    client.db = client
+    return client
+
+def default_client():
+    return make_client("v1")
