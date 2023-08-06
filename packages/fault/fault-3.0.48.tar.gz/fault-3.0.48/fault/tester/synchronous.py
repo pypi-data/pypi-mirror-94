195fa1bb7900126b0fc93b2a4d5ac23db0ddf96e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:01 +0000

    Add package fault/3.0.48/fault-3.0.48.tar.gz

---
+++ packages/fault/fault-3.0.48.tar.gz/fault-3.0.48/fault/tester/synchronous.py
@@ -0,0 +1,28 @@
+from .staged_tester import StagedTester
+from ..system_verilog_target import SynchronousSystemVerilogTarget
+from ..verilator_target import SynchronousVerilatorTarget
+
+from fault.tester.control import add_control_structures
+
+
+@add_control_structures
+class SynchronousTester(StagedTester):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.clock is None:
+            raise ValueError("SynchronousTester requires a clock")
+
+    def eval(self):
+        raise TypeError("Cannot eval with synchronous tester")
+
+    def advance_cycle(self):
+        self.step(2)
+
+    def make_target(self, target, **kwargs):
+        if target == "system-verilog":
+            return SynchronousSystemVerilogTarget(self._circuit,
+                                                  clock=self.clock, **kwargs)
+        if target == "system-verilog":
+            return SynchronousVerilatorTarget(self._circuit, clock=self.clock,
+                                              **kwargs)
+        return super().make_target(target, **kwargs)
