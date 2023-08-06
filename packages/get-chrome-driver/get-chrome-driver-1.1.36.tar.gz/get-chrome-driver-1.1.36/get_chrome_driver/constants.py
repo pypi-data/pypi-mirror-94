27c39b5aca7b0b77b87f7fbba302f9ad38c6eda6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:34 +0000

    Add package get-chrome-driver/1.1.36/get-chrome-driver-1.1.36.tar.gz

---
+++ packages/get-chrome-driver/get-chrome-driver-1.1.36.tar.gz/get-chrome-driver-1.1.36/get_chrome_driver/constants.py
@@ -0,0 +1,13 @@
+CHROMEDRIVER_CHROMIUM_URL = 'https://chromedriver.chromium.org'
+CHROMEDRIVER_STORAGE_URL = 'https://chromedriver.storage.googleapis.com'
+
+ZIP_TYPE = '.zip'
+
+CHROMEDRIVER = 'chromedriver'
+FILE_NAME_CHROMEDRIVER = 'chromedriver'
+FILE_NAME_CHROMEDRIVER_EXE = 'chromedriver.exe'
+
+UL_VERSIONS_SELECTOR = '#sites-canvas-main-content > table > tbody > tr > td > div > div:nth-child(5) > ul'
+
+LATEST_STABLE_VERSION_STR = 'Latest stable release'
+LATEST_BETA_VERSION_STR = 'Latest beta release'
