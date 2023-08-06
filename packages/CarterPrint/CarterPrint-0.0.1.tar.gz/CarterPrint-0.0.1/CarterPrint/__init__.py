0ab0b660c6f023b8d775d2eb6ca57615538c4473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:47 +0000

    Add package CarterPrint/0.0.1/CarterPrint-0.0.1.tar.gz

---
+++ packages/CarterPrint/CarterPrint-0.0.1.tar.gz/CarterPrint-0.0.1/CarterPrint/__init__.py
@@ -0,0 +1,7 @@
+def start():
+    usermsg = input("Please enter a message you would like the console to print back out to you: ")
+    print_msg(usermsg)
+
+def print_msg(msg):
+    print(f"Your message was {msg}")
+
---
+++ packages/CarterPrint/CarterPrint-0.0.1.tar.gz/CarterPrint-0.0.1/README.txt
@@ -0,0 +1,4 @@
+This is a module where you can enter a message and the console will print it back out.
+
+Developed by Carter Hagans
+Version 1
