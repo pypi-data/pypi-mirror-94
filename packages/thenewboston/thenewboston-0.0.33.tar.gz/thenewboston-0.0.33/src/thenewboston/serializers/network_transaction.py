9048b79ec836e1294d2a738cfdf4454da7dc1954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:23 +0000

    Add package thenewboston/0.0.33/thenewboston-0.0.33.tar.gz

---
+++ packages/thenewboston/thenewboston-0.0.33.tar.gz/thenewboston-0.0.33/src/thenewboston/serializers/network_transaction.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+
+from thenewboston.constants.network import MAX_POINT_VALUE, MIN_POINT_VALUE, VERIFY_KEY_LENGTH
+from thenewboston.utils.serializers import validate_keys
+
+
+class NetworkTransactionSerializer(serializers.Serializer):
+    amount = serializers.IntegerField(max_value=MAX_POINT_VALUE, min_value=MIN_POINT_VALUE)
+    recipient = serializers.CharField(max_length=VERIFY_KEY_LENGTH, min_length=VERIFY_KEY_LENGTH)
+
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
+
+    def validate(self, data):
+        """Check that there are no additional keys included in the data"""
+        validate_keys(self, data)
+
+        return data
+
+    @staticmethod
+    def validate_amount(amount):
+        """Check that amount is not 0"""
+        if amount == 0:
+            raise serializers.ValidationError('Tx amount can not be 0 (Tx should be excluded)')
+
+        return amount
