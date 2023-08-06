592feb15b233576967d4129937b02fdad45fbe1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:28 +0000

    Add package vantetider-scraper/0.2.0/vantetider_scraper-0.2.0.tar.gz

---
+++ packages/vantetider-scraper/vantetider_scraper-0.2.0.tar.gz/vantetider_scraper-0.2.0/setup.py
@@ -0,0 +1,33 @@
+# encoding: utf-8
+from setuptools import setup
+
+def readme():
+    """Import README for use as long_description."""
+    with open("README.rst") as f:
+        return f.read()
+
+with open("VERSION.txt") as f:
+    version = f.read().strip()
+
+setup(
+    name="vantetider_scraper",
+    version=version,
+    description="A scraper of statistical data from Vantetider.se built on top of Statscraper.",
+    long_description=readme(),
+    url="https://github.com/jplusplus/vantetider-scraper",
+    author="Jens Finnäs",
+    author_email="jens.finnas@gmail.com",
+    license="MIT",
+    packages=["vantetider"],
+    zip_safe=False,
+    install_requires=[
+        "requests",
+        "requests_cache",
+        "BeautifulSoup4",
+    ],
+    test_suite="nose.collector",
+    tests_require=["nose"],
+    include_package_data=True,
+    download_url="https://github.com/jplusplus/vantetider-scraper/archive/%s.tar.gz"
+                 % version,
+)
