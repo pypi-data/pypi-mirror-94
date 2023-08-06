450cc98151e95276c66f6b67a4e8e125595c4f39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:57 +0000

    Add package GooseSLURM/0.3.1/GooseSLURM-0.3.1-py3-none-any.whl

---
+++ packages/GooseSLURM/GooseSLURM-0.3.1-py3-none-any.whl/GooseSLURM/__init__.py
@@ -0,0 +1,12 @@
+
+from . import scripts
+from . import duration
+from . import memory
+from . import files
+from . import table
+from . import rich
+from . import squeue
+from . import sinfo
+from . import ps
+
+__version__ = '0.3.1'
