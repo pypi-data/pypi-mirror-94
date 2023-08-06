2a4ddf8c0915bae395d1883dd041a0046916a1bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:44 +0000

    Add package ccf-spark/0.1.1/ccf_spark-0.1.1.tar.gz

---
+++ packages/ccf-spark/ccf_spark-0.1.1.tar.gz/ccf_spark-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+from os import path
+
+# dir = path.abspath(path.dirname(__file__))
+# with open(path.join(dir, 'README.md'), encoding='utf-8') as f:
+#     long_description = f.read()
+
+setup(
+    name='ccf_spark',
+    version='0.1.1',
+    description='A little CCF implementation in a Spark context with networkx',
+    # long_description=long_description,
+    # long_description_content_type=text/markdown,
+    author='Théo Chennebault',
+    author_email='theo.chennebault@le-cab-politique.fr',
+    url='https://github.com/ErnestBidouille/ccf-spark',
+    packages=[
+        'ccf_spark',
+    ],
+    install_requires=['networkx==2.5', 'pyspark==3.0.1'])
