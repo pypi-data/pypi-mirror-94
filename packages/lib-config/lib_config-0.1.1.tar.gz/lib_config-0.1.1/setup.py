a4ad674ab16bdb43c3d4518d97c79d1828042234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:37 +0000

    Add package lib-config/0.1.1/lib_config-0.1.1.tar.gz

---
+++ packages/lib-config/lib_config-0.1.1.tar.gz/lib_config-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import sys
+
+setup(
+    name='lib_config',
+    packages=find_packages(),
+    version='0.1.1',
+    author='Justin Furuness',
+    author_email='jfuruness@gmail.com',
+    url='https://github.com/jfuruness/lib_config.git',
+    download_url='https://github.com/jfuruness/lib_config.git',
+    keywords=['Furuness', 'Config', 'Wrapper', 'ETL', 'Helper Functions'],
+    install_requires=[
+        'lib_utils',
+        'pytest',
+        ],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3'],
+    entry_points={},
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+)
