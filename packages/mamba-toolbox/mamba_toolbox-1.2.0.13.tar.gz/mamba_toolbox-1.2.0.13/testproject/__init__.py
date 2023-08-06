5cc72441054f983e8000900f372f5d6a99ad649c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:19 +0000

    Add package mamba-toolbox/1.2.0.13/mamba_toolbox-1.2.0.13.tar.gz

---
+++ packages/mamba-toolbox/mamba_toolbox-1.2.0.13.tar.gz/mamba_toolbox-1.2.0.13/testproject/__init__.py
@@ -0,0 +1,16 @@
+# -*- encoding=utf-8 -*-
+# This file was generated automatically
+
+__version__ = (5,1,8,1129)
+
+def start():
+    try:
+        print("testproject v"+".".join(str(x) for x in __version__))
+        # YOUR CODE HERE
+        pass
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == "__main__":
+    start()
+                
