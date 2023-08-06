5317ebdf858ce5014089789198d990ace83c3139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:11 +0000

    Add package Harsh-Simple-Calculator/0.0.1/Harsh-Simple-Calculator-0.0.1.tar.gz

---
+++ packages/Harsh-Simple-Calculator/Harsh-Simple-Calculator-0.0.1.tar.gz/Harsh-Simple-Calculator-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup,find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name = 'Harsh-Simple-Calculator',
+    version = '0.0.1',
+    description = 'Our package will give u a basic calculator function such as add, sub, multiply and divide.',
+    Long_description = open('README.txt').read() + '\n\n\n' + open('CHANGELOG.txt').read(),
+    url = '',
+    author = 'Harsh Mishra',
+    author_email = 'harsh2002.m@gmail.com',
+    license = 'MIT',
+    classifiers = classifiers,
+    keywords = ('add','sub','mul','div'),
+    packages = find_packages(),
+    install_require = ['']
+)
