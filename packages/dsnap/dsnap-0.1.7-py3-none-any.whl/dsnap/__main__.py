5a77bf5d97b391e8d9258b3c43e7eee836b43d19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:47 +0000

    Add package dsnap/0.1.7/dsnap-0.1.7-py3-none-any.whl

---
+++ packages/dsnap/dsnap-0.1.7-py3-none-any.whl/dsnap/__main__.py
@@ -0,0 +1,11 @@
+from botocore.exceptions import NoCredentialsError, NoRegionError
+
+from dsnap.main import app
+import logging
+
+logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.WARNING)
+
+try:
+    app()
+except (NoCredentialsError, NoRegionError) as e:
+    logging.error(e.args[0])
