0f73c945532258d6229793aabef3f56842177447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:38 +0000

    Add package sword2/0.3/sword2-0.3-py3-none-any.whl

---
+++ packages/sword2/sword2-0.3-py3-none-any.whl/sword2/http_layer.py
@@ -0,0 +1,189 @@
+import json
+from .sword2_logging import logging
+http_l = logging.getLogger(__name__)
+
+class HttpResponse(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __getitem__(self, att):
+        # needs to behave like a dictionary
+        # we need to be able to look up at least:
+
+        # content-type
+        # status (as an integer)
+        # location
+        pass
+
+    def __repr__(self):
+        return json.dumps(self.__dict__, indent=True)
+
+    def get(self, att, default=None):
+        # same as __getattr__ but with default return
+        pass
+
+    def keys(self):
+        pass
+
+
+class HttpLayer(object):
+    def __init__(self, *args, **kwargs): pass
+    def add_credentials(self, username, password): pass
+    def request(self, uri, method, headers=None, payload=None):
+        # should return a tuple of an HttpResponse object and the content
+        pass
+
+################################################################################
+# Default httplib2 implementation
+################################################################################
+
+import httplib2
+
+class HttpLib2Response(HttpResponse):
+    def __init__(self, response):
+        self.resp = response
+        self.status = int(self.resp.status)
+
+    def __getitem__(self, att):
+        if att == "status":
+            return self.status
+        return self.resp.get(att)
+
+    def get(self, att, default=None):
+        if att == "status":
+            return self.status
+        return self.resp.get(att, default)
+
+    def keys(self):
+        return list(self.resp.keys())
+
+class HttpLib2Layer(HttpLayer):
+    def __init__(self, cache_dir=".cache", timeout=30.0, ca_certs=None):
+        self.h = httplib2.Http(cache_dir, timeout=timeout, ca_certs=ca_certs)
+
+    def add_credentials(self, username, password):
+        self.h.add_credentials(username, password)
+
+    def request(self, uri, method, headers=None, payload=None):
+        if hasattr(payload, 'read'):
+            # Need to work out why a 401 challenge will stop httplib2 from sending the file...
+            # likely need to make it re-seek to 0...
+            # FIXME: In the meantime, read the file into memory... *sigh*
+            payload = payload.read()
+        resp, content = self.h.request(uri, method, headers=headers, body=payload)
+        return (HttpLib2Response(resp), content)
+
+################################################################################    
+# Guest urllib2 implementation
+################################################################################
+
+import urllib.request, urllib.error, urllib.parse, base64
+
+class PreemptiveBasicAuthHandler(urllib.request.HTTPBasicAuthHandler):
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def http_request(self, request):
+        request.add_header(self.auth_header, 'Basic %s' % base64.b64encode(self.username + ':' + self.password))
+        return request
+
+    https_request = http_request
+
+class UrlLib2Response(HttpResponse):
+    def __init__(self, response):
+        self.response = response
+        self.headers = dict(response.info())
+        self.status = int(self.response.code)
+
+    def __getitem__(self, att):
+        # needs to behave like a dictionary
+        # we need to be able to look up at least:
+
+        # content-type
+        # status
+        # location
+        if att == "status":
+            return self.status
+        return self.headers[att]
+
+    def get(self, att, default=None):
+        # same as __getattr__ but with default return
+        if att == "status":
+            return self.status
+        return self.headers.get(att, default)
+
+    def keys(self):
+        return list(self.headers.keys()) + ["status"]
+
+# http://stackoverflow.com/questions/2502596/python-http-post-a-large-file-with-streaming
+"""
+import urllib2
+import mmap
+
+# Open the file as a memory mapped string. Looks like a string, but 
+# actually accesses the file behind the scenes. 
+f = open('somelargefile.zip','rb')
+mmapped_file_as_string = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+
+# Do the request
+request = urllib2.Request(url, mmapped_file_as_string)
+request.add_header("Content-Type", "application/zip")
+response = urllib2.urlopen(request)
+
+#close everything
+mmapped_file_as_string.close()
+f.close()
+"""
+
+class UrlLib2Layer(HttpLayer):
+    def __init__(self, opener=None):
+        self.opener = opener
+        if self.opener is None:
+            self.opener = urllib.request.build_opener()
+
+    def add_credentials(self, username, password):
+        auth_handler = PreemptiveBasicAuthHandler(username, password)
+        current_handlers = self.opener.handlers
+        new_handlers = current_handlers + [auth_handler]
+        self.opener = urllib.request.build_opener(*new_handlers)
+
+    def request(self, uri, method, headers=None, payload=None):
+        # NOTE: payload can be a file or a string
+
+        if headers is None:
+            headers = {}
+        # should return a tuple of an HttpResponse object and the content
+        try:
+            if method == "GET":
+                req = urllib.request.Request(uri, None, headers)
+                response = self.opener.open(req)
+                return UrlLib2Response(response), response.read()
+            elif method == "POST":
+                req = urllib.request.Request(uri, payload, headers)
+                response = self.opener.open(req)
+                return UrlLib2Response(response), response.read()
+            elif method == "PUT":
+                req = urllib.request.Request(uri, payload, headers)
+                # monkey-patch the request method (which seems to be the fastest
+                # way to do this)
+                req.get_method = lambda: 'PUT'
+                response = self.opener.open(req)
+                return UrlLib2Response(response), response.read()
+            elif method == "DELETE":
+                req = urllib.request.Request(uri, None, headers)
+                # monkey-patch the request method (which seems to be the fastest
+                # way to do this)
+                req.get_method = lambda: 'DELETE'
+                response = self.opener.open(req)
+                return UrlLib2Response(response), response.read()
+            else:
+                raise NotImplementedError()
+        except urllib.error.HTTPError as e:
+            try:
+                # treat it like a normal response
+                return UrlLib2Response(e), e.read()
+            except Exception as e:
+                # unable to read()
+                return UrlLib2Response(e), None
+
---
+++ packages/sword2/sword2-0.3-py3-none-any.whl/sword2/utils.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Utility methods used within the module
+"""
+
+from .sword2_logging import logging
+utils_l = logging.getLogger(__name__)
+
+from time import time
+from datetime import datetime
+
+from base64 import b64encode
+
+try:
+    from hashlib import md5
+except ImportError:
+    import md5
+
+import mimetypes
+
+NS = {}
+NS['dcterms'] = "{http://purl.org/dc/terms/}%s"
+NS['sword'] ="{http://purl.org/net/sword/terms/}%s"
+NS['atom'] = "{http://www.w3.org/2005/Atom}%s"
+NS['app'] = "{http://www.w3.org/2007/app}%s"
+NS['rdf'] = "{http://www.w3.org/1999/02/22-rdf-syntax-ns#}%s"
+NS['ore'] = "{http://www.openarchives.org/ore/terms/}%s"
+
+def get_text(parent, tag, plural = False):
+    """Takes an `etree.Element` and a tag name to search for and retrieves the text attribute from any
+    of the parent element's direct children.
+    
+    Returns a simple `str` if only a single element is found, or a list if multiple elements with the
+    same tag. Ignores element attributes, returning only the text."""
+    text = None
+    for item in parent.findall(tag):
+        t = item.text
+        if not text:
+            if plural:
+                text = [t]
+            else:
+                text = t
+        elif isinstance(text, list):
+            text.append(t)
+        else:
+            text = [text, t]
+    return text
+
+def get_md5(data):
+    """Takes either a `str` or a file-like object and passes back a tuple containing (md5sum, filesize)
+    
+    The file is streamed as 1Mb chunks so should work for large files. File-like object must support `seek()`
+    """
+    if hasattr(data, "read") and hasattr(data, 'seek'):
+        m = md5()
+        chunk = data.read(1024*1024)   # 1Mb
+        f_size = 0
+        while(chunk):
+            f_size += len(chunk)
+            m.update(chunk)
+            chunk = data.read(1024*1024)
+        data.seek(0)
+        return m.hexdigest(), f_size
+    else:       # normal str
+        m = md5()
+        f_size = len(data)
+        m.update(data)
+        return m.hexdigest(), f_size
+        
+
+class Timer(object):
+    """Simple timer, providing a 'stopwatch' mechanism.
+    
+    Usage example:
+        
+    >>> from sword2.utils import Timer
+    >>> from time import sleep
+    >>> t = Timer()
+    >>> t.get_timestamp()
+    datetime.datetime(2011, 6, 7, 7, 40, 53, 87248)
+    >>> t.get_loggable_timestamp()
+    '2011-06-07T07:40:53.087516'
+
+    >>> # Start a few timers
+    ... t.start("kaylee", "river", "inara")
+    >>> sleep(3)   # wait a little while
+    >>> t.time_since_start("kaylee")
+    (0, 3.0048139095306396)
+
+    # tuple -> (index of the logged .duration, time since the .start method was called)
+    # eg 't.duration['kaylee'][0]' would equal 3.00481.... 
+
+    >>> sleep(2)
+    >>> t.time_since_start("kaylee", "inara")
+    [(1, 5.00858998298645), (0, 5.00858998298645)]
+    >>> sleep(5)
+    >>> t.time_since_start("kaylee", "river")
+    [(2, 10.015379905700684), (0, 10.015379905700684)]
+    >>> sleep(4)
+    >>> t.time_since_start("kaylee", "inara", "river")
+    [(3, 14.021538972854614), (1, 14.021538972854614), (1, 14.021538972854614)]
+    
+    # The order of the response is the same as the order of the names in the method call.
+    
+    >>> # report back
+    ... t.duration['kaylee']
+    [3.0048139095306396, 5.00858998298645, 10.015379905700684, 14.021538972854614]
+    >>> t.duration['inara']
+    [5.00858998298645, 14.021538972854614]
+    >>> t.duration['river']
+    [10.015379905700684, 14.021538972854614]
+    >>> 
+    """
+    def __init__(self):
+        self.reset_all()
+        
+    def reset_all(self):
+        self.counts = {}    
+        self.duration = {}
+        self.stop = {}
+
+    def reset(self, name):
+        if name in self.counts:
+            self.counts[name] = 0
+    
+    def read_raw(self, name):
+        return self.counts.get(name, None)
+    
+    def read(self, name):
+        if name in self.counts:
+            return datetime.fromtimestamp(self.counts[name])
+        else:
+            return None
+
+    def start(self, *args):
+        st_time = time()
+        for arg in args:
+            self.counts[arg] = st_time
+
+    def stop(self, *args):
+        st_time = time()
+        for arg in args:
+            self.stop[arg] = st_time
+    
+    def get_timestamp(self):
+        # Convenience function
+        return datetime.now()
+    
+    def get_loggable_timestamp(self):
+        """Human-readable by intent"""
+        return datetime.now().isoformat()
+        
+    def time_since_start(self, *args):
+        r = []
+        st_time = time()
+        for name in args:
+            if name in self.counts:
+                duration = st_time - self.counts[name]
+                if name not in self.duration:
+                    self.duration[name] = []
+                self.duration[name].append(duration)
+                r.append((len(self.duration[name]) - 1, duration))
+            else:
+                r.append((0, 0))
+        if len(r) == 1:
+            return r.pop()
+        else:
+            return r
+            
+
+def get_content_type(filename):
+    # Does a simple .ext -> mimetype mapping.
+    # Generally better to specify the mimetype upfront.
+    return mimetypes.guess_type(filename)[0] or 'application/octet-stream'
+
+def create_multipart_related(payloads):
+    """ Expected: list of dicts with keys 'key', 'type'='content type','filename'=optional,'data'=payload, 'headers'={} 
+    
+    TODO: More mem-efficient to spool this to disc rather than hold in RAM, but until Httplib2 bug gets fixed (issue 151)
+    this might be in vain.
+    
+    Can handle more than just two files. 
+    
+    SWORD2 multipart POST/PUT expects two attachments - key = 'atom' w/ Atom Entry (metadata)
+                                                        key = 'payload' (file)
+    """
+    # Generate random boundary code
+    # TODO check that it does not occur in the payload data
+    bhash = md5(datetime.now().isoformat()).hexdigest()    # eg 'd8bb3ea6f4e0a4b4682be0cfb4e0a24e'
+    BOUNDARY = '===========%s_$' % bhash
+    CRLF = '\r\n'   # As some servers might barf without this.
+    body = []
+    for payload in payloads:   # predicatable ordering...
+        body.append('--' + BOUNDARY)
+        if payload.get('type', None):
+            body.append('Content-Type: %(type)s' % payload)
+        else:
+            body.append('Content-Type: %s' % get_content_type(payload.get("filename")))
+            
+        if payload.get('filename', None):
+            body.append('Content-Disposition: attachment; name="%(key)s"; filename="%(filename)s"' % (payload))
+        else:
+            body.append('Content-Disposition: attachment; name="%(key)s"' % (payload))
+        
+        if "headers" in payload:
+            for f,v in payload['headers'].items():
+                body.append("%s: %s" % (f, v))     # TODO force ASCII?
+        
+        body.append('MIME-Version: 1.0')
+        if payload['key'] == 'payload':
+            body.append('Content-Transfer-Encoding: base64')
+            body.append('')
+            if hasattr(payload['data'], 'read'):
+                body.append(b64encode(payload['data'].read()))
+            else:
+                body.append(b64encode(payload['data']))
+        else:
+            body.append('')
+            if hasattr(payload['data'], 'read'):
+                body.append(b64encode(payload['data'].read()))
+            else:
+                body.append(b64encode(payload['data']))
+    body.append('--' + BOUNDARY + '--')
+    body.append('')
+    body_bytes = CRLF.join(body)
+    content_type = 'multipart/related; boundary="%s"' % BOUNDARY
+    return content_type, body_bytes
