27043922ad85b300be8db5f9efec44b53efd26b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:32 +0000

    Add package dbjson/0.0.2/dbjson-0.0.2.tar.gz

---
+++ packages/dbjson/dbjson-0.0.2.tar.gz/dbjson-0.0.2/dbjson/jsondbb/test.py
@@ -0,0 +1,9 @@
+from main import DB
+db = DB()
+db.createCollection("data")
+db.createCollection("data1")
+db.createCollection("data2")
+db.createCollection("data3")
+db.createCollection("data4")
+db.createCollection("data5")
+
