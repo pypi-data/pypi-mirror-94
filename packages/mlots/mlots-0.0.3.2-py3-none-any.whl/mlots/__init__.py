12e4ea642096bb2a89a59b6102608eb517e24a9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:38 +0000

    Add package mlots/0.0.3.2/mlots-0.0.3.2-py3-none-any.whl

---
+++ packages/mlots/mlots-0.0.3.2-py3-none-any.whl/mlots/__init__.py
@@ -0,0 +1,5 @@
+from .AnnoyClassifier import AnnoyClassifier
+from .HNSWClassifier import HNSWClassifier
+from .kNNClassifier import kNNClassifier
+from .kNNClassifier_CustomDist import kNNClassifier_CustomDist
+from .NSW import NSW
