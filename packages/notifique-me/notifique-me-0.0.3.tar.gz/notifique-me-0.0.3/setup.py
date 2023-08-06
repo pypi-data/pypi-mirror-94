b74099e0cec920a4cb2aaca3c5419ef0bd18c747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:16 +0000

    Add package notifique-me/0.0.3/notifique-me-0.0.3.tar.gz

---
+++ packages/notifique-me/notifique-me-0.0.3.tar.gz/notifique-me-0.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(
+    name='notifique-me',
+    version='0.0.3',
+    url='https://notifique-me.com',
+    license='MIT License',
+    author='Marcos Rafael Rodrigues',
+	long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author_email='rafaeljacuri@gmail.com',
+    keywords='Pacote',
+    description=u'Pacote para enviar mensagem no WhatsApp e SMS, de graça para desenvolvedores',
+    packages=['notifique-me'],
+    install_requires=['http.client','print_function','SimpleNamespace','Namespace'],
+)
