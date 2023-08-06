875eb51a6f9ae09c9add0514ae12da1068c8cf73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:48 +0000

    Add package signer-pdf/0.1.1/signer_pdf-0.1.1-py3-none-any.whl

---
+++ packages/signer-pdf/signer_pdf-0.1.1-py3-none-any.whl/signer_pdf/signer.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from typing import BinaryIO
+
+import PyPDF2
+import io
+
+import signer_pdf.sign_generator as sign_generator
+
+
+def sign(name, date, a_hash, in_file):
+    bs = sign_generator.convert(name, date, a_hash)
+    original = PyPDF2.PdfFileReader(in_file)
+    last_page = original.getPage(original.getNumPages() - 1)
+    foreground = PyPDF2.PdfFileReader(bs).getPage(0)
+    last_page.mergePage(foreground)
+    writer = PyPDF2.PdfFileWriter()
+    for i in range(original.getNumPages()):
+        page = original.getPage(i)
+        writer.addPage(page)
+
+    fp = io.BytesIO()
+    writer.write(fp)
+    return fp
