6146fbe4dc27c1aa7e85afdafa1520211eef49bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:38 +0000

    Add package sl-git-hooks/0.1.1105/sl-git-hooks-0.1.1105.tar.gz

---
+++ packages/sl-git-hooks/sl-git-hooks-0.1.1105.tar.gz/sl-git-hooks-0.1.1105/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sl_git_hooks', 'sl_git_hooks.hooks']
+
+package_data = \
+{'': ['*'], 'sl_git_hooks': ['configs/*', 'templates/*']}
+
+install_requires = \
+['pygit2>=1.5.0,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['sl-git-hooks = sl_git_hooks:main']}
+
+setup_kwargs = {
+    'name': 'sl-git-hooks',
+    'version': '0.1.1105',
+    'description': '',
+    'long_description': None,
+    'author': 'Alexander Memer',
+    'author_email': 'mkoaleksedos@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/sl-git-hooks/sl-git-hooks-0.1.1105.tar.gz/sl-git-hooks-0.1.1105/sl_git_hooks/hooks/pre_commit.py
@@ -0,0 +1,33 @@
+import pygit2
+
+import os
+import sys
+
+import sl_git_hooks.config as config
+import sl_git_hooks.utils as utils
+
+
+def precommitHandler():
+    print('Precommit hook!')
+
+    cwd = os.getcwd()
+
+    repository = pygit2.Repository(cwd)
+    project_config = config.find_config_by_repo_hash(utils.find_repo_hash(repository))
+
+    if project_config.getboolean('profile', 'enabled'):
+        username = repository.config["user.name"]
+        email = repository.config["user.email"]
+
+        project_name = project_config.get('profile', 'name')
+        project_email = project_config.get('profile', 'email')
+
+        if username != project_name:
+            print(f'[ERROR] git user.name expected {project_name}, got {username}')
+            sys.exit(1)
+
+        if email != project_name:
+            print(f'[ERROR] git user.email expected {project_email}, got {email}')
+            sys.exit(1)
+
+    return 0
