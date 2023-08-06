6cc044cd00087f749d3037e60514b7ef60cd7946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:30 +0000

    Add package harmonoid-module/1.0.0.1/harmonoid-module-1.0.0.1.tar.gz

---
+++ packages/harmonoid-module/harmonoid-module-1.0.0.1.tar.gz/harmonoid-module-1.0.0.1/README.md
@@ -0,0 +1,2 @@
+# harmonoid-module
+A Python wrapper around harmonoid-service server code published to PyPI
---
+++ packages/harmonoid-module/harmonoid-module-1.0.0.1.tar.gz/harmonoid-module-1.0.0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="harmonoid-module", # Replace with your own username
+    version="1.0.0.1",
+    author="harmonoid Team",
+    description="Harmonoid-service wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/harmonoid/harmonoid-module",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'ytmusicapi',
+        'pytube',
+        "mutagen",
+        "aiofiles",
+        "httpx"
+    ],
+    python_requires='>=3.6',
+)
