cc592898f4c25926d56686d06086e56cd138716b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:24 +0000

    Add package simplification/0.5.10/simplification-0.5.10-cp37-cp37m-win32.whl

---
+++ packages/simplification/simplification-0.5.10-cp37-cp37m-win32.whl/simplification/test/cprofile_rust_cython.py
@@ -0,0 +1,10 @@
+# this tests numpy array simplification using VW
+
+from simplification.cutil import simplify_coords_vw
+import json
+import numpy as np
+
+with open("simplification/test/coords.json", "r") as f:
+    coords = np.array(json.load(f))
+for x in range(50):
+    simplify_coords_vw(coords, 0.0000075)
