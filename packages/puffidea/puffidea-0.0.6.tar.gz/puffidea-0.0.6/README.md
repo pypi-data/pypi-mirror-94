ba2c023032b6f4508ce8f7ca60837c63e00bc1fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:06 +0000

    Add package puffidea/0.0.6/puffidea-0.0.6.tar.gz

---
+++ packages/puffidea/puffidea-0.0.6.tar.gz/puffidea-0.0.6/README.md
@@ -0,0 +1,26 @@
+# 获取Jetbrains激活码
+
+### 安装方式 
+```pip install puffidea```
+<br />
+
+Or
+<br /> 
+
+```pip3 install puffieda```
+
+<br />
+
+### 使用方式 
+```python -m idea.code -h```
+
+Or
+<br /> 
+
+```python3 -m idea.code -h```
+
+<br />
+
+### 构建方式:
+##### ```python setup.py sdist bdist_wheel```
+##### ```twine upload dist/*```
---
+++ packages/puffidea/puffidea-0.0.6.tar.gz/puffidea-0.0.6/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="puffidea",
+    version="0.0.6",
+    author="puff",
+    author_email="angrypuff333@gmail.com",
+    description="获取Jetbrains激活码",
+    install_requires=['requests'],
+    long_description=open("README.md", 'r').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/PeterPuffer/puffidea",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
