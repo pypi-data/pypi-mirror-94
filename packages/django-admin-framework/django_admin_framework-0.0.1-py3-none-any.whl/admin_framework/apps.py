2f181d7977975a0e524815bad332144b56360301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:01 +0000

    Add package django-admin-framework/0.0.1/django_admin_framework-0.0.1-py3-none-any.whl

---
+++ packages/django-admin-framework/django_admin_framework-0.0.1-py3-none-any.whl/admin_framework/apps.py
@@ -0,0 +1,8 @@
+from django import apps
+
+
+class AdminFrameworkAppConfig(apps.AppConfig):
+    """Settings class for admin framework application."""
+
+    name = 'admin_framework'
+    verbose_name = 'Django Admin Framework'
