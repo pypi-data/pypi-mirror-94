a4f92e4aaa41bf85632cf703b6c0cc50e66c095a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:36 +0000

    Add package akerbp.mlops/0.20210209092138/akerbp.mlops-0.20210209092138-py3-none-any.whl

---
+++ packages/akerbp.mlops/akerbp.mlops-0.20210209092138-py3-none-any.whl/akerbp/mlops/prediction/handler.py
@@ -0,0 +1,18 @@
+# handler.py
+import warnings
+warnings.simplefilter("ignore")
+
+from akerbp.mlops.prediction.service import service
+from akerbp.mlops.core import logger
+
+logging=logger.get_logger()
+
+
+def handle(data, secrets):
+   try:
+      return service(data, secrets)
+   except Exception as error:
+      error_type = type(error).__name__
+      error_message = f" Prediction service failed. {error_type}: {error}"
+      logging.critical(error_message)
+      return dict(status='error', error_message=error_message)
