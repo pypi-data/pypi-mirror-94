e79da286bfb9209476feb113bf9e3dafa4705019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:12 +0000

    Add package aiocron/1.4/aiocron-1.4.tar.gz

---
+++ packages/aiocron/aiocron-1.4.tar.gz/aiocron-1.4/CHANGES.rst
@@ -0,0 +1,76 @@
+1.4 (2021-02-13)
+================
+
+- use async/await syntax in README
+
+
+1.3 (2018-10-28)
+================
+
+- Now accepts TZ for croniter
+
+- Now accepts arguments for func
+
+- Drop py3.3 support
+
+
+1.2 (2017-12-05)
+================
+
+- Remove print() statement
+
+
+1.1 (2017-11-11)
+================
+
+- Fixed #7: Incorrect cron calculation in croniter when DST changed.
+
+
+1.0 (2017-10-31)
+================
+
+- First stable release
+
+- Update classifiers to show it support latest py versions
+
+
+0.7 (2017-09-01)
+================
+
+- fixed issue 6: stop before first run wont work
+
+
+0.6 (2016-10-18)
+================
+
+- allow to use python -m aiocron
+
+
+0.5 (2016-03-31)
+================
+
+- Fix: Initialize cron with a correct tzinfo
+
+
+0.4 (2015-10-21)
+================
+
+-  Fix contstructor with func not None and minor changes
+
+
+0.3 (2015-05-14)
+================
+
+- Fix installation faillure on py3
+
+
+0.2 (2015-02-23)
+================
+
+Allow to use crontab as a timer
+
+
+0.1 (2015-02-22)
+================
+
+Initial release
---
+++ packages/aiocron/aiocron-1.4.tar.gz/aiocron-1.4/setup.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+from setuptools import setup
+from setuptools import find_packages
+
+version = '1.4'
+
+install_requires = ['croniter', 'tzlocal']
+test_requires = ['coverage']
+
+py_ver = sys.version_info[:2]
+if py_ver < (3, 0):
+    install_requires.extend([
+        'trollius',
+        'futures',
+    ])
+    test_requires.extend(['mock'])
+elif py_ver < (3, 3):
+    install_requires.append('trollius')
+    test_requires.append('mock')
+elif py_ver < (3, 4):
+    install_requires.append('asyncio')
+
+
+def read(name):
+    with open(os.path.join(os.path.dirname(__file__), name)) as f:
+        return f.read()
+
+
+setup(
+    name='aiocron',
+    version=version,
+    description="Crontabs for asyncio",
+    long_description=read('README.rst'),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='crontab cron asyncio',
+    author='Gael Pasgrimaud',
+    author_email='gael@gawel.org',
+    url='https://github.com/gawel/aiocron/',
+    license='MIT',
+    packages=find_packages(exclude=['docs', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    extras_require={
+        'test': test_requires,
+    },
+)
