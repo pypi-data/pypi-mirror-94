1ec2b43f33cea1eb166f154639c4bc1de03a5b01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:15 +0000

    Add package qauto.makefile.machine-action.avast/0.0.1/qauto.makefile.machine_action.avast-0.0.1.tar.gz

---
+++ packages/qauto.makefile.machine-action.avast/qauto.makefile.machine_action.avast-0.0.1.tar.gz/qauto.makefile.machine_action.avast-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='qauto.makefile.machine_action.avast',
+    description='Makefile machine actions',
+    author='QA Automation',
+    version='0.0.1',
+    classifiers=['Programming Language :: Python :: 3'],
+)
