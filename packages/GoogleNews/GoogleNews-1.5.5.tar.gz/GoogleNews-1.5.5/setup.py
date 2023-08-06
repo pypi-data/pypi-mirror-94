0dfcfe019fe95dad04a303783da5d52f3303f306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:29 +0000

    Add package GoogleNews/1.5.5/GoogleNews-1.5.5.tar.gz

---
+++ packages/GoogleNews/GoogleNews-1.5.5.tar.gz/GoogleNews-1.5.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="GoogleNews",
+    version="1.5.5",
+    author="Hurin Hu",
+    author_email="hurin@live.ca",
+    description="Google News search for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Iceloof/GoogleNews",
+    packages=setuptools.find_packages(),
+    install_requires=['beautifulsoup4','dateparser','python-dateutil'],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
