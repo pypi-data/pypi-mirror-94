f26ae50f16a5ac6b6c40e95589ee794ffa697524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:20 +0000

    Add package pipe-framework/0.1.0a0/pipe-framework-0.1.0a0.tar.gz

---
+++ packages/pipe-framework/pipe-framework-0.1.0a0.tar.gz/pipe-framework-0.1.0a0/pipe/core/exceptions.py
@@ -0,0 +1,26 @@
+class PipeException(Exception):
+    pass
+
+
+class ExtractorException(Exception):
+    pass
+
+
+class LoaderException(Exception):
+    pass
+
+
+class TransformerException(Exception):
+    pass
+
+
+class StepInitializationException(Exception):
+    pass
+
+
+class StepExecutionException(Exception):
+    pass
+
+
+class StepValidationException(Exception):
+    pass
---
+++ packages/pipe-framework/pipe-framework-0.1.0a0.tar.gz/pipe-framework-0.1.0a0/pipe/generics/__init__.py
@@ -0,0 +1,1 @@
+import pipe.generics.template as template
