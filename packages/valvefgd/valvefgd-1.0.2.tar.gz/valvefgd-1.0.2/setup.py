af7a9bf681d5669886ba80158dc5e78b5bf8ef66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:47 +0000

    Add package valvefgd/1.0.2/valvefgd-1.0.2.tar.gz

---
+++ packages/valvefgd/valvefgd-1.0.2.tar.gz/valvefgd-1.0.2/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+    name='valvefgd',
+    packages=['valvefgd'],
+    version='1.0.2',
+    license='gpl-3.0',
+    description='A library to parse .fgd files used in the source engine.',
+    author='Maxime Dupuis',
+    author_email='mdupuis@hotmail.ca',
+    url='https://maxdup.github.io/pySourceSDK/ValveFGD/',
+    download_url='https://github.com/pySourceSDK/ValveFGD/archive/v1.0.2.tar.gz',
+    keywords=['fgd', 'source', 'sourcesdk', 'hammer', 'valve'],
+    install_requires=['pyparsing', 'future'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
