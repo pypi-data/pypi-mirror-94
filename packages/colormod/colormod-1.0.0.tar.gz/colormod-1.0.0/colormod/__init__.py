ba86606cfd10b01d5404463de3abebc6d1543eed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:29 +0000

    Add package colormod/1.0.0/colormod-1.0.0.tar.gz

---
+++ packages/colormod/colormod-1.0.0.tar.gz/colormod-1.0.0/colormod/__init__.py
@@ -0,0 +1,25 @@
+import colorama
+
+def red(s: str) -> str:
+    return colorama.Fore.LIGHTRED_EX + s + colorama.Fore.RESET
+
+def blue(s: str) -> str:
+    return colorama.Fore.LIGHTBLUE_EX + s + colorama.Fore.RESET
+
+def cyan(s: str) -> str:
+    return colorama.Fore.LIGHTCYAN_EX + s + colorama.Fore.RESET
+
+def green(s: str) -> str:
+    return colorama.Fore.LIGHTGREEN_EX + s + colorama.Fore.RESET
+
+def yellow(s: str) -> str:
+    return colorama.Fore.LIGHTYELLOW_EX + s + colorama.Fore.RESET
+
+def magenta(s: str) -> str:
+    return colorama.Fore.LIGHTMAGENTA_EX + s + colorama.Fore.RESET
+
+def black(s: str) -> str:
+    return colorama.Fore.LIGHTBLACK_EX + s + colorama.Fore.RESET
+
+def white(s: str) -> str:
+    return colorama.Fore.LIGHTWHITE_EX + s + colorama.Fore.RESET
