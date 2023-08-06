692396cf4cb20704ec7f276d89de5b94b2f59cf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:11 +0000

    Add package wheezy-extractor/1.0/Wheezy-extractor-1.0.tar.gz

---
+++ packages/wheezy-extractor/Wheezy-extractor-1.0.tar.gz/Wheezy-extractor-1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(
+    name="Wheezy-extractor",
+    version="1.0",
+    description="Babel extracto for Wheezy templates",
+    url="https://github.com/Polsaker/wheezy_extractor",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    entry_points="""
+    [babel.extractors]
+    wheezyhtml = wheezy_extractor:extract_wheezy
+    """
+)
