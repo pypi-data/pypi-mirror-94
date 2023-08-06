1afaa46a8de1ed5a30458e98078487cffb7cb455
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:17 +0000

    Add package djangoloadbalancer/0.0.46/djangoloadbalancer-0.0.46-py3-none-any.whl

---
+++ packages/djangoloadbalancer/djangoloadbalancer-0.0.46-py3-none-any.whl/django_load_balancer/waiter.py
@@ -0,0 +1,41 @@
+import django
+import django.db.utils
+
+from .database import DatabaseStatus
+
+
+class Waiter:
+    def __init__(self, executor=None):
+        self.executor = executor
+
+    def run_query(self, query, database):
+        pass
+
+
+class WaitWaiter(Waiter):
+    def __init__(self, executor=None):
+        super(WaitWaiter, self).__init__(executor)
+
+    def run_query(self, query, database):
+        if database.check_status() == DatabaseStatus.RUNNING.value:
+            try:
+                self.executor.run_query(query, database)
+            except django.db.utils.OperationalError:
+                database.change_status(DatabaseStatus.DOWN.value)
+                self.wait_for_connection(query, database)
+        else:
+            self.wait_for_connection(query, database)
+
+    def wait_for_connection(self, query, database):
+        database.is_up.wait()
+        database.is_up.clear()
+        self.run_query(query, database)
+
+
+class DontWaitWaiter(Waiter):
+    def run_query(self, query, database):
+        try:
+            self.executor.run_query(query, database)
+        except django.db.utils.OperationalError:
+            database.change_status(DatabaseStatus.DOWN.value)
+            raise django.db.utils.OperationalError
