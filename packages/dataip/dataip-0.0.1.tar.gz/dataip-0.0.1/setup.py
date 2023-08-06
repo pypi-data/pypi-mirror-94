92a48be51a70538152e08b18e504c3bfc947e218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:17 +0000

    Add package dataip/0.0.1/dataip-0.0.1.tar.gz

---
+++ packages/dataip/dataip-0.0.1.tar.gz/dataip-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name = "dataip",
+    version="0.0.1",
+    description="dataip package is used to gather information based on your IP address",
+    author="Adity Khemka",
+    packages=['dataip'],
+    install_requires=['requests'],
+    author_email="khemkaaditya48@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=['ip','aditya','khemka','aditya khemka','ipinfo','ip address','python','ipdata','dataip']
+    )
