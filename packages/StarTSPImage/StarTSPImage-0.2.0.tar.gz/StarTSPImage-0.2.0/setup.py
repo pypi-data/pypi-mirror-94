f99125e788a010658c5d51c245e3ad0ebe444424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:23 +0000

    Add package StarTSPImage/0.2.0/StarTSPImage-0.2.0.tar.gz

---
+++ packages/StarTSPImage/StarTSPImage-0.2.0.tar.gz/StarTSPImage-0.2.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='StarTSPImage',
+    version='0.2.0',
+    packages=find_packages(),
+    license='MIT',
+    description='Creates Star graphics mode raster images for use with Star TSP series thermal printers',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    install_requires=['Pillow'],
+    url='https://github.com/geftactics/python-StarTSPImage',
+    download_url = 'https://github.com/geftactics/python-StarTSPImage/archive/0.2.0.tar.gz',
+    author='Geoff Kendal',
+    author_email='Geoff@squiggle.org'
+)
