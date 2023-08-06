d2c51b725d98916bf657d9b98b71abd8dc1c04bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:08:56 +0000

    Add package XenGarden/1.1.5/XenGarden-1.1.5-py3-none-any.whl

---
+++ packages/XenGarden/XenGarden-1.1.5-py3-none-any.whl/XenGarden/Console/__init__.py
@@ -0,0 +1,47 @@
+from deprecated import deprecated
+
+
+class Console:
+    """ The Virtual Console """
+
+    def __init__(self, session, console):
+        self.session = session
+        self.console = console
+
+    @deprecated
+    def serialize(self) -> dict:
+        return {
+            "location": self.get_location(),
+            "protocol": self.get_protocol(),
+            "uuid": self.get_uuid(),
+        }
+
+    @staticmethod
+    def get_by_uuid(session, uuid):
+        """ returns Console object that has specific uuid """
+
+        console = session.xenapi.console.get_by_uuid(uuid)
+        if console is not None:
+            return Console(session, console)
+        else:
+            return None
+
+    def get_location(self):
+        """ The Location for Console """
+
+        return self.session.xenapi.console.get_location(self.console)
+
+    def get_uuid(self):
+        """ Returns UUID of Console """
+
+        return self.session.xenapi.console.get_uuid(self.console)
+
+    def get_protocol(self):
+        """ Returns Protocol of Console """
+
+        return self.session.xenapi.console.get_protocol(self.console)
+
+    def get_VM(self):
+        """ Returns which VM is this console attached to """
+
+        return self.session.xenapi.console.get_VM(self.console)
