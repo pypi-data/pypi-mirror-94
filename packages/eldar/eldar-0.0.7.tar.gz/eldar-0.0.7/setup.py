774a6ceda4e81de4b914b70b6917eb0d75573362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:40 +0000

    Add package eldar/0.0.7/eldar-0.0.7.tar.gz

---
+++ packages/eldar/eldar-0.0.7.tar.gz/eldar-0.0.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="eldar",
+    version="0.0.7",
+    author="Maixent Chenebaux",
+    author_email="max.chbx@gmail.com",
+    description="Boolean text search in Python",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kerighan/eldar",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ],
+)
