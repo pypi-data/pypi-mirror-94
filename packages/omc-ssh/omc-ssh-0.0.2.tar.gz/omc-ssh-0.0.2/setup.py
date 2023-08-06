5c8d13ae2facd9dce9b17e5265b4cf3ee0282115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:36 +0000

    Add package omc-ssh/0.0.2/omc-ssh-0.0.2.tar.gz

---
+++ packages/omc-ssh/omc-ssh-0.0.2.tar.gz/omc-ssh-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+install_requires = [
+    'omc>0.2.1'
+]
+
+
+setup(
+    name='omc-ssh',
+    version="0.0.2",
+    description='ssh plugin for omc',
+    license='MIT',
+    author='Lu Ganlin',
+    author_email='linewx1981@gmail.com',
+    url='https://github.com/linewx/omc-ssh',
+    packages=find_packages(),
+    # package_data={'omc.config': ['*.yaml'], 'omc.lib': ['**', '**/*', '**/**/*']},
+    install_requires=install_requires,
+)
