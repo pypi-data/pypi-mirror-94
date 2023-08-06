58f95c2aa9528f4507aaefddc46297cf4e082a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:06 +0000

    Add package wagtail-bifrost/3.0.1/wagtail_bifrost-3.0.1-py3-none-any.whl

---
+++ packages/wagtail-bifrost/wagtail_bifrost-3.0.1-py3-none-any.whl/bifrost/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+from graphene_subscriptions.consumers import GraphqlSubscriptionConsumer
+
+from .views import BifrostView
+
+# Traditional URL routing
+urlpatterns = [url(r"^graphql", csrf_exempt(BifrostView.as_view()))]
+
+websocket_urlpatterns = [path("graphql/", GraphqlSubscriptionConsumer)]
+
+# if SHOULD_EXPOSE_GRAPHIQL:
+#     urlpatterns.append(url(r"^graphiql", graphiql))
