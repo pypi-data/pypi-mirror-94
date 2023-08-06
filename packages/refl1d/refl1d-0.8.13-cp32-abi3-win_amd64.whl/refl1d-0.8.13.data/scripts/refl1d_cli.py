b62a22d92e92f12d6cc1aa3a0c30c3e35df6f087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:27 +0000

    Add package refl1d/0.8.13/refl1d-0.8.13-cp32-abi3-win_amd64.whl

---
+++ packages/refl1d/refl1d-0.8.13-cp32-abi3-win_amd64.whl/refl1d-0.8.13.data/scripts/refl1d_cli.py
@@ -0,0 +1,18 @@
+#!python
+# This program is in the public domain.
+# Authors: Paul Kienzle and James Krycka
+"""
+Start Refl1D with the command line interface.
+"""
+
+# ========================== Start program ==================================
+# Process the command line that has been entered.
+if __name__ == "__main__":
+    # This is necessary when running the application from a frozen image and
+    # using the --parallel option.  Note that freeze_support() has no effect
+    # when running from a python script (i.e., in a non-frozen environment).
+    import multiprocessing
+    multiprocessing.freeze_support()
+
+    import refl1d.main
+    refl1d.main.cli()
