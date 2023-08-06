f80277e409cf7a763d6b3e50f9f92a353e646821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:40 +0000

    Add package TextJustify/0.0.4/TextJustify-0.0.4.tar.gz

---
+++ packages/TextJustify/TextJustify-0.0.4.tar.gz/TextJustify-0.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(
+    name='TextJustify',
+    version='0.0.4',
+    description='A text justify package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/mhhabib/TextJustify',
+    author='MH Habib',
+    author_email='mhrahman7096@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='justify',
+    packages=find_packages(),
+    install_requires=['']
+)
