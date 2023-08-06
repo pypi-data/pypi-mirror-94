eb6163d0ec93a6a9321d3aa7b79c94b1829dc4e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:34 +0000

    Add package teneva/0.3.2/teneva-0.3.2-py3.7.egg

---
+++ packages/teneva/teneva-0.3.2-py3.7.egg/teneva/__init__.py
@@ -0,0 +1,20 @@
+from .cross import cross
+
+
+from .maxvol import maxvol
+from .maxvol import rect_maxvol
+
+
+from .probability import confidence
+from .probability import get_cdf
+
+
+from .tensor import erank
+from .tensor import get
+from .tensor import getter
+from .tensor import mean
+from .tensor import mul
+from .tensor import norm
+from .tensor import rand
+from .tensor import recap
+from .tensor import truncate
