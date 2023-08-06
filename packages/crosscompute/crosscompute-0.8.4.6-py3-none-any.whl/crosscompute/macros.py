d52270c9a7d299ddd52be566cf86982a2e65d804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:07 +0000

    Add package crosscompute/0.8.4.6/crosscompute-0.8.4.6-py3-none-any.whl

---
+++ packages/crosscompute/crosscompute-0.8.4.6-py3-none-any.whl/crosscompute/macros.py
@@ -0,0 +1,88 @@
+import sys
+from math import isnan
+from os import environ
+from os.path import split
+from packaging.version import parse as parse_version
+
+
+def get_environment_value(name, default=None):
+    try:
+        value = environ[name]
+    except KeyError:
+        if default is None:
+            sys.exit(f'{name} is required in the environment')
+        value = default
+    return value
+
+
+def sanitize_json_value(value):
+    if isinstance(value, dict):
+        return {
+            sanitize_json_value(k): sanitize_json_value(v)
+            for k, v in value.items()}
+    if isinstance(value, list):
+        return [sanitize_json_value(_) for _ in value]
+    if isinstance(value, str):
+        return value
+    return None if value is None or isnan(value) else value
+
+
+def parse_number_safely(raw_value):
+    # TODO: Consider moving to invisibleroads_macros_configuration
+    try:
+        value = parse_number(raw_value)
+    except (TypeError, ValueError):
+        value = raw_value
+    return value
+
+
+def parse_number(raw_value):
+    # TODO: Consider moving to invisibleroads_macros_configuration
+    try:
+        integer_value = int(raw_value)
+    except ValueError:
+        integer_value = None
+    try:
+        float_value = float(raw_value)
+    except ValueError:
+        float_value = None
+    if integer_value is None and float_value is None:
+        raise ValueError
+    elif integer_value != float_value:
+        value = float_value
+    else:
+        value = integer_value
+    return value
+
+
+def split_path(path):
+    pieces = []
+    chunk = path
+    while True:
+        chunk, piece = split(chunk)
+        if not piece:
+            break
+        pieces.append(piece)
+    return pieces[::-1]
+
+
+def is_compatible_version(target_version_name, source_version_name):
+    target_version = parse_version(target_version_name)
+    source_version = parse_version(source_version_name)
+    try:
+        has_same_major = target_version.major == source_version.major
+        has_same_minor = target_version.minor == source_version.minor
+        has_same_micro = target_version.micro == source_version.micro
+    except AttributeError:
+        is_compatible = target_version_name == source_version_name
+    else:
+        is_compatible = has_same_major and has_same_minor and has_same_micro
+    return is_compatible
+
+
+def get_plain_value(x):
+    if hasattr(x, 'items'):
+        return {k: get_plain_value(v) for k, v in x.items()}
+    if hasattr(x, 'append'):
+        return [get_plain_value(_) for _ in x]
+    return x
---
+++ packages/crosscompute/crosscompute-0.8.4.6-py3-none-any.whl/crosscompute/routines/connection.py
@@ -0,0 +1,109 @@
+import json
+import requests
+from sseclient import SSEClient
+
+from ..constants import (
+    BASH_CONFIGURATION_TEXT,
+    CLIENT_URL,
+    SERVER_URL)
+from ..exceptions import (
+    CrossComputeConnectionError,
+    CrossComputeExecutionError,
+    CrossComputeImplementationError,
+    CrossComputeKeyboardInterrupt)
+from ..macros import (
+    get_environment_value)
+from .serialization import render_object
+
+
+def get_bash_configuration_text():
+    return BASH_CONFIGURATION_TEXT.format(
+        client_url=get_client_url(),
+        server_url=get_server_url(),
+        token=get_token('YOUR-TOKEN'))
+
+
+def fetch_resource(
+        resource_name, resource_id=None, method='GET', data=None,
+        server_url=None, token=None):
+    f = getattr(requests, method.lower())
+    server_url = server_url if server_url else get_server_url()
+    url = get_resource_url(server_url, resource_name, resource_id)
+    token = token if token else get_token()
+    kw = {} if data is None else {'json': data}
+    try:
+        response = f(url, headers={'Authorization': 'Bearer ' + token}, **kw)
+    except requests.ConnectionError:
+        raise CrossComputeConnectionError({
+            'url': 'could not connect to server ' + url})
+    status_code = response.status_code
+    d = {'url': url, 'token': token, 'statusCode': status_code}
+    if status_code in [401, 403]:
+        d['statusHelp'] = 'please check your server url and token'
+        raise CrossComputeConnectionError(d)
+    try:
+        response_json = response.json()
+    except ValueError:
+        d['statusHelp'] = 'could not parse response as json'
+        d['responseContent'] = response.content.decode('utf-8')
+        raise CrossComputeConnectionError(d)
+    if status_code != 200:
+        if response_json:
+            d.update(response_json)
+        raise (
+            CrossComputeExecutionError if status_code == 400 else
+            CrossComputeImplementationError)(d)
+    return response_json
+
+
+def get_client_url():
+    return get_environment_value('CROSSCOMPUTE_CLIENT', CLIENT_URL)
+
+
+def get_server_url():
+    return get_environment_value('CROSSCOMPUTE_SERVER', SERVER_URL)
+
+
+def get_echoes_url():
+    server_url = get_environment_value('CROSSCOMPUTE_ECHOES', get_server_url())
+    token = get_token()
+    return f'{server_url}/echoes/{token}.json'
+
+
+def get_token(default=None):
+    return get_environment_value('CROSSCOMPUTE_TOKEN', default)
+
+
+def get_resource_url(server_url, resource_name, resource_id=None):
+    url = server_url + '/' + resource_name
+    if resource_id:
+        url += '/' + resource_id
+    return url + '.json'
+
+
+def get_echoes_client():
+    echoes_url = get_echoes_url()
+    try:
+        client = SSEClient(echoes_url)
+    except Exception:
+        raise CrossComputeConnectionError({
+            'url': 'could not connect to echoes ' + echoes_url})
+    return client
+
+
+def yield_echo(statistics_dictionary, is_quiet=False, as_json=False):
+    statistics_dictionary['ping count'] = 0
+    try:
+        for echo_message in get_echoes_client():
+            event_name = echo_message.event
+            if event_name == 'message':
+                if not is_quiet and not as_json:
+                    print('.', end='', flush=True)
+                statistics_dictionary['ping count'] += 1
+                continue
+            elif not is_quiet:
+                print('\n' + render_object(echo_message.__dict__, as_json))
+            event_dictionary = json.loads(echo_message.data)
+            yield event_name, event_dictionary
+    except KeyboardInterrupt:
+        raise CrossComputeKeyboardInterrupt
