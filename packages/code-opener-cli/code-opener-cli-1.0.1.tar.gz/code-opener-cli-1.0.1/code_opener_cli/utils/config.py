7ecab87445a2de1aa8ffcc671fd99b51963d8020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:06 +0000

    Add package code-opener-cli/1.0.1/code-opener-cli-1.0.1.tar.gz

---
+++ packages/code-opener-cli/code-opener-cli-1.0.1.tar.gz/code-opener-cli-1.0.1/code_opener_cli/utils/config.py
@@ -0,0 +1,12 @@
+"""
+This file contains configuration defaults for the project 
+"""
+from enum import Enum
+
+class DefaultConfiguration(Enum):
+    """
+    This class includes all the default values for the application
+    """
+
+    APP_NAME = 'code_opener_cli'
+    CONFIGURATION_FILE_NAME = 'settings.json'
