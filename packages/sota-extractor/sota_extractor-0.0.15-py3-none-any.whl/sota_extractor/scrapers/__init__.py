16e1982f004affff1078bf3f05a0affcfaa1bcdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:09 +0000

    Add package sota-extractor/0.0.15/sota_extractor-0.0.15-py3-none-any.whl

---
+++ packages/sota-extractor/sota_extractor-0.0.15-py3-none-any.whl/sota_extractor/scrapers/__init__.py
@@ -0,0 +1,22 @@
+__all__ = [
+    "eff",
+    "snli",
+    "squad",
+    "reddit",
+    "cityscapes",
+    "nlp_progress",
+    "coqa",
+    "chexpert",
+    "cmrc",
+]
+
+
+from sota_extractor.scrapers.eff import eff
+from sota_extractor.scrapers.snli import snli
+from sota_extractor.scrapers.squad import squad
+from sota_extractor.scrapers.reddit import reddit
+from sota_extractor.scrapers.cityscapes import cityscapes
+from sota_extractor.scrapers.nlp_progress import nlp_progress
+from sota_extractor.scrapers.coqa import coqa
+from sota_extractor.scrapers.chexpert import chexpert
+from sota_extractor.scrapers.cmrc import cmrc
