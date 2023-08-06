fa210d18b97e1c66fb6a13eb5786b0718dc6fc46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:46 +0000

    Add package httpie-astra/0.0.7/httpie-astra-0.0.7.tar.gz

---
+++ packages/httpie-astra/httpie-astra-0.0.7.tar.gz/httpie-astra-0.0.7/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
+
+setup(
+    name='httpie-astra',
+    description='Astra plugin for HTTPie.',
+    python_requires=">=3.3",
+    long_description=open('README.rst').read().strip(),
+    version='0.0.7',
+    author='Kirsten Hunter',
+    author_email='kirsten.hunter@datastax.com',
+    license='Apache 2.0',
+    url='https://github.com/synedra-datastax/httpie-astra',
+    download_url='https://github.com/synedra-datastax/httpie-astra',
+    py_modules=['httpie_astra'],
+    zip_safe=False,
+    entry_points={
+        'httpie.plugins.auth.v1': [
+            'httpie_oauth1 = httpie_astra:AstraPlugin'
+        ]
+    },
+    install_requires=[
+        'httpie >= 0.9.2'
+    ],
+)
