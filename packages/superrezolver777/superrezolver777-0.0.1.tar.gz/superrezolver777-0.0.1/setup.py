88e7cb90dccaad94cf39f8220dacc3ccc2159383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:56 +0000

    Add package superrezolver777/0.0.1/superrezolver777-0.0.1.tar.gz

---
+++ packages/superrezolver777/superrezolver777-0.0.1.tar.gz/superrezolver777-0.0.1/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['superrezolver777']
+setup_kwargs = {
+    'name': 'superrezolver777',
+    'version': '0.0.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'python_requires': '>=2.7,<3.0',
+}
+
+
+setup(**setup_kwargs)
