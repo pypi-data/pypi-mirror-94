4de6c3fcfbd6e69121903df5418c8abbfa23d5fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:01 +0000

    Add package phovea-security-flask/7.0.0/phovea_security_flask-7.0.0-py2.py3-none-any.whl

---
+++ packages/phovea-security-flask/phovea_security_flask-7.0.0-py2.py3-none-any.whl/phovea_security_flask/__init__.py
@@ -0,0 +1,24 @@
+###############################################################################
+# Caleydo - Visualization for Molecular Biology - http://caleydo.org
+# Copyright (c) The Caleydo Team. All rights reserved.
+# Licensed under the new BSD license, available at http://caleydo.org/license
+###############################################################################
+
+
+def phovea(registry):
+  """
+  register extension points
+  :param registry:
+  """
+  registry.append('manager', 'security_manager', 'phovea_security_flask.flask_login_impl', dict(singleton=True))
+  pass
+
+
+def phovea_config():
+  """
+  :return: file pointer to config file
+  """
+  from os import path
+  here = path.abspath(path.dirname(__file__))
+  config_file = path.join(here, 'config.json')
+  return config_file if path.exists(config_file) else None
