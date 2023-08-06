1b5b77159b7302c663145d1aa2768691fe16dbfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:54 +0000

    Add package naspi/0.1.4/naspi-0.1.4.tar.gz

---
+++ packages/naspi/naspi-0.1.4.tar.gz/naspi-0.1.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(name='naspi',
+      version='0.1.4',
+      description='Simple NAS for Raspberry Pi',
+      long_description=long_description,
+      url='https://github.com/fgiroult321/simple-nas-pi',
+      author='Frederic Giroult',
+      author_email='frederic.giroult@gmail.com',
+      license='MIT',
+      packages=['naspi'],
+      entry_points = {
+        'console_scripts': ['naspi=naspi:main'],
+        },
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+      ],
+      python_requires='>=3.6',
+      install_requires=[
+        'boto3',
+        'awscli'
+      ],
+      zip_safe=False)
