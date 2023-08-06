b55ea84aa81d89471935cb8d8a71d70b95934820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:33 +0000

    Add package atlassian-InstEnv-CLI/0.0.0.0/atlassian-InstEnv-CLI-0.0.0.0.tar.gz

---
+++ packages/atlassian-InstEnv-CLI/atlassian-InstEnv-CLI-0.0.0.0.tar.gz/atlassian-InstEnv-CLI-0.0.0.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+
+setuptools.setup(
+    name='atlassian-InstEnv-CLI',
+    version='0.0.0.0',
+    url='https://atlassian.com/',
+    author='Atlassian',
+    author_email='dblack@atlassian.com',
+    description='This package name has been been reserved by Atlassian',
+    long_description='This name has reserved by Atlassian.',
+    licence='Apache',
+)
