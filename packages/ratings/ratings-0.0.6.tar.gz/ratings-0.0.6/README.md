2df8d5c474cf71217b5fe73449887f9529098a76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:48 +0000

    Add package ratings/0.0.6/ratings-0.0.6.tar.gz

---
+++ packages/ratings/ratings-0.0.6.tar.gz/ratings-0.0.6/README.md
@@ -0,0 +1,6 @@
+# ratings
+Elo ratings, and generalizations
+
+
+    from ratings.elo import elo_expected
+    elo_expected(d=234)
---
+++ packages/ratings/ratings-0.0.6.tar.gz/ratings-0.0.6/setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="ratings",
+    version="0.0.6",
+    description="Elo ratings and generalizations",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/microprediction/ratings",
+    author="microprediction",
+    author_email="pcotton@intechinvestments.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["ratings"],
+    test_suite='pytest',
+    tests_require=['pytest'],
+    include_package_data=True,
+    install_requires=["wheel","pathlib","numpy>=1.16.5"],
+    entry_points={
+        "console_scripts": [
+            "ratings=ratings.__main__:main",
+        ]
+    },
+)
