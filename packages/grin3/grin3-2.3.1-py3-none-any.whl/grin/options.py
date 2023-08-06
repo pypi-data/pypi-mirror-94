5611a69354fff967eebbf152d24099c9c24305bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:14 +0000

    Add package grin3/2.3.1/grin3-2.3.1-py3-none-any.whl

---
+++ packages/grin3/grin3-2.3.1-py3-none-any.whl/grin/options.py
@@ -0,0 +1,64 @@
+# Copyright (C) 2017-2021, Raffaele Salmaso <raffaele@salmaso.org>
+# Copyright (C) 2007, Enthought, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#  * Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#  * Neither the name of Enthought, Inc. nor the names of its contributors may
+#    be used to endorse or promote products derived from this software without
+#    specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+__all__ = ["default_options", "Options"]
+import warnings
+
+
+class Options(dict):
+    """Simple options."""
+
+    def __init__(self, *args, **kwds):
+        dict.__init__(self, *args, **kwds)
+        self.__dict__ = self
+
+    def __call__(self):
+        warnings.warn(
+            "calling default_options is deprecated, it will removed in next version.",
+            DeprecationWarning,
+        )
+        return self
+
+
+default_options = Options(
+    before_context=0,
+    after_context=0,
+    show_line_numbers=True,
+    show_match=True,
+    show_filename=True,
+    show_emacs=False,
+    skip_hidden_dirs=False,
+    skip_hidden_files=False,
+    skip_backup_files=True,
+    skip_dirs=set(),
+    skip_exts=set(),
+    skip_symlink_dirs=True,
+    skip_symlink_files=True,
+    binary_bytes=4096,
+    re_flags=[],
+    fixed_string=False,
+)
