8126f2ebfdeb25e742468116d441a3f04ca5a2cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:24 +0000

    Add package ocdsextensionregistry/0.0.25/ocdsextensionregistry-0.0.25.tar.gz

---
+++ packages/ocdsextensionregistry/ocdsextensionregistry-0.0.25.tar.gz/ocdsextensionregistry-0.0.25/tests/commands/test_generate_data_file.py
@@ -0,0 +1,115 @@
+import json
+import sys
+from io import StringIO
+from unittest.mock import patch
+
+import pytest
+
+from ocdsextensionregistry.cli.__main__ import main
+from tests import read
+
+args = ['ocdsextensionregistry', 'generate-data-file']
+
+
+@patch('sys.stdout', new_callable=StringIO)
+def test_command(stdout, monkeypatch):
+    monkeypatch.setattr(sys, 'argv', args + ['location==v1.1.3'])
+    main()
+
+    assert stdout.getvalue() == read('location-v1.1.3.json')
+
+
+@patch('sys.stdout', new_callable=StringIO)
+def test_command_latest_version_master(stdout, monkeypatch):
+    monkeypatch.setattr(sys, 'argv', args + ['location==v1.1.3', 'location==master'])
+    main()
+
+    assert json.loads(stdout.getvalue())['location']['latest_version'] == 'master'
+
+
+@patch('sys.stdout', new_callable=StringIO)
+def test_command_latest_version_dated(stdout, monkeypatch):
+    monkeypatch.setattr(sys, 'argv', args + ['location==v1.1.3', 'location==v1.1.1'])
+    main()
+
+    assert json.loads(stdout.getvalue())['location']['latest_version'] == 'v1.1.3'
+
+
+@patch('sys.stderr', new_callable=StringIO)
+@patch('sys.stdout', new_callable=StringIO)
+def test_command_missing_locale_dir(stdout, stderr, monkeypatch):
+    with pytest.raises(SystemExit) as excinfo:
+        monkeypatch.setattr(sys, 'argv', args + ['--languages', 'es', 'location==v1.1.3'])
+        main()
+
+    assert stdout.getvalue() == ''
+    assert '--locale-dir is required if --languages is set.' in stderr.getvalue()
+    assert excinfo.value.code == 2
+
+
+@patch('sys.stdout', new_callable=StringIO)
+def test_command_directory(stdout, monkeypatch, tmpdir):
+    versions_dir = tmpdir.mkdir('outputdir')
+    version_dir = versions_dir.mkdir('location').mkdir('v1.1.3')
+    locale_dir = tmpdir.mkdir('localedir')
+    for locale in ('en', 'es'):
+        locale_dir.mkdir(locale)
+
+    version_dir.join('extension.json').write('{"name": "Location", "description": "…"}')
+    version_dir.join('README.md').write('# Location')
+
+    monkeypatch.setattr(sys, 'argv', args + ['--versions-dir', str(versions_dir), '--locale-dir', str(locale_dir),
+                                             'location==v1.1.3'])
+    main()
+
+    assert json.loads(stdout.getvalue()) == {
+        'location': {
+            'id': 'location',
+            'category': 'item',
+            'core': True,
+            'name': {
+                'en': 'Location',
+                'es': 'Location',
+            },
+            'description': {
+                'en': '…',
+                'es': '…',
+            },
+            'latest_version': 'v1.1.3',
+            'versions': {
+                'v1.1.3': {
+                    'id': 'location',
+                    'date': '2018-02-01',
+                    'version': 'v1.1.3',
+                    'base_url': 'https://raw.githubusercontent.com/open-contracting-extensions/ocds_location_extension/v1.1.3/',  # noqa: E501
+                    'download_url': 'https://api.github.com/repos/open-contracting-extensions/ocds_location_extension/zipball/v1.1.3',  # noqa: E501
+                    'publisher': {
+                        'name': 'open-contracting-extensions',
+                        'url': 'https://github.com/open-contracting-extensions',
+                    },
+                    'metadata': {
+                        'name': {
+                            'en': 'Location',
+                            'es': 'Location',
+                        },
+                        'description': {
+                            'en': '…',
+                            'es': '…',
+                        },
+                        'documentationUrl': {},
+                        'compatibility': ['1.1'],
+                    },
+                    'schemas': {
+                        'record-package-schema.json': {},
+                        'release-package-schema.json': {},
+                        'release-schema.json': {},
+                    },
+                    'codelists': {},
+                    'readme': {
+                        'en': '# Location\n',
+                        'es': '# Location\n',
+                    },
+                },
+            },
+        },
+    }
---
+++ packages/ocdsextensionregistry/ocdsextensionregistry-0.0.25.tar.gz/ocdsextensionregistry-0.0.25/tests/test_util.py
@@ -0,0 +1,58 @@
+import pytest
+
+from ocdsextensionregistry import ExtensionVersion
+from ocdsextensionregistry.exceptions import UnknownLatestVersion
+from ocdsextensionregistry.util import get_latest_version
+
+
+def test_get_latest_version_one():
+    versions = [
+        ExtensionVersion(arguments(**{'Version': '1'})),
+    ]
+
+    assert get_latest_version(versions).version == '1'
+
+
+def test_get_latest_version_master():
+    versions = [
+        ExtensionVersion(arguments(**{'Version': '1'})),
+        ExtensionVersion(arguments(**{'Date': None})),
+        ExtensionVersion(arguments(**{'Date': '1000-01-01', 'Version': 'master'})),
+    ]
+
+    assert get_latest_version(versions).version == 'master'
+
+
+def test_get_latest_version_dated():
+    versions = [
+        ExtensionVersion(arguments(**{'Version': '1'})),
+        ExtensionVersion(arguments(**{'Date': None})),
+        ExtensionVersion(arguments(**{'Date': '1000-01-01'})),
+    ]
+
+    assert get_latest_version(versions).version == '1'
+
+
+def test_get_latest_version_dateless():
+    versions = [
+        ExtensionVersion(arguments(**{'Date': None})),
+        ExtensionVersion(arguments(**{'Date': None, 'Version': '1'})),
+    ]
+
+    with pytest.raises(UnknownLatestVersion) as excinfo:
+        get_latest_version(versions)
+
+    assert str(excinfo.value) == ''
+
+
+def arguments(**kwargs):
+    data = {
+        'Id': 'location',
+        'Date': '2018-02-01',
+        'Version': 'v1.1.3',
+        'Base URL': 'https://raw.githubusercontent.com/open-contracting-extensions/ocds_location_extension/v1.1.3/',
+        'Download URL': 'https://api.github.com/repos/open-contracting-extensions/ocds_location_extension/zipball/v1.1.3',  # noqa: E501
+    }
+
+    data.update(kwargs)
+    return data
