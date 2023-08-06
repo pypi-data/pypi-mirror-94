19de34f641c766d2b648d94fc9662c8efbb141e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:34 +0000

    Add package dbsettings/0.9.5/dbsettings-0.9.5-py3-none-any.whl

---
+++ packages/dbsettings/dbsettings-0.9.5-py3-none-any.whl/dbsettings/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from dbsettings.models import Setting
+
+# Register your models here.
+
+admin.site.register(Setting)
---
+++ packages/dbsettings/dbsettings-0.9.5-py3-none-any.whl/dbsettings/views.py
@@ -0,0 +1,1 @@
+from dbsettings.functions import *
