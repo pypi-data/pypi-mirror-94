bcdfae11074a666977531095f36c11c45f50d33e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:12 +0000

    Add package django-amenities/0.0.3/django_amenities-0.0.3.tar.gz

---
+++ packages/django-amenities/django_amenities-0.0.3.tar.gz/django_amenities-0.0.3/django_amenities/models.py
@@ -0,0 +1,52 @@
+from django.contrib.gis.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.postgres.fields import ArrayField
+
+
+class Amenity(models.Model):
+    osm_id = models.BigIntegerField()
+    name = models.CharField(max_length=1000, blank=True)
+    last_update = models.DateTimeField(null=True, blank=True)
+
+    geo = models.PointField(
+        null=True, blank=True,
+        geography=True, spatial_index=True
+    )
+    topics = ArrayField(
+        models.CharField(max_length=200),
+        default=list, blank=True
+    )
+
+    country = models.CharField(max_length=2, blank=False)
+    street = models.CharField(max_length=255, blank=True)
+    housenumber = models.CharField(max_length=10, blank=True)
+    postcode = models.CharField(max_length=20, blank=True)
+    city = models.CharField(max_length=255, blank=True)
+    category = models.CharField(max_length=255, blank=True)
+    version = models.IntegerField(default=0)
+
+    tags = models.JSONField(blank=True, default=dict)
+
+    class Meta:
+        verbose_name = _('amenity')
+        verbose_name_plural = _('amenities')
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def ident(self):
+        return '{}_{}'.format(self.id, self.osm_id)
+
+    @property
+    def address(self):
+        return '\n'.join(x for x in (
+            '{} {}'.format(
+                self.street,
+                self.housenumber
+            ).strip(),
+            '{} {}'.format(
+                self.postcode,
+                self.city
+            ).strip()
+        ) if x)
