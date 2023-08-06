c05b0c3e67a67f16c2a347ad1d6d64b8ab848378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:00 +0000

    Add package pbiapi/0.2.1/pbiapi-0.2.1.tar.gz

---
+++ packages/pbiapi/pbiapi-0.2.1.tar.gz/pbiapi-0.2.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pbiapi']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.23.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'pbiapi',
+    'version': '0.2.1',
+    'description': 'A Python library for working with the Power BI API',
+    'long_description': None,
+    'author': 'Scott Melhop',
+    'author_email': 'scott.melhop@cognite.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/scottmelhop/PowerBI-API-Python',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
