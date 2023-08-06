aba74e9a946a527660b611f3e88d4522566ac3c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:59 +0000

    Add package Av1an/6/Av1an-6-py3-none-any.whl

---
+++ packages/Av1an/Av1an-6-py3-none-any.whl/av1an/manager/Pipes.py
@@ -0,0 +1,71 @@
+from collections import deque
+
+from av1an.chunk import Chunk
+from av1an.encoder import ENCODERS
+from av1an.project import Project
+from av1an.logger import log
+
+
+def process_pipe(pipe, chunk: Chunk):
+    encoder_history = deque(maxlen=20)
+    while True:
+        line = pipe.stdout.readline().strip()
+        if len(line) == 0 and pipe.poll() is not None:
+            break
+        if len(line) == 0:
+            continue
+        if line:
+            encoder_history.append(line)
+
+    if pipe.returncode != 0 and pipe.returncode != -2:
+        msg = f':: Encoder encountered an error: {pipe.returncode}\n:: Chunk: {chunk.index}' + \
+             '\n'.join(encoder_history)
+        log(msg + '\n\n')
+        print(msg)
+        raise Exception("Error in processing pipe")
+
+
+def process_encoding_pipe(pipe, encoder, counter, chunk: Chunk):
+    encoder_history = deque(maxlen=20)
+    frame = 0
+    enc = ENCODERS[encoder]
+    while True:
+        line = pipe.stdout.readline().strip()
+
+        if len(line) == 0 and pipe.poll() is not None:
+            break
+
+        if len(line) == 0:
+            continue
+
+        match = enc.match_line(line)
+
+        if match:
+            new = int(match.group(1))
+            if new > frame:
+                counter.update(new - frame)
+                frame = new
+
+        if line:
+            encoder_history.append(line)
+
+    if pipe.returncode != 0 and pipe.returncode != -2:  # -2 is Ctrl+C for aom
+        msg = f':: Encoder encountered an error: {pipe.returncode}\n:: Chunk: {chunk.index}\n' + \
+             '\n'.join(encoder_history)
+        log(msg + '\n\n')
+        print(msg)
+        raise Exception("Error in processing encoding pipe")
+
+
+def tqdm_bar(a: Project, c: Chunk, encoder, counter, frame_probe_source,
+             passes, current_pass):
+    enc = ENCODERS[encoder]
+    pipe = enc.make_pipes(a, c, passes, current_pass, c.output)
+
+    if encoder in ('aom', 'vpx', 'rav1e', 'x265', 'x264', 'vvc', 'svt_av1'):
+        process_encoding_pipe(pipe, encoder, counter, c)
+
+    if encoder in ('svt_vp9'):
+        # SVT-VP9 is special
+        process_pipe(pipe, c)
+        counter.update(frame_probe_source // passes)
---
+++ packages/Av1an/Av1an-6-py3-none-any.whl/av1an/manager/Queue.py
@@ -0,0 +1,119 @@
+import time
+import sys
+import concurrent
+import concurrent.futures
+from typing import List
+from av1an.target_quality import (per_frame_target_quality_routine,
+                                  per_shot_target_quality_routine)
+from av1an.encoder import ENCODERS
+from av1an.utils import frame_probe, terminate
+from av1an.resume import write_progress_file
+from av1an.chunk import Chunk
+from av1an.logger import log
+from pathlib import Path
+from .Pipes import tqdm_bar
+
+
+class Queue:
+    """
+    Queue manager with ability to add/remove/restart jobs
+    """
+    def __init__(self, project, chunk_queue):
+        self.chunk_queue = chunk_queue
+        self.queue = []
+        self.project = project
+        self.thread_executor = concurrent.futures.ThreadPoolExecutor()
+        self.status = 'Ok'
+
+    def encoding_loop(self):
+        with concurrent.futures.ThreadPoolExecutor(
+                max_workers=self.project.workers) as executor:
+            future_cmd = {
+                executor.submit(self.encode_chunk, cmd): cmd
+                for cmd in self.chunk_queue
+            }
+            for future in concurrent.futures.as_completed(future_cmd):
+                try:
+                    future.result()
+                except Exception as exc:
+                    _, _, exc_tb = sys.exc_info()
+                    print(f'Encoding error {exc}\nAt line {exc_tb.tb_lineno}')
+                    terminate()
+        self.project.counter.close()
+
+    def encode_chunk(self, chunk: Chunk):
+        """
+        Encodes a chunk. If chunk fails, restarts it limited amount of times.
+        Return if executed just fine, sets status fatal for queue if failed
+
+        :param chunk: The chunk to encode
+        :return: None
+        """
+        restart_count = 0
+
+        while restart_count < 3:
+            try:
+                st_time = time.time()
+
+                chunk_frames = chunk.frames
+
+                log(f'Enc: {chunk.index}, {chunk_frames} fr\n\n')
+
+                # Target Quality Mode
+                if self.project.target_quality:
+                    if self.project.target_quality_method == 'per_shot':
+                        per_shot_target_quality_routine(self.project, chunk)
+                    if self.project.target_quality_method == 'per_frame':
+                        per_frame_target_quality_routine(self.project, chunk)
+
+                ENCODERS[self.project.encoder].on_before_chunk(
+                    self.project, chunk)
+
+                # skip first pass if reusing
+                start = 2 if self.project.reuse_first_pass and self.project.passes >= 2 else 1
+
+                # Run all passes for this chunk
+                for current_pass in range(start, self.project.passes + 1):
+                    tqdm_bar(self.project, chunk, self.project.encoder,
+                             self.project.counter, chunk_frames,
+                             self.project.passes, current_pass)
+
+                ENCODERS[self.project.encoder].on_after_chunk(
+                    self.project, chunk)
+
+                # get the number of encoded frames, if no check assume it worked and encoded same number of frames
+                encoded_frames = chunk_frames if self.project.no_check else self.frame_check_output(
+                    chunk, chunk_frames)
+
+                # write this chunk as done if it encoded correctly
+                if encoded_frames == chunk_frames:
+                    write_progress_file(Path(self.project.temp / 'done.json'),
+                                        chunk, encoded_frames)
+
+                enc_time = round(time.time() - st_time, 2)
+                log(f'Done: {chunk.index} Fr: {encoded_frames}/{chunk_frames}\n'
+                    f'Fps: {round(encoded_frames / enc_time, 4)} Time: {enc_time} sec.\n\n'
+                    )
+                return
+
+            except Exception as e:
+                msg = f':: Chunk #{chunk.index} crashed with:\n:: Exception: {type(e)}\n {e}\n:: Restarting chunk\n'
+                log(msg + '\n')
+                print(msg)
+                restart_count += 1
+
+        msg = f'::FATAL::\n::Chunk #{chunk.index} failed more than 3 times, shutting down thread\n\n'
+        log(msg)
+        print(msg)
+        self.status = 'FATAL'
+
+    def frame_check_output(self,
+                           chunk: Chunk,
+                           expected_frames: int,
+                           last_chunk=False) -> int:
+        actual_frames = frame_probe(chunk.output_path)
+        if actual_frames != expected_frames:
+            msg = f':: Chunk #{chunk.index}: {actual_frames}/{expected_frames} fr'
+            log(msg)
+            print(msg)
+        return actual_frames
