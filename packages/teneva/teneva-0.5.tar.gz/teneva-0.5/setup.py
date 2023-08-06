3e06bed9a8f033130f57bf405b7dd050150c10f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:33 +0000

    Add package teneva/0.5/teneva-0.5.tar.gz

---
+++ packages/teneva/teneva-0.5.tar.gz/teneva-0.5/setup.py
@@ -0,0 +1,64 @@
+import os
+from setuptools import setup
+
+
+def find_packages(package, basepath):
+    packages = [package]
+    for name in os.listdir(basepath):
+        path = os.path.join(basepath, name)
+        if not os.path.isdir(path):
+            continue
+        packages.extend(find_packages('%s.%s'%(package, name), path))
+    return packages
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+desc = 'Fast multidimensional cross approximation in the tensor-train (TT) format.'
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    desc_long = f.read()
+
+
+setup_args = dict(
+    name = 'teneva',
+    version = '0.5',
+    description=desc,
+    long_description=desc_long,
+    long_description_content_type='text/markdown',
+    author='Andrei Chertkov',
+    author_email='a.chertkov@skoltech.ru',
+    url='https://github.com/AndreiChertkov/teneva',
+    classifiers=[
+        'Development Status :: 3 - Alpha', # 4 - Beta, 5 - Production/Stable
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Framework :: Jupyter',
+    ],
+    keywords='low-rank representation tensor train format TT-decomposition cross approximation',
+    packages=find_packages('teneva', './teneva/'),
+    python_requires='>=3.6',
+    project_urls={
+        'Source': 'https://github.com/AndreiChertkov/teneva',
+    },
+)
+
+
+install_requires = [
+    'numba',
+    'numpy',
+    'scipy',
+]
+
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires, include_package_data=True)
---
+++ packages/teneva/teneva-0.5.tar.gz/teneva-0.5/teneva/__init__.py
@@ -0,0 +1,20 @@
+from .cross import cross
+
+
+from .maxvol import maxvol
+from .maxvol import rect_maxvol
+
+
+from .tensor import erank
+from .tensor import get
+from .tensor import getter
+from .tensor import mean
+from .tensor import mul
+from .tensor import norm
+from .tensor import rand
+from .tensor import recap
+from .tensor import truncate
+
+
+from .utils import confidence
+from .utils import get_cdf
