27abb7740e1821d2256c1addcffd97906732df5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:58 +0000

    Add package cflib-pomo/1.3.0.0/cflib_pomo-1.3.0.0-py3-none-any.whl

---
+++ packages/cflib-pomo/cflib_pomo-1.3.0.0-py3-none-any.whl/cflib/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""cflib
+----------------------------------------------------------------------
+
+Import all counts file library modules.
+
+"""
+
+name = "cflib"
+
+import cflib.main
+import cflib.seqbase
+import cflib.fasta
+import cflib.vcf
+import cflib.cf
+import cflib.gp
