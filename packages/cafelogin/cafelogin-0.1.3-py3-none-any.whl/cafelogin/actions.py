f198b1a1d39f7be6b3746a5764f2853cb3797dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:47 +0000

    Add package cafelogin/0.1.3/cafelogin-0.1.3-py3-none-any.whl

---
+++ packages/cafelogin/cafelogin-0.1.3-py3-none-any.whl/cafelogin/actions.py
@@ -0,0 +1,72 @@
+import requests
+import time
+from datetime import datetime
+from contextlib import contextmanager
+from selenium import webdriver
+from selenium.webdriver.remote.webdriver import WebDriver
+from webdriver_manager.firefox import GeckoDriverManager
+from typing import Sequence
+from .portals import login
+from .print_util import print_clr
+from colorama import Style, Fore
+
+DETECT_PORTAL_URL = "http://detectportal.firefox.com/"
+DEFAULT_DRIVER_VERSION = "v0.29.0"
+
+
+def install_webdriver(version: str = DEFAULT_DRIVER_VERSION) -> str:
+    return GeckoDriverManager(
+        version=version,
+        cache_valid_range=100,
+        print_first_line=False,
+    ).install()
+
+
+@contextmanager
+def create_webdriver_context(driver_version: str = DEFAULT_DRIVER_VERSION):
+    options = webdriver.firefox.options.Options()
+    options.headless = True
+    with webdriver.Firefox(
+        options=options,
+        executable_path=install_webdriver(version=driver_version),
+    ) as d:
+        yield d
+
+
+def portal_connected():
+    response = requests.get(DETECT_PORTAL_URL)
+    return response.ok and response.text.strip() == "success"
+
+
+def ensure_portal_connection(driver: WebDriver):
+    if portal_connected():
+        print("Already connected")
+        return
+
+    login.try_login(driver=driver)
+
+    if portal_connected():
+        print("Login succeeded")
+    else:
+        print("Login failed")
+
+
+def watch_portal_connection(driver: WebDriver, watch_interval: float):
+    print_clr(
+        f"{Style.BRIGHT}Checking portal every ",
+        f"{Style.BRIGHT}{Fore.CYAN}{watch_interval} ",
+        f"{Style.BRIGHT}seconds.  Ctrl-c to exit.",
+    )
+    while True:
+        if not portal_connected():
+            print_clr(
+                f"{Fore.YELLOW}{datetime.now()}",
+                f"{Fore.YELLOW}{Style.DIM} - Portal connection down",
+            )
+            login.try_login(driver=driver)
+            if portal_connected():
+                print("Login succeeded")
+            else:
+                print("Login failed.  Exiting.")
+                return
+        time.sleep(watch_interval)
---
+++ packages/cafelogin/cafelogin-0.1.3-py3-none-any.whl/cafelogin/console.py
@@ -0,0 +1,70 @@
+import sys
+import requests
+import configargparse
+import cafelogin.actions as actions
+
+DEFAULT_CONFIG_FILES = ["cafelogin.config", "~/cafelogin.config"]
+
+
+def add_help_argument(parser: configargparse.ArgumentParser):
+    parser.add_argument(
+        "-h",
+        "--help",
+        action="help",
+        help="Show this help message.",
+    )
+
+
+def add_config_argument(parser: configargparse.ArgumentParser):
+    parser.add_argument(
+        "-c",
+        "--config-file",
+        is_config_file=True,
+        help=f"Args that start with '--' (eg. --driver-version) can also be set in a config file ({', '.join(DEFAULT_CONFIG_FILES)} or specified via -c). "
+        + "Config file syntax allows: key=value, flag=true, stuff=[a,b,c] (details here https://goo.gl/R74nmi). "
+        + "Arg precedence: commandline > config-file > defaults.",
+    )
+
+
+def run():
+    parser = configargparse.ArgParser(
+        default_config_files=DEFAULT_CONFIG_FILES,
+        formatter_class=configargparse.ArgumentDefaultsHelpFormatter,
+        add_help=False,
+        add_config_file_help=False,
+    )
+    add_help_argument(parser)
+    add_config_argument(parser)
+    parser.add_argument(
+        "--driver-version",
+        default=actions.DEFAULT_DRIVER_VERSION,
+        help="Specify the webdriver version to use.",
+    )
+    parser.add_argument(
+        "--watch",
+        action="store_true",
+        help="Continue watching for changes in portal connectivity.",
+    )
+    parser.add_argument(
+        "--watch-interval",
+        type=int,
+        default=20,
+        help="The frequency with which to watch for portal changes.",
+    )
+
+    args, unused_args = parser.parse_known_args()
+
+    try:
+        with actions.create_webdriver_context(
+            driver_version=args.driver_version
+        ) as driver:
+            if args.watch:
+                actions.watch_portal_connection(
+                    driver=driver, watch_interval=args.watch_interval
+                )
+            else:
+                actions.ensure_portal_connection(driver=driver)
+    except requests.exceptions.ConnectionError as error:
+        sys.exit(error)
+    except KeyboardInterrupt:
+        pass
