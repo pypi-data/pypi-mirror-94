16d924305fb2cc842414bb90aa494944de64bfd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:23 +0000

    Add package ibis-mssql/0.1.5/ibis-mssql-0.1.5.tar.gz

---
+++ packages/ibis-mssql/ibis-mssql-0.1.5.tar.gz/ibis-mssql-0.1.5/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='ibis-mssql',
+    url='https://github.com/quansight/ibis-mssql',
+    version='0.1.5',
+    python_requires='>=3.7,<3.9',
+    description="Ibis backend for MSSQL",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering',
+    ],
+    license='Apache License, Version 2.0',
+    maintainer="Quansight",
+    maintainer_email="costrouchov@quansight.com",
+    packages=find_packages(),
+    install_requires=["ibis-framework>=1.4", "sqlalchemy", "pyodbc"],
+    extras_require={
+        'develop': [
+            'black',
+            'click',
+            'pydocstyle==4.0.1',
+            'flake8',
+            'isort',
+            'mypy',
+            'pre-commit',
+            'pygit2',
+            'pytest>=4.5',
+            'plumbum',
+            'toolz',
+            'pandas',
+        ]
+    },
+)
