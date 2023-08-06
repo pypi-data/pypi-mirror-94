808c523bf73e569c6d0ec96ab860ace90c48e3be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:39 +0000

    Add package frictionless/4.0.0a15/frictionless-4.0.0a15.tar.gz

---
+++ packages/frictionless/frictionless-4.0.0a15.tar.gz/frictionless-4.0.0a15/frictionless/checks/__init__.py
@@ -0,0 +1,4 @@
+from .baseline import baseline
+from .checksum import checksum
+from .heuristic import duplicate_row, deviated_value, truncated_value
+from .regulation import forbidden_value, sequential_value, row_constraint
