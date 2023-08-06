50c91c4d5f3b00ffe596d4221b95dfb03f8c23a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:53 +0000

    Add package nidhoggr-core/0.10.0/nidhoggr_core-0.10.0-py2.py3-none-any.whl

---
+++ packages/nidhoggr-core/nidhoggr_core-0.10.0-py2.py3-none-any.whl/nidhoggr/core/texture.py
@@ -0,0 +1,47 @@
+from enum import Enum
+from typing import Optional, Dict, List
+
+from pydantic import UUID4
+from pydantic.main import BaseModel
+
+TextureMeta = Optional[Dict[str, str]]
+
+
+class TextureType(Enum):
+    SKIN = "SKIN"
+    CAPE = "CAPE"
+    ELYTRA = "ELYTRA"
+
+
+class TextureItem(BaseModel):
+    url: str
+    metadata: TextureMeta = None
+
+    class Config:
+        allow_mutation = False
+
+
+class TextureRequest(BaseModel):
+    uuid: UUID4
+    texture_types: List[TextureType] = [TextureType.SKIN]
+
+    class Config:
+        allow_mutation = False
+
+
+class TextureUploadRequest(BaseModel):
+    uuid: UUID4
+    data: str
+    kind: TextureType
+    metadata: TextureMeta
+
+    class Config:
+        allow_mutation = False
+
+
+class TextureResponse(BaseModel):
+    textures: Dict[TextureType, TextureItem]
+
+    class Config:
+        allow_mutation = False
+        use_enum_values = True
