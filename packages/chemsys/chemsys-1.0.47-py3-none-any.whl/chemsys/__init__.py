7731601e4a19b81e7d386aa8b56e50cb22a26e23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:26 +0000

    Add package chemsys/1.0.47/chemsys-1.0.47-py3-none-any.whl

---
+++ packages/chemsys/chemsys-1.0.47-py3-none-any.whl/chemsys/__init__.py
@@ -0,0 +1,14 @@
+from . import cs
+from . import cspdf
+
+from .cs import *
+from .cspdf import *
+
+__all__ = ["cs", "cspdf"]
+
+
+# __all__.extend(cs.__all__)
+# __all__.extend(cspdf.__all__)
+
+#__all__.extend(chemsystest.__all__)
+
