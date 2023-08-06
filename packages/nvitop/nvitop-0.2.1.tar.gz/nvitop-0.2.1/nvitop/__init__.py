95fb34eb3fd34a6e47dc67dbe9e48d44cb7318cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:30 +0000

    Add package nvitop/0.2.1/nvitop-0.2.1.tar.gz

---
+++ packages/nvitop/nvitop-0.2.1.tar.gz/nvitop-0.2.1/nvitop/__init__.py
@@ -0,0 +1,9 @@
+# This file is part of nvitop, the interactive Nvidia-GPU process viewer.
+# License: GNU GPL version 3.
+
+"""An interactive Nvidia-GPU process viewer."""
+
+__version__ = '0.2.1'
+__license__ = 'GPLv3'
+__author__ = __maintainer__ = 'Xuehai Pan'
+__email__ = 'XuehaiPan@pku.edu.cn'
