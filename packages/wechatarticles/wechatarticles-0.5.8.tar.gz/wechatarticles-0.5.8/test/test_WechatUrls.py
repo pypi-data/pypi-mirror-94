de3996c1e1800ced36d1efe75dab23cc910d80f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:10 +0000

    Add package wechatarticles/0.5.8/wechatarticles-0.5.8.tar.gz

---
+++ packages/wechatarticles/wechatarticles-0.5.8.tar.gz/wechatarticles-0.5.8/test/test_WechatUrls.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+import os
+from pprint import pprint
+from wechatarticles import ArticlesUrls, tools
+
+if __name__ == "__main__":
+    # 模拟登录微信公众号平台，获取微信文章的url
+    cookie = "cookie"
+    token = "token"
+    nickname = "nickname"
+    query = "query"
+
+    test = ArticlesUrls(cookie=cookie, token=token)
+    # articles_sum = test.articles_nums(nickname)
+    artiacle_data = test.articles(nickname, begin="0", count="5")
+    # officical_info = test.official_info(nickname)
+
+    # print("articles_sum:", end=" ")
+    # print(articles_sum)
+    print("artcles_data:")
+    pprint(artiacle_data)
+    # print("officical_info:")
+    # pprint(officical_info)
+
+    tools.save_json("test.json", artiacle_data)
---
+++ packages/wechatarticles/wechatarticles-0.5.8.tar.gz/wechatarticles-0.5.8/wechatarticles/__init__.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+from .ArticlesUrls import ArticlesUrls
+from .ArticlesInfo import ArticlesInfo
+from .ArticlesAPI import ArticlesAPI
+from .Url2Html import Url2Html
+from .nickname2biz import nickname2biz
+from .GetUrls import PCUrls, MobileUrls
+from .WechatBook import WechatBook
