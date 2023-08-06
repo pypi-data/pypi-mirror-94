572ced742215dd6b778e965f59ea50ddfccfd527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:06 +0000

    Add package flytekit/0.16.0b7/flytekit-0.16.0b7-py3-none-any.whl

---
+++ packages/flytekit/flytekit-0.16.0b7-py3-none-any.whl/flytekit/__init__.py
@@ -0,0 +1,37 @@
+import flytekit.plugins
+from flytekit.core.base_sql_task import SQLTask
+from flytekit.core.base_task import TaskMetadata, kwtypes
+from flytekit.core.condition import conditional
+from flytekit.core.container_task import ContainerTask
+from flytekit.core.context_manager import ExecutionParameters, FlyteContext
+from flytekit.core.dynamic_workflow_task import dynamic
+from flytekit.core.launch_plan import LaunchPlan
+from flytekit.core.map_task import maptask
+from flytekit.core.notification import Email, PagerDuty, Slack
+from flytekit.core.python_function_task import PythonFunctionTask
+from flytekit.core.reference import get_reference_entity
+from flytekit.core.reference_entity import LaunchPlanReference, TaskReference, WorkflowReference
+from flytekit.core.resources import Resources
+from flytekit.core.schedule import CronSchedule, FixedRate
+from flytekit.core.task import reference_task, task
+from flytekit.core.workflow import WorkflowFailurePolicy, reference_workflow, workflow
+from flytekit.loggers import logger
+from flytekit.types import schema
+
+__version__ = "0.16.0b7"
+
+
+def current_context() -> ExecutionParameters:
+    """
+    Use this method to get a handle of specific parameters available in a flyte task.
+
+    Usage
+
+    .. code-block::
+
+        flytekit.current_context().logging.info(...)
+
+    Available params are documented in :py:class:`flytekit.core.context_manager.ExecutionParams`.
+    There are some special params, that should be available
+    """
+    return FlyteContext.current_context().user_space_params
