33e15aefd41ec37048aabe314dcc362306870c6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:15 +0000

    Add package kirisearch/0.0.1/kirisearch-0.0.1-py3-none-any.whl

---
+++ packages/kirisearch/kirisearch-0.0.1-py3-none-any.whl/kiri_search/models/__init__.py
@@ -0,0 +1,8 @@
+from .tasks.qa import QA
+from .tasks.summarisation import Summarisation
+from .tasks.emotion import Emotion
+from .tasks.classification import Classification
+from .tasks.vectorisation import Vectorisation
+from .tasks.generation import Generation
+from .custom_models import T5QASummaryEmotion
+from .models import BaseModel, PathModel, HuggingModel, GenerationModel, ClassificationModel
