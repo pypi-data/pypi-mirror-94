affb9bad32be188cbd2f357fc32382e97452ec98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:02 +0000

    Add package ih-distributions/0.1/ih_distributions-0.1.tar.gz

---
+++ packages/ih-distributions/ih_distributions-0.1.tar.gz/ih_distributions-0.1/README.md
@@ -0,0 +1,1 @@
+Placeholder for how this package works
---
+++ packages/ih-distributions/ih_distributions-0.1.tar.gz/ih_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ih_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ih_distributions'],
+      zip_safe=False)
