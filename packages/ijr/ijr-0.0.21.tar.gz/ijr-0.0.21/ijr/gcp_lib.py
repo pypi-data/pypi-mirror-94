d0cdc610291b7087af7761243d8d0024b26019af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:49 +0000

    Add package ijr/0.0.21/ijr-0.0.21.tar.gz

---
+++ packages/ijr/ijr-0.0.21.tar.gz/ijr-0.0.21/ijr/gcp_lib.py
@@ -0,0 +1,54 @@
+import json
+
+from google.cloud import pubsub
+
+from ijr.generic_lib import running_in_gcf, default_object
+
+
+class PubSubPublisher(object):
+
+    def __init__(self, topic, msg_type='Generic', threshold=25, **kwargs):
+        self._messages = list()
+        self._topic = topic
+        self.msg_type = msg_type
+        self._msg_kwargs = {k: str(v) for k, v in kwargs.items()}  # cast values to string for passing to pub/sub
+        self._threshold = threshold
+        if running_in_gcf():
+            self._client = pubsub.PublisherClient()
+        else:
+            import logging
+            logging.warning('PubSubPublisher -> Running local; using ./account.json')
+            self._client = pubsub.PublisherClient.from_service_account_json('account.json')
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._publish_messages()
+
+    @property
+    def msg_type(self):
+        return self.__msg_type
+
+    @msg_type.setter
+    def msg_type(self, prop_value):
+        if prop_value is None:
+            raise Exception("msg_type can't be None")
+        self.__msg_type = prop_value
+
+    def _publish_messages(self):
+        if not self._messages:
+            return
+        msg_dict = dict(_type=self.msg_type,
+                        data=self._messages)
+
+        msg = json.dumps(msg_dict, sort_keys=True, default=default_object)
+        ret = self._client.publish(self._topic, msg.encode(), **self._msg_kwargs).result()
+
+        self._messages.clear()
+        return ret
+
+    def publish(self, msg):
+        self._messages.append(msg)
+        if len(self._messages) > self._threshold:
+            self._publish_messages()
---
+++ packages/ijr/ijr-0.0.21.tar.gz/ijr-0.0.21/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+
+__version__ = '0.0.21'
+
+setup(
+    name='ijr',
+    version=__version__,
+    packages=['ijr'],
+    url='https://github.com/IJsvogel/ijr',
+    license='MIT',
+    author='IT team IJsvogel Retail',
+    author_email='it@ijsvogelretail.nl',
+    description='IJsvogel Package',
+    install_requires=['pymongo', 'google-cloud-pubsub', 'google-cloud-secret-manager']
+)
