782cc404dcf5f29e7c542d5610d361cab7dc0b27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:09 +0000

    Add package django-cache-url/3.2.3/django-cache-url-3.2.3.tar.gz

---
+++ packages/django-cache-url/django-cache-url-3.2.3.tar.gz/django-cache-url-3.2.3/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+django-cache-url
+~~~~~~~~~~~~~~~~
+
+This simple Django utility allows you to utilize the
+`12factor <http://www.12factor.net/backing-services>`_ inspired
+``CACHE_URL`` environment variable to configure your Django application.
+
+
+Usage
+-----
+
+Configure your cache in ``settings.py``::
+
+    CACHES={'default': django_cache_url.config()}
+
+Nice and simple.
+"""
+from setuptools import setup
+
+
+setup(
+    name='django-cache-url',
+    version='3.2.3',
+    url='https://github.com/epicserve/django-cache-url',
+    license='MIT',
+    author="Brent O'Connor",
+    author_email='epicserve@gmail.com',
+    description='Use Cache URLs in your Django application.',
+    long_description=__doc__,
+    packages=('django_cache_url',),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
