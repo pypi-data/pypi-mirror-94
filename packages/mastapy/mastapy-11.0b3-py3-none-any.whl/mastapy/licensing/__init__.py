6d8f7e9fce34062483701c903d97c11c93222c3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:05 +0000

    Add package mastapy/11.0b3/mastapy-11.0b3-py3-none-any.whl

---
+++ packages/mastapy/mastapy-11.0b3-py3-none-any.whl/mastapy/licensing/__init__.py
@@ -0,0 +1,11 @@
+﻿'''__init__.py'''
+
+
+from mastapy._internal.dummy_base_class_importer import _DummyBaseClassImport
+
+
+with _DummyBaseClassImport():
+    from ._1249 import LicenceServer
+    from ._7199 import LicenceServerDetails
+    from ._7200 import ModuleDetails
+    from ._7201 import ModuleLicenceStatus
---
+++ packages/mastapy/mastapy-11.0b3-py3-none-any.whl/mastapy/scripting/__init__.py
@@ -0,0 +1,16 @@
+﻿'''__init__.py'''
+
+
+from mastapy._internal.dummy_base_class_importer import _DummyBaseClassImport
+
+
+with _DummyBaseClassImport():
+    from ._7189 import ApiEnumForAttribute
+    from ._7190 import ApiVersion
+    from ._7191 import SMTBitmap
+    from ._7193 import MastaPropertyAttribute
+    from ._7194 import PythonCommand
+    from ._7195 import ScriptingCommand
+    from ._7196 import ScriptingExecutionCommand
+    from ._7197 import ScriptingObjectCommand
+    from ._7198 import ApiVersioning
