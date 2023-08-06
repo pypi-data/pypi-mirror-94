6baaefe3dcf8bfd06d68812d2888416f1c29f104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:46 +0000

    Add package cvdastwrapper/1.48.16/cvdastwrapper-1.48.16.tar.gz

---
+++ packages/cvdastwrapper/cvdastwrapper-1.48.16.tar.gz/cvdastwrapper-1.48.16/cvdastwrapper/entry.py
@@ -0,0 +1,18 @@
+def main():
+    import sys
+    from cvdastwrapper import fuzzallspecs
+    from cvdastwrapper import runall
+    
+    print(sys.argv[1:], "argv")
+    if sys.argv[1:]:
+        if "--generate-tests" in sys.argv[1:]:
+            print(sys.argv)
+            fuzzallspecs.fuzzspecs()                
+            return
+    runall.main()            
+    
+        
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+        
