ecec1b5c6ad45247838c052c261f7cfee57b3774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:07 +0000

    Add package GenerIter/0.2.0.dev23/GenerIter-0.2.0.dev23.tar.gz

---
+++ packages/GenerIter/GenerIter-0.2.0.dev23.tar.gz/GenerIter-0.2.0.dev23/GenerIter/app/algorithm.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+"""
+App to catalogue and select source files into a configuration.
+
+Copyright 2020 Thomas Jackson Park & Jeremy Pavier
+
+"""
+import argparse
+from pathlib import Path
+
+from GenerIter.app.clibase import CLIBase
+from GenerIter.selector import Selector
+import GenerIter.excepts as gix
+from GenerIter.util import debug
+
+class Algorithm(CLIBase):
+
+    klass_template = '''
+from GenerIter.process import Process
+
+class {KLASS}(Process):
+
+    def __init__(self):
+        super().__init__()
+
+
+'''
+
+    alg_template = '''
+    deg {ALG}(self):
+        print("Executes")
+
+'''
+
+    def _init_(self):
+        super(Algorithm, self).__init__()
+
+
+    def parseArguments(self):
+        # Set up positional and optional arguments
+        parser = argparse.ArgumentParser()
+        # Zero or more -I parameters are allowed
+        parser.add_argument("-A", help="Algorithm name",
+                            action='store', required=True)
+        # Zero or more -I parameters are allowed
+        parser.add_argument("-M", help="Module name",
+                            action='store', required=True)
+        # This is a mandatory output name parameter.
+        parser.add_argument("-L", help="Library name",
+                            action='store', required=True)
+        
+        # Parse the command line as supplied
+        args = parser.parse_args()
+        # Set up the member values according to the command line options
+        self._library = args.L.lower()
+        self._module = args.M.lower()
+        self._klass = self._module.capitalize()
+        self._algorithm = args.A.lower()
+
+    def build_lib(self):
+        lib = Path(self._library)
+        lib.mkdir(parents=True, exist_ok=True)
+        init = lib / '__init__.py'
+        if init.exists() is False:
+            init.touch()
+
+    def build_class(self):
+        prog = '{0}.py'.format(self._module)
+        mod = Path(self._library) / prog
+        if mod.exists() is False:
+            #mod.open(mode='w')
+            mod.write_text(self.klass_template.format(KLASS=self._klass))
+
+    def build_method(self):
+        prog = '{0}.py'.format(self._module)
+        mod = Path(self._library) / prog
+        modfile = open(mod, mode='a+')
+        modfile.write(self.alg_template.format(ALG=self._algorithm))
+            
+    def process(self):
+        self.build_lib()
+        self.build_class()
+        self.build_method()
