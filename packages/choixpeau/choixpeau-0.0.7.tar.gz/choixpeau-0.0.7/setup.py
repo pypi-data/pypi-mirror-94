3e9763a250bfc87fc4400f9eb92f307c8753421b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:29 +0000

    Add package choixpeau/0.0.7/choixpeau-0.0.7.tar.gz

---
+++ packages/choixpeau/choixpeau-0.0.7.tar.gz/choixpeau-0.0.7/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+def dependencies():
+    import os
+    """
+    Obtain the dependencies from requirements.txt.
+    """
+    with open('requirements.txt') as reqs:
+        return reqs.read().splitlines()
+
+setuptools.setup(
+    name="choixpeau",
+    version="0.0.7",
+    author="Keurcien Luu",
+    author_email="keurcien@appchoose.io",
+    description="Efficiently assign users to buckets.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(exclude=['tests']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "redis"
+    ]
+)
