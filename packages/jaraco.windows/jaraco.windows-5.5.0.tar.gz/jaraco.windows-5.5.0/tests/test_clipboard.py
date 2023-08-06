1c2c86c3679d8a24ff963989f2792c9d0f6d6329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:58 +0000

    Add package jaraco.windows/5.5.0/jaraco.windows-5.5.0.tar.gz

---
+++ packages/jaraco.windows/jaraco.windows-5.5.0.tar.gz/jaraco.windows-5.5.0/tests/test_clipboard.py
@@ -0,0 +1,70 @@
+import textwrap
+
+import pytest
+
+import jaraco.windows.clipboard as wc
+from jaraco.windows.api import clipboard as api
+
+
+def test_unicode_clipboard():
+    wc.set_unicode_text('foo' * 100)
+    assert wc.get_unicode_text() == 'foo' * 100
+    wc.set_unicode_text('☃')
+    assert wc.get_unicode_text() == '☃'
+
+
+example_html = textwrap.dedent(
+    """
+    Version:0.9
+    StartHTML:71
+    EndHTML:170
+    StartFragment:140
+    EndFragment:160
+    StartSelection:140
+    EndSelection:160
+    <!DOCTYPE>
+    <HTML>
+    <HEAD>
+    <TITLE> The HTML Clipboard</TITLE>
+    <BASE HREF="http://sample/specs">
+    </HEAD>
+    <BODY>
+    <UL>
+    <!--StartFragment -->
+    <LI> The Fragment </LI>
+    <!--EndFragment -->
+    </UL>
+    </BODY>
+    </HTML>
+    """
+).lstrip()
+"""
+Example taken from
+https://msdn.microsoft.com/en-us/library/windows/desktop/ms649015(v=vs.85).aspx#Description
+"""
+
+
+@pytest.fixture
+def sample_html():
+    with wc.context():
+        wc.SetClipboardData(api.CF_HTML, example_html.encode('utf-8'))
+
+
+def test_html_paste(sample_html):
+    res = wc.get_html()
+    assert len(res.html) < len(res.data)
+    assert res.headers == dict(
+        StartHTML=71,
+        Version=0.9,
+        EndHTML=170,
+        StartFragment=140,
+        EndFragment=160,
+        StartSelection=140,
+        EndSelection=160,
+    )
+
+
+@pytest.mark.xfail(reason="example is buggy")
+def test_html_fragment(sample_html):
+    snippet = wc.get_html()
+    assert snippet.fragment == '<LI> The Fragment </LI>'
---
+++ packages/jaraco.windows/jaraco.windows-5.5.0.tar.gz/jaraco.windows-5.5.0/tests/test_filesystem.py
@@ -0,0 +1,34 @@
+import os
+
+import pytest
+
+from jaraco.windows import filesystem
+
+
+def test_islink_on_nonexistent_target():
+    assert filesystem.islink('/doesnotexist') is False
+
+
+def test_is_symlink(tmpdir):
+    with tmpdir.as_cwd():
+        filesystem.symlink('foobaz', 'foobar')
+        assert filesystem.is_symlink('foobar')
+
+
+def test_FileAttributes():
+    attrs = filesystem.FileAttributes.get(__file__)
+    assert not attrs.hidden
+
+
+@pytest.mark.skip('sys.version_info < (3, 2)')
+def test_compat_stat():
+    """
+    Ensure compat stat produces comparable results to
+    normal stat on Python 3.2 and later.
+    """
+    assert filesystem.compat_stat(__file__) == os.stat(__file__)
+
+
+def test_samefile():
+    assert filesystem.samefile(__file__, __file__)
+    assert not filesystem.samefile(__file__, filesystem.__file__)
