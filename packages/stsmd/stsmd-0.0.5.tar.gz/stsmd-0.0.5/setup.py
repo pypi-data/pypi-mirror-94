bf65561125fad801a4a5fca29b7792d7a9ac0e89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:44 +0000

    Add package stsmd/0.0.5/stsmd-0.0.5.tar.gz

---
+++ packages/stsmd/stsmd-0.0.5.tar.gz/stsmd-0.0.5/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="stsmd", # Replace with your own username
+    version="0.0.5",
+    author="Alice Petit, Nolwenn Sourice",
+    description="A small package to get closest text document with SMD and Sentence transformers embeddings",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
