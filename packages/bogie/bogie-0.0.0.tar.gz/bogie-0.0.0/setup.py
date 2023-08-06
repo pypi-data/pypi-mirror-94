9f44c3d0653bd4103d13f5980914b9d5161de151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:51 +0000

    Add package bogie/0.0.0/bogie-0.0.0.tar.gz

---
+++ packages/bogie/bogie-0.0.0.tar.gz/bogie-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='bogie',
+    version='0.0.0',
+    description='',
+    long_description='',
+    url='https://github.com/gforsyth/bogie',
+    author='Gilbert Forsyth',
+    author_email='pypi@gil.forsyth.dev',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
