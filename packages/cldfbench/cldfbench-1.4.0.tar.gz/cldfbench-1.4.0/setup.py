6afbaf8880a3ebd5158ba946d361b1ef918f13ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:13 +0000

    Add package cldfbench/1.4.0/cldfbench-1.4.0.tar.gz

---
+++ packages/cldfbench/cldfbench-1.4.0.tar.gz/cldfbench-1.4.0/setup.py
@@ -0,0 +1,75 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='cldfbench',
+    version='1.4.0',
+    author='Robert Forkel',
+    author_email='forkel@shh.mpg.de',
+    description='Python library implementing a CLDF workbench',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    keywords='',
+    license='Apache 2.0',
+    url='https://github.com/cldf/cldfbench',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'cldfbench=cldfbench.__main__:main',
+        ],
+    },
+    platforms='any',
+    python_requires='>=3.5',
+    install_requires=[
+        'csvw>=1.5.6',
+        'cldfcatalog>=1.3',
+        'clldutils>=3.6.0',
+        'pycldf>=1.8.2',
+        'termcolor',
+        'requests',
+        'appdirs',
+        'pytest',
+        'zenodoclient>=0.3',
+    ],
+    extras_require={
+        'excel': [
+            'openpyxl<3.0.1; python_version <= "3.5"', 
+            'openpyxl; python_version > "3.5"',
+            'xlrd<2; python_version <= "3.5"',
+            'xlrd>=; python_version > "3.5"',
+        ],
+        'glottolog': ['pyglottolog'],  # Access the Glottolog catalog.
+        'concepticon': ['pyconcepticon'],  # Access the Concepticon catalog.
+        'clts': ['pyclts'],  # Access the CLTS catalog.
+        'dev': ['flake8', 'wheel', 'twine'],
+        'test': [
+            'pytest>=5',
+            'pytest-mock',
+            'pytest-cov',
+            'coverage>=4.2',
+            'pyglottolog>=3.2.2',
+            'pyconcepticon',
+            'openpyxl<3.0.1; python_version <= "3.5"', 
+            'openpyxl; python_version > "3.5"',
+            'xlrd<2; python_version <= "3.5"',
+            'xlrd>=; python_version > "3.5"',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+)
