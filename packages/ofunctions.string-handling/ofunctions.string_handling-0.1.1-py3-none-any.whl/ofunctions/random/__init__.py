2318d77f774d9dac1f505d35ab474ec5098dd7ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:12 +0000

    Add package ofunctions.string-handling/0.1.1/ofunctions.string_handling-0.1.1-py3-none-any.whl

---
+++ packages/ofunctions.string-handling/ofunctions.string_handling-0.1.1-py3-none-any.whl/ofunctions/random/__init__.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+#  -*- coding: utf-8 -*-
+#
+# This file is part of ofunctions package
+
+"""
+ofunctions is a general library for basic repetitive tasks that should be no brainers :)
+
+Versioning semantics:
+    Major version: backward compatibility breaking changes
+    Minor version: New functionality
+    Patch version: Backwards compatible bug fixes
+
+"""
+
+__intname__ = 'ofunctions.random'
+__author__ = 'Orsiris de Jong'
+__copyright__ = 'Copyright (C) 2014-2021 Orsiris de Jong'
+__description__ = 'Simple random string generator including password generator'
+__licence__ = 'BSD 3 Clause'
+__version__ = '0.1.1'
+__build__ = '2020102801'
+
+
+import string
+import random
+
+def random_string(size: int = 8, chars: list = string.ascii_letters + string.digits) -> str:
+    """
+    Simple password generator function
+    """
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+def pw_gen(size: int = 16, chars: list = string.ascii_letters + string.digits) -> str:
+    return random_string(size, chars)
