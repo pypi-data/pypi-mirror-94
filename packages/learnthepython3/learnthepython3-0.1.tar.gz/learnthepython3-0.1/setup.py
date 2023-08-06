90e238535ae0e273a9dc79e545b2af2963bc2542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:58 +0000

    Add package learnthepython3/0.1/learnthepython3-0.1.tar.gz

---
+++ packages/learnthepython3/learnthepython3-0.1.tar.gz/learnthepython3-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+VERSION = "0.1"
+DESCRIPTION = "Learn how code Python! Very simple"
+
+def hello():
+    print("hello!")
+
+setup(
+    name="learnthepython3",
+    version=VERSION,
+    description=DESCRIPTION   
+)
