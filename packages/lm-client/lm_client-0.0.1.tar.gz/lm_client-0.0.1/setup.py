e03f57ef28e83c5f822ff6d45945577e067ea2e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:18 +0000

    Add package lm-client/0.0.1/lm_client-0.0.1.tar.gz

---
+++ packages/lm-client/lm_client-0.0.1.tar.gz/lm_client-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='lm_client',
+    description='LabManager client',
+    author='QA Automation',
+    version='0.0.1',
+    classifiers=['Programming Language :: Python :: 3'],
+)
