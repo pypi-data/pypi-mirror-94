3a5f7f921b17118bfe9285721e2f84b2c3795392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:58 +0000

    Add package stemia/0.1.0/stemia-0.1.0-py3-none-any.whl

---
+++ packages/stemia/stemia-0.1.0-py3-none-any.whl/stemia/utils.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import mrcfile
+import starfile
+
+
+def read_mrc(path):
+    with mrcfile.open(path) as mrc:
+        return mrc.data.copy(), mrc.header
+
+
+def write_mrc(data, path, overwrite=False, from_header=None):
+    with mrcfile.new(path, data, overwrite=overwrite) as mrc:
+        if from_header is not None:
+            mrc.header.cella = from_header.cella
+
+
+def read_particle_star(path):
+    dct = starfile.read(path)
+    if isinstance(dct, pd.DataFrame):
+        df = dct
+    else:
+        df = dct['particles']
+        df.merge(dct['optics'])
+    items = dct.values()
+    df = next(items)
+    for i in items:
+        df = df.merge(i)
