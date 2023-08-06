6d5fa785de0ee15b902df34c3c0a2e08a4ecd71f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:11 +0000

    Add package aimmo/0.63.1b156/aimmo-0.63.1b156-py2.py3-none-any.whl

---
+++ packages/aimmo/aimmo-0.63.1b156-py2.py3-none-any.whl/aimmo-game/service.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python
+
+import asyncio
+import json
+import logging
+import os
+import sys
+from urllib.parse import parse_qs
+
+import aiohttp_cors
+import socketio
+from activity_monitor import ActivityMonitor
+from aiohttp import web
+from aiohttp_wsgi import WSGIHandler
+from authentication import initialize_game_token
+from prometheus_client import make_wsgi_app
+from simulation import map_generator
+from simulation.django_communicator import DjangoCommunicator
+from simulation.game_runner import GameRunner
+from simulation.log_collector import LogCollector
+from turn_collector import TurnCollector
+
+LOGGER = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+django_api_url = os.environ.get(
+    "GAME_API_URL", "http://localhost:8000/kurono/api/games/"
+)
+
+communicator = DjangoCommunicator(django_api_url=django_api_url)
+activity_monitor = ActivityMonitor(communicator)
+
+
+def setup_application(should_clean_token=True):
+    async def clean_token(app):
+        LOGGER.info("Cleaning token!")
+        await communicator.patch_token(data={"token": ""})
+
+    application = web.Application()
+
+    if should_clean_token:
+        application.on_shutdown.append(clean_token)
+
+    application.on_shutdown.append(communicator.close_session)
+
+    return application
+
+
+def setup_prometheus():
+    wsgi_handler = WSGIHandler(make_wsgi_app())
+    app.add_routes([web.get("/{path_info:metrics}", wsgi_handler)])
+
+
+def setup_socketIO_server(application, async_handlers=True):
+    socket_server = socketio.AsyncServer(
+        async_mode="aiohttp",
+        client_manager=socketio.AsyncManager(),
+        async_handlers=async_handlers,
+        cors_allowed_origins=[
+            "http://localhost:8000",
+            "https://dev-dot-decent-digit-629.appspot.com",
+            "https://staging-dot-decent-digit-629.appspot.com",
+            "https://www.codeforlife.education",
+        ],
+    )
+
+    socket_server.attach(
+        application, socketio_path=os.environ.get("SOCKETIO_RESOURCE", "socket.io")
+    )
+
+    return socket_server
+
+
+app = setup_application()
+cors = aiohttp_cors.setup(app)
+
+socketio_server = setup_socketIO_server(app)
+
+
+class GameAPI(object):
+    def __init__(self, game_state, application=app, server=socketio_server):
+        self.app = application
+        self.socketio_server = server
+        self.register_endpoints()
+        self.game_state = game_state
+        self.log_collector = LogCollector(game_state.avatar_manager)
+
+    async def async_map(self, func, iterable_args):
+        futures = [func(arg) for arg in iterable_args]
+        await asyncio.gather(*futures)
+
+    def register_endpoints(self):
+        self.routes = web.RouteTableDef()
+        self.register_healthcheck()
+        self.register_world_update_on_connect()
+        self.register_remove_session_id_from_mappings()
+        self.app.add_routes(self.routes)
+
+    def open_connections_number(self):
+        try:
+            return len(
+                [x for x in self.socketio_server.manager.get_participants("/", None)]
+            )
+        except KeyError:
+            return 0
+
+    def update_active_users(self):
+        activity_monitor.active_users = self.open_connections_number()
+
+    def register_healthcheck(self):
+        @self.routes.get("/game-{game_id}")
+        async def healthcheck(request):
+            return web.Response(text="HEALTHY")
+
+        return healthcheck
+
+    def register_world_update_on_connect(self):
+        @self.socketio_server.on("connect")
+        async def world_update_on_connect(sid, environ):
+            LOGGER.info(f"Socket connected for session id: {sid}")
+            query = environ["QUERY_STRING"]
+            avatar_id = self._find_avatar_id_from_query(sid, query)
+            await self.socketio_server.save_session(sid, {"id": avatar_id})
+
+        return world_update_on_connect
+
+    def register_remove_session_id_from_mappings(self):
+        @self.socketio_server.on("disconnect")
+        async def remove_session_id_from_mappings(sid):
+            LOGGER.info("Socket disconnected for session id: {}. ".format(sid))
+
+        return remove_session_id_from_mappings
+
+    async def send_updates(self, sid):
+        try:
+            await self._send_game_state(sid)
+        except KeyError:
+            LOGGER.error(
+                f"Failed to send updates. No worker for player in session {sid}"
+            )
+
+    async def send_updates_to_all(self):
+        try:
+            socket_ids = self.socketio_server.manager.get_participants("/", None)
+            await self.async_map(self.send_updates, socket_ids)
+        except KeyError as e:
+            LOGGER.error("No open socket connections")
+        self.update_active_users()
+
+    def _find_avatar_id_from_query(self, session_id, query_string):
+        """
+        :param session_id: Int with the session id
+        :param query_string: String from the environment settings,
+        usually located as the key 'QUERY_STRING'.
+        """
+        parsed_qs = parse_qs(query_string)
+
+        try:
+            avatar_id = int(parsed_qs["avatar_id"][0])
+            return avatar_id
+        except ValueError:
+            LOGGER.error("Avatar ID could not be casted into an integer")
+        except KeyError:
+            LOGGER.error("No avatar ID found. User may not be authorised")
+            LOGGER.error(f"query_string: {query_string}")
+
+    async def _send_game_state(self, sid):
+        session_data = await self.socketio_server.get_session(sid)
+        serialized_game_state = self.game_state.serialize()
+        serialized_game_state["playerLog"] = self.log_collector.collect_logs(
+            session_data["id"]
+        )
+        await self.socketio_server.emit("game-state", serialized_game_state, room=sid)
+
+
+def create_runner(port):
+    settings = json.loads(os.environ["settings"])
+    generator = getattr(map_generator, settings["GENERATOR"])(settings)
+    turn_collector = TurnCollector(socketio_server)
+    return GameRunner(
+        game_state_generator=generator.get_game_state,
+        communicator=communicator,
+        port=port,
+        turn_collector=turn_collector,
+    )
+
+
+def run_game(port):
+    game_runner = create_runner(port)
+
+    game_api = GameAPI(game_state=game_runner.game_state)
+    game_runner.set_end_turn_callback(game_api.send_updates_to_all)
+    asyncio.ensure_future(game_runner.run())
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    host = sys.argv[1]
+
+    if os.environ["WORKER"] == "local":
+        port = int(os.environ["EXTERNAL_PORT"])
+    else:
+        port = int(sys.argv[2])
+
+    asyncio.ensure_future(initialize_game_token(communicator))
+    run_game(port)
+
+    setup_prometheus()
+
+    logging.getLogger("socketio").setLevel(logging.ERROR)
+    logging.getLogger("engineio").setLevel(logging.ERROR)
+    LOGGER.info("starting the server")
+    web.run_app(app, host=host, port=port)
---
+++ packages/aimmo/aimmo-0.63.1b156-py2.py3-none-any.whl/aimmo_runner/minikube.py
@@ -0,0 +1,171 @@
+#!/user/bin/env python
+from __future__ import absolute_import
+from __future__ import print_function
+
+import os
+import platform
+import socket
+import time
+from subprocess import CalledProcessError
+
+import kubernetes
+import yaml
+
+from .docker_scripts import build_docker_images
+from .shell_api import BASE_DIR, create_test_bin, run_command
+
+MINIKUBE_EXECUTABLE = "minikube"
+TIME_FOR_COMPONENTS_TO_DELETE = 5
+
+
+def get_ip():
+    """
+    Get a single primary IP address. This will not return all IPs in the
+    interface. See http://stackoverflow.com/a/28950776/671626
+    :return: Integer with the IP of the user.
+    """
+    os_name = platform.system()
+    if os_name == "Darwin":
+        return "192.168.64.1"
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # noinspection PyBroadException
+    try:
+        # doesn't even have to be reachable
+        client_socket.connect(("10.255.255.255", 0))
+        IP = client_socket.getsockname()[0]
+    except:
+        IP = "127.0.0.1"
+    finally:
+        client_socket.close()
+    return IP
+
+
+def enable_ingress_addon(minikube):
+    """
+    Start Ingress. If it's already started, this command still succeeds.
+    :param minikube: Executable minikube installed beforehand.
+    """
+    run_command([minikube, "addons", "enable", "ingress"])
+
+
+def create_ingress_yaml():
+    """
+    Loads a ingress yaml file into a python object.
+    """
+    path = os.path.join(BASE_DIR, "ingress.yaml")
+    with open(path) as yaml_file:
+        content = yaml.safe_load(yaml_file.read())
+    return content
+
+
+def create_creator_yaml():
+    """
+    Loads a replication controller yaml file into a python object.
+    """
+    orig_path = os.path.join(
+        BASE_DIR, "aimmo-game-creator", "rc-aimmo-game-creator.yaml"
+    )
+    with open(orig_path) as orig_file:
+        content = yaml.safe_load(
+            orig_file.read()
+            .replace("latest", "test")
+            .replace("REPLACE_ME", "http://%s:8000/kurono/api/games/" % get_ip())
+        )
+    return content
+
+
+def start_cluster(minikube):
+    """
+    Starts the cluster unless it has been already started by the user.
+    :param minikube: Executable minikube installed beforehand.
+    """
+    try:
+        run_command([minikube, "status"], True)
+        print("Cluster already running")
+    except CalledProcessError:
+        run_command([minikube, "start", "--memory=2048", "--cpus=2"])
+
+
+def delete_components(api_instance, apps_api_instance, networking_api_instance):
+    for rs in apps_api_instance.list_namespaced_replica_set("default").items:
+        apps_api_instance.delete_namespaced_replica_set(
+            body=kubernetes.client.V1DeleteOptions(),
+            name=rs.metadata.name,
+            namespace="default",
+            grace_period_seconds=0,
+        )
+    for rc in api_instance.list_namespaced_replication_controller("default").items:
+        api_instance.delete_namespaced_replication_controller(
+            body=kubernetes.client.V1DeleteOptions(),
+            name=rc.metadata.name,
+            namespace="default",
+            grace_period_seconds=0,
+        )
+    for pod in api_instance.list_namespaced_pod("default").items:
+        api_instance.delete_namespaced_pod(
+            body=kubernetes.client.V1DeleteOptions(),
+            name=pod.metadata.name,
+            namespace="default",
+            grace_period_seconds=0,
+        )
+    for service in api_instance.list_namespaced_service("default").items:
+        api_instance.delete_namespaced_service(
+            name=service.metadata.name, namespace="default"
+        )
+    for ingress in networking_api_instance.list_namespaced_ingress("default").items:
+        networking_api_instance.delete_namespaced_ingress(
+            name=ingress.metadata.name,
+            namespace="default",
+            body=kubernetes.client.V1DeleteOptions(),
+        )
+
+
+def restart_pods(game_creator_yaml, ingress_yaml):
+    """
+    Disables all the components running in the cluster and starts them again
+    with fresh updated state.
+    :param game_creator_yaml: Replication controller yaml settings file.
+    :param ingress_yaml: Ingress yaml settings file.
+    """
+    print("Restarting pods")
+    kubernetes.config.load_kube_config(context="minikube")
+    api_instance = kubernetes.client.CoreV1Api()
+    apps_api_instance = kubernetes.client.AppsV1Api()
+    networking_api_instance = kubernetes.client.NetworkingV1beta1Api()
+
+    delete_components(api_instance, apps_api_instance, networking_api_instance)
+    time.sleep(TIME_FOR_COMPONENTS_TO_DELETE)
+
+    networking_api_instance.create_namespaced_ingress("default", ingress_yaml)
+    apps_api_instance.create_namespaced_replica_set(
+        body=game_creator_yaml, namespace="default"
+    )
+
+
+def create_roles():
+    """
+    Applies the service accounts, roles, and bindings for restricting
+    the rights of certain pods and their processses.
+    """
+    run_command(["kubectl", "apply", "-Rf", "rbac"])
+
+
+def start(build_target=None):
+    """
+    The entry point to the minikube class. Sends calls appropriately to set
+    up minikube.
+    """
+    if platform.machine().lower() not in ("amd64", "x86_64"):
+        raise ValueError("Requires 64-bit")
+    create_test_bin()
+    os.environ["MINIKUBE_PATH"] = MINIKUBE_EXECUTABLE
+    start_cluster(MINIKUBE_EXECUTABLE)
+    enable_ingress_addon(MINIKUBE_EXECUTABLE)
+    create_roles()
+    build_docker_images(MINIKUBE_EXECUTABLE, build_target=build_target)
+    ingress = create_ingress_yaml()
+    game_creator = create_creator_yaml()
+    restart_pods(game_creator, ingress)
+    print("Cluster ready")
