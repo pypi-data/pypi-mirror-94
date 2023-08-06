42a9b1b3fa49e5251c8350721db6d4de19dd982e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:42 +0000

    Add package pyiron-contrib/0.0.2/pyiron_contrib-0.0.2.tar.gz

---
+++ packages/pyiron-contrib/pyiron_contrib-0.0.2.tar.gz/pyiron_contrib-0.0.2/setup.py
@@ -0,0 +1,46 @@
+"""
+Setuptools based setup module
+"""
+from setuptools import setup, find_packages
+import versioneer
+
+
+setup(
+    name='pyiron_contrib',
+    version=versioneer.get_version(),
+    description='Repository for user-generated plugins to the pyiron IDE.',
+    long_description='http://pyiron.org',
+
+    url='https://github.com/pyiron/pyiron_contrib',
+    author='Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department',
+    author_email='huber@mpie.de',
+    license='BSD',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Scientific/Engineering :: Physics',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
+
+    keywords='pyiron',
+    packages=find_packages(exclude=["*tests*"]),
+    install_requires=[
+        'ase>=3.21.1',
+        'matplotlib>=3.3.4',
+        'numpy>=1.20.0',
+        'pyiron_atomistics>=0.2.3',
+        'scipy>=1.6.0',
+        'seaborn>=0.11.1',
+        'scikit-image>=0.18.1',
+    ],
+    cmdclass=versioneer.get_cmdclass(),
+    
+)
