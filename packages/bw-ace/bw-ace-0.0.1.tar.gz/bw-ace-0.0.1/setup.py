12d4cd73b43017733a8da9d5ffabb0abbd0f4d7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:09 +0000

    Add package bw-ace/0.0.1/bw-ace-0.0.1.tar.gz

---
+++ packages/bw-ace/bw-ace-0.0.1.tar.gz/bw-ace-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="bw-ace",
+    version="0.0.1",
+    description="Placekeeper package for security purposes",
+    author="Ville M. Vainio",
+    author_email="ville.vainio@basware.com",
+    url="https://github.com/vivainio/scaffer-templates",
+    packages=["bwade"],
+    install_requires=[],
+)
