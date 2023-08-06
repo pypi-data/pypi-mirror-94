0921ddc7b62fee40ec67af10595a5f1e7401be06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:15 +0000

    Add package sc-compression/0.3.1/sc-compression-0.3.1.tar.gz

---
+++ packages/sc-compression/sc-compression-0.3.1.tar.gz/sc-compression-0.3.1/README.md
@@ -0,0 +1,11 @@
+### SC Compression
+
+Version 0.3.1
+-
+
+### Tools:
+- Decompression
+- Compression
+
+TestPyPi - https://test.pypi.org/project/sc-compression/ <br>
+PyPi - https://pypi.org/project/sc-compression/
---
+++ packages/sc-compression/sc-compression-0.3.1.tar.gz/sc-compression-0.3.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open('README.md') as fh:
+    long_description = fh.read()
+    fh.close()
+
+with open('requirements.txt') as fh:
+    requirements = [line for line in fh.readlines() if line != '']
+    fh.close()
+
+setuptools.setup(
+    name='sc-compression',
+    version='0.3.1',
+    author='Vorono4ka',
+    author_email='crowo4ka@gmail.com',
+    description='SC Compression',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/vorono4ka/sc-compression',
+    license='GPLv3',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.5',
+    install_require=requirements
+)
