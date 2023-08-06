480b815d6faf4aa19a8a95a531cdba1aa8fb4566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:30 +0000

    Add package xchg/5.1.3/xchg-5.1.3.tar.gz

---
+++ packages/xchg/xchg-5.1.3.tar.gz/xchg-5.1.3/xchg/__init__.py
@@ -0,0 +1,4 @@
+from .xchg import Xchg
+
+__version__ = '5.1.3'
+__all__ = ['Xchg']
---
+++ packages/xchg/xchg-5.1.3.tar.gz/xchg-5.1.3/xchg/common.py
@@ -0,0 +1,48 @@
+'''Common functions used by several sub-modules. It's for internal use only.'''
+
+import csv
+from os import path
+from os import listdir
+
+
+def _read_candles(data_path: str) -> list:
+    '''Read all csv files with candles inside the directory.
+
+    Args:
+        data_path: Where csv files with data are stored.
+
+    Returns:
+        A dictionary containing a list of currencies and a list candles.
+    '''
+    candles = []
+    csv_files = {}
+    filenames = sorted(listdir(data_path))
+    currencies = []
+
+    for filename in filenames:
+        currency = path.splitext(filename)[0]
+        currencies.append(currency)
+        csv_files[currency] = _read_csv(path.join(data_path, filename))
+
+    candles_number = len(csv_files[currency])
+
+    for i in range(candles_number):
+        candles.append({})
+        for currency in currencies:
+            candles[i][currency] = {k: float(v) for k, v
+                                    in dict(csv_files[currency][i]).items()}
+    return {'currencies': sorted(currencies), 'candles': candles}
+
+
+def _read_csv(filepath: str) -> list:
+    '''Read csv file into list of rows.
+
+    Args:
+      filepath: Path to a csv file with data is stored.
+
+    Returns:
+        A list of rows.
+    '''
+    with open(filepath, newline='') as csvfile:
+        result = list(csv.DictReader(csvfile))
+    return result
