07ea4e981b5d4c150a8eb9fd5dff41a642eda167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:40 +0000

    Add package bronotes/1.7.2/bronotes-1.7.2.tar.gz

---
+++ packages/bronotes/bronotes-1.7.2.tar.gz/bronotes-1.7.2/bronotes/actions/set.py
@@ -0,0 +1,48 @@
+"""Set the bronotes config."""
+from bronotes.actions.base_action import BronoteAction
+from bronotes.config import Text
+from pathlib import Path
+
+
+class ActionSet(BronoteAction):
+    """Set config options."""
+
+    action = 'set'
+    arguments = {}
+    flags = {
+        '--dir': {
+            'short': '-d',
+            'action': 'store',
+            'help': 'The new notes directory to use.'
+        },
+        '--sync': {
+            'short': '-s',
+            'action': 'store',
+            'help': 'Autosync (yes/no/true/false)'
+        },
+        '--default': {
+            'short': '-D',
+            'action': 'store',
+            'help': 'The default action to take if an argument is not known.'
+        },
+    }
+
+    def init(self, args):
+        """Construct the action."""
+        self.set_attributes(args)
+
+    def process(self):
+        """Process the action."""
+        if self.dir:
+            self.cfg.set_dir(Path(self.dir))
+
+        if self.sync is not None:
+            if self.sync in ['yes', 'true']:
+                self.cfg.enable_autosync()
+            elif self.sync in ['no', 'false']:
+                self.cfg.disable_autosync()
+
+        if self.default:
+            self.cfg.set_default_action(self.default)
+
+        return Text.I_CONFIG_UPDATE.value
