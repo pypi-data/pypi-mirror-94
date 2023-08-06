0bd4276ac977b20f69faf95ad9eee0560573b46e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:04 +0000

    Add package payout/1.0.4/payout-1.0.4.tar.gz

---
+++ packages/payout/payout-1.0.4.tar.gz/payout-1.0.4/payout/payout.py
@@ -0,0 +1,43 @@
+import http.client
+import json
+from payout.create_payout_account import CreatePayoutAccount
+
+
+class Payout:
+    def __init__(self, app, key) -> None:
+        self.app = app
+        self.key = key
+        # Get app endpoints
+        conn = http.client.HTTPSConnection("reddash.azurewebsites.net")
+        route = "/app/endpoints/" + self.app
+        headers = {"Accept": "application/json"}
+        # Send the request
+        conn.request("POST", route, "", headers)
+        response = json.loads(conn.getresponse().read().decode())
+        # Parse and set dataEndpoints
+        if "dataEndpoints" in response:
+            self.dataEndpoints = response["dataEndpoints"]
+        else:
+            self.dataEndpoints = "ERROR Unable to get dataEndpoints from server"
+        print("dataEndpoints -> " + self.dataEndpoints)
+
+        # Parse and set redpayEndpoints
+        if "redpayEndpoints" in response:
+            self.redpayEndpoints = response["redpayEndpoints"]
+        else:
+            self.redpayEndpoints = "ERROR Unable to get redpayEndpoints from server"
+        print("redpayEndpoints -> " + self.redpayEndpoints)
+
+    def CreatePayoutAccount(self, request):
+        create_payout_account = CreatePayoutAccount(
+            self.app, self.dataEndpoints)
+        return create_payout_account.Process(request)
+
+    def UpdatePayoutAccount(self, request):
+        pass
+
+    def AddPaymentRate(self, request):
+        pass
+
+    def AddPaymentToken(self, request):
+        pass
