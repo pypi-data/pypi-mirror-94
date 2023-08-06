938a8cf82ad72004ed4e7821e302ec5ae7bdce67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:20 +0000

    Add package plk-package2/0.0.1/plk_package2-0.0.1.tar.gz

---
+++ packages/plk-package2/plk_package2-0.0.1.tar.gz/plk_package2-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'My second Python package'
+LONG_DESCRIPTION = 'My second Python package that can sum and multiply two numbers'
+
+# Setting up
+setup(
+       # the name must match the folder name 'plk_package'
+        name="plk_package2",
+        version=VERSION,
+        author="Philip Luke K ",
+        author_email="philip_luke@ce.iitr.ac.in",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that
+        # needs to be installed along with your package. Eg: 'caer'
+
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: OS Independent",
+        ]
+)
