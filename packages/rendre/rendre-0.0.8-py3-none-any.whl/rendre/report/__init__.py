18ea4f17cf6b827e92f68de5873bee53bde9e8f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:59 +0000

    Add package rendre/0.0.8/rendre-0.0.8-py3-none-any.whl

---
+++ packages/rendre/rendre-0.0.8-py3-none-any.whl/rendre/report/__init__.py
@@ -0,0 +1,15 @@
+
+def init_report(args, config)->tuple:
+    if args.template=="tmpl-0001":
+        from .tmpl_0001 import init, item, close
+    if args.template=="tmpl-0002":
+        from .tmpl_0002 import init, item, close
+    if args.template=="tmpl-0003":
+        from .tmpl_0003 import init, item, close
+    if args.template=="tmpl-0004":
+        from .tmpl_0004 import init, item, close
+    if args.template=="tmpl-0007":
+        from .tmpl_0007 import init, item, close
+    if args.template=="tmpl-0008":
+        from .tmpl_0008 import init, item, close
+    return init, item, close
