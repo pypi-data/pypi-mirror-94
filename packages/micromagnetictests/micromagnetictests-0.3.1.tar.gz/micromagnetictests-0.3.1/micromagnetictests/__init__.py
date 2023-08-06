a070de74d4a24ca5de806913d62979a383f34884
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:44 +0000

    Add package micromagnetictests/0.3.1/micromagnetictests-0.3.1.tar.gz

---
+++ packages/micromagnetictests/micromagnetictests-0.3.1.tar.gz/micromagnetictests-0.3.1/micromagnetictests/__init__.py
@@ -0,0 +1,23 @@
+"""Main package."""
+import pytest
+import pkg_resources
+import micromagnetictests.calculatortests
+from .get_tests import get_tests
+
+__version__ = pkg_resources.get_distribution(__name__).version
+
+
+def test():
+    """Run all package tests.
+
+    Examples
+    --------
+    1. Run all tests.
+
+    >>> import micromagnetictests as mt
+    ...
+    >>> # mt.test()
+
+    """
+    return pytest.main(['-v', '--pyargs',
+                        'micromagnetictests', '-l'])  # pragma: no cover
