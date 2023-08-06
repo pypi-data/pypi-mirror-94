13440d757649e2e7b315e7ca0710eb4ec7d72789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:28 +0000

    Add package aio-request/0.0.12/aio-request-0.0.12.tar.gz

---
+++ packages/aio-request/aio-request-0.0.12.tar.gz/aio-request-0.0.12/aio_request/__init__.py
@@ -0,0 +1,19 @@
+# flake8: noqa
+__version__ = "0.0.12"
+
+from .base import ClosableResponse, EmptyResponse, Request, Response
+from .context import get_context, set_context
+from .deadline import Deadline
+from .delays_provider import constant_delays, linear_delays
+from .priority import Priority
+from .request_sender import RequestSender
+from .requests import delete, get, post, post_json, put, put_json
+from .response_classifier import DefaultResponseClassifier, ResponseClassifier, ResponseVerdict
+from .strategy import MethodBasedStrategy, RequestStrategiesFactory, RequestStrategy, setup
+
+try:
+    import aiohttp
+
+    from .aiohttp import AioHttpRequestSender, aiohttp_middleware_factory
+except ImportError:
+    pass
