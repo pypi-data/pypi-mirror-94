b000bb05d3dcd20d9bb6db65754157a76ecf71c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:51 +0000

    Add package fsleyes-widgets/0.10.0/fsleyes-widgets-0.10.0.tar.gz

---
+++ packages/fsleyes-widgets/fsleyes-widgets-0.10.0.tar.gz/fsleyes-widgets-0.10.0/fsleyes_widgets/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#
+# __init__.py - fsleyes-widgets
+#
+# Author: Paul McCarthy <pauldmccarthy@gmail.com>
+#
+"""The ``fsleyes_widgets`` package contains various custom :mod:`wx` widgets
+and utilities used by FSLeyes.
+
+
+Some of the controls in ``fsleyes_widgets`` are duplicates of controls which
+are already available in ``wx`` or ``wx.lib.agw``. In these instances, I wrote
+my own implementations to work around annoying, quirky, and/or downright buggy
+behaviour in the existing controls.
+
+
+This file is used to store the current ``fsleyes-widgets`` version.
+"""
+
+
+__version__ = '0.10.0'
+
+
+from fsleyes_widgets.utils import (WX_PYTHON,  # noqa
+                                   WX_PHOENIX,
+                                   wxversion,
+                                   isalive)
