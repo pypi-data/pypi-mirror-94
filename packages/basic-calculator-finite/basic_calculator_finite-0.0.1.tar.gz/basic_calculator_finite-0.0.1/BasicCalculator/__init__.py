d360d7d11623cc2d3486581382332f692eff8bd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:32 +0000

    Add package basic-calculator-finite/0.0.1/basic_calculator_finite-0.0.1.tar.gz

---
+++ packages/basic-calculator-finite/basic_calculator_finite-0.0.1.tar.gz/basic_calculator_finite-0.0.1/BasicCalculator/__init__.py
@@ -0,0 +1,16 @@
+def add_numbers(num1,num2):
+    return num1+num2
+def subtract_numbers(num1,num2):
+    return num1-num2
+def multiply_numbers(num1,num2):
+    return num1*num2
+def divide_numbers(num1,num2):
+    if num2!=0:
+        return num1/num2
+    else:
+        raise Exception
+def modulus(num1,num2):
+    return num1%num2
+def power(num1,num2):
+    return num1**num2
+
---
+++ packages/basic-calculator-finite/basic_calculator_finite-0.0.1.tar.gz/basic_calculator_finite-0.0.1/README.txt
@@ -0,0 +1,1 @@
+A simple calculator for carrying out basic operations like add, subtract, multiply, divide, modulus, power.
