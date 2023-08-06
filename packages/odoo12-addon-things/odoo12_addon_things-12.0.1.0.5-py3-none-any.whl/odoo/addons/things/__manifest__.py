95fd974b40d9933c3713411665aeca7f94b79995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:34 +0000

    Add package odoo12-addon-things/12.0.1.0.5/odoo12_addon_things-12.0.1.0.5-py3-none-any.whl

---
+++ packages/odoo12-addon-things/odoo12_addon_things-12.0.1.0.5-py3-none-any.whl/odoo/addons/things/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': "Gateway for ThingsInTouch Devices",
+    'summary': "Manage your Gateways and connect to your net of sensors and devices",       
+    'description': """Receive and send data
+        from/to your net of sensors and devices""",
+
+    'version': '12.0.1.0.5',
+    'category': 'Things',
+    'website': "http://www.thingsintouch.com",
+    'author': "thingsintouch.com",
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,    
+    'depends': ['base'],
+    'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        'views/things_menus.xml',
+        'views/things_gate.xml',
+        'views/popup_wizard.xml',
+    ],
+# 'demo': ['demo.xml'],
+}
