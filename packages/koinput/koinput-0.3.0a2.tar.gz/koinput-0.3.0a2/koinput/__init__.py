cdde5207e9bf4fd01fe6915543f7c23a4604a937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:57 +0000

    Add package koinput/0.3.0a2/koinput-0.3.0a2.tar.gz

---
+++ packages/koinput/koinput-0.3.0a2.tar.gz/koinput-0.3.0a2/koinput/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '0.3.0a2'
+__title__ = 'koinput'
+__author__ = 'k0per'
+__copyright__ = 'Copyright 2021-present k0per'
+
+from koinput.menu import Menu
+from koinput.inputs import int_input, float_input
+from koinput.progress_bar import ProgressBar
+from colorama import init
+
+init()
+
+__all__ = [
+    'int_input',
+    'float_input',
+    'Menu',
+    'ProgressBar'
+]
