d428e982db93e1f5602d49c5b7e639d3800b551c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:35 +0000

    Add package django-network-conditions/1.0.2/django-network-conditions-1.0.2.tar.gz

---
+++ packages/django-network-conditions/django-network-conditions-1.0.2.tar.gz/django-network-conditions-1.0.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    description = fh.read()
+
+setuptools.setup(
+    name = 'django-network-conditions',
+    version = '1.0.2',
+    packages = ['django_network_conditions'],
+    url = 'http://pypi.python.org/pypi/django-network-conditions',
+    license = 'LICENSE',
+    description = description,
+
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=["scipy"],
+)
