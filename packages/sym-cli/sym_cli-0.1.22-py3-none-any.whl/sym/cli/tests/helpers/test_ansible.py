ad05c60f5e58a6f3467791abac58d1b082f269d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:45 +0000

    Add package sym-cli/0.1.22/sym_cli-0.1.22-py3-none-any.whl

---
+++ packages/sym-cli/sym_cli-0.1.22-py3-none-any.whl/sym/cli/tests/helpers/test_ansible.py
@@ -0,0 +1,34 @@
+from unittest.mock import patch
+
+from expects import expect, match
+
+from sym.cli.helpers.ansible import get_ansible_bucket_name
+from sym.cli.helpers.params import Profile
+from sym.cli.tests.commands.test_ansible import TEST_ACCOUNT, get_caller_identity_stub
+
+
+def create_fake_profile_with_ansible_bucket(resource: str) -> Profile:
+    """Mock function to return the sym organization's "test" AWS profile."""
+    return Profile(
+        display_name="Test",
+        region="us-east-1",
+        arn="arn:aws:iam::838419636750:role/SSMTestRole",
+        ansible_bucket="foo",
+    )
+
+
+class TestAnsibleHelpers:
+    """Test suite for ansible helper methods."""
+
+    def test_get_default_ansible_bucket_name(self, click_context, boto_stub, saml_client):
+        get_caller_identity_stub(boto_stub)
+        actual = get_ansible_bucket_name(saml_client)
+        expect(actual).to(match(f"sym-ansible-{TEST_ACCOUNT}"))
+
+    @patch(
+        "sym.cli.saml_clients.saml_client.get_profile",
+        new=create_fake_profile_with_ansible_bucket,
+    )
+    def test_get_ansible_bucket_name_from_profile(self, boto_stub, saml_client):
+        actual = get_ansible_bucket_name(saml_client)
+        expect(actual).to(match("foo"))
---
+++ packages/sym-cli/sym_cli-0.1.22-py3-none-any.whl/sym/cli/tests/integration/conftest.py
@@ -0,0 +1,51 @@
+import random
+from contextlib import contextmanager
+
+import boto3
+import pytest
+from expects import *
+
+from sym.cli.sym import sym as click_command
+from sym.cli.tests.matchers import succeed
+
+
+def gen_instances():
+    ec2 = boto3.client("ec2")
+    paginator = ec2.get_paginator("describe_instances")
+    for response in paginator.paginate(
+        Filters=[{"Name": "tag:CLITest", "Values": ["integration"]}]
+    ):
+        for reservation in response["Reservations"]:
+            for instance in reservation["Instances"]:
+                yield instance["InstanceId"]
+
+
+INSTANCES = list(gen_instances())
+
+
+@pytest.fixture
+def integration_runner(capfdbinary, sandbox, wrapped_cli_runner):
+    @contextmanager
+    def context():
+        runner = wrapped_cli_runner
+        with sandbox.push_xdg_config_home():
+
+            def run(*args):
+                result = runner.invoke(click_command, args, catch_exceptions=False)
+                cap = capfdbinary.readouterr()
+                result.stdout_bytes = cap.out
+                result.stderr_bytes = cap.err
+
+                expect(result).to(succeed())
+                return result
+
+            yield run
+
+    return context
+
+
+def pytest_addoption(parser):
+    parser.addoption("--email", default="ci@symops.io")
+    parser.addoption("--org", default="sym")
+    parser.addoption("--instance", default=random.choice(INSTANCES))
+    parser.addoption("--resource", default="test")
