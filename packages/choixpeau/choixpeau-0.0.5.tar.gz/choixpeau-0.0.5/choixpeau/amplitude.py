9d684dcf12ccbd469327c5d303017e4f1ecdb9a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:00 +0000

    Add package choixpeau/0.0.5/choixpeau-0.0.5.tar.gz

---
+++ packages/choixpeau/choixpeau-0.0.5.tar.gz/choixpeau-0.0.5/choixpeau/amplitude.py
@@ -0,0 +1,39 @@
+import json
+import requests
+
+class Amplitude:
+
+    def __init__(self, api_key):
+        self.url = "https://api.amplitude.com/identify"
+        self.api_key = api_key
+    
+    @staticmethod
+    def _parse(key):
+        return key.split(":")
+
+    def post(self, key, value):
+
+        _, ab_test_id, user_id = self._parse(key)
+
+        user_properties = {
+            f"abTest.{ab_test_id}": value.get("ab_test_group")
+        }
+
+        data = {
+            "api_key": self.api_key,
+            "identification": json.dumps(
+                [
+                    {
+                        "user_id": user_id,
+                        "user_properties": user_properties
+                    }
+                ]
+            )
+        }
+
+        response = requests.post(self.url, data=data)
+
+        if response.status_code == 200:
+            print("User has been successfully updated!")
+        else:
+            print("Update failed!")
