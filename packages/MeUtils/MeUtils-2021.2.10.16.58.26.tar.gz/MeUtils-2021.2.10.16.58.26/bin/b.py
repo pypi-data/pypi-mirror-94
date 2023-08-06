744364f3caa3ae94c77ca1a66ba49cbacfcb2aaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:20 +0000

    Add package MeUtils/2021.2.10.16.58.26/MeUtils-2021.2.10.16.58.26.tar.gz

---
+++ packages/MeUtils/MeUtils-2021.2.10.16.58.26.tar.gz/MeUtils-2021.2.10.16.58.26/bin/b.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : MeUtils.
+# @File         : b
+# @Time         : 2021/2/7 9:19 下午
+# @Author       : yuanjie
+# @WeChat       : 313303303
+# @Software     : PyCharm
+# @Description  : 
+
+
+def f1():
+    print('b')
