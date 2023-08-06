264463ebe4eb23554e42c25ce567b42b69b33988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:40 +0000

    Add package pyarma/0.500.0/pyarma-0.500.0-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/pyarma/pyarma-0.500.0-cp38-cp38-macosx_10_9_x86_64.whl/pyarma/share/Armadillo/CMake/ArmadilloConfig.cmake
@@ -0,0 +1,16 @@
+# - Config file for the Armadillo package
+# It defines the following variables
+#  ARMADILLO_INCLUDE_DIRS - include directories for Armadillo
+#  ARMADILLO_LIBRARY_DIRS - library directories for Armadillo (normally not used!)
+#  ARMADILLO_LIBRARIES    - libraries to link against
+
+# Tell the user project where to find our headers and libraries
+set(ARMADILLO_INCLUDE_DIRS "/private/var/folders/zc/w54y8dpx71z33yjn8gmrlq3w0000gn/T/pip-req-build-9qdisovb/_skbuild/macosx-10.9-x86_64-3.8/cmake-install/src/pyarma/include")
+set(ARMADILLO_LIBRARY_DIRS "/private/var/folders/zc/w54y8dpx71z33yjn8gmrlq3w0000gn/T/pip-req-build-9qdisovb/_skbuild/macosx-10.9-x86_64-3.8/cmake-install/src/pyarma/lib")
+
+# Our library dependencies (contains definitions for IMPORTED targets)
+include("/private/var/folders/zc/w54y8dpx71z33yjn8gmrlq3w0000gn/T/pip-req-build-9qdisovb/_skbuild/macosx-10.9-x86_64-3.8/cmake-install/src/pyarma/share/Armadillo/CMake/ArmadilloLibraryDepends.cmake")
+
+# These are IMPORTED targets created by ArmadilloLibraryDepends.cmake
+set(ARMADILLO_LIBRARIES armadillo)
+
