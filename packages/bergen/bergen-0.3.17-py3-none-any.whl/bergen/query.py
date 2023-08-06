4d04e13a9d8e6f471257ff6cbc0e83d9502dc0f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:36 +0000

    Add package bergen/0.3.17/bergen-0.3.17-py3-none-any.whl

---
+++ packages/bergen/bergen-0.3.17-py3-none-any.whl/bergen/query.py
@@ -0,0 +1,239 @@
+import re
+from typing import Generator, Generic, List, Type, TypeVar
+
+
+
+gqlparsed_with_variables = re.compile(r"[\s]*(?P<type>subscription|query|mutation)\s*(?P<operation>[a-zA-Z]*)\((?P<arguments>[^\)]*)\)[\s]*{[\s]*(?P<firstchild>[^\(:]*).*")
+gqlparser_without_variables = re.compile(r"[\s]*(?P<type>subscription|query|mutation)\s*(?P<operation>[a-zA-Z]*)[\s]*{[\s]*(?P<firstchild>[^\(\{\s:]*).*")
+
+class GQLException(Exception):
+    pass
+
+
+MyType = TypeVar("MyType")
+
+
+class GQL(object):
+
+    def __init__(self, query: str) -> None:
+        self.query = query
+        self.variables = None
+        self._type = None
+        self.m = gqlparsed_with_variables.match(self.query)
+        self.has_variables = True
+        if not self.m:
+            self.m = gqlparser_without_variables.match(self.query)
+            self.has_variables = False
+            if not self.m:
+                raise GQLException("Illformed request")
+
+    def combine(self, variables: dict):
+        self.variables = variables
+        return {"query": self.query, "variables": self.variables}
+        
+    def parsed(self):
+        assert self.variables and self.query, "Please specify query and set variables before parsing"
+        return {"query": self.query, "variables": self.variables}
+
+    @property
+    def firstchild(self):
+        return self.m.group("firstchild")
+
+    @property
+    def operation_name(self):
+        return self.m.group("operation")
+
+    @property
+    def type(self):
+        return self.m.group("type")
+
+    def extract(self, result: dict):
+        return result[self.firstchild]
+
+
+
+class ListQuery(list):
+
+
+     def _repr_html_(self) -> str:
+        listified = list(self)
+        string = "Result List with <br/><table>"
+        for x, item in enumerate(listified):
+             string += f"""
+                <tr>
+                    <td>{x}<td/>
+                    <td>{item._repr_list_() if hasattr(item,"_repr_list_") else str(item)}</td>
+                </tr>
+
+            """
+        string + "</table>"
+        return string
+
+
+    
+class TypedGQL(GQL, Generic[MyType]):
+
+    def __init__(self, query: str, cls: Type[MyType], aslist=False):
+        self.aslist = aslist
+        self.query = query
+        self._cls = cls
+        super().__init__(query)
+
+    @property
+    def cls(self) -> MyType:
+        return self._cls
+
+    async def run_async(self, ward=None, variables=None, **kwargs) -> MyType:
+        from bergen.registries.arnheim import get_current_arnheim
+        ward = ward or self._cls.get_ward()
+        returnedobject = await ward.run_async(self, variables=variables,**kwargs)
+        assert returnedobject is not None, "We received nothing back from the Server! Refine your Query!"
+        if isinstance(returnedobject,list): return [self.cls(**item) for item in returnedobject]
+        return self.cls(**returnedobject)
+
+    def run(self, ward=None, variables=None, **kwargs) -> MyType:
+        from bergen.registries.arnheim import get_current_arnheim
+        ward = ward or self._cls.get_ward()
+        returnedobject = ward.run(self, variables=variables, **kwargs)
+        assert returnedobject is not None, "We received nothing back from the Server! Refine your Query!"
+        if isinstance(returnedobject,list): return [self.cls(**item) for item in returnedobject]
+        return self.cls(**returnedobject)
+
+    def subscribe(self, ward=None, **kwargs) -> Generator[MyType, None,None]:
+        from bergen.registries.arnheim import get_current_arnheim
+        ward = ward or get_current_arnheim().getWard()
+        return ward.subscribe(self, **kwargs)
+
+    def _repr_html_(self) -> str:
+        string = "Query <br/><table>"
+        string + "</table>"
+        return string
+
+
+class Query(GQL, Generic[MyType]):
+
+    def __init__(self, query: str, cls: Type[MyType], aslist=False):
+        self.aslist = aslist
+        self.query = query
+        self._cls = cls
+        super().__init__(query)
+
+    @property
+    def cls(self) -> MyType:
+        return self._cls
+
+    def run(self, ward=None, variables=None, **kwargs) -> MyType:
+        from bergen.registries.arnheim import get_current_arnheim
+        ward = ward or self._cls.get_ward()
+        returnedobject = ward.run(self, variables=variables,**kwargs)
+        assert returnedobject is not None, "We received nothing back from the Server! Refine your Query!"
+        if isinstance(returnedobject,list): raise Exception("Received a freaking list..., Please run QueryList")
+        return self.cls(**returnedobject)
+
+    def subscribe(self, ward=None, **kwargs) -> Generator[MyType, None,None]:
+        from bergen.registries.arnheim import get_current_arnheim
+        ward = ward or get_current_arnheim().getWard()
+        return ward.subscribe(self, **kwargs)
+
+    def _repr_html_(self) -> str:
+        string = "Query <br/><table>"
+        string + "</table>"
+        return string
+
+
+class QueryList(GQL, Generic[MyType]):
+
+    def __init__(self, query: str, cls: Type[MyType], aslist=False):
+        self.aslist = aslist
+        self.query = query
+        self._cls = cls
+        super().__init__(query)
+
+    @property
+    def cls(self) -> MyType:
+        return self._cls
+
+    def run(self, ward=None, variables=None, **kwargs) -> List[MyType]:
+        from bergen.registries.arnheim import get_current_arnheim
+        ward = ward or self._cls.get_ward()
+        returnedobject = ward.run(self, variables=variables, **kwargs)
+        assert returnedobject is not None, "We received nothing back from the Server! Refine your Query!"
+        assert isinstance(returnedobject,list), "Cannot unpack values, sure you are receiving a List?"
+        return [self.cls(**item) for item in returnedobject]
+
+    def subscribe(self, ward=None, **kwargs) -> Generator[List[MyType], None,None]:
+        from bergen.registries.arnheim import get_current_arnheim
+        ward = ward or get_current_arnheim().getWard()
+        return ward.subscribe(self, **kwargs)
+
+    def _repr_html_(self) -> str:
+        string = "Query <br/><table>"
+        string + "</table>"
+        return string
+
+class AsyncQuery(GQL, Generic[MyType]):
+
+    def __init__(self, query: str, cls: Type[MyType], aslist=False):
+        self.aslist = aslist
+        self.query = query
+        self._cls = cls
+        super().__init__(query)
+
+    @property
+    def cls(self) -> MyType:
+        return self._cls
+
+    async def run(self, ward=None, variables=None, **kwargs) -> MyType:
+        from bergen.registries.arnheim import get_current_arnheim
+        ward = ward or self._cls.get_ward()
+        returnedobject = await ward.run_async(self, variables=variables,**kwargs)
+        assert returnedobject is not None, "We received nothing back from the Server! Refine your Query!"
+        if isinstance(returnedobject,list): raise Exception("Received a freaking list..., Please run AsyncQueryList")
+        return self.cls(**returnedobject)
+
+    def subscribe(self, ward=None, **kwargs) -> Generator[MyType, None,None]:
+        from bergen.registries.arnheim import get_current_arnheim
+        ward = ward or get_current_arnheim().getWard()
+        return ward.subscribe(self, **kwargs)
+
+    def _repr_html_(self) -> str:
+        string = "Query <br/><table>"
+        string + "</table>"
+        return string
+
+class AsyncQueryList(GQL, Generic[MyType]):
+
+    def __init__(self, query: str, cls: Type[MyType], aslist=False):
+        self.aslist = aslist
+        self.query = query
+        self._cls = cls
+        super().__init__(query)
+
+    @property
+    def cls(self) -> MyType:
+        return self._cls
+
+    async def run(self, ward=None, variables=None, **kwargs) -> List[MyType]:
+        from bergen.registries.arnheim import get_current_arnheim
+        ward = ward or self._cls.get_ward()
+        returnedobject = await ward.run_async(self, variables=variables,**kwargs)
+        assert returnedobject is not None, "We received nothing back from the Server! Refine your Query!"
+        assert isinstance(returnedobject,list), "Cannot unpack values, sure you are receiving a List?"
+        return [self.cls(**item) for item in returnedobject]
+
+    def subscribe(self, ward=None, **kwargs) -> Generator[MyType, None,None]:
+        from bergen.registries.arnheim import get_current_arnheim
+        ward = ward or get_current_arnheim().getWard()
+        return ward.subscribe(self, **kwargs)
+
+    def _repr_html_(self) -> str:
+        string = "Query <br/><table>"
+        string + "</table>"
+        return string
+
+def DelayedGQL(gqlstring):
+    return lambda model : TypedGQL(gqlstring, model)
+
+
+
+
