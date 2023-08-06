ae979a396ee94083298fcbe1ed03fe0e4c802dc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:42 +0000

    Add package astro-kittens/1.4.4/astro-kittens-1.4.4.tar.gz

---
+++ packages/astro-kittens/astro-kittens-1.4.4.tar.gz/astro-kittens-1.4.4/README.rst
@@ -0,0 +1,25 @@
+kittens
+=======
+
+Collection of Python utility functions for purr, tigger, meqtrees and others
+
+
+Installation
+------------
+
+To install run::
+
+    $ python setup.py install
+
+ 
+Documentation
+-------------
+
+Read everything about MeqTrees on the wiki:
+
+https://github.com/ska-sa/meqtrees/wiki
+
+development
+-----------
+
+[![Build Status](https://travis-ci.org/ska-sa/kittens.svg?branch=master)](https://travis-ci.org/ska-sa/kittens)
---
+++ packages/astro-kittens/astro-kittens-1.4.4.tar.gz/astro-kittens-1.4.4/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+from Kittens import __version__
+
+setup(name='astro-kittens',
+      version=__version__,
+      description='Collection of Python utility functions for purr, tigger, meqtrees and others',
+      author='Oleg Smirnov',
+      author_email='Oleg Smirnov <osmirnov@gmail.com>',
+      url='https://github.com/ska-sa/kittens',
+      packages=find_packages(),
+      install_requires=['configparser', 'astropy'],
+     )
+
