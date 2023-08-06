acf8a3d369faa5ef1a89807ecdb8c540ea02f28c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:03 +0000

    Add package lpshipit/0.4.7/lpshipit-0.4.7.tar.gz

---
+++ packages/lpshipit/lpshipit-0.4.7.tar.gz/lpshipit-0.4.7/lpmptox.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+"""
+lpmptox script will run tox on changes in a selected MP
+
+lpmptox depends on ``launchpadlib``, which isn't
+necessarily up-to-date in PyPI, so we install it from the archive::
+
+`sudo apt-get install python-launchpadlib` OR
+
+`sudo apt-get install python3-launchpadlib` OR
+
+As we're using ``launchpadlib`` from the archive (which is therefore
+installed in the system), you'll need to create your virtualenvs
+with the ``--system-site-packages`` option.
+
+Activate your virtualenv and install the requirements::
+
+`pip install -r requirements.txt`
+
+
+"""
+import click
+import git
+import os
+import subprocess
+from tempfile import TemporaryDirectory
+import urwid
+
+from lpshipit import (
+    _get_launchpad_client,
+    _set_urwid_widget,
+    summarize_git_mps,
+)
+from lxc import lxc_container
+
+# Global var to store the chosen MP
+CHOSEN_MP = None
+
+def _write_debug(output_file, message):
+    output_file.write('{}\n'.format(message))
+    output_file.flush()
+    print(message)
+
+
+def runtox(source_repo, source_branch,
+           tox_command='tox --recreate --parallel auto',
+           output_filepath=os.devnull,
+           environment=None):
+    with open(output_filepath, "a") as output_file, TemporaryDirectory() as local_repo:
+        _write_debug(output_file, 'Cloning {} (branch {}) in to tmp directory {} ...'.format(
+            source_repo,
+            source_branch,
+            local_repo))
+        repo = git.Repo.clone_from(
+            source_repo,
+            local_repo,
+            depth=1,
+            single_branch=True,
+            branch=source_branch
+        )
+        _write_debug(output_file, '{} {}'.format(
+            repo.head.object.hexsha,
+            repo.head.object.summary
+        ))
+
+        if environment is not None:
+            _write_debug(output_file, 'Running `{}` in {} lxc environment ...'.format(tox_command, environment))
+            return _run_tox_in_lxc(environment, local_repo, tox_command, output_file)
+        else:
+           _write_debug(output_file, 'Running `{}` in {} ...'.format(tox_command, local_repo))
+           return _run_tox_locally(local_repo, tox_command, output_file)
+
+def _run_tox_in_lxc(environment, local_repo, tox_command, output_file):
+    with lxc_container(environment, local_repo) as container:
+        # If a proxy has been configured on the host using environment
+        # variables http_proxy or https_proxy then the container will inherit
+        # this proxy config for the default user. However then calling sudo
+        # this proxy config is no longer configured. To fix this and so sudo
+        # calls use the same proxy we can use `--preserve-env`
+        # http://manpages.ubuntu.com/manpages/focal/man8/sudo.8.html to pass
+        # in a list of environment variables we want set for sudo user too.
+        sudo_preserve_proxy = '--preserve-env="http_proxy,https_proxy"'
+        container.run_command('sudo {} apt-get update'.format(sudo_preserve_proxy))
+        container.run_command('sudo {} apt-get install -y python3-pip'.format(sudo_preserve_proxy))
+        container.run_command('sudo {} pip3 install tox'.format(sudo_preserve_proxy))
+        # local_repo is same path in the container
+        return container.run_command(tox_command + ' -c ' + local_repo)
+
+def _run_tox_locally(local_repo, tox_command, output_file):
+    process = subprocess.Popen(tox_command,
+                            stdout=subprocess.PIPE,
+                            shell=True,
+                            cwd=local_repo)
+    while process.poll() is None:
+        _write_debug(output_file, process.stdout.readline().decode('utf-8').rstrip())
+    return process.returncode
+
+
+@click.command()
+@click.option('--mp-owner', help='LP username of the owner of the MP '
+                                 '(Defaults to system configured user)',
+              default=None)
+@click.option('--debug/--no-debug', default=False)
+@click.option('--environment', default=None, help = 'release (16.04, 18.04, etc) to run tox in')
+def lpmptox(mp_owner, debug, environment):
+    """Invokes the commit building with proper user inputs."""
+    lp = _get_launchpad_client()
+    lp_user = lp.me
+
+    print('Retrieving Merge Proposals from Launchpad...')
+    person = lp.people[lp_user.name if mp_owner is None else mp_owner]
+    mps = person.getMergeProposals(status=['Needs review', 'Approved'])
+    if debug:
+        print('Debug: Launchad returned {} merge proposals'.format(len(mps)))
+    mp_summaries = summarize_git_mps(mps)
+
+    if mp_summaries:
+
+        def urwid_exit_on_q(key):
+            if key in ('q', 'Q'):
+                raise urwid.ExitMainLoop()
+
+        def mp_chosen(button, chosen_mp):
+            global CHOSEN_MP
+            CHOSEN_MP = chosen_mp
+
+            raise urwid.ExitMainLoop()
+
+        listwalker = urwid.SimpleFocusListWalker(list())
+        listwalker.append(urwid.Text(u'Merge Proposal to Merge'))
+        listwalker.append(urwid.Divider())
+
+        for mp in mp_summaries:
+            button = urwid.Button(mp['summary'])
+            urwid.connect_signal(button, 'click', mp_chosen, mp)
+            listwalker.append(button)
+        mp_box = urwid.ListBox(listwalker)
+        try:
+            _set_urwid_widget(mp_box, urwid_exit_on_q)
+        finally:
+            if CHOSEN_MP:
+                source_repo = CHOSEN_MP['source_repo']
+                source_branch = CHOSEN_MP['source_branch']
+                runtox(source_repo, source_branch, environment=environment)
+    else:
+        print("You have no Merge Proposals in either "
+              "'Needs review' or 'Approved' state")
+
+
+if __name__ == "__main__":
+    lpmptox()
---
+++ packages/lpshipit/lpshipit-0.4.7.tar.gz/lpshipit-0.4.7/lxc.py
@@ -0,0 +1,82 @@
+import contextlib
+import os
+import subprocess
+import time
+
+
+class LxcContainer:
+    def __init__(self, environment, name):
+        self.name = name
+        image='ubuntu:{}'.format(environment)
+        subprocess.check_output('lxc launch {} {}'.format(image,name), 
+                                stdin=subprocess.PIPE,
+                                stderr=subprocess.STDOUT,
+                                shell=True) 
+        self.wait_for_networking()
+        self.user = subprocess.check_output('lxc exec {} -- whoami'.format(self.name),
+                                            stdin=subprocess.PIPE,
+                                            stderr=subprocess.STDOUT,
+                                            shell=True).decode('utf-8').strip()
+        self.home = subprocess.check_output('lxc exec {} -- pwd'.format(self.name),
+                                             stdin=subprocess.PIPE,
+                                             stderr=subprocess.STDOUT,
+                                             shell=True).decode('utf-8').strip()
+
+    def wait_for_networking(self):
+        for _ in range(10):
+            if self.run_command('sh -c "curl -s --head http://archive.ubuntu.com > /dev/null"') == 0:
+                return  # We have networking, exit out
+            time.sleep(6)
+        raise Exception('Networking did not come up in 60 seconds')
+    
+    def setup_code_directory(self, tmp_directory):
+        subprocess.check_call('lxc file push -rp {} {}'.format(tmp_directory,
+                                                               self.name + '/tmp'),
+                               stdin=subprocess.PIPE,
+                               stderr=subprocess.STDOUT,
+                               shell=True)
+        subprocess.check_call('lxc file push {} -rp {}'.format( 
+                                                       os.environ['HOME'] + '/.ssh', 
+                                                       self.name + self.home),
+                               stdin=subprocess.PIPE,
+                               stderr=subprocess.STDOUT,
+                               shell=True)
+        subprocess.check_call('lxc file push {} -rp {}'.format( 
+                                                       os.environ['HOME'] + '/.gitconfig', 
+                                                       self.name + self.home),
+                               stdin=subprocess.PIPE,
+                               stderr=subprocess.STDOUT,
+                               shell=True)
+        # need to change ownership for ssh to work
+        self.run_command('chown -R {0}:{0} {1}'.format(self.user, self.home))
+
+
+
+    def run_command(self, cmd):
+        lxc_command = 'lxc exec {} -- {}'.format(self.name, cmd)
+        print("Running {}".format(lxc_command))
+        process = subprocess.Popen(lxc_command,
+                        stdin=subprocess.PIPE,
+                        stderr=subprocess.STDOUT,
+                        stdout=subprocess.PIPE,
+                        shell=True)
+        while process.poll() is None:
+            debug_message = process.stdout.readline().decode('utf-8').rstrip()
+            print(debug_message)
+        return process.returncode
+
+@contextlib.contextmanager
+def lxc_container(environment, cwd):
+    name = 'cpc-' + subprocess.check_output('petname',
+                                            stdin=subprocess.PIPE,
+                                            stderr=subprocess.STDOUT,
+                                            shell=True).decode('utf-8').strip()
+    try:
+        instance = LxcContainer(environment, name)
+        instance.setup_code_directory(cwd)
+        yield instance
+    finally:
+        subprocess.check_call('lxc delete --force {}'.format(name),
+                              stdin=subprocess.PIPE,
+                              stderr=subprocess.PIPE,
+                              shell=True)
