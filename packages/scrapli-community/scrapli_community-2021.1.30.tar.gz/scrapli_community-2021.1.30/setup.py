53c4d117297e8960e1926f0a7818e5738ee9e592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:17 +0000

    Add package scrapli-community/2021.1.30/scrapli_community-2021.1.30.tar.gz

---
+++ packages/scrapli-community/scrapli_community-2021.1.30.tar.gz/scrapli_community-2021.1.30/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+"""scrapli_community - scrapli community platforms"""
+import setuptools
+
+__author__ = "Carl Montanari"
+
+with open("README.md", "r", encoding="utf-8") as f:
+    README = f.read()
+
+with open("requirements.txt", "r") as f:
+    INSTALL_REQUIRES = f.read().splitlines()
+
+setuptools.setup(
+    name="scrapli_community",
+    version="2021.01.30",
+    author=__author__,
+    author_email="carl.r.montanari@gmail.com",
+    description="",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/scrapli/scrapli_community",
+    project_urls={
+        "Changelog": "https://github.com/scrapli/scrapli_community/blob/master/CHANGELOG.md"
+    },
+    license="MIT",
+    packages=setuptools.find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    dependency_links=[],
+    extras_require={},
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.6",
+)
