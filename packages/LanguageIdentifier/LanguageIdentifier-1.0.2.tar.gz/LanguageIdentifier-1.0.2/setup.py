a374b1ff3766680b472c81590aa6977d671687fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:28 +0000

    Add package LanguageIdentifier/1.0.2/LanguageIdentifier-1.0.2.tar.gz

---
+++ packages/LanguageIdentifier/LanguageIdentifier-1.0.2.tar.gz/LanguageIdentifier-1.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+setup_args = dict(
+    name='LanguageIdentifier',
+    version='1.0.2',
+    description='Language identifier based on an LSTM model',
+    long_description_content_type="text/markdown",
+    # long_description=README + '\n\n' + HISTORY,
+    license='MIT',
+    py_modules=['LanguageIdentifier', 'LIDModel', 'LSTMLID'],
+    # author='Mads, Søren, Manuel',
+    # author_email='mabeto5p@live.dk',
+    keywords=['Lid', 'LSTM', 'Language identifier',  'Language'],
+    # url='https://github.com/ncthuc/elastictools',
+    download_url='https://pypi.org/project/LanguageIdentifier/'
+)
+print(find_packages())
+
+install_requires = [
+    'torch'
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires, include_package_data=True)
