55997d0f75574312c3c9dbe4acf4f0fae2c33d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:16 +0000

    Add package dcekit/2.5.1/dcekit-2.5.1-py3-none-any.whl

---
+++ packages/dcekit/dcekit-2.5.1-py3-none-any.whl/dcekit/learning/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# %reset -f
+"""
+@author: Hiromasa Kaneko
+"""
+
+from .transfer_learning import TransferLearningSample
+from .semi_supervised_learning import SemiSupervisedLearningLowDimension
+from .ensemble_learning import DCEBaggingRegressor
+from .ensemble_learning import DCEBaggingClassifier
+from .ensemble_learning import ensemble_outlier_sample_detection
