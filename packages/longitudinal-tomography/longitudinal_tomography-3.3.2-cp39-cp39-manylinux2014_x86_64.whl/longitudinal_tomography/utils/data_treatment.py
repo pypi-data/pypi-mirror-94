9e48bcb764fba0a2e4b5753362646fc2990b5a43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:57 +0000

    Add package longitudinal-tomography/3.3.2/longitudinal_tomography-3.3.2-cp39-cp39-manylinux2014_x86_64.whl

---
+++ packages/longitudinal-tomography/longitudinal_tomography-3.3.2-cp39-cp39-manylinux2014_x86_64.whl/longitudinal_tomography/utils/data_treatment.py
@@ -0,0 +1,11 @@
+"""
+This module is deprecated. Its contents have been moved to
+longitudinal_tomography.data.data_treatment.
+This module exists solely for backwards compatibility and can be removed
+without notice.
+"""
+
+from ..data.data_treatment import *
+from warnings import warn
+
+#warn('longitudinal_tomography.utils.data_treatment has been moved to longitudinal_tomography.data.data_treatment')
