1438d7f8a653bd05ced035e0db60416e717fe1f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:12 +0000

    Add package CWGP/1.2.5/CWGP-1.2.5.tar.gz

---
+++ packages/CWGP/CWGP-1.2.5.tar.gz/CWGP-1.2.5/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="CWGP",
+    version="1.2.5",
+    author="Andy Chen",
+    author_email="andy97_861022_chen@hotmail.com",
+    description="CWGP",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/andy971022/CWGP",
+    packages=setuptools.find_packages(),
+    classifiers=[
+                "Programming Language :: Python :: 3",
+                "License :: OSI Approved :: MIT License",
+                "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'autograd',
+        'scipy>=1.5.4',
+    ],
+    python_requires='>=3.6',
+)
