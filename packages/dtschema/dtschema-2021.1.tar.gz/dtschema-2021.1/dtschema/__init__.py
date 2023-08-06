853cc5eb73a1b70dbaa45aa9f43e930fba4eac47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:03 +0000

    Add package dtschema/2021.1/dtschema-2021.1.tar.gz

---
+++ packages/dtschema/dtschema-2021.1.tar.gz/dtschema-2021.1/dtschema/__init__.py
@@ -0,0 +1,13 @@
+from dtschema.lib import (
+    add_schema_path,
+    load_schema,
+    load,
+    process_schemas,
+    set_schema,
+    DTValidator,
+    format_error,
+)
+
+from dtschema.version import (
+    __version__
+)
