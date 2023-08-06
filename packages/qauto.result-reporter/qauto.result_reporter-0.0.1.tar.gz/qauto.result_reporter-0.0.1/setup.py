3d002a5e7fef8ecf5c7be838b6ed360072b813d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:16 +0000

    Add package qauto.result-reporter/0.0.1/qauto.result_reporter-0.0.1.tar.gz

---
+++ packages/qauto.result-reporter/qauto.result_reporter-0.0.1.tar.gz/qauto.result_reporter-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='qauto.result_reporter',
+    description='result visualization for Makefile and C4',
+    author='QA Automation',
+    version='0.0.1',
+    classifiers=['Programming Language :: Python :: 3'],
+)
