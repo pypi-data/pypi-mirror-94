eae03fbf942dbf49021658eb151112bb17f39171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:02 +0000

    Add package desktop-notifier/1.2.0.dev0/desktop_notifier-1.2.0.dev0-py2.py3-none-any.whl

---
+++ packages/desktop-notifier/desktop_notifier-1.2.0.dev0-py2.py3-none-any.whl/desktop_notifier/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from .main import DesktopNotifier, Notification, NotificationLevel
+
+
+__version__ = "1.2.0.dev0"
+__author__ = "Sam Schott"
+__url__ = "https://github.com/samschott/desktop-notifier"
