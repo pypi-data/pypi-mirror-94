eed62de718ae570ef748628356a83338fa0f1f0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:14 +0000

    Add package sixgill-clients/0.2.2/sixgill_clients-0.2.2-py2.py3-none-any.whl

---
+++ packages/sixgill-clients/sixgill_clients-0.2.2-py2.py3-none-any.whl/sixgill/sixgill_request_classes/sixgill_specific_actionable_alert_request.py
@@ -0,0 +1,11 @@
+from sixgill.sixgill_request_classes.sixgill_base_post_auth_request import SixgillBasePostAuthRequest
+
+
+class SixgillSpecificActionableAlertGetRequest(SixgillBasePostAuthRequest):
+    end_point = None
+    method = 'GET'
+
+    def __init__(self, channel_id, access_token, actionable_alert_id):
+        super(SixgillSpecificActionableAlertGetRequest, self).__init__(channel_id, access_token)
+
+        self.end_point = 'alerts/actionable_alert/{}'.format(actionable_alert_id)
