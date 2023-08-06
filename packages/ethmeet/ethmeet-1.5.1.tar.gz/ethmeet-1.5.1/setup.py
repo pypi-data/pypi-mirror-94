10ffcf0e57fdf6ffdfcdec19d60251d86bb7b03f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:18 +0000

    Add package ethmeet/1.5.1/ethmeet-1.5.1.tar.gz

---
+++ packages/ethmeet/ethmeet-1.5.1.tar.gz/ethmeet-1.5.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ethmeet",
+    version="1.5.1",
+    author="Lo Han",
+    author_email="lohan.uchsa@protonmail.com",
+    description="Online meeting automation tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sourcerer0/ethmeet",
+    packages=setuptools.find_packages(),
+    keywords="bot firefox automation browser selenium meeting zoom google",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.6',
+)
