4a7a1fc7e9b90f4459fae0e068ed5ab7bbc7878b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:00 +0000

    Add package fabumokuaiceshi/1.0/fabumokuaiceshi-1.0.tar.gz

---
+++ packages/fabumokuaiceshi/fabumokuaiceshi-1.0.tar.gz/fabumokuaiceshi-1.0/setup.py
@@ -0,0 +1,10 @@
+#coding=gbk
+from distutils.core import setup
+setup(
+    name = 'fabumokuaiceshi',
+    version = '1.0',
+    description = 'I want to try this.',
+    author = 'PengPeng3g',
+    author_email = '2784017006@qq.com',
+    py_modules = ['1','2']
+)
