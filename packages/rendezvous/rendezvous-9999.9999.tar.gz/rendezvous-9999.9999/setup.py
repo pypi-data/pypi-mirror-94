e085ef6125d60b1726529bde65d7b0c6db1451b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:28 +0000

    Add package rendezvous/9999.9999/rendezvous-9999.9999.tar.gz

---
+++ packages/rendezvous/rendezvous-9999.9999.tar.gz/rendezvous-9999.9999/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='rendezvous',  
+     version='9999.9999',
+     scripts=['rendezvous'] ,
+     author="X",
+     author_email="chooqu7geb@protonmail.com",
+     description="An utility package",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
