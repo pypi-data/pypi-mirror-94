4648d1df966f204c6956a98caffcde301436614a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:59 +0000

    Add package galaxychop/0.1.0/galaxychop-0.1.0.tar.gz

---
+++ packages/galaxychop/galaxychop-0.1.0.tar.gz/galaxychop-0.1.0/galaxychop/__init__.py
@@ -0,0 +1,31 @@
+# This file is part of
+# the galxy-chop project (https://github.com/vcristiani/galaxy-chop)
+# Copyright (c) 2020, Valeria Cristiani
+# License: MIT
+# Full Text: https://github.com/vcristiani/galaxy-chop/blob/master/LICENSE.txt
+
+# =============================================================================
+# DOCS
+# =============================================================================
+
+"""
+GalaxyChop.
+
+Implementation of a few galaxy dynamic decomposition methods.
+"""
+
+# =============================================================================
+# META
+# =============================================================================
+
+__version__ = "0.1.0"
+
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+
+from .core import *  # noqa
+from .utils import *  # noqa
+from .models import *  # noqa
+from .dataset import *  # noqa
