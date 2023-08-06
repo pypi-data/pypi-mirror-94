8ff43032127abf09d5cfbc440fcfbe6fee1aa6fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:18 +0000

    Add package pypi-template-castellani-davide/1.0/pypi_template_castellani_davide-1.0-py3-none-any.whl

---
+++ packages/pypi-template-castellani-davide/pypi_template_castellani_davide-1.0-py3-none-any.whl/template/__init__.py
@@ -0,0 +1,19 @@
+"""template
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2021-02-10"
+
+class template:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(template.template())
+	
+	def template():
+		"""template first funtion
+		"""
+		return "template" #for the test (see test_template.py)
+		
+if __name__ == "__main__":
+	template()
