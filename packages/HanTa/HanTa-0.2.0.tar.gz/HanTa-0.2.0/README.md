3f4c12d8a371050e01bfc3e2b350c53d50a8b57c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:01 +0000

    Add package HanTa/0.2.0/HanTa-0.2.0.tar.gz

---
+++ packages/HanTa/HanTa-0.2.0.tar.gz/HanTa-0.2.0/README.md
@@ -0,0 +1,2 @@
+# HanTa
+The Hanover Tagger - A simple approach to lemmatization and POS-tagging of German text based on heuristics and Hidden Markov Models 
---
+++ packages/HanTa/HanTa-0.2.0.tar.gz/HanTa-0.2.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="HanTa",
+    version="0.2.0",
+    author="Christian Wartena",
+    author_email="Christian.Wartena@hs-hannover.de",
+    description="Hannover Tagger: Morphological Analysis and POS Tagging",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wartaal/HanTa",
+    packages=setuptools.find_packages(),
+    package_data = {'HanTa': ['*.pgz']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Natural Language :: German",
+        "Topic :: Text Processing :: Linguistic",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers"
+    ],
+)
