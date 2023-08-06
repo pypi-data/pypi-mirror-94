71dcf5f1095b2210a0fb64f9d09c38d654c9dec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:04 +0000

    Add package django-graphql-jwt-reload/1.3.4/django-graphql-jwt-reload-1.3.4.tar.gz

---
+++ packages/django-graphql-jwt-reload/django-graphql-jwt-reload-1.3.4.tar.gz/django-graphql-jwt-reload-1.3.4/graphql_jwt/__init__.py
@@ -0,0 +1,18 @@
+from . import relay
+from .mutations import (
+    DeleteJSONWebTokenCookie, DeleteRefreshTokenCookie, JSONWebTokenMutation,
+    ObtainJSONWebToken, Refresh, Revoke, Verify,
+)
+
+__all__ = [
+    'relay',
+    'JSONWebTokenMutation',
+    'ObtainJSONWebToken',
+    'Verify',
+    'Refresh',
+    'Revoke',
+    'DeleteJSONWebTokenCookie',
+    'DeleteRefreshTokenCookie',
+]
+
+__version__ = '1.3.4'
