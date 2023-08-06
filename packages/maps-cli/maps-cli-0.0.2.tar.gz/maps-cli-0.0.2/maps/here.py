fa2d315aeef01b62dca732d11aff377e29f3990b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:04 +0000

    Add package maps-cli/0.0.2/maps-cli-0.0.2.tar.gz

---
+++ packages/maps-cli/maps-cli-0.0.2.tar.gz/maps-cli-0.0.2/maps/here.py
@@ -0,0 +1,69 @@
+"""This module defines all the HERE commands."""
+import json
+import os
+
+import click
+from geopy.geocoders import Here
+
+from maps.exceptions import ApiKeyNotFoundError
+from maps.utils import yield_subcommands
+
+
+@click.group()
+@click.pass_context
+def here(ctx):
+    """Here provider."""
+    ctx.obj = {}
+
+
+@here.command()
+def show():
+    """show list of all sub commands."""
+    for sub in yield_subcommands(here):
+        click.secho(sub, fg="green")
+
+
+@here.command(short_help="forward or reverse geocode for an address or coordinates.")
+@click.argument("query", required=True)
+@click.option("--apikey", help="Your HERE API key", type=str)
+@click.option(
+    "--forward/--reverse",
+    default=True,
+    show_default=True,
+    help="Perform a forward or reverse geocode",
+)
+@click.option("--raw", is_flag=True)
+@click.pass_context
+def geocoding(ctx, query, apikey, forward, raw):
+    """
+    HERE's geocoding service.
+    \f
+
+    :param ctx: A context dictionary.
+    :param query: A string to represent address query for geocoding.
+    :param apikey: An API key for authentication.
+    :param forward: A boolean flag for forward/reverse geocoding.
+    :param raw: A boolean flag to show api response as it is.
+    :return: None.
+    """
+    apikey = apikey or os.environ.get("HERE_APIKEY")
+    if apikey is None:
+        raise ApiKeyNotFoundError(
+            "Please pass HERE API KEY as --apikey or set it as environment "
+            "variable in HERE_APIKEY "
+        )
+    ctx.obj["apikey"] = apikey
+    geolocator = Here(apikey=ctx.obj["apikey"])
+    if forward:
+        location = geolocator.geocode(query)
+        if raw:
+            click.secho(json.dumps(location.raw, indent=2), fg="green")
+        else:
+            result = {"lat": location.latitude, "lon": location.longitude}
+            click.secho(json.dumps(result, indent=2), fg="green")
+    else:
+        location = geolocator.reverse(query)
+        if raw:
+            click.secho(json.dumps(location.raw, indent=2), fg="green")
+        else:
+            click.secho(location.address, fg="green")
