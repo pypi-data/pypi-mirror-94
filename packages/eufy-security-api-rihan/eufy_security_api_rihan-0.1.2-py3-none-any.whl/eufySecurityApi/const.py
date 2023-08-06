c039ebd9d8bf7ed9999a6d4e0a5753d0d035dc1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:31 +0000

    Add package eufy-security-api-rihan/0.1.2/eufy_security_api_rihan-0.1.2-py3-none-any.whl

---
+++ packages/eufy-security-api-rihan/eufy_security_api_rihan-0.1.2-py3-none-any.whl/eufySecurityApi/const.py
@@ -0,0 +1,183 @@
+from enum import Enum
+
+class TWO_FACTOR_AUTH_METHODS(Enum):
+    SMS = 0
+    PUSH = 1
+    EMAIL = 2
+
+API_BASE_URL = 'mysecurity.eufylife.com/api'
+# API_BASE_URL = 'security-app-eu.eufylife.com'
+ENDPOINT_LOGIN = '/passport/login'
+ENDPOINT_REQUEST_VERIFY_CODE = '/sms/send/verify_code'
+ENDPOINT_LOGOUT = '/passport/logout'
+ENDPOINT_DEVICE_LIST = '/app/get_devs_list'
+ENDPOINT_STATION_LIST = '/app/get_hub_list'
+ENDPOINT_TRUST_DEVICE_ADD = '/app/trust_device/add'
+ENDPOINT_TRUST_DEVICE_LIST = '/app/trust_device/list'
+MOTION_DETECTION_COOLDOWN_MS = 3000
+
+API_HEADERS = {
+    'app_version': "v2.3.0_792",
+    'os_type': "android",
+    'os_version': "29",
+    'phone_model': "Python Api",
+    'country': "IT",
+    'language': "en",
+    'openudid': "5e4621b0152c0d13",
+    'uid': "",
+    'net_type': "wifi",
+    'mnc': "02",
+    'mcc': "262",
+    'sn': "75814221ee75",
+    'Model_type': "PHONE",
+    'timezone': "GMT+01:00",
+    'Content-Type': 'application/json'
+}
+
+class RESPONSE_ERROR_CODE(Enum):
+    WHATEVER_ERROR          = 0
+    SESSION_TIMEOUT         = 401
+    CONNECT_ERROR           = 997
+    NETWORK_ERROR           = 998
+    SERVER_ERROR            = 999
+    UNVERIFIED_EMAIL        = 26015
+    VERIFY_CODE_ERROR       = 26050
+    VERIFY_CODE_EXPIRED     = 26051
+    NEED_VERIFY_CODE        = 26052
+    VERIFY_CODE_MAX         = 26053
+    VERIFY_CODE_NONE_MATCH  = 26054
+    VERIFY_PASSWORD_ERROR   = 26055
+    PHONE_NONE_SUPPORT      = 26058
+    TOO_MANY_LOGIN_ATTEMPTS = 100028
+
+class DEVICE_STATE(Enum):
+    OFFLINE                 = 0
+    ONLINE                  = 1
+    MANUALLY_DISABLED       = 2
+    OFFLINE_LOWBAT          = 3
+    REMOVE_AND_READD        = 4
+    RESET_AND_READD         = 5
+
+class DEVICE_TYPE(Enum):
+    UNKNOWN                 = -1
+    STATION                 = 0
+    CAMERA                  = 1
+    SENSOR                  = 2
+    FLOODLIGHT              = 3
+    CAMERA_E                = 4
+    DOORBELL                = 5
+    BATTERY_DOORBELL        = 7
+    CAMERA2C                = 8
+    CAMERA2                 = 9
+    MOTION_SENSOR           = 10
+    KEYPAD                  = 11
+    CAMERA2_PRO             = 14
+    CAMERA2C_PRO            = 15
+    BATTERY_DOORBELL_2      = 16
+    INDOOR_CAMERA           = 30
+    INDOOR_PT_CAMERA        = 31
+    SOLO_CAMERA             = 32
+    SOLO_CAMERA_PRO         = 33
+    INDOOR_CAMERA_1080      = 34
+    INDOOR_PT_CAMERA_1080   = 35
+    LOCK_BASIC              = 50
+    LOCK_ADVANCED           = 51
+    LOCK_BASIC_NO_FINGER    = 52
+    LOCK_ADVANCED_NO_FINGER = 53
+
+class PARAM_TYPE(Enum):
+    #
+    PROP_EVENT_NUM          = 'event_num'
+    PROP_UPDATE_TIME        = 'update_time'
+    # PROP_MAIN_SW_VERSION    = 'main_sw_version'
+    # PROP_MAIN_HW_VERSION    = 'main_hw_version'
+    # PROP_SEC_SW_VERSION     = 'sec_sw_version'
+    # PROP_SEC_HW_VERSION     = 'sec_hw_version'
+    # PROP_WIFI_MAC             = 'wifi_mac'
+    PROP_STATUS             = 'status'
+    #
+    BATTERY_LEVEL                   = 1101
+    POWER_MODE                      = 2111
+    DEVICE_UPGRADE_NOW              = 1134
+    DEVICE_RSSI                     = 1141
+    
+    OPEN_DEVICE                     = 2001
+    NIGHT_VISUAL                    = 2002
+    VOLUME                          = 2003
+    DETECT_MODE                     = 2004
+    DETECT_MOTION_SENSITIVE         = 2005
+    DETECT_ZONE                     = 2006
+    UN_DETECT_ZONE                  = 2007
+    SDCARD                          = 2010
+    CHIME_STATE                     = 2015
+    RINGING_VOLUME                  = 2022
+    DETECT_EXPOSURE                 = 2023
+    DETECT_SWITCH                   = 2027
+    DETECT_SCENARIO                 = 2028
+    DOORBELL_HDR                    = 2029
+    DOORBELL_IR_MODE                = 2030
+    DOORBELL_VIDEO_QUALITY          = 2031
+    DOORBELL_BRIGHTNESS             = 2032
+    DOORBELL_DISTORTION             = 2033
+    DOORBELL_RECORD_QUALITY         = 2034
+    DOORBELL_MOTION_NOTIFICATION    = 2035
+    DOORBELL_NOTIFICATION_OPEN      = 2036
+    DOORBELL_SNOOZE_START_TIME      = 2037
+    DOORBELL_NOTIFICATION_JUMP_MODE = 2038
+    DOORBELL_LED_NIGHT_MODE         = 2039
+    DOORBELL_RING_RECORD            = 2040
+    DOORBELL_AUDIO_RECODE           = 2042
+
+
+    WATERMARK_MODE          = 1214              # 1 - hide, 2 - show
+    SNOOZE_MODE             = 1271              # The value is base64 encoded
+    
+    SCHEDULE_MODE           = 1257              # 0 - Away, 1 - Home, 63 - Disarmed
+    GUARD_MODE              = 1224              # 0 - Away, 1 - Home, 63 - Disarmed, 2 - SCHEDULE_MODE
+    
+    FLOODLIGHT_MANUAL_SWITCH        = 1400
+    FLOODLIGHT_MANUAL_BRIGHTNESS    = 1401         # The range is 22-100
+    FLOODLIGHT_MOTION_BRIGHTNESS    = 1412         # The range is 22-100
+    FLOODLIGHT_SCHEDULE_BRIGHTNESS  = 1413       # The range is 22-100
+    FLOODLIGHT_MOTION_SENSITIVTY    = 1272         # The range is 1-5
+    
+    CAMERA_UPGRADE_NOW                  = 1133
+    CAMERA_SPEAKER_VOLUME               = 1230
+    CAMERA_RECORD_ENABLE_AUDIO          = 1366           # Enable microphone
+    CAMERA_RECORD_RETRIGGER_INTERVAL    = 1250     # In seconds
+    CAMERA_RECORD_CLIP_LENGTH           = 1249            # In seconds
+    CAMERA_IR_CUT                       = 1013
+    CAMERA_PIR                          = 1011
+    CAMERA_WIFI_RSSI                    = 1142
+    CAMERA_MOTION_ZONES                 = 1204
+    CAMERA_MOTION_NOTIFICATION_SETTINGS = 1225
+
+    SENSOR_CONTACT_OPEN                 = 1550  # 0 - Close, 1 - Open
+    
+    MOTION_SENSOR_ENABLE_LED            = 1607
+    MOTION_SENSOR_LAST_EVENT_MS         = 1605
+    MOTION_SENSOR_PIR_SENSITIVITY       = 1609
+# 
+#     // Set only params?
+#     PUSH_MSG_MODE = 1252,                       // 0 to ???
+# }
+# 
+
+class GUARD_MODE(Enum):
+    AWAY        = '0'
+    HOME        = '1'
+    DISARMED    = '63'
+    SCHEDULED   = '2'
+    GEOLOCATION = '47'
+    CUSTOM1     = '3'
+    CUSTOM2     = '4'
+    CUSTOM3     = '5'
+    OFF         = '6'
+
+EXCLUDED_ROOT_PROPERTY = ['station_conn', 'member', 'permission', 'params', 'status']
+
+class POWER_MODE(Enum):
+#    CHARGING = '0'
+    PLUGGED     = '1'
+    UNPLUGGED   = '2'
+    LINE_MODE   = '3'
---
+++ packages/eufy-security-api-rihan/eufy_security_api_rihan-0.1.2-py3-none-any.whl/eufySecurityApi/model.py
@@ -0,0 +1,297 @@
+from eufySecurityApi.const import PARAM_TYPE, DEVICE_TYPE, DEVICE_STATE, MOTION_DETECTION_COOLDOWN_MS, EXCLUDED_ROOT_PROPERTY
+import logging, traceback, asyncio
+from datetime import datetime, timedelta
+from eufySecurityApi.utils import getUniqueId
+
+class AttributeWrapper(dict):
+
+    def __init__(self):
+        self._internalDict = {}
+    def __setitem__(self, key, value):
+        if(not isinstance(key, PARAM_TYPE)):
+            try:
+                key = PARAM_TYPE(key)
+            except:
+               raise AttributeError('Key is not a PARAM_TYPE value') 
+        frameStack = traceback.extract_stack(limit=2)[0]
+        if(not(frameStack.filename == __file__)):
+            raise AttributeError('Attribute is read only')
+        self._internalDict[key] = value
+
+    def __getitem__(self, key):
+        if(isinstance(key, PARAM_TYPE)):
+            return self._internalDict[key]
+        else:
+            return self._internalDict[PARAM_TYPE(key)]
+
+    def __setattr__(self, key, value):
+        frameStack = traceback.extract_stack(limit=2)[0]
+        if(not(frameStack.name in self.__dir__() and frameStack.filename == __file__)):
+            raise AttributeError('Attribute is read only')
+        else:
+            self.__dict__[key] = value
+
+    def __getattr__(self, key):
+        raise KeyError('Unhautorized')
+
+    def __contains__(self, key):
+        return key in self._internalDict
+    
+    def __iter__(self): 
+        return self._internalDict.__iter__()
+
+
+class callbackStruct(object):
+    def __init__(self, cId, attributes, callback):
+        self.cId = cId
+        self.attributes = attributes
+        self.call = callback
+
+class Device(object):
+
+    deviceType= DEVICE_TYPE(-1)
+    def __init__(self, api):
+        self.api = api
+        self._logger  = logging.getLogger(__name__)
+        # self._attribute = {}
+        self._notRecognizedAttribute = {}
+        self._attribute = AttributeWrapper()
+        self._callbacks = {}
+    
+    def __setattr__(self, key, value):
+        frameStack = traceback.extract_stack(limit=2)[0]
+        if(not(frameStack.filename == __file__)):
+            raise AttributeError('Device is read only')
+        self.__dict__[key] = value
+
+    def __str__(self):
+        output = ''
+        output += '%s\t(type:%s, model:%s, serial:%s)\t %s \n' % (self.name, self._type, self.model, self.serial, self.status)
+        output += '['
+        for attributeName, attributeValue in self._attribute.items():
+            output += '%s: %s, ' %(attributeName, attributeValue)
+        output = output[:-2]
+        output += ']'
+        return output
+
+    # def __repr__(self):
+    #     return self.__str__()
+
+    def init(self, apiDict):
+        self.serial = apiDict['device_sn']
+        self.name = apiDict['device_name']
+        self.model = apiDict['device_model']
+        self.status = DEVICE_STATE(apiDict['status'])
+        self._type = DEVICE_TYPE(apiDict['device_type'])
+        for attribute in apiDict['params']:
+            try:
+                self._attribute[PARAM_TYPE(attribute['param_type'])] = attribute['param_value']
+            except:
+                self._notRecognizedAttribute[attribute['param_type']] = attribute['param_value']
+                self._logger.debug('%s: attribute type \'%s\' is not recognized: %s' % (self.model, attribute['param_type'], attribute['param_value']))
+        
+        for key in apiDict.keys():
+            if key not in EXCLUDED_ROOT_PROPERTY:
+                if(key == 'wifi_mac' and ':' not in apiDict[key]):
+                    new_mac = ''
+                    for i in range(0,len(apiDict[key]),2):
+                        new_mac += apiDict[key][i:i+2] + ':'
+                    apiDict[key] = new_mac[:-1]
+                try:
+                    self._attribute[PARAM_TYPE(key)] = apiDict[key]
+                except:
+                    self.__dict__[key] = apiDict[key]
+                    pass
+        # self.deviceType = DEVICE_TYPE(apiDict['device_type'])
+    
+    def update(self, apiDict):
+        updatedAttributes = []
+        for key in apiDict.keys():
+            if key not in EXCLUDED_ROOT_PROPERTY:
+                if(key == 'wifi_mac' and ':' not in apiDict[key]):
+                    new_mac = ''
+                    for i in range(0,len(apiDict[key]),2):
+                        new_mac += apiDict[key][i:i+2] + ':'
+                    apiDict[key] = new_mac[:-1]
+                if(key in self.__dict__ and self.__dict__[key] != apiDict[key]):
+                    self._logger.info('%s updated %s: %s -> %s' %(self.name, key, self.__dict__[key], apiDict[key]))
+                    self.__dict__[key] = apiDict[key]
+                try:
+                    if(PARAM_TYPE(key) in self._attribute and self._attribute[PARAM_TYPE(key)] != apiDict[key]):
+                        self._logger.info('%s updated %s: %s -> %s' %(self.name, PARAM_TYPE(key), self._attribute[PARAM_TYPE(key)], apiDict[key]))
+                        updatedAttributes.append((PARAM_TYPE(key), self._attribute[PARAM_TYPE(key)], apiDict[key]))
+                        self._attribute[PARAM_TYPE(key)] = apiDict[key]
+                    elif(PARAM_TYPE(key) not in self._attribute):
+                        self._logger.info('%s new attribute %s: %s -> %s' %(self.name, PARAM_TYPE(key), self._attribute[PARAM_TYPE(key)], apiDict[key]))
+                        updatedAttributes.append((PARAM_TYPE(key), self._attribute[PARAM_TYPE(key)], apiDict[key]))
+                        self._attribute[PARAM_TYPE(key)] = apiDict[key]
+                except:
+                    pass
+                
+        for attribute in apiDict['params']:
+            try:
+                if(PARAM_TYPE(attribute['param_type']) in self._attribute and self._attribute[PARAM_TYPE(attribute['param_type'])] != attribute['param_value']):
+                    updatedAttributes.append((PARAM_TYPE(attribute['param_type']), self._attribute[PARAM_TYPE(attribute['param_type'])], attribute['param_value']))
+                    self._logger.info('%s updated %s: %s -> %s' %(self.name, PARAM_TYPE(attribute['param_type']), self._attribute[PARAM_TYPE(attribute['param_type'])], attribute['param_value']))
+                    self._attribute[PARAM_TYPE(attribute['param_type'])] = attribute['param_value']
+                elif(PARAM_TYPE(attribute['param_type']) not in self._attribute):
+                    updatedAttributes.append((PARAM_TYPE(attribute['param_type']), self._attribute[PARAM_TYPE(attribute['param_type'])], attribute['param_value']))
+                    self._attribute[PARAM_TYPE(attribute['param_type'])] = attribute['param_value']
+                    self._logger.info('%s new attribute %s: %s' % (self.name, PARAM_TYPE(attribute['param_type']), attribute['param_value']))
+            except:
+                if(attribute['param_type'] in self._notRecognizedAttribute and self._notRecognizedAttribute[attribute['param_type']] != attribute['param_value']):
+                    self._logger.info('%s updated %s: %s -> %s' %(self.name, attribute['param_type'], self._notRecognizedAttribute[attribute['param_type']], attribute['param_value']))
+                    self._notRecognizedAttribute[attribute['param_type']] = attribute['param_value']
+                elif(attribute['param_type'] not in self._notRecognizedAttribute):
+                    self._notRecognizedAttribute[attribute['param_type']] = attribute['param_value']
+                    self._logger.info('%s new attribute %s: %s' % (self.name, attribute['param_type'], attribute['param_value']))
+
+        if(len(updatedAttributes) > 0):
+            self._call(updatedAttributes)
+    
+    def _call(self, updatedAttributes):
+        for cId in self._callbacks:
+            data = []
+            for attributeTuple in updatedAttributes:
+                if (attributeTuple[0] in self._callbacks[cId].attributes or len(self._callbacks[cId].attributes) == 0):
+                    data.append(attributeTuple)
+            if(len(data) > 0):
+                asyncio.create_task(self._callbacks[cId].call(data))
+        pass
+    
+    def subscribe(self, attributes: [PARAM_TYPE], callback):
+        cId = getUniqueId(callback)
+        if(cId in self._callbacks):
+            self._callbacks[cId].attributes = list(set(self._callbacks[cId].attributes + attributes))
+        else:
+            self._callbacks[cId] = callbackStruct(cId, attributes, callback)
+        return 'OK'
+
+    def unsubscribe(self, callback):
+        cId = getUniqueId(callback)
+        if(cId in self._callbacks):
+            del self._callbacks[cId]
+            return 'OK'
+        else:
+            return 'KO'
+    
+    @classmethod
+    def fromType(cls, api, deviceType: DEVICE_TYPE):
+        if cls.__name__ == 'Device':
+            for subclass in cls.__subclasses__():
+                if deviceType in subclass.deviceType:
+                    return subclass(api)
+            return Device(api)
+        else:
+            return Device.fromType(api, deviceType)
+        pass
+
+    @property
+    def attribute(self):
+        return self._attribute
+
+    @property
+    def state(self):
+        if(PARAM_TYPE.PROP_STATUS in self._attribute):
+            return self._attribute[PARAM_TYPE.PROP_STATUS].name.replace('_', ' ').lower()
+        else:
+            return self.status.name.replace('_', ' ').lower()
+
+    @property
+    def hasbattery(self):
+        return PARAM_TYPE.BATTERY_LEVEL in self._attribute
+
+    @property
+    def battery_level(self):
+        return self._attribute[PARAM_TYPE.BATTERY_LEVEL] if self.hasbattery else None
+    
+    @property
+    def isCamera(self):
+        return self._type in [
+            DEVICE_TYPE.CAMERA_E,
+            DEVICE_TYPE.CAMERA,
+            DEVICE_TYPE.CAMERA2_PRO,
+            DEVICE_TYPE.CAMERA2C_PRO,
+            DEVICE_TYPE.CAMERA2C,
+            DEVICE_TYPE.CAMERA2,
+            DEVICE_TYPE.INDOOR_CAMERA_1080,
+            DEVICE_TYPE.INDOOR_PT_CAMERA_1080,
+            DEVICE_TYPE.SOLO_CAMERA_PRO,
+            DEVICE_TYPE.SOLO_CAMERA,
+            DEVICE_TYPE.DOORBELL,
+            DEVICE_TYPE.BATTERY_DOORBELL,
+            DEVICE_TYPE.BATTERY_DOORBELL_2,
+            DEVICE_TYPE.FLOODLIGHT
+        ]
+    
+    @property
+    def isMotionSensor(self):
+        return self._type in [
+            DEVICE_TYPE.MOTION_SENSOR
+        ] or self.isCamera
+    
+    @property
+    def isDoorLock(self):
+        return self._type in [
+            DEVICE_TYPE.LOCK_BASIC,
+            DEVICE_TYPE.LOCK_ADVANCED,
+            DEVICE_TYPE.LOCK_BASIC_NO_FINGER,
+            DEVICE_TYPE.LOCK_ADVANCED_NO_FINGER
+        ]
+
+
+class Station(Device):
+    deviceType= [DEVICE_TYPE.STATION]
+
+    def init(self, apiDict):
+        self.serial = apiDict['station_sn']
+        self.name = apiDict['station_name']
+        self.model = apiDict['station_model']
+        self.status = DEVICE_STATE(apiDict['status'])
+        self._type = Station.deviceType
+        for attribute in apiDict['params']:
+            try:
+                self._attribute[PARAM_TYPE(attribute['param_type'])] = attribute['param_value']
+            except:
+                self._notRecognizedAttribute[attribute['param_type']] = attribute['param_value']
+                self._logger.debug('%s: attribute type \'%s\' is not recognized: %s' % (self.model, attribute['param_type'], attribute['param_value']))        
+        for key in apiDict.keys():
+            if key not in EXCLUDED_ROOT_PROPERTY:
+                try:
+                    self._attribute[PARAM_TYPE(key)] = apiDict[key]
+                except:
+                    self.__dict__[key] = apiDict[key]
+                    pass
+        # self.deviceType = DEVICE_TYPE(apiDict['device_type'])
+
+
+class MotionSensor(Device):
+    deviceType = [DEVICE_TYPE.MOTION_SENSOR]
+
+    @property
+    def motionDetected(self):
+        lowerWindowLimit = datetime.now()-timedelta(milliseconds=MOTION_DETECTION_COOLDOWN_MS)
+        return  lowerWindowLimit <= datetime.fromtimestamp(self._attribute[PARAM_TYPE.PROP_UPDATE_TIME])
+
+class GenericCamera(Device):
+    deviceType = [
+        DEVICE_TYPE.CAMERA_E,
+        DEVICE_TYPE.CAMERA,
+        DEVICE_TYPE.CAMERA2_PRO,
+        DEVICE_TYPE.CAMERA2C_PRO,
+        DEVICE_TYPE.CAMERA2C,
+        DEVICE_TYPE.CAMERA2,
+        DEVICE_TYPE.INDOOR_CAMERA_1080,
+        DEVICE_TYPE.INDOOR_PT_CAMERA_1080,
+        DEVICE_TYPE.SOLO_CAMERA_PRO,
+        DEVICE_TYPE.SOLO_CAMERA,
+        DEVICE_TYPE.DOORBELL,
+        DEVICE_TYPE.BATTERY_DOORBELL,
+        DEVICE_TYPE.BATTERY_DOORBELL_2,
+        DEVICE_TYPE.FLOODLIGHT
+    ]
+    
+    @property
+    def motionDetected(self):
+        lowerWindowLimit = datetime.now()-timedelta(milliseconds=MOTION_DETECTION_COOLDOWN_MS)
+        return  lowerWindowLimit <= datetime.fromtimestamp(self._attribute[PARAM_TYPE.PROP_UPDATE_TIME])
