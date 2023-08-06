1a8f50b43191c46d4120bb79d0998c4b4b616e03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:13 +0000

    Add package DIRAC/7.2.0a33/DIRAC-7.2.0a33.tar.gz

---
+++ packages/DIRAC/DIRAC-7.2.0a33.tar.gz/DIRAC-7.2.0a33/src/DIRAC/Core/DISET/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+__RCSID__ = "$Id$"
+
+# Define here a few constants useful for all the transport layer
+
+#: Default timeout for the RPC call
+DEFAULT_RPC_TIMEOUT = 600
+#: Default timeout to establish a connection
+DEFAULT_CONNECTION_TIMEOUT = 10
