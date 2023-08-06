c886062a1adcddf482ffe9d74c2c4118bc65c9c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:01 +0000

    Add package sqlalchemy-dataflex/0.1.5/sqlalchemy-dataflex-0.1.5.tar.gz

---
+++ packages/sqlalchemy-dataflex/sqlalchemy-dataflex-0.1.5.tar.gz/sqlalchemy-dataflex-0.1.5/sqlalchemy_dataflex/__init__.py
@@ -0,0 +1,55 @@
+"""SQLAlchemy support for Dataflex databases."""
+# coding=utf-8
+
+import pyodbc
+from sqlalchemy.dialects import registry as _registry
+
+from .base import (
+    nc,
+    cg,
+    Char,
+    Date,
+    Time,
+    cl_in,
+    BigInt,
+    Double,
+    Binary,
+    Decimal,
+    Integer,
+    Logical,
+    Numeric,
+    VarChar,
+    strtobool,
+    Timestamp,
+    LongVarChar,
+    LongVarBinary,
+    DoublePrecision,
+)
+
+__version__ = "0.1.5"
+
+pyodbc.pooling = True  # Makes the ODBC overhead a little more manageable
+_registry.register("dataflex.pyodbc", "sqlalchemy_dataflex.pyodbc", "DataflexDialect_pyodbc")
+
+__all__ = (
+    "nc",
+    "cg",
+    "Char",
+    "Date",
+    "Time",
+    "cl_in",
+    "BigInt",
+    "Double",
+    "Binary",
+    "Decimal",
+    "Logical",
+    "Integer",
+    "Numeric",
+    "VarChar",
+    "strtobool",
+    "Timestamp",
+    "__version__",
+    "LongVarChar",
+    "LongVarBinary",
+    "DoublePrecision",
+)
