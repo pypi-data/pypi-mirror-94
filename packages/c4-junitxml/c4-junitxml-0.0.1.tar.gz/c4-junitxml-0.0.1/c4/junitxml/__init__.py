8f524ac1cefd51b3305a7c700114566505654860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:11 +0000

    Add package c4-junitxml/0.0.1/c4-junitxml-0.0.1.tar.gz

---
+++ packages/c4-junitxml/c4-junitxml-0.0.1.tar.gz/c4-junitxml-0.0.1/c4/junitxml/__init__.py
@@ -0,0 +1,7 @@
+"""
+JUnit XML handling for C4 test automation framework
+
+    import c4.junitxml
+"""
+
+print("Placeholder for c4.junitxml project to be opensourced soon")
---
+++ packages/c4-junitxml/c4-junitxml-0.0.1.tar.gz/c4-junitxml-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='c4-junitxml',
+    description='c4.junitxml',
+    author='QA Automation',
+    version='0.0.1',
+    packages=['c4.junitxml'],
+    classifiers=['Programming Language :: Python :: 3'],
+)
