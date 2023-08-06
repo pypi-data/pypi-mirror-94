fc252931a7608e0b053d21abe2474e64e3f2cbcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:15 +0000

    Add package h5netcdf/0.9.0/h5netcdf-0.9.0.tar.gz

---
+++ packages/h5netcdf/h5netcdf-0.9.0.tar.gz/h5netcdf-0.9.0/h5netcdf/tests/conftest.py
@@ -0,0 +1,8 @@
+def pytest_addoption(parser):
+    parser.addoption(
+        "--restapi",
+        action="store_true",
+        dest="restapi",
+        default=False,
+        help="Enable HDF5 REST API tests",
+    )
