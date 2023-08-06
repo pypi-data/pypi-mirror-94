3d66968d2d2b9e7d25b66649b9ae32b01fe2fccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:20 +0000

    Add package cloudmesh-pi-cluster/4.3.8/cloudmesh_pi_cluster-4.3.8-py2.py3-none-any.whl

---
+++ packages/cloudmesh-pi-cluster/cloudmesh_pi_cluster-4.3.8-py2.py3-none-any.whl/cloudmesh/pi/wifi.py
@@ -0,0 +1,37 @@
+import textwrap
+
+from cloudmesh.common.console import Console
+from cloudmesh.common.util import writefile
+
+
+class Wifi:
+    location = "/etc/wpa_supplicant/wpa_supplicant.conf"
+
+    template = textwrap.dedent("""
+        ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+        update_config=1
+        country=US
+        
+        network={{
+                ssid="{ssid}"
+                psk="{password}"
+                key_mgmt=WPA-PSK
+        }}
+    """)  # noqa: W293
+
+    @staticmethod
+    def set(ssid, password, dryrun=False):
+        if ssid is None or password is None:
+            Console.error("SSID or password not set")
+        if dryrun:
+            password = "********"
+        config = Wifi.template.format(**locals()) \
+            .replace("BEGIN", "{").replace("END", "}")
+        if dryrun:
+            print(Wifi.location)
+            print(config)
+        else:
+            try:
+                writefile(Wifi.location, config)
+            except FileNotFoundError as e:  # noqa: F841
+                Console.error(f"The file does not exist: {Wifi.location}")
