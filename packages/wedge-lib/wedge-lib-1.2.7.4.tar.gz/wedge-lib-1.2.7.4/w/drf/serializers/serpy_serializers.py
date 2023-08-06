4749f90a8b239629f09e54c748ca5ee816221f17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:37 +0000

    Add package wedge-lib/1.2.7.4/wedge-lib-1.2.7.4.tar.gz

---
+++ packages/wedge-lib/wedge-lib-1.2.7.4.tar.gz/wedge-lib-1.2.7.4/w/drf/serializers/serpy_serializers.py
@@ -0,0 +1,19 @@
+from w.serializers import serializer
+
+
+class ReferenceSerializer(serializer.SerpySerializer):
+    code = serializer.StrField()
+    label = serializer.StrField()
+
+
+class UserSerializer(serializer.SerpySerializer):
+    id = serializer.IntField()
+    username = serializer.Field()
+    first_name = serializer.Field()
+    last_name = serializer.Field()
+    email = serializer.Field()
+    is_active = serializer.BoolField()
+
+
+class UserWithDateSerializer(UserSerializer):
+    date_joined = serializer.DateField()
