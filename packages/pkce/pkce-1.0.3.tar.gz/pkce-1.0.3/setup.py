41339629308a76baf25ddda1de720a10da145c41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:55 +0000

    Add package pkce/1.0.3/pkce-1.0.3.tar.gz

---
+++ packages/pkce/pkce-1.0.3.tar.gz/pkce-1.0.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='pkce',
+    version='1.0.3',
+    author='Roméo Després',
+    author_email='despres.romeo@gmail.com',
+    description='PKCE Pyhton generator.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    url='https://github.com/RomeoDespres/pkce',
+    package_data={'pkce': ['py.typed']},
+    packages=['pkce'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Typing :: Typed',
+    ],
+    python_requires='>=3',
+    zip_safe=False
+)
