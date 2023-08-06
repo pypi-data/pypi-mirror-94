5687d0e798c06333019d03ffa8391baa10d0b15c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:50 +0000

    Add package dict-to-object/1.0.0/dict_to_object-1.0.0-py3-none-any.whl

---
+++ packages/dict-to-object/dict_to_object-1.0.0-py3-none-any.whl/dicttoobject/__init__.py
@@ -0,0 +1,7 @@
+from . import impl
+
+DoNotWriteError = impl.DoNotWriteError
+WritableObject = impl.WritableObject
+ReadOnlyObject = impl.ReadOnlyObject
+dict_to_object = impl.dict_to_object
+object_to_dict = impl.object_to_dict
