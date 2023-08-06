aecadbe1dfb671ab96b2920e7913464191596669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:11 +0000

    Add package cs101-testing/0.0.1/cs101-testing-0.0.1.tar.gz

---
+++ packages/cs101-testing/cs101-testing-0.0.1.tar.gz/cs101-testing-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "cs101-testing",
+    version = "0.0.1",
+    url = "https://github.com/karthiksharma/cs101-testing",
+    author = "Kartikeya Sharma",
+    author_email = "ksharma@illinois.edu",
+    classifiers =
+        ["Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"],
+    description = "Testing module for cs101 labs",
+    packages = setuptools.find_packages(),
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    python_requires='>=3.0',
+)
