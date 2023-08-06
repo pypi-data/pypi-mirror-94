ab647398096af89b488c7248c729d9150bbdc55e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:08 +0000

    Add package pyexlatex/0.13.7/pyexlatex-0.13.7.tar.gz

---
+++ packages/pyexlatex/pyexlatex-0.13.7.tar.gz/pyexlatex-0.13.7/pyexlatex/models/caption.py
@@ -0,0 +1,10 @@
+from pyexlatex.models.item import SimpleItem
+
+
+class Caption(SimpleItem):
+    name = 'caption'
+
+    def __init__(self, contents):
+        super().__init__(self.name, contents)
+
+
