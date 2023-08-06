b0a1b7a775e0d7ef6d9687ac1e48c039bae9234d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:58 +0000

    Add package manga-py/1.26.1/manga_py-1.26.1.tar.gz

---
+++ packages/manga-py/manga_py-1.26.1.tar.gz/manga_py-1.26.1/manga_py/providers/mangalib_me.py
@@ -0,0 +1,40 @@
+from manga_py.provider import Provider
+from .helpers.std import Std
+
+
+class MangaLibMe(Provider, Std):
+
+    def get_chapter_index(self) -> str:
+        selector = r'\.\w{2,7}/[^/]+/[^\d]+(\d+)/[^\d]+([^/]+)'
+        idx = self.re.search(selector, self.chapter).groups()
+        return '-'.join(idx)
+
+    def get_content(self):
+        return self._get_content('{}/{}')
+
+    def get_manga_name(self) -> str:
+        return self._get_name(r'\.\w{2,7}/([^/]+)')
+
+    def get_chapters(self):
+        return self._elements('.chapters-list .chapter-item__name a')
+
+    def get_files(self):
+        content = self.http_get(self.chapter)
+        images = self.re.search(r'__pg\s*=\s*(\[.+\])', content).group(1)
+        info = self.re.search(r'__info\s*=\s*(\{.+\})', content).group(1)
+        images = self.json.loads(images)
+        info = self.json.loads(info)
+        _manga = info['img']['url']
+        _s = info['servers']
+        _server = _s.get('fourth', _s.get('compress'))
+
+        return ['{}{}{}'.format(_server, _manga, i['u']) for i in images]
+
+    def get_cover(self):
+        return self._cover_from_content('img.manga__cover')
+
+    def prepare_cookies(self):
+        self.cf_scrape(self.get_url())
+
+
+main = MangaLibMe
