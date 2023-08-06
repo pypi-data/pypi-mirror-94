4f28862ed8e191c427d744dae09b60c40b0dc3ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:50 +0000

    Add package arena-py/0.1.7/arena_py-0.1.7-py3-none-any.whl

---
+++ packages/arena-py/arena_py-0.1.7-py3-none-any.whl/arena/__init__.py
@@ -0,0 +1,13 @@
+"""
+ARENA-py
+
+Draw objects in the ARENA using Python!
+"""
+
+__version__     = "0.1.7"
+__author__      = "CONIX Research Center"
+
+__license__     = "BSD 3-Clause"
+__maintainer__  = "WiSE Lab, Carnegie Mellon University"
+
+from .arena import *
