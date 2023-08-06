c29ac10cd4ed13441e048686a37bb1847ee970ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:05 +0000

    Add package ssht00ls/1.5.8/ssht00ls-1.5.8.tar.gz

---
+++ packages/ssht00ls/ssht00ls-1.5.8.tar.gz/ssht00ls-1.5.8/ssht00ls/classes/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# imports.
+from ssht00ls.classes.ssh import ssh, sshfs, scp, agent, key, installation, config, sshd, connections
+from ssht00ls.classes.smart_cards import smart_cards, SmartCard
