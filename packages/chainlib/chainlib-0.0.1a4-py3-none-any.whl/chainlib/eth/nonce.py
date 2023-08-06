cd56fa499a846129d2cf2d438bdc5c8b4ee1b4a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:46 +0000

    Add package chainlib/0.0.1a4/chainlib-0.0.1a4-py3-none-any.whl

---
+++ packages/chainlib/chainlib-0.0.1a4-py3-none-any.whl/chainlib/eth/nonce.py
@@ -0,0 +1,30 @@
+# third-party imports
+from hexathon import (
+        add_0x,
+        strip_0x,
+        )
+
+# local imports
+from chainlib.eth.rpc import jsonrpc_template
+
+
+def nonce(address):
+    o = jsonrpc_template()
+    o['method'] = 'eth_getTransactionCount'
+    o['params'].append(address)
+    o['params'].append('pending')
+    return o
+
+
+class DefaultNonceOracle:
+
+    def __init__(self, address, conn):
+        self.address = address
+        self.conn = conn
+
+
+    def next(self):
+        o = nonce(self.address)
+        r = self.conn.do(o)
+        n = strip_0x(r)
+        return int(n, 16)
