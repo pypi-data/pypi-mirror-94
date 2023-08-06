17e6d38d2e9441396aafc4b5f5f389911a10913f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:31 +0000

    Add package pyuavcan/1.2.0b2/pyuavcan-1.2.0b2-py3-none-any.whl

---
+++ packages/pyuavcan/pyuavcan-1.2.0b2-py3-none-any.whl/pyuavcan/__init__.py
@@ -0,0 +1,83 @@
+# Copyright (c) 2019 UAVCAN Consortium
+# This software is distributed under the terms of the MIT License.
+# Author: Pavel Kirienko <pavel@uavcan.org>
+
+r"""
+Submodule import policy
++++++++++++++++++++++++
+
+The following submodules are auto-imported when the root module ``pyuavcan`` is imported:
+
+- :mod:`pyuavcan.dsdl`
+
+- :mod:`pyuavcan.transport`, but not concrete transport implementation submodules.
+  For example, if you need the CAN transport, import :mod:`pyuavcan.transport.can` manually.
+
+- :mod:`pyuavcan.presentation`
+
+- :mod:`pyuavcan.util`
+
+The submodule :mod:`pyuavcan.application` is not auto-imported because in order to have it imported
+the DSDL-generated package ``uavcan`` containing the standard data types must be generated first.
+
+There are no internal (hidden) API between the submodules; they rely only on each other's public API.
+
+
+Log level override
+++++++++++++++++++
+
+The environment variable ``PYUAVCAN_LOGLEVEL`` can be set to one of the following values to override
+the library log level:
+
+- ``CRITICAL``
+- ``FATAL``
+- ``ERROR``
+- ``WARNING``
+- ``INFO``
+- ``DEBUG``
+
+If not set, the log level is determined following the regular policies of the Python's standard ``logging`` library.
+"""
+
+import os as _os
+import sys as _sys
+
+
+with open(_os.path.join(_os.path.dirname(__file__), "VERSION")) as _version:
+    __version__ = _version.read().strip()
+__version_info__ = tuple(map(int, __version__.split(".")[:3]))
+__author__ = "UAVCAN Consortium"
+__copyright__ = "Copyright (c) 2019 UAVCAN Consortium"
+__email__ = "consortium@uavcan.org"
+__license__ = "MIT"
+
+
+UAVCAN_SPECIFICATION_VERSION = 1, 0
+"""
+Version of the UAVCAN protocol implemented by this library, major and minor.
+Use this value to populate the corresponding field in ``uavcan.node.GetInfo.Response``.
+"""
+
+
+if _sys.version_info[:2] < (3, 7):  # pragma: no cover
+    raise RuntimeError("A newer version of Python is required")
+
+
+# Configure logging if requested via environment variable.
+# Accepted values: CRITICAL, ERROR, WARNING, INFO, DEBUG
+_log_level_from_env = _os.environ.get("PYUAVCAN_LOGLEVEL")
+if _log_level_from_env is not None:
+    import logging as _logging
+
+    _logging.basicConfig(
+        format="%(asctime)s %(process)5d %(levelname)-8s %(name)s: %(message)s", level=_log_level_from_env
+    )
+    _logging.getLogger(__name__).setLevel(_log_level_from_env)
+    _logging.getLogger(__name__).info("Log config from env var; level: %r", _log_level_from_env)
+
+
+# The sub-packages are imported in the order of their interdependency.
+import pyuavcan.util as util  # pylint: disable=wrong-import-position  # noqa
+import pyuavcan.dsdl as dsdl  # pylint: disable=wrong-import-position  # noqa
+import pyuavcan.transport as transport  # pylint: disable=wrong-import-position  # noqa
+import pyuavcan.presentation as presentation  # pylint: disable=wrong-import-position  # noqa
---
+++ packages/pyuavcan/pyuavcan-1.2.0b2-py3-none-any.whl/pyuavcan/application/__init__.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2019 UAVCAN Consortium
+# This software is distributed under the terms of the MIT License.
+# Author: Pavel Kirienko <pavel@uavcan.org>
+
+# noinspection PyUnresolvedReferences
+"""
+The application module contains application-level protocol entities.
+This module is not imported automatically because:
+
+- Some applications may choose to rely on the presentation-level API directly instead of using this higher-level module,
+  in which case having it imported would be counter-productive due to increased initialization time, memory footprint,
+  and possible reliability issues. The presentation level is the main abstraction layer provided by this library.
+  The functionality of this application-level module is built on top of the public user-facing API of the
+  presentation layer (no internal interfaces between modules exist). See :mod:`pyuavcan.presentation`.
+
+- The module depends on DSDL generated packages, particularly on that of the standard root namespace ``uavcan``,
+  so this module cannot be imported until the required code is generated.
+
+>>> import pyuavcan
+>>> pyuavcan.transport   # Works.
+<module ...>
+>>> pyuavcan.application
+Traceback (most recent call last):
+    ...
+AttributeError: module 'pyuavcan' has no attribute 'application'
+>>> import pyuavcan.application  # Will fail unless the DSDL package "uavcan" is generated and importable.
+
+Classes contained here affect the state of the bus by publishing data, responding to service requests, or otherwise.
+It is expected that some applications may need to complete early initialization procedures before
+they are ready to begin interaction with the outside world.
+Hence, many classes in this module are equipped with a method ``start()``,
+which must be invoked once to bring the instance into a functional state.
+The instance will remain operational until ``close()`` is invoked on it.
+Both methods are idempotent.
+
+The main entity of this module is the class :class:`pyuavcan.application.Node`.
+There are several nested submodules,
+each dedicated to a particular *application-level function* of the UAVCAN protocol,
+such as :mod:`pyuavcan.application.heartbeat_publisher`;
+each such module shall be imported explicitly.
+Read the UAVCAN specification for background.
+"""
+
+from ._node import Node as Node, NodeInfo as NodeInfo
