1bc90394093e8da0e091966da13079b104edaa5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:32 +0000

    Add package Py-PDM/0.5/Py-PDM-0.5.tar.gz

---
+++ packages/Py-PDM/Py-PDM-0.5.tar.gz/Py-PDM-0.5/README.md
@@ -0,0 +1,43 @@
+# Py-PDM
+
+[![image](http://img.shields.io/pypi/v/Py-PDM.svg)](https://pypi.python.org/pypi/Py-PDM/)
+
+A Python wrapper of the Phase Dispersion Minimization (PDM), which is a [C code written by Stellingwerf](https://www.stellingwerf.com/rfs-bin/index.cgi?action=PageView&id=34).
+
+Compared with other Python implementations, with the help of Cython, we can obtain a much faster PDM tool.
+
+![Example result](/examples/Py-PDM-example.png)
+
+*The red lines show the true frequency and its n times period.*
+
+# Performance
+Compared to the [Pure Python implementation of PDM](https://pyastronomy.readthedocs.io/en/latest/pyTimingDoc/pyPDMDoc/pdm.html) of ``PyAstronomy``:
+
+![Comparison result](/examples/Comparison.png)
+
+# Installation
+**Before installing, make sure you have already installed the cython and numpy in your Python3 environment.**
+
+To install Py-PDM with pip:
+
+```
+pip install py-pdm
+```
+
+Alternatively you can install it manually:
+```
+git clone https://github.com/ckm3/Py-PDM.git
+cd Py-PDM
+python setup.py install
+```
+
+# Usage
+```python3
+from pdmpy import pdm
+
+freq, theta = pdm(time, y_value, y_sigma, frequency_min, frequency_max, frequency_step, number_of_bins)
+```
+Please refer to the example directory to see in details.
+
+# Citing
+If you find Py-PDM useful in your research, please cite the orginal paper [Stellingwerf, Astrophysical Journal, v224, p953, 1978](https://ui.adsabs.harvard.edu/abs/1978ApJ...224..953S/abstract) and add a footnote of this Github repository.
---
+++ packages/Py-PDM/Py-PDM-0.5.tar.gz/Py-PDM-0.5/src/Py-PDM/Py-PDM.pyx
@@ -0,0 +1,111 @@
+#cython: language_level=3
+
+'''
+Py-PDM - a Python wrapper of PMD
+Copyright (C) 2020, Kaiming Cui
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+import numpy as np
+import warnings
+cimport numpy as np
+np.import_array()
+
+
+cdef extern from "PyPDM.c":
+    int pdm2(int ne, double datx[], double daty[], double sig[], double f_min, double f_max, double delf, int nbins)
+    double* f_array
+    double* theta_array
+    int nf
+
+
+def pdm(np.ndarray t, np.ndarray y, np.ndarray s, double f_min, double f_max, double delf, int nbin):
+    """
+    The core function of the Phase Dispersion Minimization
+
+    Parameters:
+        t : np.ndarray
+            Input time array
+        y : np.ndarray
+            Input value array
+        s : np.ndarray
+            Sigma of each y, if no sigma, it should be zeros
+        f_min : double
+            The minima of the frequency range
+        f_max : double
+            The maxima of the frequency range
+        delf : double
+            Delta frequency
+        nbins : int
+            The number of bins
+
+    Returns:
+        frequency_array : np.ndarry
+        theta_array : np.ndarray
+    """
+
+    t = np.ascontiguousarray(t, dtype=np.float64)
+    y = np.ascontiguousarray(y, dtype=np.float64)
+    s = np.ascontiguousarray(s, dtype=np.float64)
+
+    if len(t)==len(y)==len(s):
+        if t.ndim!=1:
+            raise ValueError('Inputs (t, y, s) must be 1-dimensional')
+        pass
+    else:
+        raise ValueError('Dimensions are not same of input parameters')
+
+    if f_min >= f_max:
+        raise ValueError('f_min should be less than f_max')
+    elif f_max <= delf:
+        raise ValueError('delf should be less than f_max')
+
+    if nbin > 100:
+        raise ValueError('The max allowable bins are 100')
+    elif nbin <= 0:
+        raise ValueError('The number of bins should be greater than 0')
+
+    stacked_array = np.vstack((t, y, s))
+
+    if ~np.isfinite(stacked_array).all():
+        warnings.warn("The input arrays contain some NaNs or Infinites, pdm will ignore those points", RuntimeWarning)
+        stacked_array = stacked_array[:, np.isfinite(stacked_array).all(axis=0)]
+        
+    # Add zero at the begining because the one-based number of C source code
+    t_copy = np.insert(stacked_array[0, :], 0, 0).copy()
+    y_copy = np.insert(stacked_array[1, :], 0, 0).copy()
+    sigs_copy = np.insert(stacked_array[2, :], 0, 0).copy()
+
+    cdef double [::1] x2 = t_copy
+    cdef double [::1] y2 = y_copy
+    cdef double [::1] s2 = sigs_copy
+
+    cdef int data_number = stacked_array.shape[1]
+
+    return_code = pdm2(data_number, &x2[0], &y2[0], &s2[0], f_min, f_max, delf, nbin)
+
+    if return_code == 1:
+        pass
+    elif return_code == -1:
+        raise ValueError('pdm: too few data points, 100 points at least')
+    elif return_code == -2:
+        raise ValueError('pdm: too many data points, maximum of 100,000 points allowed')
+
+    freq_np_array = np.asarray(<double[:nf]> f_array)
+    theta_np_array = np.asarray(<double[:nf]> theta_array)
+
+    return freq_np_array, theta_np_array
+
+
