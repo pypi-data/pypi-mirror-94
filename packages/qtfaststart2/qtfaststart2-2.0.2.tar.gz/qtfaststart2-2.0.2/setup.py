b85dd956314afe85dfacbb0bff4b4cd6574e2729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:13 +0000

    Add package qtfaststart2/2.0.2/qtfaststart2-2.0.2.tar.gz

---
+++ packages/qtfaststart2/qtfaststart2-2.0.2.tar.gz/qtfaststart2-2.0.2/setup.py
@@ -0,0 +1,36 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import qtfaststart2
+
+setup_params = dict(
+    name='qtfaststart2',
+    author='danielyaa5',
+    author_email='danyagithub@gmail.com',
+    version=qtfaststart2.VERSION,
+    description='Quicktime atom positioning in Python for fast streaming.',
+    url='https://github.com/danielyaa5/qtfaststart2',
+    download_url = 'https://github.com/danielyaa5/qtfaststart2/archive/{version}.tar.gz'.format(version=qtfaststart2.VERSION),
+    license='MIT License',
+    platforms=["any"],
+    provides=['qtfaststart2'],
+    packages=[
+        'qtfaststart2',
+    ],
+    scripts=['bin/qtfaststart2'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Multimedia :: Video :: Conversion',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
+
+if __name__ == '__main__':
+    setup(**setup_params)
