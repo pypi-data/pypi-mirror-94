0226fe7bbeadb99a9c66e1e700f88c7dbe5edc92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:52 +0000

    Add package notmike/0.0.0/notmike-0.0.0.tar.gz

---
+++ packages/notmike/notmike-0.0.0.tar.gz/notmike-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name='notmike',
+    version='0.0.0',
+    author='Mihir Singh (@citruspi)',
+    author_email='pypi.service@mihirsingh.com',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+)
