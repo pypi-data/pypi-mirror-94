b7f5fd8738cfd1144c05170df57d598e9a279476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:49 +0000

    Add package THPoker/2.2.0/THPoker-2.2.0.tar.gz

---
+++ packages/THPoker/THPoker-2.2.0.tar.gz/THPoker-2.2.0/setup.py
@@ -0,0 +1,47 @@
+# Copyright 2018-2021 Yegor Bitensky
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='THPoker',
+    version='2.2.0',
+    description="Texas Hold'em Poker tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/YegorDB/THPoker',
+    author='Yegor Bitensky',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Topic :: Games/Entertainment",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    keywords='poker cards',
+    packages=find_packages(exclude=['tests*', 'examples*']),
+    python_requires='>=3.8',
+    install_requires=['CTHPoker', 'AGStuff'],
+)
