472295199a5dbe3297ede506dd54e53182be131b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:57 +0000

    Add package sveder/0.7/sveder-0.7.tar.gz

---
+++ packages/sveder/sveder-0.7.tar.gz/sveder-0.7/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+from setuptools.command.install import install
+
+
+class PostInstallCommand(install):
+    def __init__(self, *args, **kwargs):
+        super(PostInstallCommand, self).__init__(*args, **kwargs)
+
+    def run(self):
+        install.run(self)
+        import sveder
+        sveder.open_sveder_com()
+
+
+setup(
+    name = 'sveder',
+    packages = ['sveder'],
+    version = '0.7',
+    description = 'Visit sveder.com :)',
+    author = 'Sveder',
+    author_email = 'm@sveder.com',
+    url = 'https://github.com/Sveder/import-sveder',
+    download_url = 'https://github.com/Sveder/import-sveder/archive/0.1.tar.gz',
+    keywords = ['sveder', 'shameless', 'self', 'promotion'],
+    cmdclass={'install': PostInstallCommand},
+)
