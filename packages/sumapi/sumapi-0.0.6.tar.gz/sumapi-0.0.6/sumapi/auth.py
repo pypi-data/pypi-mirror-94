25518aba155b5b27c4c6344c338d91964a478242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:46 +0000

    Add package sumapi/0.0.6/sumapi-0.0.6.tar.gz

---
+++ packages/sumapi/sumapi-0.0.6.tar.gz/sumapi-0.0.6/sumapi/auth.py
@@ -0,0 +1,47 @@
+import requests
+from .config import URL
+from json import JSONDecodeError
+from requests.exceptions import ConnectionError
+
+def auth(username, password):
+    """
+        With this function, you will receive the token you need to define while using the API.
+
+        Parameters
+        ----------
+        username : str
+            Your API Username
+        password : str
+            Your API Password
+
+        Returns
+        -------
+        dict:
+            access_token: str
+                Your API Access Token
+            token_type: str
+                Your Token Type
+
+        Examples
+        --------
+        from sumapi.auth import auth
+
+        token = auth(username='<your_username>', password='<your_password')
+    """
+    login_data = {
+        'username': username,
+        'password': password
+    }
+
+    try:
+        response = requests.post(URL["tokenURL"], data=login_data).json()
+    except JSONDecodeError:
+        response = requests.post(URL["tokenURL"], data=login_data).json()
+    except (ConnectionError) as e:
+        raise ConnectionError("Error with Connection, Check your Internet Connection or visit api.summarify.io/status for SumAPI Status")
+
+    if "detail" in response.keys():
+        if response['detail'] == 'Incorrect username or password':
+            raise ValueError("There is an error in the login information. Try again by checking your username and password.")
+
+    return response
