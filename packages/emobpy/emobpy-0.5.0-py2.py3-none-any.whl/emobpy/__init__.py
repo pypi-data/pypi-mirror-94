d57d273b53f5ac488662ffd59b07499bc4b89785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:27 +0000

    Add package emobpy/0.5.0/emobpy-0.5.0-py2.py3-none-any.whl

---
+++ packages/emobpy/emobpy-0.5.0-py2.py3-none-any.whl/emobpy/__init__.py
@@ -0,0 +1,35 @@
+__version__ = (0, 5, 0)
+__all__ = (
+    "Mobility",
+    "Availability",
+    "Charging",
+    "DataBase",
+    "DataManager",
+    "Export",
+    "Weather",
+    "BEVspecs",
+    "ModelSpecs",
+    "MGefficiency",
+    "DrivingCycle",
+    "Trips",
+    "Trip",
+    "HeatInsulation",
+    "Consumption",
+)
+
+from .mobility import Mobility
+from .availability import Availability
+from .charging import Charging
+from .database import DataBase, DataManager
+from .consumption import (
+    Weather,
+    BEVspecs,
+    ModelSpecs,
+    MGefficiency,
+    DrivingCycle,
+    Trips,
+    Trip,
+    HeatInsulation,
+    Consumption,
+)
+from .export import Export
