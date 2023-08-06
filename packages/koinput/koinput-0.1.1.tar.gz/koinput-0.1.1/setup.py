0d2c4c9e36f59f207f9bee736fc10960e0ca5a4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:01 +0000

    Add package koinput/0.1.1/koinput-0.1.1.tar.gz

---
+++ packages/koinput/koinput-0.1.1.tar.gz/koinput-0.1.1/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['koinput']
+setup_kwargs = {
+    'name': 'koinput',
+    'version': '0.1.1',
+    'description': 'Maximum simplification of Input / Output for text programs.',
+    'long_description': None,
+    'author': 'k0per',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'python_requires': '>=3.6',
+}
+
+
+setup(**setup_kwargs)
