c189cfe40bdf770e5160235973fe87f74f497237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:43 +0000

    Add package scikit-bonus/0.0.7/scikit_bonus-0.0.7-py3-none-any.whl

---
+++ packages/scikit-bonus/scikit_bonus-0.0.7-py3-none-any.whl/skbonus/metrics/__init__.py
@@ -0,0 +1,13 @@
+from ._regression import (
+    mean_absolute_percentage_error,
+    symmetric_mean_absolute_percentage_error,
+    mean_directional_accuracy,
+    mean_absolute_deviation,
+)
+
+__all__ = [
+    "mean_absolute_percentage_error",
+    "symmetric_mean_absolute_percentage_error",
+    "mean_directional_accuracy",
+    "mean_absolute_deviation",
+]
