d2ea652d0dee5d7cc52584ab047dcc5e9017b974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:46 +0000

    Add package benfordslaw-analysis/1.0.3/benfordslaw_analysis-1.0.3.tar.gz

---
+++ packages/benfordslaw-analysis/benfordslaw_analysis-1.0.3.tar.gz/benfordslaw_analysis-1.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+   long_description = f.read()
+
+setup(
+   name="benfordslaw_analysis",
+   version='1.0.3',
+   author='Jurjen de Jong',
+   author_email='jurjendejong93@gmail.com',
+   url='https://github.com/jurjen93/Benfords_law',
+   license='LICENSE.txt',
+   description="Use this package to analyse your data with Benford's law",
+   long_description=long_description,
+   long_description_content_type="text/markdown",
+   install_requires=[
+       "matplotlib",
+       "scipy",
+      "pandas"
+      ],
+   packages=find_packages()
+   )
