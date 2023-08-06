6c35feb5b08355c7f419b97f4bcc747adb154dfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:40 +0000

    Add package dtlpy/1.23.23/dtlpy-1.23.23-py3-none-any.whl

---
+++ packages/dtlpy/dtlpy-1.23.23-py3-none-any.whl/dtlpy/entities/annotation_definitions/__init__.py
@@ -0,0 +1,15 @@
+from .base_annotation_definition import BaseAnnotationDefinition
+from .box import Box
+from .note import Note
+from .classification import Classification
+from .comparison import Comparison
+from .ellipse import Ellipse
+from .point import Point
+from .polygon import Polygon
+from .polyline import Polyline
+from .segmentation import Segmentation
+from .subtitle import Subtitle
+from .description import Description
+from .undefined_annotation import UndefinedAnnotationType
+from .pose import Pose
+
