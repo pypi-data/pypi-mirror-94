643942b8fa8f289a6f36e77f8c9cc18ecb6bf42a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:37 +0000

    Add package anonlink/0.14.2/anonlink-0.14.2-cp37-cp37m-win_amd64.whl

---
+++ packages/anonlink/anonlink-0.14.2-cp37-cp37m-win_amd64.whl/anonlink/typechecking.py
@@ -0,0 +1,43 @@
+"""Types for typechecking. Aimed at Mypy."""
+
+import array as _array
+import typing as _typing
+
+import mypy_extensions as _mypy_extensions
+
+# This is for parametrisation only. You shouldn't need to import it.
+Record = _typing.TypeVar('Record')
+
+Dataset = _typing.Sequence[Record]
+
+if _typing.TYPE_CHECKING:
+    FloatArrayType = _array.array[float]
+    IntArrayType = _array.array[int]
+    CharArrayType = _array.array[int]
+else:
+    FloatArrayType = _array.array
+    IntArrayType = _array.array
+    CharArrayType = _array.array
+
+CandidatePairs = _typing.Tuple[FloatArrayType,
+                       _typing.Tuple[IntArrayType, ...],
+                       _typing.Tuple[IntArrayType, ...]]
+
+BlockingFunction = _typing.Callable[
+    [int, int, Record],
+    _typing.Iterable[_typing.Hashable]]
+
+SimilarityFunction = _typing.Callable[
+    [_typing.Sequence[Dataset],
+     float,
+     _mypy_extensions.DefaultNamedArg(_typing.Optional[int], 'k')],
+    _typing.Tuple[FloatArrayType, _typing.Sequence[IntArrayType]]]
+
+DatasetChunkInfo = _mypy_extensions.TypedDict(
+    'DatasetChunkInfo',
+    {'datasetIndex': int,
+     'range': _typing.List[int]})
+ChunkInfo = _typing.List[DatasetChunkInfo]
+
+DatasetAndRecordIndex = _typing.Tuple[int, int]
+MatchGroups = _typing.Sequence[_typing.Sequence[DatasetAndRecordIndex]]
