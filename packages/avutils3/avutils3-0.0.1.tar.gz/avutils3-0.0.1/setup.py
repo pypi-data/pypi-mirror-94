05de0b1bb8617a8507b75b81fb59a6bd07931852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:16 +0000

    Add package avutils3/0.0.1/avutils3-0.0.1.tar.gz

---
+++ packages/avutils3/avutils3-0.0.1.tar.gz/avutils3-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='avutils3',
+    description='Module for AV product automation',
+    author='QA Automation',
+    version='0.0.1',
+    classifiers=['Programming Language :: Python :: 3'],
+)
