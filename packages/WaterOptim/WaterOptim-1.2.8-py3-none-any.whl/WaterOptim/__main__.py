79f8cc48444056e9d5e2292ba561610bed76a31a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:58 +0000

    Add package WaterOptim/1.2.8/WaterOptim-1.2.8-py3-none-any.whl

---
+++ packages/WaterOptim/WaterOptim-1.2.8-py3-none-any.whl/WaterOptim/__main__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 23 08:49:16 2020
+
+@author: HEDI
+"""
+
+from . import PY
+print('PY: {}'.format(PY))
+print('@author: hedi.romdhana@agroparistech.fr')
