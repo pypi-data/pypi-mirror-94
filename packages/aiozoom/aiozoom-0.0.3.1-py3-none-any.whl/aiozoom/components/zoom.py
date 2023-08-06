61f334311a5a26193f982f7db424d8fa5753249a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:38 +0000

    Add package aiozoom/0.0.3.1/aiozoom-0.0.3.1-py3-none-any.whl

---
+++ packages/aiozoom/aiozoom-0.0.3.1-py3-none-any.whl/aiozoom/components/zoom.py
@@ -0,0 +1,9 @@
+from aiozoom.components.meeting import Meeting
+from aiozoom.components.dashboard import Dashboard
+
+
+class Zoom(Meeting, Dashboard):
+    """
+    Main entrypoint for all components
+    """
+    pass
