f3fc272373e485ede620a1895d284018b9841581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:49 +0000

    Add package djangobible/0.0.15/djangobible-0.0.15-py3-none-any.whl

---
+++ packages/djangobible/djangobible-0.0.15-py3-none-any.whl/djangobible/__init__.py
@@ -0,0 +1,9 @@
+"""
+djangobible python library.
+
+djangobible lets you easily associate Django models with Bible Scripture references and search accordingly.
+"""
+
+__version__ = "0.0.15"
+
+from pythonbible import *
