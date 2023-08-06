6d7ae05a2bfaf4c4352a08920eafa35a44ff49d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:39 +0000

    Add package pyLuoyeeID/1.0.0/pyLuoyeeID-1.0.0.tar.gz

---
+++ packages/pyLuoyeeID/pyLuoyeeID-1.0.0.tar.gz/pyLuoyeeID-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name='pyLuoyeeID',
+    version='1.0.0',
+    description='A python SDK for LuoyeeID',
+    author='KainHao',
+    author_email='kain@geekore.ink',
+    url='https://paperfold.coding.net/public/luoyeidpass-pythonsdk/LuoyeeIDPass-Python-SDk/git/files',
+    #install_requires=['requests'],
+)
