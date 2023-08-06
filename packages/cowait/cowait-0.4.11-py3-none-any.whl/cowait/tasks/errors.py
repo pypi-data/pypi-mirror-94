b73776a8d40f8acf50cc735e0ec1b86f8ee8ccde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:16 +0000

    Add package cowait/0.4.11/cowait-0.4.11-py3-none-any.whl

---
+++ packages/cowait/cowait-0.4.11-py3-none-any.whl/cowait/tasks/errors.py
@@ -0,0 +1,25 @@
+
+
+class StoppedError(RuntimeError):
+    pass
+
+
+class TaskError(RuntimeError):
+    """
+    Raised when an error is received from a subtask
+
+    Attributes:
+        error (str): Original error message
+    """
+
+    def __init__(self, error: str):
+        """
+        Arguments:
+            error (str): Original error message
+        """
+        self.error = error
+
+
+class TaskNotFoundError(RuntimeError):
+    """ Raised when a task can not be instantiated """
+    pass
