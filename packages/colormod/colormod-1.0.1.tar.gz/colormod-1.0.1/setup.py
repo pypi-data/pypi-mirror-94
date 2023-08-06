f54008b797dfd346c4e326c500482e6027f9c0ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:32 +0000

    Add package colormod/1.0.1/colormod-1.0.1.tar.gz

---
+++ packages/colormod/colormod-1.0.1.tar.gz/colormod-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name="colormod",
+    version="1.0.1",
+    author="LiquidDevect",
+    license="Do whatever you want",
+    description="Makes the use of ANSI colors much easier. Its just a simplified version of colorama.Fore",
+    keywords="colors",
+    requires=["colorama"],
+    packages=["colormod"]
+)
