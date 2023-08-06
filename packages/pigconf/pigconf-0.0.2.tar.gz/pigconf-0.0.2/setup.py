2747828a7815b56df62f1c016c73daf338bdfb5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:24 +0000

    Add package pigconf/0.0.2/pigconf-0.0.2.tar.gz

---
+++ packages/pigconf/pigconf-0.0.2.tar.gz/pigconf-0.0.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pigconf',
+    version='0.0.2',
+    author="Anshuman Rohella",
+    author_email="rohella.anshuman@gmail.com",
+    description='A yaml config mapper to create static config classes.',
+    url="https://github.com/AnshumanRohella/pigconf",
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=['mapper'],
+    packages=find_packages(),
+    install_requires=['PyYAML>=5.4.1'],
+    extras_require={
+        "dev": [
+            "pytest>=6.2.2",
+            "check-manifest>=0.46"
+        ],
+    },
+)
