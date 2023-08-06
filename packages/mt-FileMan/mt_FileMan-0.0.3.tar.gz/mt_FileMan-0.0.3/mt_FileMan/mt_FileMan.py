bec9107a724656a49a5c10ba486670152d513900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:34 +0000

    Add package mt-FileMan/0.0.3/mt_FileMan-0.0.3.tar.gz

---
+++ packages/mt-FileMan/mt_FileMan-0.0.3.tar.gz/mt_FileMan-0.0.3/mt_FileMan/mt_FileMan.py
@@ -0,0 +1,55 @@
+import sys, os
+import pathlib
+from datetime import datetime
+
+import re
+
+
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
+
+class Tee(object):
+    def __init__(self, *files):
+        self.files = files
+    def write(self, obj):
+        for f in self.files:
+            f.write(obj)
+            f.flush() # If you want the output to be visible immediately
+    def flush(self) :
+        for f in self.files:
+            f.flush()
+
+class mtIOMan(object):
+
+    def __init__(self): 
+        super().__init__() 
+
+    def startLog(self, logLevel=0):
+    
+        theCurrentDatetime = datetime.now()
+        #Make dir if not exist (python 3.5+), use pathlib2 < 3.5
+        pathlib.Path('LOG').mkdir(parents=True, exist_ok=True)
+        pathlib.Path('UI').mkdir(parents=True, exist_ok=True) 
+        logFN = 'LOG/log_'+theCurrentDatetime.strftime("%Y-%m-%d_%H-%M-%S")+'.log'
+        f = open(logFN, 'w')
+        original = sys.stdout
+        sys.stdout = Tee(sys.stdout, f)
+        
+        return f
+        
+    def sorted_nicely( l ): 
+        """ Sort the given iterable in the way that humans expect.""" 
+        convert = lambda text: int(text) if text.isdigit() else text 
+        alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
+        return sorted(l, key = alphanum_key)
+
+if __name__ == '__main__':
+    from datetime import datetime
+
+    theCurrentDatetime = datetime.now()
+    today = theCurrentDatetime.strftime('%Y-%m-%d')
+    print('Today\'s date is: '+today)
---
+++ packages/mt-FileMan/mt_FileMan-0.0.3.tar.gz/mt_FileMan-0.0.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name="mt_FileMan",
+    version="0.0.3",
+    author="mole1000",
+    author_email="mole1000@protonmail.com",
+    description="A package for custom file managing classes and methods for MaxTrack",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    test_suite='tests',
+    extras_require={
+        'testing': ['pytest']
+    },
+)
+    
