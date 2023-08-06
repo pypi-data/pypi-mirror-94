cea8b492e2385c04020fd16941934d621e7ed098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:09 +0000

    Add package chromecast-mpris/0.7.0/chromecast_mpris-0.7.0-py2.py3-none-any.whl

---
+++ packages/chromecast-mpris/chromecast_mpris-0.7.0-py2.py3-none-any.whl/chromecast_mpris/command.py
@@ -0,0 +1,43 @@
+from typing import Optional
+import logging
+import sys
+
+import click
+
+from .base import Seconds, NoChromecastFoundException, \
+  RC_NO_CHROMECAST, LOG_LEVEL, DEFAULT_RETRY_WAIT
+from .run import run_server
+
+
+@click.command(help="Control casting devices through MPRIS media controls.")
+@click.option('--name', '-n', default=None, show_default=True, type=click.STRING,
+  help="Specify a device name, otherwise control the first device found.\n")
+@click.option('--host', '-h', default=None, show_default=True, type=click.STRING,
+  help="Hostname or IP address of streaming device.\n")
+@click.option('--uuid', '-u', default=None, show_default=True, type=click.STRING,
+  help="Streaming device's UUID.\n")
+@click.option('--wait', '-w', default=None, show_default=True, type=click.INT,
+  help="Retry after specified amount of seconds if a device isn't found.")
+@click.option('--retry-wait', '-r',
+  default=DEFAULT_RETRY_WAIT, show_default=True, type=click.FLOAT,
+  help="Seconds to wait between reconnection attempts if the connection is interrupted.")
+@click.option('--log-level', '-l', default=LOG_LEVEL, show_default=True, type=click.STRING,
+  help='Debugging log level.')
+def cmd(
+  name: Optional[str],
+  host: Optional[str],
+  uuid: Optional[str],
+  wait: Optional[Seconds],
+  retry_wait: Optional[float],
+  log_level: str
+):
+  try:
+    run_server(name, host, uuid, wait, retry_wait, log_level)
+
+  except NoChromecastFoundException as e:
+    logging.warning(f"{e} not found")
+    sys.exit(RC_NO_CHROMECAST)
+
+
+if __name__ == "__main__":
+  cmd()
---
+++ packages/chromecast-mpris/chromecast_mpris-0.7.0-py2.py3-none-any.whl/chromecast_mpris/listeners.py
@@ -0,0 +1,75 @@
+from typing import Union, Optional
+from abc import ABC
+import logging
+
+from mpris_server.adapters import MprisAdapter
+from mpris_server.events import EventAdapter
+from mpris_server.server import Server
+
+from pychromecast.controllers.media import MediaStatus
+from pychromecast.socket_client import CastStatus
+from pychromecast import Chromecast
+
+from .base import Status
+
+
+DEBUG_FN = 'chromecast_mpris.txt'
+
+#logging.basicConfig(filename=DEBUG_FN)
+
+
+class ChromecastEventListener(ABC):
+  """
+  Event listeners that conform to pychromecast's API
+  """
+  def new_media_status(self, status: MediaStatus):
+    # logging.info(repr(status))
+    pass
+
+  def new_cast_status(self, status: CastStatus):
+    # logging.info(repr(status))
+    pass
+
+
+class ChromecastEventAdapter(EventAdapter):
+  def __init__(self,
+               name: str,
+               chromecast: Chromecast,
+               server: Server,
+               adapter: MprisAdapter = None):
+    self.name = name
+    self.chromecast = chromecast
+    self.server = server
+    self.adapter = adapter
+    self.cc = chromecast
+    super().__init__(self.server.player, self.server.root)
+
+
+class ChromecastEventHandler(ChromecastEventAdapter,
+                             ChromecastEventListener):
+  def check_volume(self, status: Status):
+    vol = status.volume_level
+    muted = status.volume_muted
+
+    if vol != self.adapter.get_volume() \
+       or muted != self.adapter.is_mute():
+      self.on_volume()
+
+  def new_media_status(self, status: MediaStatus):
+    super().new_media_status(status)
+    self.check_volume(status)
+    self.on_playback()
+    self.on_options()
+
+  def new_cast_status(self, status: CastStatus):
+    super().new_cast_status(status)
+    self.check_volume(status)
+
+
+def register_mpris_adapter(chromecast: Chromecast,
+                           server: Server,
+                           adapter: MprisAdapter):
+  event_listener = \
+    ChromecastEventHandler(chromecast.name, chromecast, server, adapter)
+  chromecast.media_controller.register_status_listener(event_listener)
+  chromecast.register_status_listener(event_listener)
