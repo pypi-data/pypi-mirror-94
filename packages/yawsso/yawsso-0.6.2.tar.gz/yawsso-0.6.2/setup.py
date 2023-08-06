9b3209b260703b076b7f15222d87485e9f55d8a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:07 +0000

    Add package yawsso/0.6.2/yawsso-0.6.2.tar.gz

---
+++ packages/yawsso/yawsso-0.6.2.tar.gz/yawsso-0.6.2/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup
+
+from yawsso import __version__
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="yawsso",
+    version=__version__,
+    description="Yet Another AWS SSO - sync up AWS CLI v2 SSO login session to legacy CLI v1 credentials",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/victorskl/yawsso",
+    author="Victor San Kho Lin",
+    author_email="victor@sankholin.com",
+    license="MIT",
+    packages=["yawsso"],
+    zip_safe=False,
+    entry_points={
+        "console_scripts": ["yawsso=yawsso.cli:main"],
+    },
+    extras_require={
+        "all": [
+            "pyperclip",
+        ],
+        "test": [
+            "pytest",
+            "pytest-cov",
+            "flake8",
+            "mockito",
+            "cli-test-helpers",
+            "coveralls",
+            "pyperclip==1.7.0",
+        ],
+        "dev": [
+            "twine",
+            "setuptools",
+            "wheel",
+            "aws-cdk.aws-ec2",
+            "tox",
+            "nose2",
+        ],
+    },
+    python_requires=">=3.6",
+)
