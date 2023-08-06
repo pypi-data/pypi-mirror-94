afdfe0f5480ede0c75bd08cea17398a2ea4db37e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:26 +0000

    Add package buycoins-client/1.3/buycoins_client-1.3.tar.gz

---
+++ packages/buycoins-client/buycoins_client-1.3.tar.gz/buycoins_client-1.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(name='buycoins_client', 
+    version='1.3', 
+    description='Buycoins API Python client', 
+    url='https://github.com/ChukwuEmekaAjah/buycoins_python', 
+    author='Chukwuemeka Ajah', 
+    author_email='talk2ajah@gmail.com', 
+    license='MIT', 
+    packages=['buycoins_client'], 
+    install_requires=['requests'], 
+    zip_safe=True, 
+    long_description=README, 
+    long_description_content_type='text/markdown'
+)
