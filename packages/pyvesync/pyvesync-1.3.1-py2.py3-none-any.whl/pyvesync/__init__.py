5a97ea3bdd5892ca6bddfd9d71ad4ac2b03fa8c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:04 +0000

    Add package pyvesync/1.3.1/pyvesync-1.3.1-py2.py3-none-any.whl

---
+++ packages/pyvesync/pyvesync-1.3.1-py2.py3-none-any.whl/pyvesync/__init__.py
@@ -0,0 +1,20 @@
+"""VeSync API Library."""
+
+# pylint: skip-file
+# flake8: noqa
+from .vesync import VeSync
+from .vesyncoutlet import (
+    VeSyncOutlet10A,
+    VeSyncOutlet15A,
+    VeSyncOutlet7A,
+    VeSyncOutdoorPlug,
+)
+from .vesyncswitch import VeSyncWallSwitch, VeSyncDimmerSwitch
+from .vesyncfan import VeSyncAir131
+from .vesyncbulb import VeSyncBulbESL100
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO, format='%(asctime)s - %(levelname)5s - %(message)s'
+)
