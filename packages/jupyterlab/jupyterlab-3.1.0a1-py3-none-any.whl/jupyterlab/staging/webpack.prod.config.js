e7a84b4f089bd191d60c18695133014be7dd8ca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:30 +0000

    Add package jupyterlab/3.1.0a1/jupyterlab-3.1.0a1-py3-none-any.whl

---
+++ packages/jupyterlab/jupyterlab-3.1.0a1-py3-none-any.whl/jupyterlab/staging/webpack.prod.config.js
@@ -0,0 +1,23 @@
+// This file is auto-generated from the corresponding file in /dev_mode
+const merge = require('webpack-merge').default;
+const config = require('./webpack.config');
+const LicenseWebpackPlugin = require('license-webpack-plugin')
+  .LicenseWebpackPlugin;
+
+config[0] = merge(config[0], {
+  mode: 'production',
+  devtool: 'source-map',
+  optimization: {
+    minimize: false
+  },
+  plugins: [
+    new LicenseWebpackPlugin({
+      perChunkOutput: false,
+      outputFilename: 'third-party-licenses.txt',
+      excludedPackageTest: packageName =>
+        packageName === '@jupyterlab/application-top'
+    })
+  ]
+});
+
+module.exports = config;
