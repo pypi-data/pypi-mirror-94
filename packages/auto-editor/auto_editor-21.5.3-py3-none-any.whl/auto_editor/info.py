b3f2f3957baf61df3e46a80e902e5d42974d9bcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:08 +0000

    Add package auto-editor/21.5.3/auto_editor-21.5.3-py3-none-any.whl

---
+++ packages/auto-editor/auto_editor-21.5.3-py3-none-any.whl/auto_editor/info.py
@@ -0,0 +1,50 @@
+'''info.py'''
+
+import os
+
+def getInfo(files, ffmpeg, ffprobe, log):
+    for file in files:
+        if(os.path.exists(file)):
+            print(f'file: {file}')
+        else:
+            log.error(f'Could not find file: {file}')
+
+        hasVid = len(ffprobe.pipe(['-show_streams', '-select_streams', 'v', file])) > 5
+        hasAud = len(ffprobe.pipe(['-show_streams', '-select_streams', 'a', file])) > 5
+
+        if(hasVid):
+            print(f' - fps: {ffprobe.getFrameRate(file)}')
+            print(f' - duration: {ffprobe.getDuration(file)}')
+            print(f' - resolution: {ffprobe.getResolution(file)}')
+
+            raw_data = ffprobe.pipe(['-select_streams', 'v:0', '-show_entries',
+                'stream=codec_name,bit_rate', '-of',
+                'compact=p=0:nk=1', file]).split('|')
+
+            print(f' - video codec: {raw_data[0]}')
+
+            if(raw_data[1].strip().isnumeric()):
+                vbit = str(int(int(raw_data[1]) / 1000)) + 'k'
+            else:
+                vbit = 'N/A'
+            print(f' - video bitrate: {vbit}')
+
+            if(hasAud):
+                tracks = ffprobe.getAudioTracks(file)
+                print(f' - audio tracks: {tracks}')
+
+                for track in range(tracks):
+                    print(f'   - Track #{track}')
+                    print(f'     - codec: {ffprobe.getAudioCodec(file, track)}')
+                    print(f'     - samplerate: {ffprobe.getPrettySampleRate(file, track)}')
+                    print(f'     - bitrate: {ffprobe.getPrettyABitrate(file, track)}')
+            else:
+                print(' - audio tracks: 0')
+        elif(hasAud):
+            print(f' - duration: {ffprobe.getAudioDuration(file)}')
+            print(f' - codec: {ffprobe.getAudioCodec(file, track=0)}')
+            print(f' - samplerate: {ffprobe.getPrettySampleRate(file, track=0)}')
+            print(f' - bitrate: {ffprobe.getPrettyABitrate(file, track=0)}')
+        else:
+            print('Invalid media.')
+    print('')
---
+++ packages/auto-editor/auto_editor-21.5.3-py3-none-any.whl/auto_editor/testAutoEditor.py
@@ -0,0 +1,173 @@
+'''test.py'''
+
+"""
+Test auto-editor and make sure everything is working.
+"""
+
+import os
+import sys
+import shutil
+import platform
+import subprocess
+
+from usefulFunctions import sep
+
+
+def getRunner():
+    if(platform.system() == 'Windows'):
+        return ['py', 'auto_editor/__main__.py']
+    return ['python3', 'auto_editor/__main__.py']
+
+
+def pipeToConsole(myCommands: list):
+    print(myCommands)
+    import subprocess
+    process = subprocess.Popen(myCommands, stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    return process.returncode, stdout.decode(), stderr.decode()
+
+
+def runTest(cmd):
+    pretty_cmd = ' '.join(cmd)
+    print(f'Running test: {pretty_cmd}')
+
+    add_no_open = '.' in cmd[0]
+    cmd = getRunner() + cmd
+    if(add_no_open):
+        cmd += ['--no_open']
+
+    returncode, stdout, stderr = pipeToConsole(cmd)
+    if(returncode > 0):
+        print('Test Failed.\n')
+        print(stdout)
+        print(stderr)
+        sys.exit(1)
+    else:
+        print('Test Succeeded.\n')
+
+
+def checkForError(cmd):
+    pretty_cmd = ' '.join(cmd)
+    print(f'Running Error Test: {pretty_cmd}')
+
+    returncode, stdout, stderr = pipeToConsole(getRunner() + cmd)
+    if(returncode > 0):
+        if('Error!' in stderr):
+            print('Test Succeeded.')
+        else:
+            print('Test Failed.\n')
+            print(f'Program crashed.\n{e}')
+            sys.exit(1)
+    else:
+        print('Test Failed.\n')
+        print('Program responsed with a code 0, but should have failed.')
+        sys.exit(1)
+
+
+def cleanup(the_dir):
+    for item in os.listdir(the_dir):
+        item = f'{the_dir}{sep()}{item}'
+        if('_ALTERED' in item or item.endswith('.xml') or item.endswith('.json')):
+            os.remove(item)
+        if(item.endswith('_tracks')):
+            shutil.rmtree(item)
+
+def testAutoEditor():
+    # Test Help Command
+    runTest(['--help'])
+    runTest(['-h'])
+    runTest(['--frame_margin', '--help'])
+    runTest(['--frame_margin', '-h'])
+    runTest(['exportMediaOps', '--help'])
+    runTest(['exportMediaOps', '-h'])
+    runTest(['progressOps', '-h'])
+
+    # Test the Help Command on itself.
+    runTest(['--help', '--help'])
+    runTest(['-h', '--help'])
+    runTest(['--help', '-h'])
+    runTest(['-h', '--help'])
+
+    # Test version info
+    runTest(['--version'])
+    runTest(['-v'])
+    runTest(['-V'])
+
+    # Test debug info
+    runTest(['--debug'])
+    # --verbose by itself is UB.
+
+    # Test info subcommand.
+    runTest(['info', 'example.mp4'])
+    runTest(['info', 'resources/man_on_green_screen.mp4'])
+    runTest(['info', 'resources/multi-track.mov'])
+    runTest(['info', 'resources/newCommentary.mp3'])
+    runTest(['info', 'resources/test.mkv'])
+
+    # Test example video.
+    runTest(['example.mp4'])
+    runTest(['example.mp4', '--verbose'])
+
+    runTest(['example.mp4', '-m', '3'])
+    runTest(['example.mp4', '-m', '0.3sec'])
+
+
+    shutil.copy('example.mp4', 'example')
+
+    checkForError(['example', '--no_open'])
+
+    os.remove('example')
+
+    # Test ProgressOps
+    runTest(['example.mp4', 'progressOps', '--machine_readable_progress'])
+    runTest(['example.mp4', 'progressOps', '--no_progress'])
+
+    # Test Audio File Input and Exporting
+    runTest(['resources/newCommentary.mp3', '--silent_threshold', '0.1'])
+
+    # Test Cut by All Tracks
+    runTest(['resources/multi-track.mov', '--cut_by_all_tracks'])
+
+    runTest(['resources/multi-track.mov', '--keep_tracks_seperate'])
+
+    runTest(['example.mp4', '--cut_by_this_audio', 'resources/newCommentary.mp3'])
+
+    runTest(['example.mp4', '--export_as_json'])
+    runTest(['example.json'])
+
+    runTest(['example.mp4', '-s', '2', '-mcut', '10'])
+    runTest(['example.mp4', '-v', '2', '-mclip', '4'])
+    runTest(['example.mp4', '--sounded_speed', '0.5'])
+    runTest(['example.mp4', '--silent_speed', '0.5'])
+
+    cleanup(os.getcwd())
+    cleanup('resources')
+
+    for item in os.listdir('resources'):
+
+        if('man_on_green_screen' in item or item.startswith('.') or item.endswith('.txt')):
+            continue
+
+        item = f'resources/{item}'
+        runTest([item])
+        runTest([item, '-exp'])
+        runTest([item, '-exr'])
+        runTest([item, '--preview'])
+
+    runTest(['example.mp4', 'exportMediaOps', '--video_codec', 'h264'])
+    runTest(['example.mp4', 'exportMediaOps', '-vcodec', 'h264', '--preset', 'faster'])
+    runTest(['example.mp4', 'exportMediaOps', '--audio_codec', 'ac3'])
+    runTest(['resources/newCommentary.mp3', 'exportMediaOps', '-acodec', 'pcm_s16le'])
+
+    runTest(['example.mp4', '--cut_out', '0-5.7', '-o', 'hmm.mp4'])
+    runTest(['example.mp4', 'hmm.mp4', '--combine_files', '--debug'])
+
+    os.remove('hmm.mp4')
+
+    runTest(['resources/man_on_green_screen.mp4', '--edit_based_on', 'motion', '--debug', '--frame_margin', '0', '-mcut', '0', '-mclip', '0'])
+
+    cleanup('resources')
+    cleanup(os.getcwd())
+if(__name__ == '__main__'):
+    testAutoEditor()
