c3032cbb41ce2a0f0acfb17f09d5fa6fbfbbf689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:19 +0000

    Add package nos/0.0.1/nos-0.0.1.tar.gz

---
+++ packages/nos/nos-0.0.1.tar.gz/nos-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='nos',
+    description='NOS main library',
+    author='QA Automation',
+    version='0.0.1',
+    classifiers=['Programming Language :: Python :: 3'],
+)
