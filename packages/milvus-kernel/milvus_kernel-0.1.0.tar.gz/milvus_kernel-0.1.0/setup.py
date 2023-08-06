036e631740f836219752711b937e22da80dbabef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:01 +0000

    Add package milvus-kernel/0.1.0/milvus_kernel-0.1.0.tar.gz

---
+++ packages/milvus-kernel/milvus_kernel-0.1.0.tar.gz/milvus_kernel-0.1.0/setup.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import json
+
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md', encoding='utf-8') as f:
+        return f.read()
+
+setup(name='milvus_kernel',
+      version='0.1.0',
+      description='A milvus kernel for Jupyter.',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      url='https://github.com/Hourout/milvus_kernel',
+      author='JinQing Lee',
+      author_email='hourout@163.com',
+      keywords=['jupyter_kernel', 'milvus_kernel'],
+      license='Apache License Version 2.0',
+      install_requires=['pymilvus', 'pandas', 'jupyter'],
+      classifiers = [
+          'Framework :: IPython',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering',
+          'Topic :: System :: Shells',
+      ],
+      packages=['milvus_kernel'],
+      zip_safe=False
+)
