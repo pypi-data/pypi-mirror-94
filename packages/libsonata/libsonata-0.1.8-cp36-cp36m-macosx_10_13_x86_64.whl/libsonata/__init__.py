8fe01f759488a7f2c38e82ff965ebe7761ca7e72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:33 +0000

    Add package libsonata/0.1.8/libsonata-0.1.8-cp36-cp36m-macosx_10_13_x86_64.whl

---
+++ packages/libsonata/libsonata-0.1.8-cp36-cp36m-macosx_10_13_x86_64.whl/libsonata/__init__.py
@@ -0,0 +1,20 @@
+# Needed so that mac wheels properly delocate:
+#  https://github.com/matthew-brett/delocate/issues/22
+
+from libsonata._libsonata import (EdgePopulation,
+                                  EdgeStorage,
+                                  ElementDataFrame,
+                                  ElementReportPopulation,
+                                  ElementReportReader,
+                                  NodePopulation,
+                                  NodeSets,
+                                  NodeStorage,
+                                  Selection,
+                                  SomaDataFrame,
+                                  SomaReportPopulation,
+                                  SomaReportReader,
+                                  SonataError,
+                                  SpikePopulation,
+                                  SpikeReader,
+                                  version,
+                                  )
