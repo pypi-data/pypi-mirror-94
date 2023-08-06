c0f07228ddc55eab47cadf7dc782ce5567b9f46b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:17 +0000

    Add package py-Ultroid/5.1b0/py-Ultroid-5.1b0.tar.gz

---
+++ packages/py-Ultroid/py-Ultroid-5.1b0.tar.gz/py-Ultroid-5.1b0/pyUltroid/dB/redisdb.py
@@ -0,0 +1,11 @@
+from pyUltroid import udB
+
+
+class Redis(object):
+	ALIVE_TEXT = udB.get("ALIVE_TEXT")
+	PMPERMIT = udB.get("PMPERMIT")
+	ALIVE_PIC = udB.get("ALIVE_PIC")
+	PLUGIN_CHANNEL = udB.get("PLUGIN_CHANNEL")
+	RMBG_API = udB.get("RMBG_API")
+	SUDOS = udB.get("SUDOS")
+	BOT_USERS = udB.get("BOT_USERS")
