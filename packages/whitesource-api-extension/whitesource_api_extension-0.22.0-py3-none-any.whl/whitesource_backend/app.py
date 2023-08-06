5b850c28402e40f0558d463883aa8fdd46b3de1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:14 +0000

    Add package whitesource-api-extension/0.22.0/whitesource_api_extension-0.22.0-py3-none-any.whl

---
+++ packages/whitesource-api-extension/whitesource_api_extension-0.22.0-py3-none-any.whl/whitesource_backend/app.py
@@ -0,0 +1,27 @@
+import logging
+import threading
+
+import falcon.asgi
+
+import component
+import util
+
+
+util.configure_default_logging(force=True)
+
+
+def create_app():
+    logger = logging.getLogger(__name__)
+    logger.info('initializing falcon')
+    app = falcon.asgi.App()
+    app.add_route('/component', component.Component())
+
+    threading.Thread(
+        target=util.update_or_download_agent,
+        daemon=True,
+    ).start()
+
+    return app
+
+
+app = create_app()
