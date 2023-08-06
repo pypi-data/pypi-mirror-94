1cb835586f85f2aee50410b523b2f6065f30c362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:35 +0000

    Add package calculator-test-one/0.0.1/calculator_test_one-0.0.1.tar.gz

---
+++ packages/calculator-test-one/calculator_test_one-0.0.1.tar.gz/calculator_test_one-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='calculator_test_one',
+    version='0.0.1',
+    description='calculator ',
+    author='Jihenbouguerra',
+
+    license='MIT',
+
+    packages=find_packages(),
+
+)
