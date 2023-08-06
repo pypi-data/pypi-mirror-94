eddf487f32bfb42a8de0ec418d89f1542a9fae21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:01 +0000

    Add package verify-email/2.4.3/verify_email-2.4.3.tar.gz

---
+++ packages/verify-email/verify_email-2.4.3.tar.gz/verify_email-2.4.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='verify_email',
+    version='2.4.3',
+    author='Kumar Akshay',
+    author_email='k.akshay9721@gmail.com',
+    description='A small package for email verification',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/kakshay21/verify_email',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ),
+    install_requires=[
+        # -*- Extra requirements: -*-
+        'aiosmtpd',
+        'aiodns'
+    ],
+)
