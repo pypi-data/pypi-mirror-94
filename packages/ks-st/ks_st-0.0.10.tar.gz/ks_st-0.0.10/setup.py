b01fa7c056ff896813c40becd684f96c9e1cda1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:03 +0000

    Add package ks-st/0.0.10/ks_st-0.0.10.tar.gz

---
+++ packages/ks-st/ks_st-0.0.10.tar.gz/ks_st-0.0.10/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ks_st", # Replace with your own username
+    version="0.0.10", author="Armandpl",
+    author_email="adpl33@gmail.com",
+    description="Style transfer toolbox",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Armandpl/ks_st",
+    install_requires=[
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
